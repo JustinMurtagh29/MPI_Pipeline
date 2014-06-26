56a2131b0f8a80081c084d152415da95643b97b7
Manuel Berning <mberning@neuro.mpg.de>
2014-06-26 02:14:58 +0200

    gallery update

--- gallery/contactDetectionFor2ndstory.m
+++ gallery/contactDetectionFor2ndstory.m
@@ -4,14 +4,21 @@ pathGC = '/zdata/manuel/sync/fromLap/ekSkel/2ndStorySkel/gcl/';
 filesBP = dir([pathBP '*.nml']);
 filesGC = dir([pathGC '*.nml']);
 
-tic;
+display('----------------------------------------------------------------------------------------');
+tall = tic;
 for i=1:length(filesBP)
     for j=1:length(filesGC)
+	tSingle = tic;
 	display(['Evaluating contacts from ' filesBP(i).name  ' to ' filesGC(j).name]);
 	[skel{i,j} data{i,j}] = contactDetection({[pathBP filesBP(i).name] [pathGC filesGC(j).name]});
-	writeNmlOld([outputDir filesBP(i).name(1:end-4) 'TO' filesGC(j).name(1:end-4) '.nml'], skel{i,j});
-    end
+	if length(skel{i,j}) > 2
+		writeNmlOld([outputDir filesBP(i).name(1:end-4) 'TO' filesGC(j).name(1:end-4) '.nml'], skel{i,j});
+	end
+	toc(tSingle);
+	display('----------------------------------------------------------------------------------------');
+	end
 end
+toc(tall);
 save([outputDir 'data.mat'], 'data', 'skel', '-v7.3');
-toc
+
 
