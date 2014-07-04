73529ad9872944adab6ed85600a6c19f53b15553
Manuel Berning <mberning@neuro.mpg.de>
2014-07-04 14:22:20 +0200

    cloned to gaba and edited startup script and settings file accordingly

--- startup.m
+++ startup.m
@@ -1,35 +1,43 @@
 % Startup File manuelCode repo
 global GLOBAL_CODE_DIR GLOBAL_DATA_DIR GLOBAL_RESULT_DIR GLOBAL_OUTPUT_DIR GLOBAL_CPU_JM GLOBAL_GPU_JM;
 [~,hostname]= system('hostname');
-hostnameShort = hostname(1:6);
+hostnameShort = hostname(1:4);
 switch hostnameShort
-	case 'turing'
+	case 'turi'
 		base = '/zdata/manuel/';
 		GLOBAL_CPU_JM = 'fermat-cpu';
 		GLOBAL_GPU_JM = 'fermat-cnn';
 		GLOBAL_CODE_DIR = [base 'code' filesep];
 		GLOBAL_DATA_DIR = [base 'data' filesep];
-	        GLOBAL_RESULT_DIR = [base 'results' filesep];
-	        GLOBAL_OUTPUT_DIR = [base 'sync' filesep];
-	case 'fermat'
+	    GLOBAL_RESULT_DIR = [base 'results' filesep];
+	    GLOBAL_OUTPUT_DIR = [base 'sync' filesep];
+	case 'ferm'
 		base = '/zdata/manuel/';
 		GLOBAL_CPU_JM = 'fermat-cpu';
 		GLOBAL_GPU_JM = 'fermat-cnn';
 		GLOBAL_CODE_DIR = [base 'code' filesep];
 		GLOBAL_DATA_DIR = [base 'data' filesep];
-	        GLOBAL_RESULT_DIR = [base 'results' filesep];
-	        GLOBAL_OUTPUT_DIR = [base 'sync' filesep];
+	    GLOBAL_RESULT_DIR = [base 'results' filesep];
+	    GLOBAL_OUTPUT_DIR = [base 'sync' filesep];
 		addpath('/usr/local/jacket/');
 		addpath('/usr/local/jacket/engine');
-	case 'P1-380'
-	        base = 'I:\CortexConnectomics\Manuel\';
+	case 'P1-3'
+	    base = 'I:\CortexConnectomics\Manuel\';
 		GLOBAL_CPU_JM = 'p1-380-cpu';
 		GLOBAL_GPU_JM = 'p1-380-gpu';
 		GLOBAL_CODE_DIR = 'C:\manuelCode\';
 		GLOBAL_DATA_DIR = [base 'data' filesep];
-	        GLOBAL_RESULT_DIR = [base 'results' filesep];
-	        GLOBAL_OUTPUT_DIR = [base 'sync' filesep];
-	otherwise
+	    GLOBAL_RESULT_DIR = [base 'results' filesep];
+	    GLOBAL_OUTPUT_DIR = [base 'sync' filesep];
+    case 'gaba'
+        base = '/gaba/u/mberning/';
+		GLOBAL_CPU_JM = 'mjs';
+		GLOBAL_GPU_JM = 'mjs';
+		GLOBAL_CODE_DIR = [base 'code' filesep];
+		GLOBAL_DATA_DIR = [base 'data' filesep];
+	    GLOBAL_RESULT_DIR = [base 'results' filesep];
+	    GLOBAL_OUTPUT_DIR = [base 'sync' filesep];
+    otherwise
 		warning(manuelCode:unknownHost,['Warning: Computer ' hostname ' unknown. Please add to startup.m']);
 end
 clear hostname* base;
