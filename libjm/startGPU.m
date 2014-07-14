97f6c00c0fef83831c03592531a497143bcb2f37
Manuel Berning <mberning@neuro.mpg.de>
2014-07-14 13:04:12 +0200

    code for gpuBenchmark @RZG and some last fixes for running CNN on gaba (related)

--- libjm/startGPU.m
+++ libjm/startGPU.m
@@ -1,5 +1,10 @@
-function job = startGPU( functionHandle, inputCell, jobName )
+function job = startGPU( functionHandle, inputCell, jobName, nrOutputArguments )
 global GLOBAL_HOST;
+
+if nargin < 4
+    nrOutputArguments = 0;
+end
+
 
 % We only need CNN path dependencies on the GPU part of the cluster
 pathDependencies = {'/usr/local/jacket/' '/usr/local/jacket/engine/' '/zdata/manuel/code/CNN/' '/zdata/manuel/code/auxiliaryMethods/' '/zdata/manuel/code/auxiliaryMethods/cubes/'};
@@ -10,14 +15,14 @@ jm = findJm();
 if strcmp(GLOBAL_HOST,'fermat01')
     job = createJob(jm(2), 'RestartWorker', true, 'PathDependencies', pathDependencies, 'Name', jobName);
     for i=1:length(functionHandle);
-    	createTask(job, functionHandle{i}, 0, inputCell{i}, 'MaximumNumberOfRetries', 5, 'Timeout', 90000, 'CaptureCommandWindowOutput', 0);
+    	createTask(job, functionHandle{i}, nrOutputArguments, inputCell{i}, 'MaximumNumberOfRetries', 5, 'Timeout', 90000, 'CaptureCommandWindowOutput', 0);
     end
 elseif strcmp(GLOBAL_HOST, 'gaba')
     job = createJob(jm(2), 'RestartWorker', true, 'Name', jobName);
     job.AdditionalPaths = {'/gaba/u/mberning/code/CNN/' '/gaba/u/mberning/code/pipeline/' '/gaba/u/mberning/code/auxiliaryMethods/' '/gaba/u/mberning/code/auxiliaryMethods/cubes/'};
     job.AutoAttachFiles = false;
     for i=1:length(functionHandle);
-    	createTask(job, functionHandle{i}, 0, inputCell{i});
+    	createTask(job, functionHandle{i}, nrOutputArguments, inputCell{i});
     end
 end
 % Start job
