4b2c6d518bccba7d649eb5cd5c8812081754cfd5
Manuel Berning <mberning@neuro.mpg.de>
2014-07-18 12:53:15 +0200

    resolved issue where wrong classification was loaded for labelled region (just speed up for starting segmentation optimization)

--- filterbank/calcFeatures.m
+++ filterbank/calcFeatures.m
@@ -11,8 +11,13 @@ for l=1:length(parameter.feature.input)
 	if strcmp(parameter.feature.input{l}, 'raw')
 		imfeat = loadRawData(parameter.raw.root, parameter.raw.prefix, parameter.local(sub(1),sub(2),sub(3)).bboxSmall, 1);           
 	elseif strcmp(parameter.feature.input{l}, 'aff')
-		imfeat = loadClassData(parameter.class.root, parameter.class.prefix, parameter.local(sub(1),sub(2),sub(3)).bboxSmall);
-	end
+        % Add Manuel: Here we need to differentiate between densly labeled regions and normal regions at least if we want to process LR beforehand (for training GP)
+        if isfield(parameter.local(sub(1),sub(2),sub(3)), 'class')
+		    imfeat = loadClassData(parameter.local(sub(1),sub(2),sub(3)).class.root, parameter.local(sub(1),sub(2),sub(3)).class.prefix, parameter.local(sub(1),sub(2),sub(3)).bboxSmall);
+        else
+            imfeat = loadClassData(parameter.class.root, parameter.class.prefix, parameter.local(sub(1),sub(2),sub(3)).bboxSmall);
+        end
+    end
 	load(parameter.local(sub(1),sub(2),sub(3)).borderFile);
 	for m=1:size(parameter.filter,2)
 		for n=1:length(parameter.filter{m}{2})
--- pipeline/miniFeature.m
+++ pipeline/miniFeature.m
@@ -6,11 +6,11 @@ for i=1:size(parameter.local,1)
 		for k=1:size(parameter.local,3)
 			if ~exist(parameter.local(i,j,k).saveFolder, 'dir')
 				mkdir(parameter.local(i,j,k).saveFolder);
-            		end
-           	        sub = [i,j,k];
-			idx = sub2ind(size(parameter.local), i, j, k);
+       		end
+            sub = [i,j,k];
+    		idx = sub2ind(size(parameter.local), i, j, k);
 			functionH{idx} = parameter.feature.func; 
-       			inputCell{idx} = {parameter, sub};
+   			inputCell{idx} = {parameter, sub};
 		end
 	end
 end
