a46447eeadd323fc300e9fe642699e75327f232c
Manuel Berning <mberning@neuro.mpg.de>
2014-10-17 08:45:08 +0200

    fix merge conflict in calcFeatures from merging with Benjamins glia branch, make sure it works after segmentation & graph construction has run (closes #1)

--- filterbank/calcFeatures.m
+++ filterbank/calcFeatures.m
@@ -8,75 +8,52 @@ if ~exist(parameter.local(sub(1),sub(2),sub(3)).saveFolder, 'dir')
 	mkdir(parameter.local(sub(1),sub(2),sub(3)).saveFolder);
 end
 
-<<<<<<< HEAD
-for l = 1: length(parameter.feature.input) 
-
- if strcmp(parameter.feature.input{l}, 'raw')
-	imfeat = loadRawData(parameter.raw.root, parameter.raw.prefix, parameter.local(sub(1),sub(2),sub(3)).bboxSmall, 1);           
-
- elseif strcmp(parameter.feature.input{l}, 'aff')
-	imfeat = loadClassData(parameter.class.root, parameter.class.prefix, parameter.local(sub(1),sub(2),sub(3)).bboxSmall);
- end
-
- border = load(parameter.local(sub(1),sub(2),sub(3)).borderFile);
- border = border.borders
-
- for m=1:size(parameter.filter,2)
-	for n = 1:length(parameter.filter{m}{2})
-	 imfeats = filter3d(parameter, imfeat, m, n);
- 	 if isa(imfeats, 'cell')
-		for p=1:length(imfeats)
-		weights_new = featureDesign(real(imfeats{p}), border);
-		weights = [weights weights_new];
-		disp('m:');
-		disp(m);
-		disp('n:');
-		disp(n);
-=======
 for l=1:length(parameter.feature.input) 
 	if strcmp(parameter.feature.input{l}, 'raw')
 		imfeat = loadRawData(parameter.raw.root, parameter.raw.prefix, parameter.local(sub(1),sub(2),sub(3)).bboxSmall, 1);           
 	elseif strcmp(parameter.feature.input{l}, 'aff')
-        % Add Manuel: Here we need to differentiate between densly labeled regions and normal regions at least if we want to process LR beforehand (for training GP)
-        if isfield(parameter.local(sub(1),sub(2),sub(3)), 'class')
-		    imfeat = loadClassData(parameter.local(sub(1),sub(2),sub(3)).class.root, parameter.local(sub(1),sub(2),sub(3)).class.prefix, parameter.local(sub(1),sub(2),sub(3)).bboxSmall);
-        else
-            imfeat = loadClassData(parameter.class.root, parameter.class.prefix, parameter.local(sub(1),sub(2),sub(3)).bboxSmall);
-        end
-    end
-	load(parameter.local(sub(1),sub(2),sub(3)).borderFile);
-        load(parameter.local(sub(1),sub(2),sub(3)).segmentFile);
-	% change data typ of PixelIdxLists to be consistent with borders. Adapt PixelIdxList computation for segmetns (memory/faster than regionprops?)
-	for s = 1:length(segments)
-		segments(s).PixelIdxList = int32(segments(s).PixelIdxList');
+		% Add Manuel: Here we need to differentiate between densly labeled regions and normal regions at least if we want to process LR beforehand (for training GP)
+		if isfield(parameter.local(sub(1),sub(2),sub(3)), 'class')
+			local = parameter.local(sub(1),sub(2),sub(3));
+			imfeat = loadClassData(local.class.root, local.class.prefix, local.bboxSmall);
+		else
+			imfeat = loadClassData(parameter.class.root, parameter.class.prefix, parameter.local(sub(1),sub(2),sub(3)).bboxSmall);
+		end
 	end
-	for m=1:size(parameter.filter,2)
-		for n=1:length(parameter.filter{m}{2})
-	 		imfeats = filter3d(parameter, imfeat, m, n);
- 	 		if isa(imfeats, 'cell')
-				for p=1:length(imfeats)
-					weights_new = featureDesign(real(imfeats{p}), borders);
-					weights = [weights weights_new];
-                    segmentWeights_new = featureDesign(real(imfeats{p}), segments);
-					segmentWeights = [segmentWeights segmentWeights_new];
-				end
-	       	else
-				weights_new = featureDesign(imfeats, borders);
+end
+
+load(parameter.local(sub(1),sub(2),sub(3)).borderFile);
+load(parameter.local(sub(1),sub(2),sub(3)).segmentFile);
+% change data typ of PixelIdxLists to be consistent with borders. Adapt PixelIdxList computation for segmetns (memory/faster than regionprops?)
+for s = 1:length(segments)
+	segments(s).PixelIdxList = int32(segments(s).PixelIdxList');
+end
+for m=1:size(parameter.filter,2)
+	for n=1:length(parameter.filter{m}{2})
+		imfeats = filter3d(parameter, imfeat, m, n);
+		if isa(imfeats, 'cell')
+			for p=1:length(imfeats)
+				weights_new = featureDesign(real(imfeats{p}), borders);
 				weights = [weights weights_new];
-                segmentWeights_new = featureDesign(imfeats, segments);
-                segmentWeights = [segmentWeights segmentWeights_new];
+				segmentWeights_new = featureDesign(real(imfeats{p}), segments);
+				segmentWeights = [segmentWeights segmentWeights_new];
 			end
->>>>>>> 747bced9e443f4198c168da8a8399bef30104969
+		else
+			weights_new = featureDesign(imfeats, borders);
+			weights = [weights weights_new];
+			segmentWeights_new = featureDesign(imfeats, segments);
+			segmentWeights = [segmentWeights segmentWeights_new];
 		end
 	end
 end
 
 % calculate shape features and add to weights
 if isfield(parameter.local(sub(1),sub(2),sub(3)), 'class')
-    siz = (parameter.local(sub(1),sub(2),sub(3)).bboxSmall(:,2) - parameter.local(sub(1),sub(2),sub(3)).bboxSmall(:,1) + 1)';
+	siz = (parameter.local(sub(1),sub(2),sub(3)).bboxSmall(:,2) - parameter.local(sub(1),sub(2),sub(3)).bboxSmall(:,1) + 1)';
 else
-    siz = parameter.tileSize';
+	siz = parameter.tileSize';
 end
+
 weightsShape_borders = shapeFeatures(borders,siz);
 weightsShape_segments = shapeFeatures(segments,siz);
 
