a355f91429d06faeaffecd848fd4b191a83a3f0f
Manuel Berning <mberning@neuro.mpg.de>
2014-11-07 11:13:55 +0100

    added function to find CC for supervoxel graph

---
+++ globalization/findGlobalCC.m
@@ -0,0 +1,17 @@
+function findGlobalCC(p)
+
+% Collect all global correspondences
+files = dir([p.correspondence.saveFolder, '*global.mat']);
+correspondences = [];
+for i=1:length(files)
+    load([p.correspondence.saveFolder files(i).name]);
+    correspondences = [correspondences; corrGlobal1 corrGlobal2];
+end
+
+% Find connected components
+uniqueIds = unique(correspondences);
+components = findConnectedComponents(correspondences, uniqueIds);
+save([p.saveFolder 'globalCorrespondences.mat'], 'components');
+
+end
+
