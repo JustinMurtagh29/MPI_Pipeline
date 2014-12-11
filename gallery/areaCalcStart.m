4e7aea0c3eadee6503344b21818e77b74b157f7a
Manuel Berning <mberning@neuro.mpg.de>
2014-12-11 12:07:38 +0100

    modified version of gallery file to calculate total surface of neuron in each KNOSSOS cube for ek0563

---
+++ gallery/areaCalcStart.m
@@ -0,0 +1,15 @@
+function areaCalcStart()
+
+skelPath = '/zdata/manuel/sync/fromLap/ekSkel/full/gcl/';
+
+files = dir([skelPath '*.nml']);
+
+for i=1:length(files)
+    functionH{i} = @galleryNew;
+	inputCell{i} = {skelPath, files(i).name, ['/zdata/manuel/sync/wholeCell/retina/gcl_forPetersCorrection/' strrep(files(i).name, '.nml', '.mat')]};
+end
+
+startCPU(functionH, inputCell, 'surface area calculation');
+
+end
+
