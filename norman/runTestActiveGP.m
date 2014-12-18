57203af3bace021383db49207cc4deba6c12dec5
Norman Rzepka <code@normanrzepka.de>
2014-12-18 18:31:02 +0100

    various gp plots

--- norman/runTestActiveGP.m
+++ norman/runTestActiveGP.m
@@ -1,23 +1,42 @@
-function runTestActiveGP(parameter)
+function runTestActiveGP(parameter, testSetFilename)
 
 % Same as runTestGP, but repartitions training and test
 % prior to prediction.
 
-  groundTruth = load(parameter.gp.initalGroundTruth);
-  gpOptions = load(parameter.gp.hyperParameter);
+  load(testSetFilename);
 
   outputFile = [parameter.gp.stateFolder 'testSetActive.' datestr(clock, 30) '.mat'];
   disp(['Output to: ' outputFile]);
 
-
   %% Repartition training/test data
-  idx = randperm(floor(0.5 * size(groundTruth.testData, 1)));
+  lowerThreshold = 0.21;
+  upperThreshold = 0.90;
 
+  % Find test samples in query region
+  idx = find(lowerThreshold <= predictedLabelProb & predictedLabelProb <= upperThreshold);
+
+  % % Calc confidences (size of confidence interval 95%)
+  % confidences = abs(...
+  %   exp(feval(gpOptions.likfunc,[],ones(size(idx)),latentMean(idx)-2*sqrt(latentCov(idx)), latentCov(idx))) - ...
+  %   exp(feval(gpOptions.likfunc,[],ones(size(idx)),latentMean(idx)+2*sqrt(latentCov(idx)), latentCov(idx))));
+
+  % % Sort by confidences
+  % [~, idx2] = sort(confidences);
+  
+  % % Pick the `k` most unconfident samples
+  % k = 100;
+  % idx = idx(idx2(end-k:end));
+
+  idx2 = randperm(length(idx), 100);
+  idx = idx(idx2);
+
+  % Repartition
   groundTruth.trainingData = [groundTruth.trainingData; groundTruth.testData(idx,:)];
   groundTruth.trainingLabels = [groundTruth.trainingLabels; groundTruth.testLabels(idx,:)];
 
   groundTruth.testData = groundTruth.testData(setdiff(1:end,idx),:);
   groundTruth.testLabels = groundTruth.testLabels(setdiff(1:end,idx),:);
+
 
   %% RUN prediction
   [labelMean, labelCov, latentMean, latentCov, predictedLabelProb] = ...
