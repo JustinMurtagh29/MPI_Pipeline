a83c476a0919260bd534a543cf290a43a0aa6611
Manuel Berning <manuel.berning@brain.mpg.de>
2015-04-08 16:29:51 +0200

    Save graph and do not recalculate every time

--- globalization/oneMappingForProblemGeneration.m
+++ globalization/oneMappingForProblemGeneration.m
@@ -1,36 +1,43 @@
 function oneMappingForProblemGeneration(p)
 
-% Collect all edges of local supervoxel graph
-graph.edges = NaN(1e8,2); % 1e8 heuristic for 07x2, turns out actually ~7.5e7
-graph.prob = NaN(1e8,1);
-idx = 1;
-for i=1:size(p.local,1)
-    for j=1:size(p.local,2)
-        for k=1:size(p.local,3)
-            load(p.local(i,j,k).edgeFile);
-            load(p.local(i,j,k).probFile); 
-            % Put everything in one structure
-            nrElements = length(prob);
-            graph.edges(idx:idx+nrElements-1,:) = edges;
-            graph.prob(idx:idx+nrElements-1) = prob;
-            idx = idx+nrElements;
+if ~exist([p.saveFolder 'graph.mat'], 'dir')
+    % Collect all edges of local supervoxel graph
+    graph.edges = NaN(1e8,2); % 1e8 heuristic for 07x2, turns out actually ~7.5e7
+    graph.prob = NaN(1e8,1);
+    idx = 1;
+    for i=1:size(p.local,1)
+        for j=1:size(p.local,2)
+            for k=1:size(p.local,3)
+                load(p.local(i,j,k).edgeFile);
+                load(p.local(i,j,k).probFile); 
+                % Put everything in one structure
+                nrElements = length(prob);
+                graph.edges(idx:idx+nrElements-1,:) = edges;
+                graph.prob(idx:idx+nrElements-1) = prob;
+                idx = idx+nrElements;
+            end
         end
     end
+
+    % Collect all correspondences between local supervoxel graph
+    files = dir([p.correspondence.saveFolder, '*global.mat']);
+    for i=1:length(files)
+        load([p.correspondence.saveFolder files(i).name]);
+        nrElements = length(corrGlobal1);
+        graph.edges(idx:idx+nrElements-1,:) = [corrGlobal1 corrGlobal2];
+        graph.prob(idx:idx+nrElements-1) = ones(length(corrGlobal1),1);
+        idx = idx+nrElements;
+    end
+    % Drop part of arrays preallocated but not assigned
+    graph.edges(idx:end,:) = [];
+    graph.prob(idx:end) = [];
+    save([p.saveFolder 'graph.mat'], graph);
+else
+    display('NOTE: Graph loaded from HD. Rename for recalculation');
+    load([p.saveFolder 'graph.mat']);
 end
 
-% Collect all correspondences between local supervoxel graph
-files = dir([p.correspondence.saveFolder, '*global.mat']);
-for i=1:length(files)
-    load([p.correspondence.saveFolder files(i).name]);
-    nrElements = length(corrGlobal1);
-    graph.edges(idx:idx+nrElements-1,:) = [corrGlobal1 corrGlobal2];
-    graph.prob(idx:idx+nrElements-1) = ones(length(corrGlobal1),1);
-    idx = idx+nrElements;
-end
 
-% Drop part of arrays preallocated but not assigned
-graph.edges(idx:end,:) = [];
-graph.prob(idx:end) = [];
 
 if ~exist([p.saveFolder 'bothMappings.mat'], 'dir');
     % Load cube border correspondences and all local cube components based on GP
