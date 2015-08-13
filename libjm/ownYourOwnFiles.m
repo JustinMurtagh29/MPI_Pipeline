bb940ec93076af08436de780d558d57f7a14414f
Manuel Berning <manuel.berning@brain.mpg.de>
2015-08-13 15:51:52 +0200

    redo libjm to work on gaba, ready for first tests

--- libjm/ownYourOwnFiles.m
+++
@@ -1,8 +0,0 @@
-function ownYourOwnFiles(directory)
-%% Get ownership (was only possible as root) or at least r+w access (mdceproc) to a folder
-jm = findJm();
-job = createJob(jm(1));
-task = createTask(job, @system, 0, {['chmod -R 770 ' directory]});
-submit(job);
-end
-
