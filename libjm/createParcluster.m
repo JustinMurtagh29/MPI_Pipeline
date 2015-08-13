565bd7e2fbd409cd29efd87f553234122f31e9aa
Manuel Berning <manuel.berning@brain.mpg.de>
2015-08-13 15:52:06 +0200

    forgot to add new files

---
+++ libjm/createParcluster.m
@@ -0,0 +1,31 @@
+function cluster = getCluster( clusterConfiguration, jobStorageLocation)
+
+    % Default values for all input arguments
+    if nargin < 1
+        clusterConfiguration = 'cpu';
+    end
+    if nargin < 2
+        jobStorageLocation = '/u/mberning/temp/';
+    end
+    % Generate generic cluster profile
+    cluster = parallel.cluster.Generic();
+    cluster.JobStorageLocation = jobStorageLocation;
+    cluster.HasSharedFilesystem =  true;
+    cluster.ClusterMatlabRoot = '/gaba/u/system/SLES11/soft/matlab/R2014bn/';
+    cluster.OperatingSystem = 'unix';
+    % Set all kind of utility functions
+    cluster.GetJobStateFcn = @getJobStateFcn;
+    cluster.DeleteJobFcn = @deleteJobFcn;
+    % Set independent submit function parameter dependent on cluster configuration
+    switch clusterConfiguration
+        case 'm2090'
+            cluster.IndependentSubmitFcn = {@myIndependentSubmitFcn,'-pe openmp 1 -l h_vmem=12G,h_rt=10:00:00,num_m2090=1'};
+        case 'k40'
+            cluster.IndependentSubmitFcn = {@myIndependentSubmitFcn,'-pe openmp 1 -l h_vmem=36G,h_rt=100:00:00,num_k40=1'};
+        case 'cpu'
+            cluster.IndependentSubmitFcn = {@myIndependentSubmitFcn,'-pe openmp 1 -l h_vmem=16G,h_rt=100:00:00'};
+        otherwise
+            error('getCluster:unknownClusterConfiguration', 'Unkown cluster configuration string passed');
+    end
+end
+
---
+++ libjm/startJob.m
@@ -0,0 +1,22 @@
+function job = startJob(jm,  functionHandle, inputCell, jobName )
+    global GLOBAL_CODE_DIR;
+
+    % Get and format path dependencies  
+    pathDependencies = genpathGit(GLOBAL_CODE_DIR);
+    pathDependencies = regexp(pathDependencies, ':', 'split');
+    pathDependencies = pathDependencies(cellfun(@(x)not(isempty(x)), pathDependencies));
+    % Load cluster configuration
+    % Create job on cluster
+    job = createJob(jm);
+    job.AutoAttachFiles = false;
+    job.Name = [datestr(clock, 30) '_' jobName];
+    job.AdditionalPaths = genpathGit(GLOBAL_CODE_DIR); 
+    for i=1:length(functionHandle)
+        task = createTask(job, functionHandle{i}, 0, inputCell{i});
+        task.CaptureDiary = true;
+    end  
+    % Start job
+    submit(job);
+
+end
+
