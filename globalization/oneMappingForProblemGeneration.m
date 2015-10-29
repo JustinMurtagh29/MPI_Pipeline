6ca2213df25537e83808eadc4ffcb44f67fdf966
Manuel Berning <manuel.berning@brain.mpg.de>
2015-10-29 10:30:47 +0100

    last changes from fermat

--- globalization/oneMappingForProblemGeneration.m
+++ globalization/oneMappingForProblemGeneration.m
@@ -1,110 +1,115 @@
 function oneMappingForProblemGeneration(p, upperT)
-% Mainly for testing GP based joining of supervoxel graph, maybe should become class later
-% Or at least put more of this stuff into more modular functions
-% WORK on this function seized after noticing that two components are two orders of magnitude LARGER than the others
-% TO DO: Find out why!
+    % Mainly for testing GP based joining of supervoxel graph, maybe should become class later
+    % Or at least put more of this stuff into more modular functions
+    % WORK on this function seized after noticing that two components are two orders of magnitude LARGER than the others
+    % TO DO: Find out why!
 
-if ~exist([p.saveFolder 'graph.mat'], 'file')
-    tic;
-    % Collect all edges of local supervoxel graph
-    graph.edges = NaN(1e8,2); % 1e8 heuristic for 07x2, turns out actually ~7.5e7
-    graph.prob = NaN(1e8,1);
-    graph.cubeLI = NaN(1e8,1); % store linear indices as well (second column for correspondences)
-    idx = 1;
-    % IMPORTANT TO DO: Remove index shifts here in next run, just quick fix for now
-    for i=2:size(p.local,1)-1
-        for j=1:size(p.local,2)
-            for k=1:size(p.local,3)
-                load(p.local(i,j,k).edgeFile);
-                load(p.local(i,j,k).probFile); 
-                % Put everything in one structure
-                nrElements = length(prob);
-                graph.edges(idx:idx+nrElements-1,:) = edges;
-                graph.prob(idx:idx+nrElements-1) = prob;
-                graph.cubeLI(idx:idx+nrElements-1) = repmat(sub2ind(size(p.local),i,j,k), [nrElements 1]);
+    if ~exist([p.saveFolder 'graph.mat'], 'file')
+        tic;
+        % Collect all edges of local supervoxel graph
+        graph.edges = NaN(1e8,2); % 1e8 heuristic for 07x2, turns out actually ~7.5e7
+        graph.prob = NaN(1e8,1);
+        graph.cubeLI = NaN(1e8,1); % store linear indices as well (second column for correspondences)
+        idx = 1;
+        % IMPORTANT TO DO: Remove index shifts here in next run, just quick fix for now
+        for i=2:size(p.local,1)-1
+            for j=1:size(p.local,2)
+                for k=1:size(p.local,3)
+                    load(p.local(i,j,k).edgeFile);
+                    load(p.local(i,j,k).probFile); 
+                    % Put everything in one structure
+                    nrElements = length(prob);
+                    graph.edges(idx:idx+nrElements-1,:) = edges;
+                    graph.prob(idx:idx+nrElements-1) = prob;
+                    graph.cubeLI(idx:idx+nrElements-1) = repmat(sub2ind(size(p.local),i,j,k), [nrElements 1]);
+                    idx = idx+nrElements;
+                end
+            end
+        end
+
+        % Collect all correspondences between local supervoxel graph
+        files = dir([p.correspondence.saveFolder, '*global.mat']);
+        for i=1:length(files)
+            % IMPORTANT TO DO: same here, change to using back all indices!
+            if ~(strcmp(files(i).name(1:2),'01') || strcmp(files(i).name(1:2), '15') || ...
+                    strcmp(files(i).name(7:8),'01') || strcmp(files(i).name(7:8), '15'))
+                load([p.correspondence.saveFolder files(i).name]);
+                nrElements = length(corrGlobal1);
+                graph.edges(idx:idx+nrElements-1,:) = [corrGlobal1 corrGlobal2];
+                graph.prob(idx:idx+nrElements-1) = ones(length(corrGlobal1),1);
                 idx = idx+nrElements;
             end
         end
+        % Drop part of arrays preallocated but not assigned
+        graph.edges(idx:end,:) = [];
+        graph.prob(idx:end) = [];
+        graph.cubeLI(idx:end) = [];
+        save([p.saveFolder 'graph.mat'], 'graph');
+        toc;
+    else
+        display('NOTE: Graph loaded from HD. Rename for recalculation');
+        load([p.saveFolder 'graph.mat']);
     end
 
-    % Collect all correspondences between local supervoxel graph
-    files = dir([p.correspondence.saveFolder, '*global.mat']);
-    for i=1:length(files)
-        % IMPORTANT TO DO: same here, change to using back all indices!
-        if ~(strcmp(files(i).name(1:2),'01') || strcmp(files(i).name(1:2), '15') || ...
-                strcmp(files(i).name(7:8),'01') || strcmp(files(i).name(7:8), '15'))
-            load([p.correspondence.saveFolder files(i).name]);
-            nrElements = length(corrGlobal1);
-            graph.edges(idx:idx+nrElements-1,:) = [corrGlobal1 corrGlobal2];
-            graph.prob(idx:idx+nrElements-1) = ones(length(corrGlobal1),1);
-            idx = idx+nrElements;
+    % Dont do this right now, big components, perculation etc. (no idea) :)
+    if ~exist([p.saveFolder 'graphNew.mat'], 'file');
+        tic;
+        % Find maximum ID before joining to determine new (joined) supervoxel IDs
+        load([p.local(end,end,end).saveFolder 'localToGlobalSegId.mat']);
+        maxGlobalId = max(globalIds);
+        % Upper threshold
+        idxJoined = graph.prob > upperT;
+        % Some bookkepping
+        edgesJoined = graph.edges(idxJoined,:);
+        %graph.probJoined = graph.prob(idxJoined);
+        %graph.edgesRemaining = graph.edges(~idxJoined,:);
+        %graph.probRemaining = graph.prob(~idxJoined);
+        [graph.ccEdgesJoined.equivalenceClasses, graph.ccEdgesJoined.objectClassLabels] = findConnectedComponents(edgesJoined);
+        % Loop for relabeling edges and calculating probabilities accordingly
+        if 0 % Do not globalize graph for now (components membership is checked in gameProblemTesting)
+            for i=1:length(graph.ccEdgesJoined.equivalenceClasses)
+                display(num2str(i));
+                tic;
+                % Find all remaining edges pointing into current (i-th) connected component (not within, see all in 2nd & 3rd line)
+                logicalFindEdges = ismember(graph.edgesRemaining, graph.ccEdgesJoined.equivalenceClasses{i});
+                idxIntoComponent = any(logicalFindEdges,2) & ~all(logicalFindEdges,2);
+                idxWithinComponent = all(logicalFindEdges,2);
+                % Store edges and probabilities into component before removing to calculate what to (re)insert
+                graph.ccEdgesJoined.edgesInto{i} = graph.edgesRemaining(idxIntoComponent,:);
+                graph.ccEdgesJoined.probInto{i} = graph.probRemaining(idxIntoComponent);
+                graph.ccEdgesJoined.edgesWithin{i} = graph.edgesRemaining(idxWithinComponent,:);
+                graph.ccEdgesJoined.probWithin{i} = graph.probRemaining(idxWithinComponent);           
+                % Remove all (into and within) of those from remaining edges and probabilities
+                graph.edgesRemaining(idxIntoComponent|idxWithinComponent,:) = [];
+                graph.probRemaining(idxIntoComponent|idxWithinComponent) = [];
+                % Renumber all edges into component to new supervoxel ID 
+                edgesNew = graph.ccEdgesJoined.edgesInto{i};
+                edgesNew(logicalFindEdges(idxIntoComponent,:)) = maxGlobalId + i;
+                % Make sure first edge column is always smaller
+                edgesNew = sort(edgesNew,2);
+                % Remove double entries / redundancies (thereby generating new edges into component after mapping)   
+                [graph.ccEdgesJoined.edgesNew{i}, idxNew, idxOld] = unique(edgesNew, 'rows');
+                % Calculate new probabilities
+                idxMatrix = sparse(1:length(idxOld), idxOld, 1);
+                probMatrix = bsxfun(@times, idxMatrix, graph.ccEdgesJoined.probInto{i});
+                probNew = zeros([size(probMatrix,2) 1]);
+                for j=1:length(probNew)
+                    probNew(j) = 1 - prod(1 - full(nonzeros(probMatrix(:,j))));
+                end
+                graph.ccEdgesJoined.probNew{i} = probNew;
+                % Reinsert into graph
+                graph.edgesRemaining = [graph.edgesRemaining; graph.ccEdgesJoined.edgesNew{i}];
+                graph.probRemaining = [graph.probRemaining; graph.ccEdgesJoined.probNew{i}];
+                toc;
+            end
         end
+        % Save for future use 
+        save([p.saveFolder 'graphNew.mat'], 'graph', '-v7.3');
+        toc;
+    else
+        display('Graph after GP and correspodence based joining of supervoxel loaded from disk, NOT recalculated')
+        load([p.saveFolder 'graphNew.mat']);
     end
-    % Drop part of arrays preallocated but not assigned
-    graph.edges(idx:end,:) = [];
-    graph.prob(idx:end) = [];
-    graph.cubeLI(idx:end) = [];
-    save([p.saveFolder 'graph.mat'], 'graph');
-    toc;
-else
-    display('NOTE: Graph loaded from HD. Rename for recalculation');
-    load([p.saveFolder 'graph.mat']);
-end
-
-% Dont do this right now, big components, perculation etc. (no idea) :)
-if 0 %~exist([p.saveFolder 'graphNew.mat'], 'file');
-    tic;
-    % Find maximum ID before joining to determine new (joined) supervoxel IDs
-    load([p.local(end,end,end).saveFolder 'localToGlobalSegId.mat']);
-    maxGlobalId = max(globalIds);
-    % Upper threshold
-    idxJoined = graph.prob > upperT;
-    % Some bookkepping
-    graph.edgesJoined = graph.edges(idxJoined,:);
-    graph.probJoined = graph.prob(idxJoined);
-    graph.edgesRemaining = graph.edges(~idxJoined,:);
-    graph.probRemaining = graph.prob(~idxJoined);
-    [graph.ccEdgesJoined.equivalenceClasses, graph.ccEdgesJoined.objectClassLabels] = findConnectedComponents(graph.edgesJoined);
-    % Loop for relabeling edges and calculating probabilities accordingly
-    for i=1:length(graph.ccEdgesJoined.equivalenceClasses)
-        % Find all remaining edges pointing into current (i-th) connected component (not within, see all in 2nd & 3rd line)
-        logicalFindEdges = ismember(graph.edgesRemaining, graph.ccEdgesJoined.equivalenceClasses{i});
-        idxIntoComponent = any(logicalFindEdges,2) & ~all(logicalFindEdges,2);
-        idxWithinComponent = all(logicalFindEdges,2);
-        % Store edges and probabilities into component before removing to calculate what to (re)insert
-        graph.ccEdgesJoined.edgesInto{i} = graph.edgesRemaining(idxIntoComponent,:);
-        graph.ccEdgesJoined.probInto{i} = graph.probRemaining(idxIntoComponent);
-        graph.ccEdgesJoined.edgesWithin{i} = graph.edgesRemaining(idxWithinComponent,:);
-        graph.ccEdgesJoined.probWithin{i} = graph.probRemaining(idxWithinComponent);           
-        % Remove all (into and within) of those from remaining edges and probabilities
-        graph.edgesRemaining(idxIntoComponent|idxWithinComponent,:) = [];
-        graph.probRemaining(idxIntoComponent|idxWithinComponent) = [];
-        % Renumber all edges into component to new supervoxel ID 
-        edgesNew = graph.ccEdgesJoined.edgesInto{i};
-        edgesNew(logicalFindEdges(idxIntoComponent,:)) = maxGlobalId + i;
-        % Make sure first edge column is always smaller
-        edgesNew = sort(edgesNew,2);
-        % Remove double entries / redundancies (thereby generating new edges into component after mapping)   
-        [graph.ccEdgesJoined.edgesNew{i}, idxNew, idxOld] = unique(edgesNew, 'rows');
-        % Calculate new probabilities
-        idxMatrix = sparse(1:length(idxOld), idxOld, 1);
-        probMatrix = bsxfun(@times, idxMatrix, graph.ccEdgesJoined.probInto{i});
-        probNew = zeros([size(probMatrix,2) 1]);
-        for j=1:length(probNew)
-            probNew(j) = 1 - prod(1 - full(nonzeros(probMatrix(:,j))));
-        end
-        graph.ccEdgesJoined.probNew{i} = probNew;
-        % Reinsert into graph
-        graph.edgesRemaining = [graph.edgesRemaining; graph.ccEdgesJoined.edgesNew{i}];
-        graph.probRemaining = [graph.probRemaining; graph.ccEdgesJoined.probNew{i}];
-    end
-    % Save for future use 
-    save([p.saveFolder 'graphNew.mat'], 'graph');
-    toc;
-else
-    display('Graph after GP and correspodence based joining of supervoxel loaded from disk, NOT recalculated')
-    load([p.saveFolder 'graphNew.mat']);
-end
 
 end
 
--- globalization/writeMissionJson.m
+++ globalization/writeMissionJson.m
@@ -1,99 +1,106 @@
-function writeMissionJson(par, problems)
+function writeMissionJson(p, problems, isControl, isTutorial)
 
     % Load global counter variables
-    if exist(par.counter, 'file')
-        load(par.counter);
+    if exist(p.kdb.counter, 'file')
+        load(p.kdb.counter);
     else
-        counter.taskId = 0;
+        counter.tasks = 0;
+    end
+
+    % put numbers in setParameterSettings (needs refactoring soon)
+    kdbBorder = [225 225 90];
+    missionsFolder = [p.saveFolder 'missions/'];
+    if ~exist(missionsFolder, 'dir')
+        mkdir(missionsFolder);
+    end
+    visualizationFolder = [missionsFolder 'visualization/'];
+    if ~exist(visualizationFolder, 'dir')
+        mkdir(visualizationFolder);
     end
 
     % Metadata fuer Tasks generieren
+    % Iterations of seeded processes
     for it=1:length(problems)
+        % Current node
         for no=1:length(problems(it).node)
-        tic;
-        % cut out local stack for speed purposes (and for having local CoM/direction)
-        errorCenter = problems(it).CoM;
-        bbox = errorCenter - [200 200 200]
-        segLocal = seg(errorCenter(1)-par.kdbBorder(1):errorCenter(1)+par.kdbBorder(1),...
-            errorCenter(2)-par.kdbBorder(2):errorCenter(2)+par.kdbBorder(2),...
-            errorCenter(3)-par.kdbBorder(3):errorCenter(3)+par.kdbBorder(3));
-        % Logical array of objects in small bounding box
-        obj1 = segLocal == edges(i,1);
-        obj2 = segLocal == edges(i,2);
-        % Determine CoM of segments
-        obj1Prop = regionprops(obj1, 'Centroid', 'Area', 'PixelList');
-        % This is necessary if object is split by by cutting out local bbox
-        if length(obj1Prop) > 1
-            comWrtEc = sum(bsxfun(@times, bsxfun(@minus, cat(1,obj1Prop(:).Centroid), par.kdbBorder), par.settings.scale) .^ 2,2);
-            [~, comIdx] = min(comWrtEc);
-            obj1Prop = obj1Prop(comIdx);
-        end
-        obj2Prop = regionprops(obj2, 'Centroid', 'Area', 'PixelList');
-        % This as well
-        if length(obj2Prop) > 1
-            comWrtEc = sum(bsxfun(@times, bsxfun(@minus, cat(1,obj2Prop(:).Centroid), par.kdbBorder), par.settings.scale) .^ 2,2);
-            [~, comIdx] = min(comWrtEc);
-            obj2Prop = obj2Prop(comIdx);
-        end
-        % Calculate mission direction in nm
-        direction = (obj2Prop.Centroid([2 1 3]) - obj1Prop.Centroid([2 1 3])) .* par.settings.scale;
-        direction = direction ./ norm(direction);
-        % First: Find a orthogonal vector & 2 more vec for ONB (uniformly sampled from vectors in orthogonal plane)
-        [randomOrthVec, orth1, orth2] = findRandomOrthogonal(direction);
-        % Calculate extend of start object wrt to error center in nm 
-        pixelPosWrtErrorCenter = bsxfun(@times, bsxfun(@minus, obj1Prop.PixelList(:,[2 1 3]), par.kdbBorder), par.settings.scale);
-        % Determine pixel list all objects
-        objAllProps = regionprops(segLocal, 'PixelList');
-        % Find objects ID of objects neighbouring object 1
-        [row, col] = find(edgesOld == edges(i,1));
-        col(col == 1) = 3;
-        col = col - 1;
-        for j=1:length(v)
-            % Set mission id
-            v(j).missions(i).missionId = taskId + i;
-            % Set control problem flag
-            v(j).missions(i).isControl = logical(par.controlFlag);
-            v(j).missions(i).isTutorial = logical(par.tutorialFlag);
-            % Start and end segment used for rendering (section-local)
-            v(j).missions(i).start.id = edges(i,1);
-            v(j).missions(i).end.id = edges(i,2);
-            % error center in voxel (global)
-            v(j).missions(i).errorCenter = errorCenter + par.bboxBig(:,1)';
-            % Calculate CoM of objects in voxel
-            v(j).missions(i).start.CoM = obj1Prop.Centroid([2 1 3]) + v(j).missions(i).errorCenter - par.kdbBorder;
-            v(j).missions(i).end.CoM = obj2Prop.Centroid([2 1 3]) + v(j).missions(i).errorCenter - par.kdbBorder;
-            % Rotated or original direction
-            if v(j).isRotated
-                v(j).missions(i).start.direction = rotate(direction, randomOrthVec, rand(1)*v(j).openingAngle);
-                [~, v(j).missions(i).start.orth1, v(j).missions(i).start.orth2] = findRandomOrthogonal(v(j).missions(i).start.direction);
-            else
-                v(j).missions(i).start.direction = direction;
-                v(j).missions(i).start.orth1 = orth1;
-                v(j).missions(i).start.orth2 = orth2;
-            end
-            % Calculate rendering length
-            [v(j).missions(i).start.firstFrame v(j).missions(i).start.lastFrame] = ...
-                calcRL(pixelPosWrtErrorCenter, v(j).missions(i).start.direction, v(j).missions(i).start.orth1, v(j).missions(i).start.orth2, ...
-                v(j).width/2*par.settings.scale(1), v(j).height/2*par.settings.scale(2));
-            % Iterate over possible ends
-            currentEnd = 1;
-            for k=1:length(row)
-                if ~isempty(find(segLocal == edgesOld(row(k),col(k))))
-                    % Write id of connected object and probability of connection to struct
-                    v(j).missions(i).possibleEnds(currentEnd).id = edgesOld(row(k),col(k));
-                    v(j).missions(i).possibleEnds(currentEnd).probability = pOld(row(k));
-                    % Calculate rendering length
-                    temp =  bsxfun(@times, bsxfun(@minus, objAllProps(v(j).missions(i).possibleEnds(currentEnd).id).PixelList(:,[2 1 3]), par.kdbBorder), par.settings.scale);
-                    [v(j).missions(i).possibleEnds(currentEnd).firstFrame v(j).missions(i).possibleEnds(currentEnd).lastFrame] = ...
-                        calcRL(temp, v(j).missions(i).start.direction, v(j).missions(i).start.orth1, v(j).missions(i).start.orth2, ...
-                        v(j).width/2*par.settings.scale(1), v(j).height/2*par.settings.scale(2));
-                    temp =  (borderOld(row(k)).Centroid([2 1 3]) - errorCenter) .* par.settings.scale;
-                    v(j).missions(i).possibleEnds(currentEnd).touchFrame = temp * v(j).missions(i).start.direction';
-                    % Increase counter for possible ends
-                    currentEnd = currentEnd + 1;
+            % All neighbours of current node
+            for ne=1:length(problems(it).node(no).CoM)
+                % all borders between current node and respective neighboue
+                for bo=1:size(problems(it).node(no).CoM{ne},1)
+                    tic;
+                    % Start and End segmentation ID for this problem
+                    startId = problems(it).node(no).self.id; 
+                    endId = problems(it).node(no).neighbours(ne).id;
+                    % cut out local stack for speed purposes (and for having local CoM/direction)
+                    errorCenter = problems(it).node(no).CoM{ne}(bo,:);
+                    bbox = bsxfun(@plus, errorCenter, [-kdbBorder; kdbBorder]);
+                    segLocal = readKnossosRoi(p.seg.root, p.seg.prefix, bbox', 'uint32', '', 'raw');
+                    % Calculate object properties of start and end segment within local stack
+                    obj1Prop = calculateObjectProperties(segLocal, startId); 
+                    obj2Prop = calculateObjectProperties(segLocal, endId); 
+                    % Calculate mission direction (normalized) in nm space
+                    direction = (obj2Prop.Centroid - obj1Prop.Centroid) .* p.kdb.settings.scale;
+                    direction = direction ./ norm(direction);
+                    % First: Find a orthogonal vector & 2 more vec for ONB (uniformly sampled from vectors in orthogonal plane)
+                    [randomOrthVec, orth1, orth2] = findRandomOrthogonal(direction);
+                    % Calculate extend of start object wrt to error center in nm 
+                    pixelPosWrtErrorCenter = bsxfun(@times, bsxfun(@minus, obj1Prop.PixelList, kdbBorder), par.settings.scale);
+                    % Determine pixel list all objects
+                    objAllProps = regionprops(segLocal, 'PixelList');
+                    % Find objects ID of objects neighbouring object 1
+                    [row, col] = find(edgesOld == edges(i,1));
+                    col(col == 1) = 3;
+                    col = col - 1;
+                    for j=1:length(v)
+                        % Set mission id
+                        v(j).missions(i).missionId = taskId + i;
+                        % Set control problem flag
+                        v(j).missions(i).isControl = isControl;
+                        v(j).missions(i).isTutorial = isTutorial;
+                        % Start and end segment used for rendering (section-local)
+                        v(j).missions(i).start.id = startId;
+                        v(j).missions(i).end.id = endId;
+                        % error center in voxel (global)
+                        v(j).missions(i).errorCenter = errorCenter;
+                        % Calculate CoM of objects in voxel
+                        v(j).missions(i).start.CoM = obj1Prop.Centroid + errorCenter - kdbBorder;
+                        v(j).missions(i).end.CoM = obj2Prop.Centroid + errorCenter - kdbBorder;
+                        % Rotated or original direction
+                        if v(j).isRotated
+                            v(j).missions(i).start.direction = rotate(direction, randomOrthVec, rand(1)*v(j).openingAngle);
+                            [~, v(j).missions(i).start.orth1, v(j).missions(i).start.orth2] = findRandomOrthogonal(v(j).missions(i).start.direction);
+                        else
+                            v(j).missions(i).start.direction = direction;
+                            v(j).missions(i).start.orth1 = orth1;
+                            v(j).missions(i).start.orth2 = orth2;
+                        end
+                        % Calculate rendering length
+                        [v(j).missions(i).start.firstFrame v(j).missions(i).start.lastFrame] = ...
+                            calcRL(pixelPosWrtErrorCenter, v(j).missions(i).start.direction, v(j).missions(i).start.orth1, v(j).missions(i).start.orth2, ...
+                            v(j).width/2*par.settings.scale(1), v(j).height/2*par.settings.scale(2));
+                        % Iterate over possible ends
+                        currentEnd = 1;
+                        for k=1:length(row)
+                            if ~isempty(find(segLocal == edgesOld(row(k),col(k))))
+                                % Write id of connected object and probability of connection to struct
+                                v(j).missions(i).possibleEnds(currentEnd).id = edgesOld(row(k),col(k));
+                                v(j).missions(i).possibleEnds(currentEnd).probability = pOld(row(k));
+                                % Calculate rendering length
+                                temp =  bsxfun(@times, bsxfun(@minus, objAllProps(v(j).missions(i).possibleEnds(currentEnd).id).PixelList(:,[2 1 3]), par.kdbBorder), par.settings.scale);
+                                [v(j).missions(i).possibleEnds(currentEnd).firstFrame v(j).missions(i).possibleEnds(currentEnd).lastFrame] = ...
+                                    calcRL(temp, v(j).missions(i).start.direction, v(j).missions(i).start.orth1, v(j).missions(i).start.orth2, ...
+                                    v(j).width/2*par.settings.scale(1), v(j).height/2*par.settings.scale(2));
+                                temp =  (borderOld(row(k)).Centroid([2 1 3]) - errorCenter) .* par.settings.scale;
+                                v(j).missions(i).possibleEnds(currentEnd).touchFrame = temp * v(j).missions(i).start.direction';
+                                % Increase counter for possible ends
+                                currentEnd = currentEnd + 1;
+                            end
+                        end
+                    end
                 end
             end
         end
+        % Some bloated code just to have an updating progress withoit flooding command line
         t = toc;
         if i==1
             fprintf(['\nMission just written (total ' num2str(size(edges,1)) '): ']);
@@ -107,28 +114,41 @@ function writeMissionJson(par, problems)
     fprintf('\n');
 
     % Save global counter variables
-    save(par.counter, 'counter');
+    save(p.counter, 'counter');
 
     % Write mission json (named according to input file)
     missionStruct.sectionId = 'section1';
     missionStruct.viewports = v;
-    savejson('', missionStruct, 'FileName', [rootFolder 'missions.json'], 'ParseLogical', 1);
+    writeJson([missionsFolder 'missions.json'], missionStruct);
 
     % save everything to synced folder for problem inspection if flag is set
     if par.saveForProblemInspector
-        raw = loadRawData(par.raw.root, par.raw.prefix, par.bboxBig, 0); 
-        save(['/zdata/manuel/sync/problemInspector/' 'kDbResults' strrep(par.start, '/', '-') '.mat'], 'par', 'raw', 'seg', 'mito', 'v', '-v7.3');
+        % call some function I guess 
     end
 
+end
+
+function obj1Prop = calculateObjectProperties(segLocal, segId)
+    obj1 = segLocal == segId;
+    obj1Prop = regionprops(obj1, 'Centroid', 'Area', 'PixelList');
+    % Invert x-y coordinated of Centroid
+    obj1Prop.Centroid = obj1Prop.Centroid([2 1 3]);
+    obj1Prop.PixelList = obj1Prop.PixelList(:, [2 1 3]);
+    % This is necessary if object is split by by cutting out local bbox
+    if length(obj1Prop) > 1
+        comWrtEc = sum(bsxfun(@times, bsxfun(@minus, cat(1,obj1Prop(:).Centroid), par.kdbBorder), par.settings.scale) .^ 2,2);
+        [~, comIdx] = min(comWrtEc);
+        obj1Prop = obj1Prop(comIdx);
+    end
 end
 
 function [randomVec, orth1, orth2] = findRandomOrthogonal(v)
     % returns random orthogonal vector and and the 2nd and 3rd vector for ONB 
     v = v ./ norm(v);
     if all(v == [0 0 1])
-        orth1 = [1 0 -1].*v([3 2 1]);  
+        orth1 = [1 0 -1].*v;  
     else
-        orth1 = [1 -1 0].*v([2 1 3]);
+        orth1 = [1 -1 0].*v;
     end
     orth1 = orth1 ./ norm(orth1);
     orth2 = cross(v, orth1); 
