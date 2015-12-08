c5119b60cfb11094ef1358a7bb28956ac7fbfd99
Manuel Berning <manuel.berning@brain.mpg.de>
2015-12-08 18:57:10 +0100

    use indexing instead of matrix multiplication xD

--- games/agglomerateSG2.m
+++ games/agglomerateSG2.m
@@ -1,6 +1,5 @@
 function [newSeeds, probabilities] = agglomerateSG2(graph, seeds, nrSteps)
     % Agglomerates supervoxel given the graph, always highest probability for nrSteps
-    % Only works for single segmentation ID seeds right now
 
     % Initalization
     newSeeds = seeds;
@@ -9,24 +8,15 @@ function [newSeeds, probabilities] = agglomerateSG2(graph, seeds, nrSteps)
     display('Agglomerating supervoxel');
     tic;
     for s=1:length(seeds)
-        thisSeed = sparse([],[],[],size(graph.adj,1), size(graph.adj,2), (nrSteps+1)*size(graph.adj,1));
         for n=1:nrSteps
-            tic;
-            % Generate logical vector representation of seed
-            if n==1
-                thisSeed(seeds{s}(1),:) = 1;
-            else
-                thisSeed(maxId,:) = 1;
-            end
             % Multiply with GP-based adjaceny matrix
-            theseProbabilities = max(graph.adj .* thisSeed,[],1);
+            theseProbabilities = max(graph.adj(newSeeds{s},:),[],1);
             % Remove self edges (within component)
             theseProbabilities(newSeeds{s}) = 0;
             % Add maxium probability ID to agglomerated seed
             [maxProb, maxId] = max(theseProbabilities);
             newSeeds{s}(n+1) = maxId;
             probabilities{s}(n+1) = full(maxProb);
-            toc;
         end
         % Display progress
         Util.progressBar(s, length(seeds));
