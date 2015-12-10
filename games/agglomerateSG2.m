9b537e67ea3509952836e497d1a690a09303aee8
Manuel Berning <manuel.berning@brain.mpg.de>
2015-12-10 10:43:37 +0100

    working changes for testing with skeleton oracles

--- games/agglomerateSG2.m
+++ games/agglomerateSG2.m
@@ -1,9 +1,10 @@
-function [newSeeds, probabilities] = agglomerateSG2(graph, seeds, nrSteps)
+function [newSeeds, probabilities, stats] = agglomerateSG2(graph, seeds, nrSteps)
     % Agglomerates supervoxel given the graph, always highest probability for nrSteps
 
     % Initalization
     newSeeds = seeds;
     probabilities = cell(size(seeds));
+    stats = struct();
 
     display('Agglomerating supervoxel');
     tic;
@@ -17,6 +18,13 @@ function [newSeeds, probabilities] = agglomerateSG2(graph, seeds, nrSteps)
             [maxProb, maxId] = max(theseProbabilities);
             newSeeds{s}(n+1) = maxId;
             probabilities{s}(n+1) = full(maxProb);
+            % Temporary section to evaluate stop conditions
+            stats(s,n).segId = maxId;
+            stats(s,n).neighbours = find(graph.adj(maxId,:));
+            stats(s,n).prob = graph.adj(maxId, stats(s,n).neighbours);
+            if maxId == 6033884 || maxId ==  6035398 || maxId == 5934237
+                break;
+            end
         end
         % Display progress
         Util.progressBar(s, length(seeds));
--- games/main3.m
+++ games/main3.m
@@ -2,20 +2,37 @@
 load('/gaba/u/mberning/results/pipeline/20151111T183414/allParameter.mat');
 load([p.saveFolder 'graph.mat']);
 load([p.saveFolder 'CoM.mat']);
+load([p.saveFolder 'agglomeration/nucleiVesselBorder.mat']);
 
 % Construct graph (now saved in graph.mat)
 %edges = [graph.edges; graph.edges(:,[2 1])];
 %prob = [graph.prob; graph.prob];
 %graph.adj = accumarray(edges, prob, [], @max, 0, 1);
-%save([p.saveFolder 'graph.mat'], 'graph', '-v7.3');
+[graph.neighbours, graph.neighbourIdx] = Graph.edges2Neighbors(graph.edges);
+save([p.saveFolder 'graph.mat'], 'graph', '-v7.3');
 
-% Get seeds from skeleton for now
-skel = parseNml('/gaba/u/mberning/3seeds.nml');
-segIds = cell(size(skel));
-for i=1:length(skel)
-    pos = skel{i}.nodes(1:3) + [1 1 1];
-    segIds{i} = readKnossosRoi(p.seg.root, p.seg.prefix, [pos; pos]', 'uint32', '', 'raw');
+% Get seeds & GT from MH skeletons for now
+folder = '/gaba/u/mberning/axonsTest/';
+files = dir([folder '*.nml']);
+tr = 1;
+for i=1:length(files)
+    skel{i} = parseNml([folder files(i).name]);
+    for j=1:length(skel{i})
+        nodes{tr} = bsxfun(@plus, skel{i}{j}.nodes(:,1:3), [1 1 1]);
+        tr = tr + 1;
+    end
 end
+segIds = cell(size(nodes));
+for i=1:length(nodes)
+    for j=1:size(nodes{i},1)
+        pos = nodes{i}(j,:);
+        segIds{i}(j) = readKnossosRoi(p.seg.root, p.seg.prefix, [pos; pos]', 'uint32', '', 'raw');
+    end
+end
+segIds = cellfun(@(x)x(x~=0), segIds, 'UniformOutput', false)
+% Get intersection of axons with border IDs
+borderId = cat(1,agglo.borderMerged{:});
+seeds = cellfun(@(x)intersect(x, borderId), segIds, 'UniformOutput', false);
 
 % Agglomerate segments and save result to skeleton
 [collectedIds, probabilities] = agglomerateSG2(graph, segIds, 100);
