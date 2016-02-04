14e1852e6799245cb1790c82a31be90439115782
Manuel Berning <manuel.berning@brain.mpg.de>
2016-02-04 11:56:55 +0100

    continue clean up of interface for others to use

--- exampleUsage.m
+++ exampleUsage.m
@@ -1,3 +1,4 @@
+%% Set these values 
 
 % Choose were to store result of the calculations, make sure you have WRITE access;
 p.saveFolder = '/gaba/u/mberning/results/pipeline/test/';
@@ -6,9 +7,15 @@ p.bbox = [1153 7808; 769 5376; 129 3200];
 % Define directory and file prefix for KNOSSOS hierachy with raw data
 p.raw.root = '/gaba/u/mberning/data/cortex/2012-09-28_ex145_07x2_corrected/color/1/';
 p.raw.prefix = '2012-09-28_ex145_07x2_corrected_mag1';
+% Control how data is normalized prior to 
+p.norm.func = @(x)normalizeStack(x,122,22);
+% This is the function used for watershed-based segmentation of the
+% dataset, increase first parameter for more undersegmentation and vice
+% versa
+p.seg.func = @(x)watershedSeg_v1_cortex(x,{.25 10});
 
 % Add constant and optional settings to the parameter structure
 p = setParameterSettings(p);
-% This will take some time, run classification, segmentation, correspondences, globalization, graph construction
-runPipeline(p);
 
+%% This will take some time, run classification, segmentation, correspondences, globalization, graph construction, feature calculation
+runPipeline(p);
--- pipeline/runPipeline.m
+++ pipeline/runPipeline.m
@@ -1,16 +1,27 @@
 function runPipeline(p)
 
-    % Runs CNN based forward pass for region defined in p and saves as p.class
+    % Runs CNN based forward pass for region defined in p.bbox,p.raw and saves as p.class
+    % Because CNN is translation invariant, saved as KNOSSOS hierachy again
     job = bigFwdPass(p);
     waitForJob(job);
-    % Runs watershed based segmentation for region defined in p and saves as p.local.segFile
+    % Runs watershed based segmentation for region defined in p.bbox,p.raw and saves as p.local.segFile
+    % Because watershed segmentation has FOV effects (no translation invariance), processed with large
+    % overlap and later joined together
     job = miniSegmentation(p);
     waitForJob(job);
-
+    % Find correspondences between tiles processed (segmented) in paralell
     job = correspondenceFinder(p);
-
+    waitForJob(job);
+    % Make segmentation IDs unique over dataset (were only unique in each
+    % tile before), this will be called global IDs from time to time
+    job = globalization(p);
+    waitForJob(job);
+    % Construct graph on globalized version of segmentation
     job = graphConstruction(p);
     waitForJob(job);
-
+    % Calculate features for edges as used for inital GP    
+    job = miniFeature(p);
+    waitForJob(job);
+    
 end
 
