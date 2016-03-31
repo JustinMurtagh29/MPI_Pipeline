d3b2d457aa75f1cd0b65543a5975791e7b1aeb2e
Sahil Loomba <sahilloo@gaba.opt.rzg.mpg.de>
2016-03-31 12:56:26 +0200

    corrected segFile to segmentFile in calcFeatures

--- filterbank/calcFeatures.m
+++ filterbank/calcFeatures.m
@@ -14,7 +14,7 @@ end
 
 % Load pixel list for borders and segments
 load(parameter.local(sub(1),sub(2),sub(3)).borderFile);
-load(parameter.local(sub(1),sub(2),sub(3)).segFile);
+load(parameter.local(sub(1),sub(2),sub(3)).segmentFile);
 
 % Define bounding box, add border so that 3D filter do not 
 bbox = parameter.local(sub(1),sub(2),sub(3)).bboxSmall + [-10 10; -10 10; -10 10];
