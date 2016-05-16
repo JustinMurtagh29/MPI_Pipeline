b224885dee698e03f7360df6679bb6cc713ac183
Jakob Straehle <jakob.straehle@gmail.com>
2016-05-16 17:45:02 +0200

    searchMovieTres is a function that allows to loop through different segmentations thresholds in a efficient way and writed movies. It was necessary to intoduce a flag into makeSegmentationPreviewMovie.m to prevent the feature calculation in each iteration.

--- pipeline/makeSegmentationPreviewMovie.m
+++ pipeline/makeSegmentationPreviewMovie.m
@@ -1,45 +1,51 @@
-function outputFile = makeSegmentationPreviewMovie(p, bbox)
-    % Make movie to visualize segmentation given some parameter set p
-    
-    % also accept webKNOSSOS-style bouding boxes,
-    % i.e. (x_min, y_min, z_min, x_max, y_max, z_max)
-    if size(bbox, 1) == 1 || size(bbox, 2) == 1
-        bbox = reshape(bbox, [3, 2]);
-    end
+function outputFile = makeSegmentationPreviewMovie(p, bbox, flag)
+% Make movie to visualize segmentation given some parameter set p
+if nargin <3
+    flag = 1; % if this function is running in a loop this flag prevents it from calculating the classification multiple times
+end
 
-    % First use same function as bigFwdPass.m to do classification
-    functionH = @onlyFwdPass3DonKnossosFolder;
-    tempClass.root = [p.tempFolder 'classForMovie/'];
-    tempClass.prefix = 'classForMovie';
-    inputCell{1} = {p.cnn.first, p.cnn.GPU, p.raw, tempClass, bbox, p.norm.func};
-    
+% also accept webKNOSSOS-style bouding boxes,
+% i.e. (x_min, y_min, z_min, x_max, y_max, z_max)
+if size(bbox, 1) == 1 || size(bbox, 2) == 1
+    bbox = reshape(bbox, [3, 2]);
+end
+
+% First use same function as bigFwdPass.m to do classification
+functionH = @onlyFwdPass3DonKnossosFolder;
+tempClass.root = [p.tempFolder 'classForMovie/'];
+tempClass.prefix = 'classForMovie';
+inputCell{1} = {p.cnn.first, p.cnn.GPU, p.raw, tempClass, bbox, p.norm.func};
+
+if flag == 1 % flag preventing the calculation classification for multiple movies
     if p.cnn.GPU
         job = startGPU(functionH, inputCell, 'classForMovie');
     else
         job = startCPU(functionH, inputCell, 'classForMovie');
     end
-    
     Cluster.waitForJob(job);
-    
-    % Now do segmentation as in miniSegmentation.m
-    tempSegFile = [p.tempFolder 'seg.mat'];
-    inputCell{1} = {tempClass.root, tempClass.prefix, bbox, p.seg.func, tempSegFile};
-    functionH = @segmentForPipeline;
-    job = startCPU(functionH, inputCell, 'segForMovie');
-    Cluster.waitForJob(job);
-    
-    % Now low raw data
-    raw = loadRawData(p.raw.root, p.raw.prefix, bbox, false);
-    
-    % Normalize to [0 1] range as this is what matlab expects of single images
-    raw = raw ./ max(raw(:));
-    
-    % Load segmentation
-    load(tempSegFile);
-    
-    % Make movie
-    outputFile = [p.tempFolder datestr(clock, 30) '.avi'];
-    makeSegMovie(seg, raw, outputFile);
+else
+    warning('omitting classification')
+end
+
+% Now do segmentation as in miniSegmentation.m
+tempSegFile = [p.tempFolder 'seg.mat'];
+inputCell{1} = {tempClass.root, tempClass.prefix, bbox, p.seg.func, tempSegFile};
+functionH = @segmentForPipeline;
+job = startCPU(functionH, inputCell, 'segForMovie');
+Cluster.waitForJob(job);
+
+% Now low raw data
+raw = loadRawData(p.raw.root, p.raw.prefix, bbox, false);
+
+% Normalize to [0 1] range as this is what matlab expects of single images
+raw = raw ./ max(raw(:));
+
+% Load segmentation
+load(tempSegFile);
+
+% Make movie
+outputFile = [p.tempFolder datestr(clock, 30) '.avi'];
+makeSegMovie(seg, raw, outputFile);
 
 end
 
---
+++ searchMovieTres.m
@@ -0,0 +1,23 @@
+function searchMovieTres()
+% function that allows you to create multiple movies of different
+% segmentations. Set the appropriate Threshold in the Threshold loop. To
+% prevent the multiple calculation of the classification a flag was
+% introduced in makeSegmentationPreviewMovie
+
+
+startup % starts everything and initiated Cluster Object
+
+run configuration.m
+
+flag = 0; % usually start with 1 whenever FOV was changed;
+for threshold = 0.1:0.05:0.3
+    
+    % change threshold and threshold function
+    p.seg.threshold = threshold;
+    p.seg.func = @(x)watershedSeg_v1_cortex(x,{p.seg.threshold 10});
+    
+    makeSegmentationPreviewMovie(p, [3933,4652; 994,2273 ; 1900 1999], flag) % second oint hast to be larger in every dimension
+    
+    flag = 0; % set flag to 0 to prevent recalculation of classification each time
+end
+end
