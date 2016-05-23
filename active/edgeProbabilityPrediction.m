032b1f522e3aec74747dda08f6b5e076c9648053
Sahil Loomba <sahilloo@gaba.opt.rzg.mpg.de>
2016-05-23 13:32:23 +0200

    gpml/startup.m path fix

--- active/edgeProbabilityPrediction.m
+++ active/edgeProbabilityPrediction.m
@@ -12,12 +12,7 @@ weights = normalizeDataForGP(weights, false, normFile);
 
 me = mfilename;                                            % what is my filename
 mydir = which(me); mydir = mydir(1:end-2-numel(me));        % where am I located
-if numel(mydir)==2
-  if strcmp(mydir,'./'), mydir = [pwd,mydir(2:end)]; end
-end                 % OCTAVE 3.0.x relative, MATLAB and newer have absolute path
-
-
-run([mydir,'active/gpml/startup.m']);
+run([mydir,'gpml/startup.m']);
 
 
 % Make predictions
