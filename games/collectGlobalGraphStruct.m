bc191f7a0d38b012cfeeadbb4ec8958c6c662e19
Loomba, Sahil (sahilloo) <sahil.loomba@brain.mpg.de>
2016-05-29 09:29:47 +0200

    Update collectGlobalGraphStruct.m

--- games/collectGlobalGraphStruct.m
+++ games/collectGlobalGraphStruct.m
@@ -3,6 +3,8 @@ function collectGlobalGraphStruct (p)
 load([p.saveFolder 'globalEdges.mat']);
 load([p.saveFolder 'globalCoMList.mat']);
 load([p.saveFolder 'globalGPProbList.mat']);
+load([p.saveFolder 'globalBorder.mat']);
+
 
 [ neighbours, neighboursIdx ] = Graph.edges2Neighbors(edges);
 
@@ -12,6 +14,9 @@ graph.neighbours = neighbours;
 graph.neighProb = neighProb;
 graph.edges = edges;
 graph.prob = prob;
+graph.borderCentroid = borderCoM;
+graph.borderSize = borderSize;
+graph.borderArea = borderArea;
 
 save([p.saveFolder 'graph.mat'],'graph','-v7.3');
 
