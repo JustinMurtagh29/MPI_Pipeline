643738a3816d43a3a742aafb8f4a0416785546f1
Sahil Loomba <sahilloo@gaba.opt.rzg.mpg.de>
2016-05-30 11:13:16 +0200

    query generation code

--- games/determineQuerryLocation.m
+++ games/determineQuerryLocation.m
@@ -1,23 +1,23 @@
-function [p, d, e, vE] = determineQuerryLocation(graph, com, cc)
+function [p, d, e, vE] = determineQuerryLocation(graphStruct, com, cc)
     % Determine querry position, direction and edge based on PCA
 
-    theseCoM = com(cc,:);
-    [coeff,score,latent] = princomp(theseCoM);
+    theseCoM = double(com(cc,:));
+    [coeff,score,latent] = pca(theseCoM);
     [~,minIdx] = min(score(:,1));
     [~,maxIdx] = max(score(:,1));
     p(1,:) = theseCoM(minIdx,:);
     p(2,:) = theseCoM(maxIdx,:);
     d(1,:) = -coeff(:,1);
     d(2,:) = coeff(:,1);
-    e(1,:) = determineEdge(graph, cc, minIdx);
-    e(2,:) = determineEdge(graph, cc, maxIdx);
+    e(1,:) = determineEdge(graphStruct, cc, minIdx);
+    e(2,:) = determineEdge(graphStruct, cc, maxIdx);
     vE = latent./sum(latent);
 
 end
 
-function edge = determineEdge(graph, cc, ccIdx)
-    theseN = graph.neighbours{cc(ccIdx)};
-    theseP = graph.neighProb{cc(ccIdx)};
+function edge = determineEdge(graphStruct, cc, ccIdx)
+    theseN = graphStruct.neighbours{cc(ccIdx)};
+    theseP = graphStruct.neighProb{cc(ccIdx)};
     keep = ~ismember(theseN, cc);
     theseN = theseN(keep);
     theseP = theseP(keep);
