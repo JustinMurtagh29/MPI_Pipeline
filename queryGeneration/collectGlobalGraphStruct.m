0adf86cc6a25d8a818b48c02cd5bb9a71152d5ee
Loomba, Sahil (sahilloo) <sahil.loomba@brain.mpg.de>
2016-06-07 14:47:25 +0200

    Update collectGlobalGraphStruct.m

--- queryGeneration/collectGlobalGraphStruct.m
+++ queryGeneration/collectGlobalGraphStruct.m
@@ -1,29 +1,3 @@
-%{
-function collectGlobalGraphStruct (p)
-
-load([p.saveFolder 'globalEdges.mat']);
-load([p.saveFolder 'globalCoMList.mat']);
-load([p.saveFolder 'globalGPProbList.mat']);
-load([p.saveFolder 'globalBorder.mat']);
-
-[ neighbours, neighboursIdx ] = Graph.edges2Neighbors(edges);
-
-neighProb = cellfun(@(x) prob(x),neighboursIdx,'UniformOutput',false);
-
-graph.neighbours = neighbours;
-graph.neighProb = neighProb;
-graph.edges = edges;
-graph.prob = prob;
-graph.borderCentroid = borderCoM;
-graph.borderSize = borderSize;
-graph.borderArea = borderArea;
-
-save([p.saveFolder 'graph.mat'],'graph','-v7.3');
-
-end
-%}
-
-
 function collectGlobalGraphStruct (p)
 
 load([p.saveFolder 'globalEdges.mat']);
@@ -32,7 +6,7 @@ load([p.saveFolder 'globalGPProbList.mat']);
 load([p.saveFolder 'globalBorder.mat']);
 
 % Correspondences were missing, load here for now, decide whether to also save globally
-correspondences = getGlobalCorrespondences(p);
+correspondences = Seg.Global.getGlobalCorrespondences(p);
 edges = cat(1, edges, correspondences);
 prob = cat(1, prob, ones(length(correspondences),1));
 
