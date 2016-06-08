f3f4e2a6c1da0a74407766c796333d11d99d900e
Alessandro Motta <alessandro.motta@brain.mpg.de>
2016-06-08 14:49:16 +0200

    Adjust calcFeatures to new API of featureDesign

--- filterbank/calcFeatures.m
+++ filterbank/calcFeatures.m
@@ -15,7 +15,10 @@ end
 % Load pixel list for borders and segments
 load(parameter.local(sub(1),sub(2),sub(3)).borderFile);
 load(parameter.local(sub(1),sub(2),sub(3)).segmentFile);
-borders = borders'; % 1xN struct to Nx1 struct
+
+% extract voxel ids
+borders = arrayfun(@(s) {s.PixelIdxList}, borders(:));
+segments = arrayfun(@(s} {s.PixelIdxList}, segments(:));
 
 % Define bounding box, add border so that 3D filter do not 
 bbox = parameter.local(sub(1),sub(2),sub(3)).bboxSmall + [-10 10; -10 10; -10 10];
