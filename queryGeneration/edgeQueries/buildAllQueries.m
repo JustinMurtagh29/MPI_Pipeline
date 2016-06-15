cc830d01fadf8cf4089ea794f04ee524d0e18658
Sahil Loomba <sahilloo@gaba.opt.rzg.mpg.de>
2016-06-15 15:54:44 +0200

    set parameters for writing nml by specifying experiment name

--- queryGeneration/edgeQueries/buildAllQueries.m
+++ queryGeneration/edgeQueries/buildAllQueries.m
@@ -1,4 +1,4 @@
-function buildAllQueries(param, outDir, edges)
+function buildAllQueries(param, outDir, edges,experimentName)
     % buildAllQueries(param, outDir, edges)
     %   Builds all queries from a list of edges and saves
     %   them as NML files to 'outDir'.
@@ -13,16 +13,16 @@ function buildAllQueries(param, outDir, edges)
     
     % run
     doFunc = @(idx) ...
-        writeQuery(outDir, com, edges(idx, :));
+        writeQuery(outDir, com, edges(idx, :),experimentName);
     arrayfun(doFunc, 1:edgeCount);
 end
 
-function writeQuery(outDir, com, edge)
+function writeQuery(outDir, com, edge,experimentName)
     fileName = [ ...
         'edge-', num2str(edge(1)), ...
         '-to-', num2str(edge(2)), '.nml'];
     filePath = fullfile(outDir, fileName);
     
-    skel = buildEdgeQuery(com, edge);
+    skel = buildEdgeQuery(com, edge,experimentName);
     skel.write(filePath);
 end
