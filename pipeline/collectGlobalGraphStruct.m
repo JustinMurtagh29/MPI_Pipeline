f2714a2c178f75b945c0e0edff63b037d0421cf0
Alessandro Motta <alessandro.motta@brain.mpg.de>
2016-06-22 10:10:08 +0200

    Make sure that edges in graph file are pre-sorted. Fix #23

--- pipeline/collectGlobalGraphStruct.m
+++ pipeline/collectGlobalGraphStruct.m
@@ -1,30 +1,46 @@
 function collectGlobalGraphStruct (p)
+    load([p.saveFolder 'globalEdges.mat']);
+    load([p.saveFolder 'globalCoMList.mat']);
+    load([p.saveFolder 'globalGPProbList.mat']);
+    load([p.saveFolder 'globalBorder.mat']);
 
-load([p.saveFolder 'globalEdges.mat']);
-load([p.saveFolder 'globalCoMList.mat']);
-load([p.saveFolder 'globalGPProbList.mat']);
-load([p.saveFolder 'globalBorder.mat']);
+    % The edge list only contains edges between segments in
+    % the same segmentation cube. Edges between different
+    % cubes are called 'correspondences' and must be added
+    % separately here.
+    corrEdges = Seg.Global.getGlobalCorrespondences(p);
+    corrProb  = ones(size(corrEdges, 1), 1);
 
-% Correspondences were missing, load here for now, decide whether to also save globally
-correspondences = Seg.Global.getGlobalCorrespondences(p);
-edges = cat(1, edges, correspondences);
-prob = cat(1, prob, ones(length(correspondences),1));
+    % build total edges and prob fields
+    edges = [edges; corrEdges];
+    prob  = [prob ; corrProb ];
 
-[ neighbours, neighboursIdx ] = Graph.edges2Neighbors(edges);
+    % sort edges and probabilities
+    [edges, edgeRows] = sortrows(edges);
+    prob = prob(edgeRows);
 
-neighProb = cellfun(@(x) prob(x),neighboursIdx,'UniformOutput',false);
+    % build look up tables for neighbouring
+    % segment IDs and their connection probabilities
+    [neighbours, neighboursIdx] = ...
+        Graph.edges2Neighbors(edges);
+    neighProb = cellfun(@(x) ...
+        prob(x), neighboursIdx, 'UniformOutput', false);
 
-graph = struct;
-graph.neighbours = neighbours;
-graph.neighProb = neighProb;
-graph.edges = edges;
-graph.prob = prob;
-graph.borderCentroid = borderCoM;
-graph.borderSize = borderSize;
-graph.borderArea = borderArea;
+    % build output
+    graph = struct;
+    graph.neighbours = neighbours;
+    graph.neighProb = neighProb;
+    graph.edges = edges;
+    graph.prob = prob;
 
-save([p.saveFolder 'graph.mat'],'-struct','graph','-v7.3');
+    % amotta: does this really belong here?
+    graph.borderCentroid = borderCoM;
+    graph.borderSize = borderSize;
+    graph.borderArea = borderArea;
 
+    % save output
+    graphFile = [p.saveFolder, 'graph.mat'];
+    save(graphFile, '-struct', 'graph', '-v7.3');
 end
 
 
