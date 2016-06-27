26dba397f4a983a4a56724673c7e4b7d94f1d1d2
Alessandro Motta <alessandro.motta@brain.mpg.de>
2016-06-27 10:34:12 +0200

    Convert globalization of segment IDs to Fortran order

--- pipeline/globalizeSegmentation.m
+++ pipeline/globalizeSegmentation.m
@@ -1,36 +1,34 @@
-
 function job = globalizeSegmentation(p)
-display('Constructing array for segmentation ID offset in cubes');
-    tic;
+    display('Collecting segment ID offsets...');
+
     % collect number of segments per cube
     cumNum = uint32(0);
     numElTotal = zeros(size(p.local), 'uint32'); 
     numElTotalUpper = zeros(size(p.local), 'uint32');
-    for i=1:size(p.local,1)
-        for j=1:size(p.local,2)
-            for k=1:size(p.local,3)
-                load(p.local(i,j,k).segFile, 'numEl');
-                numElTotal(i,j,k) = cumNum;
-                cumNum = cumNum + uint32(numEl);
-                numElTotalUpper(i,j,k) = numEl+1; 
-            end
-        end
+
+    tic();
+    for curIdx = 1:numel(p.local)
+        load(p.local(curIdx).segFile, 'numEl');
+
+        numElTotal(curIdx) = cumNum;
+        cumNum = cumNum + uint32(numEl);
+        numElTotalUpper(curIdx) = numEl + 1;
     end
+    toc();
+
     % Save numElTotal so that it only has to be added to localID of respective cube to get global one
     save([p.saveFolder 'numEl.mat'], 'numElTotal', 'numElTotalUpper'); 
-    toc;
+
+    % collect parameters
+    inputCell = cell(numel(p.local), 1);
+
+    for curIdx = 1:numel(p.local)
+        [curI, curJ, curK] = ind2sub( ...
+            size(p.local), curIdx);
+        inputCell{curIdx} = {p, curI, curJ, curK};
+    end
 
     % Globalize segmentation and save as KNOSSOS hierachy for Oxalis
-    for i=1:size(p.local,1)
-        for j=1:size(p.local,2)
-            for k=1:size(p.local,3)
-                idx = sub2ind(size(p.local), i, j, k);
-                inputCell{idx} = {p, i, j, k};
-            end
-        end
-    end
-    functionH = @globalSegId;
-    job = startCPU(functionH, inputCell, 'globalSegmentID', 12, 10);
-
+    job = startCPU(@globalSegId, inputCell, 'globalSegmentID', 12, 10);
 end
 
