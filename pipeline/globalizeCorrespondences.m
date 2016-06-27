3d9e733ef56ad6886f159d75d83381d19bb9a579
Alessandro Motta <alessandro.motta@brain.mpg.de>
2016-06-27 10:41:51 +0200

    Cosmetic changes in globalCoorespondences

--- pipeline/globalizeCorrespondences.m
+++ pipeline/globalizeCorrespondences.m
@@ -1,16 +1,20 @@
 function job = globalizeCorrespondences(p)
-% Globalize correspondences
-    
+    % Globalize correspondences
     files = dir([p.correspondence.saveFolder, '*.mat']);
+
     % Bad but internal: Sort out global correspondences in case pipeline has already run
+    % amotta: why the == 16? what does it mean?
     idx = cellfun(@length, {files(:).name}) == 16;
     files = files(idx);
-    for i=1:length(files)
+
+    % collect parameters
+    fileCount = numel(files);
+    inputCell = cell(fileCount, 1);
+
+    for i = 1:fileCount
         inputCell{i} = {p, files(i).name};
     end
+
     functionH = @globalCorrSeg;
     job = startCPU(functionH, inputCell, 'globalCorrespondences', 12, 100);
-
-
-
 end
