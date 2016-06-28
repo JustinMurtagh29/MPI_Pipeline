2ddd162f023e8bac2d426d6639284470a0b580cb
Alessandro Motta <alessandro.motta@brain.mpg.de>
2016-06-28 09:39:19 +0200

    Add yet another test, just for demonstration purposes

---
+++ tests/testKnossosModule.m
@@ -0,0 +1,28 @@
+function tests = testKnossosModule()
+    tests = functiontests(localfunctions);
+end
+
+function testMeanAndStd(testCase)
+    % prepare cube
+    cubeSize = [512, 512, 256];
+    cubeElemCount = prod(cubeSize);
+    
+    myMean = 128;
+    myStd = 25;
+    
+    % build fake raw data
+    diff = zeros(cubeSize);
+    diff(1:2:cubeElemCount) =  myStd;
+    diff(2:2:cubeElemCount) = -myStd;
+    
+    raw = myMean * ones(cubeSize);
+    raw = raw + diff;
+    raw = uint8(raw);
+    
+    % compute mean and std
+    [meanVal, stdVal] = ...
+        Knossos.calcMeanAndStd(raw);
+    
+    verifyEqual(testCase, meanVal, myMean);
+    verifyEqual(testCase, stdVal, myStd);
+end
