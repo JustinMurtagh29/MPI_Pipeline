11d4e0371836c916587aad17c00e619120e8adbc
Manuel Berning <manuel.berning@brain.mpg.de>
2016-07-21 15:31:21 +0200

    make writeSupercubes work with input that is not divisiable by 2 or too small for all subsamplings (I hope)

--- supercubes/writeSupercubes.m
+++ supercubes/writeSupercubes.m
@@ -7,7 +7,10 @@ raw = readKnossosRoi(root, prefix, bbox);
 
 % Write multiple resoultions
 for i=1:length(magToWrite)
-    if size(raw,3) > 1
+    % If any dimension is of size 1 we cannot subsample further, will only happen in cubes at border of dataset
+    if all(size(raw) > 1)
+        % Make the size of raw divisibale by 2 in each dimension
+        raw = raw(1:end-mod(size(raw,1),2),1:end-mod(size(raw,2),2),1:end-mod(size(raw,3),2));
         raw = nlfilter3(raw, @median, [2 2 2]);
         [startIdx, endIdx] = regexp(prefix, '_mag.{1,3}');
         newPrefix = [prefix(1:startIdx-1) '_mag' num2str(magToWrite(i))];
