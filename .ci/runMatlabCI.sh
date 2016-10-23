6396d061fbd56a8774670251eb2814b0d24582d7
Alessandro Motta <alessandro.motta@brain.mpg.de>
2016-10-23 14:47:13 +0200

    Update to latest version of MATLAB-CI

--- .ci/runMatlabCI.sh
+++ .ci/runMatlabCI.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+echo "> Updating .matlab-ci file..."
+echo "apiProjectId: $CI_PROJECT_ID" >> .matlab-ci
+echo "apiPrivateToken: $PRIVATE_TOKEN" >> .matlab-ci
+
 codedir=`pwd`
 cidir=`mktemp -d`
 
 # pull code for CI suite
 cd $cidir
 git clone git@gitlab.mpcdf.mpg.de:connectomics/matlab-ci.git .
-matlab -nosplash -nodisplay -nojvm -r "matlabCI('~/.config/matlab-ci/config.mat', '$codedir'); exit;"
+matlab -nosplash -nodisplay -nojvm -r "matlabCI('$codedir'); exit;"
 
 # reset
 cd $codedir
