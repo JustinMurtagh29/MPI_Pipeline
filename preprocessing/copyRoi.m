07365ed1b9ac58db6a5e76d79d92a83a0f919e93
Manuel Berning <manuel.berning@brain.mpg.de>
2016-10-26 16:52:06 +0200

    function for cropping out ROI region from 07x2 to make it useable with chosen bounding box

---
+++ preprocessing/copyRoi.m
@@ -0,0 +1,21 @@
+function raw = copyRoi()
+    % Copy part of 2012_09_28_ex145_07x2_new2 dataset availible on webKnossos to a new dataset to have the ROI for segmentation aligned to KNOSSOS cubes 
+
+    % Define source of copy process
+    chosenOne.root = '/gaba/u/kboerg/st07x2_new2/color/1/';
+    chosenOne.prefix = '2012-09-28_ex145_07x2_new2_mag1'; 
+    chosenOne.bbox = [1195, 1515, 115, 6690, 9945, 3420];
+    % Convert to Matlab format (and add +1 offset between coordinate systems)
+    chosenOne.bbox = convertWebknossosToMatlabBbox(chosenOne.bbox);
+    
+    % Define destination of copy process
+    destination.root = '/gaba/u/mberning/wkCubes/2012-09-28_ex145_07x2_ROI2016/color/1/'
+    destintaion.prefix = '2012-09-28_ex145_07x2_ROI2016_mag1';
+    destintaion.firstCoord = 129 - [25 25 10];
+
+    % Read from one, write to wKcubes
+    raw = readKnossosRoi(chosenOne.root, chosenOne.prefix, chosenOne.bbox);
+    writeKnossosRoi(destination.root, destination.prefix, destination.bbox(:,1));
+
+end
+
