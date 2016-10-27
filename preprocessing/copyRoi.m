247bf7bbb42088ad85adcf3dbbbd16f6b0671cc5
Manuel Berning <manuel.berning@brain.mpg.de>
2016-10-27 13:45:40 +0200

    bugfixes copyRoi, add preprocessing here in separate folder

--- preprocessing/copyRoi.m
+++ preprocessing/copyRoi.m
@@ -6,16 +6,21 @@ function raw = copyRoi()
     chosenOne.prefix = '2012-09-28_ex145_07x2_new2_mag1'; 
     chosenOne.bbox = [1195, 1515, 115, 6690, 9945, 3420];
     % Convert to Matlab format (and add +1 offset between coordinate systems)
-    chosenOne.bbox = convertWebknossosToMatlabBbox(chosenOne.bbox);
+    chosenOne.bbox = Util.convertWebknossosToMatlabBbox(chosenOne.bbox);
     
     % Define destination of copy process
-    destination.root = '/gaba/u/mberning/wkCubes/2012-09-28_ex145_07x2_ROI2016/color/1/'
-    destintaion.prefix = '2012-09-28_ex145_07x2_ROI2016_mag1';
-    destintaion.firstCoord = 129 - [25 25 10];
+    destination.root = '/gaba/u/mberning/wkCubes/2012-09-28_ex145_07x2_ROI2016/color/1/';
+    destination.prefix = '2012-09-28_ex145_07x2_ROI2016_mag1';
+    destination.firstCoord = 129 - [25 25 10];
 
     % Read from one, write to wKcubes
     raw = readKnossosRoi(chosenOne.root, chosenOne.prefix, chosenOne.bbox);
-    writeKnossosRoi(destination.root, destination.prefix, destination.bbox(:,1));
+    writeKnossosRoi(destination.root, destination.prefix, destination.firstCoord, raw);
+
+    % Saving raw data to HDF 5 file for faster loading in case we need multiple tries for contrast normalization
+    % Saving duplicates in memory not feasible
+    h5create('/gaba/scratch/mberning/tempData07x2.h5', '/raw', size(raw), 'Datatype', 'uint8');
+    h5write('/gaba/scratch/mberning/tempData07x2.h5', '/raw', raw);
 
 end
 
---
+++ preprocessing/preprocessData.m
@@ -0,0 +1,115 @@
+% Reading raw data from HDF5 file
+display('Loading data from HDF5 file');
+tic;
+raw = h5read('/gaba/scratch/mberning/tempData07x2.h5', '/raw');
+toc;
+
+display('Thresholding & Filling:');
+tic;
+% generate logical arrrays
+a = raw > 162 | raw < 50;
+b = zeros(size(a), 'uint8');
+toc;
+
+tic;
+fprintf('\nLayer Counter (max 3422): ');
+for i=1:size(a,3)
+	if i>1
+		for j=0:log10(i-1)
+			fprintf('\b');
+		end
+	end
+	fprintf('%d', i);
+	temp = bwareaopen(a(:,:,i), 1000, 4);
+	temp = imclose(temp, ones(5,5));
+	temp = padarray(temp, [1 1], 1);
+	idx = 1;
+	while temp(1800,idx) == 1
+		temp(1800,idx) = 0;
+		idx = idx + 1;
+	end
+	temp = imfill(temp, 'holes');
+	temp([1 end], :) = [];
+	temp(:, [1 end]) = [];
+	b(:,:,i) = temp;
+end
+fprintf('\n');
+clear a temp;
+toc;
+
+display('Saving blood vessel mask to HDF 5 file:');
+tic;
+b = uint8(b);
+h5write('/zdata/manuel/results/largeData2.h5', '/mask', b);
+toc;
+
+display('Masking the raw data:');
+tic;
+fprintf('\nLayer Counter (max 3422): ')
+for i=1:size(raw,3);
+	if i>1
+		for j=0:log10(i-1)
+			fprintf('\b');
+		end
+	end
+	fprintf('%d', i);
+	temp = raw(:,:,i);
+	temp(b(:,:,i)>0) = 121;
+	raw(:,:,i) = temp;
+end
+fprintf('\n')
+toc;
+
+display('Saving masked raw data to HDF 5 file:');
+tic;
+raw = uint8(raw);
+h5write('/zdata/manuel/results/largeData2.h5', '/rawmasked', raw);
+toc;
+
+% Mean downsampling
+filterSize = [64 64 29];
+display('Mean downsampling');
+tic;
+raw_mean = nlfilter3(raw, @mean, filterSize);
+toc;
+
+% Gradient calculation
+display('Approximating gradients');
+tic;
+raw_temp = raw_mean;
+raw_temp(raw_temp < 100 | raw_temp > 130) = 121; % Mask out somata, apical dendrites and myelin
+raw_smoothed = smooth3(raw_temp, 'gaussian', [5 5 5], 1.2);
+toc
+
+display('Gradient correction');
+tic;
+fprintf('\nLayer Counter (max 3422): ')
+for i=1:size(raw,3);
+	if i>1
+		for j=0:log10(i-1)
+			fprintf('\b');
+		end
+	end
+	fprintf('%d', i);
+	z = ceil(i/filterSize(3));
+	planeCorrectionImage = 1./(interp(mean(raw_smoothed(:,:,z),2),filterSize(1))*interp(mean(raw_smoothed(:,:,z),1),filterSize(2))./121^2);
+	temp = raw(:,:,i);
+	temp = planeCorrectionImage.*temp;
+	temp(b(:,:,i)>0) = 121;
+	raw(:,:,i) = temp;
+end
+fprintf('\n')
+toc;
+
+display('Saving gradient masked data to HDF 5 file:');
+tic;
+raw = uint8(raw);
+h5create('/zdata/manuel/results/largeData2.h5', '/rawcorrected', size(raw), 'Datatype', 'uint8');
+h5write('/zdata/manuel/results/largeData2.h5', '/rawcorrected', raw);
+toc;
+
+display('Writing to KNOSSOS hierachy');
+tic;
+writeKnossosRoi('/zdata/manuel/data/cortex/2012-09-28_ex145_07x2/mag1masked/', '2012-09-28_ex145_07x2_mag1', bbox(:,1)', raw);
+toc;
+
