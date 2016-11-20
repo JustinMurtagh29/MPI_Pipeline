6f22d72778b1db84d0f97fcc1894458272a87924
Manuel Berning <manuel.berning@brain.mpg.de>
2016-11-20 15:23:42 +0100

    add new folder with backup of this pipeline run, not sure if necessary as allParameter.mat is written anyway

---
+++ configurations/configuration_07x2_20161120.m
@@ -0,0 +1,43 @@
+% Hi there!
+%   Please do not remove the following lines of code.
+%   If you cannot resist the temptation then godspeed!
+if ~exist('PIPELINE_READY', 'var') || ~PIPELINE_READY
+    error('Please start MATLAB inside the pipeline directory');
+end
+if ~strcmp(version('-release'), '2015b')
+    error('Please run the pipeline code with Matlab R2015b, see README.md');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%% EDIT BELOW THIS LINE %%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Choose were to store result of the calculations
+% Make sure you have WRITE access
+p.saveFolder = '/gaba/u/mberning/results/pipeline/20161120_ROI/';
+
+% Define region of interest
+% This can be copied directly from webKNOSSOS bounding box field
+% Make sure p.bbox is always 100 pixels away from any black region in X-Y (50 in Z)
+p.bbox_wK = [128, 128, 128, 5446, 8381, 3286]; 
+
+% Name of the experiment. It's the same as the Dataset name on webKnossos.
+% Also in the "Info" section when you open your dataset in webKnossos.
+p.experimentName = '2012-09-28_ex145_07x2_ROI2016_corrected';
+
+% Define directory and file prefix and voxel size for KNOSSOS hierachy
+% with raw data READABLE to you on gaba
+p.raw.root = '/gaba/u/mberning/wkCubes/2012-09-28_ex145_07x2_ROI2016_corrected/color/1/';
+p.raw.prefix = '2012-09-28_ex145_07x2_ROI2016_corrected_mag1';
+
+% Voxel size in nano metres
+p.raw.voxelSize = [11.24 11.24 28];
+
+% This segmentation parameter controls over- vs. undersegmentation,
+% decrease for more smaller segments and vice versa
+p.seg.threshold = .25;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STOP EDITING HERE %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Do not change this, will add other parameters that
+% usually need no modification
+p = setParameterSettings(p);
+
