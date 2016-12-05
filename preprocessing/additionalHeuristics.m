350f13aee5a2f48949f53257b0d0d2d8eb20c5c1
Manuel Berning <manuel.berning@brain.mpg.de>
2016-12-05 17:59:48 +0100

    remove mito detection for now & some minor updates

--- preprocessing/additionalHeuristics.m
+++ preprocessing/additionalHeuristics.m
@@ -7,9 +7,9 @@ vesselsMasked.prefix = '2012-09-28_ex145_07x2_ROI2016_vessel_mag1';
 % Define bounding box for detection
 dataset.bbox = [128, 128, 128, 5573, 8508, 3413];
 % "Training region nuclei" (where heuristics where optimized)
-dataset.bbox = [2105, 2606, 740, 1000, 1000, 500];
+%dataset.bbox = [2105, 2606, 740, 1000, 1000, 500];
 % "Training region myelin" (where heuristics where optimized)
-dataset.bbox = [128, 128, 128, 1000, 1000, 500];
+%dataset.bbox = [128, 128, 128, 1000, 1000, 500];
 
 % Due to new bounding box format
 dataset.bbox(:,4:6) = dataset.bbox(:,1:3) + dataset.bbox(:,4:6) - 1;
@@ -33,29 +33,29 @@ for i=1:length(zCoords)
     % Load raw data and vessel from new dataset
     raw = readKnossosRoi(dataset.root, dataset.prefix, thisSliceBboxWithBorder);
     vessels = readKnossosRoi(vesselsMasked.root, vesselsMasked.prefix, thisSliceBbox, 'uint32');
+    % This line is to overwrite old results of detection(s) below
+    vessels = vessels == 1;
     % Apply new heuristics
     nuclei = detectNucleiZSlice(raw, borderToLoad);
     myelin = detectMyelinZSlice(raw, borderToLoad);
-    mito = detectMitoZSlice(raw, borderToLoad);
     % Exlusion heuristics, e.g. vessel might be detected as nuclei, myelin
     % as mitochondria
     nuclei(vessels) = 0;
-    mito(myelin) = 0;
+    myelin(vessels) = 0;
     % Make sure no voxels are labeled double before pasting together
-        assert(~any((vessel+nuclei+myelin+mito) == 2));
-
+    assert(~any((vessel+nuclei+myelin) == 2));
     vessels(nuclei) = 2;
+    vessles(myelin) = 3;
     writeKnossosRoi(vesselsMasked.root, vesselsMasked.prefix, thisSliceBbox(:,1)', uint32(vessels), 'uint32', '', 'noRead');
     Util.progressBar(i, length(zCoords));
-    clear raw vessels nuclei;
+    clear raw vessels nuclei myelin;
 end
-clear i lastOfKnossosCube numberValidInCube raw vessels nuclei thisSliceBbox;
+clear i lastOfKnossosCube numberValidInCube thisSliceBbox;
 toc;
 
-display('(Re)Downsampling KNOSSOS hierachies for segmentation to include updates (Nuclei, Myelin & Mitochondria)');
+display('(Re)Downsampling KNOSSOS hierachies for segmentation to include updates');
 tic;
-% Still a bit more complicated for downsampling segmentation(s)
-thisRoot = strrep(vesselsMasked.root, '/color/', '/segmentation/');
 thisBBox = [1 1 1; (ceil(dataset.bbox(:,2)./1024).*1024)']';
-createResolutionPyramid(thisRoot, vesselsMasked.prefix, thisBBox, strrep(thisRoot, '/1/', ''), true);
+createResolutionPyramid(vesselsMasked.root, vesselsMasked.prefix, thisBBox, strrep(vesselsMasked.root, '/1/', ''), true);
 toc;
+
