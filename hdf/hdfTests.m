22b7b7eb6bae42730e5730cfec5b663ae3e2dfa8
Manuel Berning <manuel.berning@brain.mpg.de>
2017-01-10 11:28:24 +0100

    benchmark random access of KHierachy vs. HDF (vs Morton soon?)

--- hdf/hdfTests.m
+++ hdf/hdfTests.m
@@ -1,48 +1,38 @@
 %% Load data
-load /gaba/u/mberning/results/pipeline/20151111T183414/allParameter.mat;
-data = load1024SegCube(p);
-save('/home/mberning/Desktop/segData.mat', 'data', '-v7.3');
+% load /gaba/u/mberning/results/pipeline/20151111T183414/allParameter.mat;
+% data = load1024SegCube(p);
+% save('/home/mberning/Desktop/segData.mat', 'data', '-v7.3');
 
 %% Load local version instead
 load('/home/mberning/Desktop/segData.mat');
 
 %% Settings
-testFilename = '/home/mberning/Desktop/test.hdf';
+testFilename1 = '/home/mberning/Desktop/test_nocomp.hdf';
+testFilename2 = '/home/mberning/Desktop/test_comp.hdf';
+testFilename3 = '/home/mberning/Desktop/testKhierachy/';
+% testFilename4 = '/home/mberning/Desktop/testMhierachy/';
 
-%% Test hdf5 writing
-h5create(testFilename, '/seg', size(data), 'Datatype', 'uint32', 'ChunkSize', [32 32 32], 'Deflate', 0);
-h5write(testFilename, '/seg', data);
+%% Write different encodings of data
+h5create(testFilename1, '/seg', size(data), 'Datatype', 'uint32', 'ChunkSize', [32 32 32], 'Deflate', 0);
+h5write(testFilename1, '/seg', data);
+h5create(testFilename2, '/seg', size(data), 'Datatype', 'uint32', 'ChunkSize', [32 32 32], 'Deflate', 1);
+h5write(testFilename2, '/seg', data);
+writeKnossosRoi(testFilename3, 'seg', [1 1 1], data, 'uint32');
+% mortonSaveRoi(testFilename4, 'seg', [1 1 1], data)
 
-%% Determine file size
-fileInfo = dir(testFilename);
+%% Perform benchmark & plot
+figure;
+hold on;
+benchmarkRead(@(x,y,z)h5read(x,'/seg', y, z), testFilename1, '-b', data);
+benchmarkRead(@(x,y,z)h5read(x,'/seg', y, z), testFilename2, '-r', data);
+benchmarkRead(@(x,y,z)readKnossosRoi(x, 'seg', [y, y + z - 1], 'uint32'), testFilename3, '-g', data);
+% benchmarkRead(@(x,y,z)mortonLoadRoi(x, 'seg', [y; z]), testFilename4);
 
-%% Test hdf5 reading
-sizeRead = 2.^(0:10);
-datasetSize = 1024;
-nrRandomReads = 10;
-
-rng default;
-voxelTroughput = cell(1,length(sizeRead));
-for i=1:length(sizeRead)
-    t = zeros(1,nrRandomReads);
-    thisReadSize = repmat(sizeRead(i), 3, 1);
-    for j=1:nrRandomReads
-        pos = randi(datasetSize - sizeRead(i) + 1, 3,1);
-        tic;
-        data = h5read(testFilename,'/seg', pos, thisReadSize);
-        t(j) = toc;
-    end
-    voxelTroughput{i} = prod(thisReadSize) ./ t;
-    display(num2str(i));
-end
-
-for i=1:length(voxelTroughput)
-    meanToPlot(i) = mean(voxelTroughput{i});
-    stdToPlot(i) = std(voxelTroughput{i});
-end
-
-errorbar(meanToPlot, stdToPlot);
-ylim([0 12e8]);
-ylabel('Voxel troughput [voxel/s]');
+% Make nicer
+ylim([0 1e9]);
 xlabel('Side of loaded cube [voxel]');
-legend('HDF5 high-level matlab');
+ylabel('Voxel troughput [voxel/s]');
+legend('HDF5 high-level matlab; no compression', 'HDF5 high-level matlab; minimal gzip compression (200MB)', 'KNOSSOS hierachy, uncompressed');
+set(gca, 'YScale', 'log');
+set(gca, 'XTick', 1:11);
+set(gca, 'XTickLabel', strseq('', 2.^(0:10)));
