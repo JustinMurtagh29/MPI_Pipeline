c8d0b25cccedafeda90026fa97f6717d8856bcd8
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-01-16 11:51:38 +0100

    Fix: Concatenate meta data along last dimension

--- pipeline/buildSegmentMetaData.m
+++ pipeline/buildSegmentMetaData.m
@@ -12,7 +12,7 @@ function buildSegmentMetaData(param)
     % collect all local results
     loadMeta = @(p) load(fullfile(p.saveFolder, 'segmentMeta.mat'));
     meta = arrayfun(loadMeta, cubes, 'UniformOutput', false);
-    meta = Util.concatStructs(1, meta{:});
+    meta = Util.concatStructs('last', meta{:});
     
     % write global result
     metaFile = fullfile(rootDir, 'segmentMeta.mat');
