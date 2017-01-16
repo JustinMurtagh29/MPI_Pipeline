1daca702bd57e4806fd36ef9220ea065b894d33f
Manuel Berning <manuel.berning@brain.mpg.de>
2017-01-17 00:44:39 +0100

    last iteration hopefully

--- preprocessing/detectMyelinLocal.m
+++ preprocessing/detectMyelinLocal.m
@@ -5,7 +5,7 @@ function myelin = detectMyelinLocal( raw )
 raw = smooth3Aniso(raw, [11 11 5], [2 2 0.8]);
 
 % Detect things that are dark
-darkThings = raw < 92;
+darkThings = raw < 93;
 
 % Myelin are large dark regions
 myelin = bwareaopen(darkThings, 1e5);
--- preprocessing/detectNucleiLocal.m
+++ preprocessing/detectNucleiLocal.m
@@ -8,11 +8,11 @@ raw = smooth3Aniso(raw, [21 21 9], [3 3 1.2]);
 gradmag = gradient3Aniso(raw, [11 11 5], [2 2 0.8]);
 
 % Extract regions that have edges
-edges = gradmag > 6 | vessels;
+edges = gradmag > 5.5 | vessels;
 edges = bwareaopen(edges, 1e3);
 edges = imclose(edges, makeSphere([10 10 4], 10));
 
 % Nuclei are regions without edges
-nuclei = bwareaopen(imfill(~edges & raw > 130 & raw < 155, 'holes') , 1e6);
+nuclei = bwareaopen(imfill(~edges & raw > 120 & raw < 150, 'holes') , 1e6);
 
 end
