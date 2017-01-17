32be7091f509aa7c446ca109d7cc541bfdecaa2d
Manuel Berning <manuel.berning@brain.mpg.de>
2017-01-17 13:59:04 +0100

    fix minor bug close to border that can propagate through overlap (especially at dataset border due to only 25 25 10)

--- preprocessing/detectMyelinLocal.m
+++ preprocessing/detectMyelinLocal.m
@@ -7,6 +7,12 @@ raw = smooth3Aniso(raw, [11 11 5], [2 2 0.8]);
 % Detect things that are dark
 darkThings = raw < 93;
 
+% Because filter creates dark areas close to borders,
+% Remove detected darkThings there
+darkThings([1:3 size(darkThings,1)-2:size(darkThings,1)],:,:) = 0;
+darkThings(:,[1:3 size(darkThings,2)-2:size(darkThings,2)],:) = 0;
+darkThings(:,:,[1:3 size(darkThings,3)-2:size(darkThings,3)]) = 0;
+
 % Myelin are large dark regions
 myelin = bwareaopen(darkThings, 1e5);
 
