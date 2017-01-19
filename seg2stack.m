a2f3de7e82422510b26f0b74761ed8efaa11f659
Marcel Beining <marcel.beining@brain.mpg.de>
2017-01-19 12:20:08 +0100

    bugfixes

--- seg2stack.m
+++ seg2stack.m
@@ -8,5 +8,5 @@ f = load(fullfile(p.saveFolder,'numEl.mat'));
 stack.section = struct('sectionID','section1','bbox', p.bbox-1, 'resolutions',1);
 stack.layer = struct('largestValue', f.numElTotalUpper(end,end,end),'typ','segmentation','class','uint32');   % largest value might be wrong
 
-prepareStack(stack)
+WK.prepareStack(stack)
 
