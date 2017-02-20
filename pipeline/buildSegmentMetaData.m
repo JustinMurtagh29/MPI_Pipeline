61efadb59dccf36f1f1d11ded477043562d3d042
mbeining <marcel.beining@brain.mpg.de>
2017-02-20 13:29:16 +0100

    buildSegmentMeta now functional for all empty segmentation cube

--- pipeline/buildSegmentMetaData.m
+++ pipeline/buildSegmentMetaData.m
@@ -6,7 +6,7 @@ function job = buildSegmentMetaData(param)
     jobInputs = arrayfun(@(curIdx) {{curIdx}}, 1:numel(cubes));
 
     % run job
-    cluster = Cluster.getCluster('-l h_vmem=12G');
+    cluster = Cluster.getCluster('-l h_vmem=12G -l s_rt=00:29:30 -l h_rt=00:29:55');
     job = Cluster.startJob( ...
         @buildInCube, jobInputs, ...
         'sharedInputs', jobSharedInputs, ...
@@ -16,8 +16,8 @@ function job = buildSegmentMetaData(param)
     % collect all local results
     loadMeta = @(p) load(fullfile(p.saveFolder, 'segmentMeta.mat'));
     meta = arrayfun(loadMeta, cubes, 'UniformOutput', false);
-    meta = Util.concatStructs('last', meta{:});
-    
+    %meta = Util.concatStructs('last', meta{:});
+    meta = Util.concatStructs(struct('segIds',1,'voxelCount',1,'box',3,'centroid',2,'point',2,'maxSegId',1,'cubeIdx',1) ,meta{:});
     % find maximum segment ID
     meta.maxSegId = max(meta.maxSegId);
     
