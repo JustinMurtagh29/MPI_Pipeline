7135a513d2ac261ee994a8f6644f83a49608e9b8
Manuel Berning <manuel.berning@brain.mpg.de>
2017-03-02 10:56:05 +0100

    remove temporary comments

--- pipeline/removeOverlaps.m
+++ pipeline/removeOverlaps.m
@@ -1,12 +1,12 @@
 function removeOverlaps(p)
 
     % Transfer local cubes to segFile
-    %for i=1:numel(p.local)
-    %    inputCell{i} = {p.local(i).tempSegFile, p.local(i).segFile, p.local(i).bboxSmall, p.local(i).bboxBig};
-    %end
-    %functionH = @rewriteWithoutOverlaps;
-    %job = startCPU(functionH, inputCell, 'overlapRemoval', 12, 10);
-    %Cluster.waitForJob(job);
+    for i=1:numel(p.local)
+        inputCell{i} = {p.local(i).tempSegFile, p.local(i).segFile, p.local(i).bboxSmall, p.local(i).bboxBig};
+    end
+    functionH = @rewriteWithoutOverlaps;
+    job = startCPU(functionH, inputCell, 'overlapRemoval', 12, 10);
+    Cluster.waitForJob(job);
 
     % Modify all correspondences accordingly
     display('Modifying correspondences');
