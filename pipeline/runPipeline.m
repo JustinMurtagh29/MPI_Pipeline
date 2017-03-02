bbba1f040e09109c67a48af903120212c88bd0e8
Manuel Berning <manuel.berning@brain.mpg.de>
2017-03-02 10:56:44 +0100

    update runPipeline to SynEM features

--- pipeline/runPipeline.m
+++ pipeline/runPipeline.m
@@ -56,7 +56,17 @@ function runPipeline(p)
     
     job = globalizeCorrespondences(p);
     Cluster.waitForJob(job);
-    
+
+    % Create resolution pyramid for the segmentation
+    display('Downsampling segmentation:');
+    tic;
+    thisBBox = [1 1 1; (ceil(p.bbox(:,2)./1024).*1024)']';
+    % This weird command line stuff is necessary to deference symbolic links
+    [~, thisRoot] = system(['readlink -f ' p.seg.root ' < /dev/null']);
+    thisRoot = [strrep(thisRoot, sprintf('\n'), '') filesep];
+    createResolutionPyramid(thisRoot, p.seg.prefix, thisBBox, strrep(thisRoot, '/1/', ''), true);
+    toc;
+
     % Construct graph on globalized version of segmentation
     % This will create p.local(:).edgeFile & borderFile
     % See graphConstruction subfolder
@@ -67,32 +77,21 @@ function runPipeline(p)
     % see: http://biorxiv.org/content/early/2017/01/22/099994
     [p, job] = SynEM.Seg.pipelineRun(p);
     % Save parameter file to new 
-    save([p.saveFolder 'allParameterWithSynapses.mat'], p);
+    Util.save([p.saveFolder 'allParameterWithSynapses.mat'], p);
     % Wait for completion of job
     Cluster.waitForJob(job);
 
-    % Comment MB: probably not needed anymore, features from SynEM call above used
-    % Calculate edge based features as used in first GP training
-    % Calculate segment based features as used in spine head detection
-    % see filterbank 
-    job = miniFeature(p);
-    Cluster.waitForJob(job);
-    
-    % Make predictions on edge based features using previously trained GP
-    job = makePredictions(p,'edges');
-    Cluster.waitForJob(job);
-    
-    % Make predictions for spine heads on segment based features using previously trained spine head classifier
-    job = spineHeadDetectionOnCluster(p);
+    % Calculate raw features on smaller (wrt SynEM) borders (down to 10 voxel)
+    job = connectEM.calculateRawFeatures(p);
     Cluster.waitForJob(job);
 
-    % Comment MB: Also not needed anymore, make sure, then delete
-    % Run interface classifier using Benedikt's trained classifier and store features
-    %job = interfaceClassificationOnCluster(p);
-    %Cluster.waitForJob(job);
-    % Use interface features to make predictions i.e. generate synapse scores 
-    %job=makeInterfacePredictionsOnCluster(p);
-    %Cluster.waitForJob(job);
+    % Calculate features on CNN output as well
+    job = connectEM.calculateClassFeatures(p);
+    Cluster.waitForJob(job);
+
+    % Calculate neurite continuity predeictions
+    job = connectEM.predictDataset(p);
+    Cluster.waitForJob(job);
 
     %Save the global SVG data
     job = collectSvgDataOnCluster(p);
@@ -102,3 +101,9 @@ function runPipeline(p)
     job = collectGraphStructOnCluster(p);
     Cluster.waitForJob(job);
 end
+
+% Some comments that one might want to run in addition:
+% connectEM.getHeuristicResult(p) -> lookups result from heuritic (nuclei, vessel, myelin) detections
+% connectEM.agglomerate -> Generate CC of graph at a threshold chosen in script
+
+
