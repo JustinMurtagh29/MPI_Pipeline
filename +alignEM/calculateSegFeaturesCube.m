5abdf9224de555d8ae2d51d2876ff8d93cd03f3b
Marcel Beining <marcel.beining@brain.mpg.de>
2017-03-03 11:47:03 +0100

    changed name of output and bugfix if segment cube was empty

--- +alignEM/calculateSegFeaturesCube.m
+++ +alignEM/calculateSegFeaturesCube.m
@@ -7,18 +7,19 @@ bboxFM = bsxfun(@plus, p.local(cubeNo).bboxSmall,[-fm.border', fm.border']./2);
 seg = loadSegDataGlobal(p.seg, bboxFM);
 
 load(p.local(cubeNo).segmentFile, 'segments');
-interfaces.surface = {segments.PixelIdxList};
-interfaces.subseg = cell(1,0); % needed for length check in fm.calculate
-clearvars segments
-
-%calculate features
-features = fm.calculate(interfaces, seg);
-
-% Save features
-if strcmp(fm.mode, 'direction')
-    features = features(1:end/2,:); %only save first direction
+if isempty(regments)
+    interfaces.surface = {segments.PixelIdxList};
+    interfaces.subseg = cell(1,0); % needed for length check in fm.calculate
+    clearvars segments
+    
+    %calculate features
+    features = fm.calculate(interfaces, seg);
+    
+    % Save features
+    if strcmp(fm.mode, 'direction')
+        features = features(1:end/2,:); %only save first direction
+    end
+    outputFile = [p.local(cubeNo).saveFolder 'SegmentFeatures.mat'];
+    Util.save(outputFile, features);
 end
-outputFile = [p.local(cubeNo).saveFolder 'InterfaceClassFeatures.mat'];
-Util.save(outputFile, features);
-
 end
