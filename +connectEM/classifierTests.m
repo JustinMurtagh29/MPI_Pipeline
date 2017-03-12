e46ca925c3e7b877f3d5fd2c814f32c147bdac62
Manuel Berning <manuel.berning@brain.mpg.de>
2017-03-12 12:55:47 +0100

    updates for classifier training

--- +connectEM/classifierTests.m
+++ +connectEM/classifierTests.m
@@ -24,27 +24,25 @@ gt = connectEM.getContinuityLabelsFromNml(p, pT);
 
 %% Collect features from SynEM feature calculation for all edges in GT
 gt = connectEM.getFeaturesForEdges(p, gt);
+gt = rmfield(gt, {'prob', 'segIdsOfGTnodes', 'segIdsGT', 'mergedSegments', 'leftSegments'});
+
+%{
 
 %% Write all labels extracted from dense cubes to nml's
-graph = load([p.saveFolder 'graph.mat'], 'edges');
-segmentMeta = load([p.saveFolder 'segmentMeta.mat'], 'point');
 for i=1:length(gt)
     posEdges = gt(i).edges(gt(i).labels == 1,:);
     negEdges = gt(i).edges(gt(i).labels == -1,:);
     ccPos = Graph.findConnectedComponents(posEdges, true, true);
     ccNeg = Graph.findConnectedComponents(negEdges, true, true);
-    skel = connectEM.generateSkeletonFromAgglo(graph, segmentMeta.point', ccPos, strseq('positive edges ', 1:length(ccPos)), {});
-    writeNml(['/home/mberning/Desktop/denseSkel/' 'newRegion' num2str(i) '_pos.nml'], skel);
-    skel = connectEM.generateSkeletonFromAgglo(graph, segmentMeta.point', ccNeg, strseq('negative edges ', 1:length(ccNeg)), {});
-    writeNml(['/home/mberning/Desktop/denseSkel/' 'newRegion' num2str(i) '_neg.nml'], skel);
+    connectEM.generateSkeletonFromAgglo(posEdges, segMeta.point, ccPos, strseq('positiveEdges ', 1:length(ccPos)), ... 
+    '+connectEM/trainingData/denseSkel/', segMeta.maxSegId);
+    connectEM.generateSkeletonFromAgglo(negEdges, segMeta.point, ccNeg, strseq('negativeEdges ', 1:length(ccNeg)), ...
+    '+connectEM/trainingData/denseSkel/', segMeta.maxSegId);
 end
+
+%}
 
 %% Divide GT into training an test set (80%/20%) in each dense cube
-
-% Make dimensionality of labels and prob consitent
-for i=1:length(gt)
-    gt(i).labels = gt(i).labels';
-end
 
 train = struct();
 test = struct();
@@ -69,33 +67,23 @@ test = Util.concatStructs(1, test(1), test(2), test(3));
 % end
 
 %% Train different classifier on all 3 feature training sets
-classifierOldFeatures = connectEM.trainClassifier( train.oldFeatures, train.labels );
-classifierRawFeatures = connectEM.trainClassifier( train.rawFeatures, train.labels );
 classifierNewFeatures = connectEM.trainClassifier( cat(2, train.rawFeatures, ...
     train.classFeatures), train.labels );
 
 %% Calculate predicition values for all classifiers (training & test)
-[~, train.scoresOld] = classifierOldFeatures.predict(train.oldFeatures);
-[~, train.scoresRaw] = classifierRawFeatures.predict(train.rawFeatures);
 [~, train.scoresClass] = classifierNewFeatures.predict(cat(2, train.rawFeatures, ...
     train.classFeatures));
 
-[~, test.scoresOld] = classifierOldFeatures.predict(test.oldFeatures);
-[~, test.scoresRaw] = classifierRawFeatures.predict(test.rawFeatures);
 [~, test.scoresClass] = classifierNewFeatures.predict(cat(2, test.rawFeatures, ...
     test.classFeatures));
 
 %% Transfer to proabilities
 sigmoid = @(x)1./(1+exp(-1.*x));
 
-train.probOld = sigmoid(train.scoresOld(:,1));
-train.probRaw = sigmoid(train.scoresRaw(:,1));
 train.probClass = sigmoid(train.scoresClass(:,1));
 
-test.probOld = sigmoid(test.scoresOld(:,1));
-test.probRaw = sigmoid(test.scoresRaw(:,1));
 test.probClass = sigmoid(test.scoresClass(:,1));
-
+%{
 %% Visualize predicted probabilities vs. frequencies in test set
 binSize = 500;
 binLowerLimit = 1:binSize:length(test.prob);
@@ -214,3 +202,5 @@ b = classifierNewFeatures.predict(cat(2, test.rawFeatures, ...
 test.classFeatures));
 
 all(a(:) == b(:))
+%}
+
