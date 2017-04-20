e25ac88e91765c663af86dd797b112668c7a7d50
Manuel Berning <manuel.berning@brain.mpg.de>
2017-04-20 19:28:30 +0200

    keep old function in case we want to do single agglomerations again

---
+++ +connectEM/aggloGridSearchTest.m
@@ -0,0 +1,35 @@
+% ... for cutting down graph
+borderSizeDendrites = 100; % border below this size are excluded from dendrite subgraph during inital agglomeration
+segmentSizeDendrites = 100; % segment below ...
+borderSizeAxons = 100; % border below this size are excluded from axon subgraph during inital agglomeration
+segmentSizeAxons = 100; % segment below ...
+
+% Restrictions of graph based on segment class proability
+axonProbThreshold = 0.5;
+dendriteProbThreshold = 0;
+% For deciding in which segments to seed spine attachment greedy search
+spineProbThreshold = 0.5;
+
+% Threshold on neurite continuity probability for CC (and final agglomerate size in voxels) 
+probThresholdDendrite = 0.99;
+sizeThresholdDendrite = 100;
+probThresholdAxon = 0.90;
+sizeThresholdAxon = 100;
+
+% NOTE: Parameters currently have no effect, commented routines for first tests
+% ... for ER reassignment
+erProbThreshold = 2;
+% ... for spine attachment
+dendriteProbSpines = 0.05;
+probThresholdSpines = 0.05;
+maxStepsSpines = 10;
+
+% Perform agglomeration once first to check whether it works the same way
+connectEM.agglomeration( ... 
+        borderSizeDendrites, segmentSizeDendrites, borderSizeAxons, segmentSizeAxons, ...
+        axonProbThreshold, dendriteProbThreshold, spineProbThreshold, ...
+        probThresholdDendrite, sizeThresholdDendrite, probThresholdAxon, sizeThresholdAxon, ...
+        erProbThreshold, ...
+        dendriteProbSpines, probThresholdSpines, maxStepsSpines, ...
+        '/gaba/scratch/mberning/aggloSearch/00017.mat');
+
