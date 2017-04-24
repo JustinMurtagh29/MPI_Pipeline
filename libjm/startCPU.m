a38b62c4db1c45c138b70ff2bb3396eb8139fb35
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-04-24 10:17:01 +0200

    Fix: Only use local parcluster for test run

--- libjm/startCPU.m
+++ libjm/startCPU.m
@@ -11,14 +11,8 @@ function job = startCPU(fH, iC, jN, requiredMemory, group, priority)
     if nargin < 6
         priority = -500;
     end
-
-    clusterCPU = Cluster.getCluster( ...
-        '-pe openmp 1', ...
-        ['-p ' num2str(priority)], ...
-        ['-l h_vmem=' num2str(requiredMemory) 'G'], ...
-        '-l s_rt=23:50:00', ...
-        '-l h_rt=24:00:00');
-
+    
+    cluster = parcluster('local');
     job = Cluster.startJob(fH, iC, 'cluster', clusterCPU, 'name', jN, 'taskGroupSize', group);
 
 end
