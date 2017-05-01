bf858f9dbf98526922c58effcbb38ed95c86850c
KevinBoergens <kevin.boergens@gmail.com>
2017-05-01 18:52:15 +0200

    make agglomeration more modular

--- +connectEM/agglomeration.m
+++ +connectEM/agglomeration.m
@@ -4,14 +4,16 @@ function agglomeration( ...
         probThresholdDendrite, sizeThresholdDendrite, probThresholdAxon, sizeThresholdAxon, ...
         erProbThreshold, ...
         dendriteProbSpines, probThresholdSpines, maxStepsSpines, ...
-        outputFile, graph, segmentMeta, borderMeta);
+        outputFile, graph, optional);
 
     %% Start by loading parameter file
     % Load parameter from newest pipeline run
     load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameterWithSynapses.mat');
     % To keep workspace clean here remove parameter for training (pT)
     clear pT;
-
+    if ~exist('optional', 'var')
+        optional = [];
+    end
     display('Loading data:');
     tic;
     % Load new graph representation including (resorted) borderIdx [p.saveFolder 'graphNew.mat']
@@ -22,11 +24,15 @@ function agglomeration( ...
         graph = load([p.saveFolder 'graph.mat'], 'prob', 'edges', 'borderIdx');
     end
     % Load information about edges
-    if ~exist('borderMeta', 'var')
+    if isfield(optional, 'borderMeta')
+        borderMeta = optional.borderMeta;
+    else
         borderMeta = load([p.saveFolder 'globalBorder.mat'], 'borderSize', 'borderCoM');
     end
     % Load meta information of segments
-    if ~exist('segmentMeta', 'var')
+    if isfield(optional, 'segmentMeta')
+        segmentMeta = optional.segmentMeta;
+    else
         segmentMeta = load([p.saveFolder 'segmentMeta.mat'], 'voxelCount', 'point', 'maxSegId', 'cubeIdx');
         segmentMeta.point = segmentMeta.point';
         % Load and preprocess segment class predictions from Alessandro
@@ -38,8 +44,10 @@ function agglomeration( ...
 
     display('Removing segments detected by heuristics & small & disconnected segments:');
     tic;
-    graphCutDendrites = connectEM.cutGraph(p, graph, segmentMeta, borderMeta, heuristics, ...
-        borderSizeDendrites, segmentSizeDendrites);
+    if (~isfield(optional, 'skipDendrites')) || (~optional.skipDendrites)
+        graphCutDendrites = connectEM.cutGraph(p, graph, segmentMeta, borderMeta, heuristics, ...
+            borderSizeDendrites, segmentSizeDendrites);
+    end
     graphCutAxons = connectEM.cutGraph(p, graph, segmentMeta, borderMeta, heuristics, ...
         borderSizeAxons, segmentSizeAxons);
     toc;
@@ -47,21 +55,25 @@ function agglomeration( ...
     display('Generating subgraphs for axon and dendrite agglomeration:');
     tic;
     % Dendrites first
-    idx = all(ismember(graphCutDendrites.edges, find(segmentMeta.dendriteProb > dendriteProbThreshold)), 2);
-    graphCutDendrites.edges = graphCutDendrites.edges(idx,:);
-    graphCutDendrites.prob = graphCutDendrites.prob(idx);
+    if (~isfield(optional, 'skipDendrites')) || (~optional.skipDendrites)
+        idx = all(ismember(graphCutDendrites.edges, find(segmentMeta.dendriteProb > dendriteProbThreshold)), 2);
+        graphCutDendrites.edges = graphCutDendrites.edges(idx,:);
+        graphCutDendrites.prob = graphCutDendrites.prob(idx);
+    end
     % Then axon
     idx = all(ismember(graphCutAxons.edges, find(segmentMeta.axonProb > axonProbThreshold)), 2);
     graphCutAxons.edges = graphCutAxons.edges(idx,:);
     graphCutAxons.prob = graphCutAxons.prob(idx);
     clear idx;
     toc;
-
-    display('Performing agglomeration on dendrite subgraph:');
-    tic;
-    [dendrites, dendriteSize, dendriteEdges] = connectEM.partitionSortAndKeepOnlyLarge(graphCutDendrites, segmentMeta, probThresholdDendrite, sizeThresholdDendrite);
-    toc;
-
+    if (~isfield(optional, 'skipDendrites')) || (~optional.skipDendrites)
+        display('Performing agglomeration on dendrite subgraph:');
+        tic;
+        [dendrites, dendriteSize, dendriteEdges] = connectEM.partitionSortAndKeepOnlyLarge(graphCutDendrites, segmentMeta, probThresholdDendrite, sizeThresholdDendrite);
+        toc;
+    else
+        dendrites = [];
+    end
     display('Performing agglomeration on axon subgraph:');
     tic;
     [axons, axonsSize, axonEdges] = connectEM.partitionSortAndKeepOnlyLarge(graphCutAxons, segmentMeta, probThresholdAxon, sizeThresholdAxon);
@@ -86,13 +98,13 @@ function agglomeration( ...
         dendritesFinal, axonsFinal, spineProbThreshold, dendriteProbSpines, probThresholdSpines, maxStepsSpines);
     toc;
     %}
-
-    display('Evaluating on a set of ground truth skeletons');
-    tic;
-    [~, runName] = fileparts(outputFile);
-    metrics = connectEM.evaluateAggloMetaMeta(graph, axonsFinal, dendritesFinal, runName, segmentMeta);
-    toc;
-
+    if (~isfield(optional, 'calculateMetrics')) || optional.calculateMetrics
+        display('Evaluating on a set of ground truth skeletons');
+        tic;
+        [~, runName] = fileparts(outputFile);
+        metrics = connectEM.evaluateAggloMetaMeta(graph, axonsFinal, dendritesFinal, runName, segmentMeta);
+        toc;
+    end
     display('Saving:');
     tic;
     % Lets save some more so that we can always understand whats happening
