b27d3e20881e7a36a75fa567471f4bab418e7150
KevinBoergens <kevin.boergens@gmail.com>
2017-05-04 00:40:35 +0200

    bugfixes

--- +connectEM/agglomerateDirectionalityCollect.m
+++ +connectEM/agglomerateDirectionalityCollect.m
@@ -1,16 +1,22 @@
 directions.edges = [];
 directions.scores = [];
-directions.latent = sparse(1, length(segmentMeta.voxelCount));
+directions.latent = zeros(1, length(segmentMeta.voxelCount));
 for idx = 1 : 200
     idx
-    directionsPre = load(['/gaba/scratch/kboerg/cluster_directionality_run/', num2str(slice, '%.4u') '.mat']);
+    directionsPre = load(['/gaba/scratch/kboerg/cluster_directionality_run/', num2str(idx, '%.4u') '.mat']);
     directions.latent(find(directionsPre.y.latent)) = directionsPre.y.latent(find(directionsPre.y.latent));
     directions.edges = [directions.edges; directionsPre.y.edges];
     directions.scores = [directions.scores; directionsPre.y.scores];
 end
-aggloLookup = [cell2mat(gridAgglo_05{564}.axonsFinal), repelem(1 : length(gridAgglo_05{564}.axonsFinal), cellfun(@length, gridAgglo_05{564}.axonsFinal))'];
-[~, ourAgglos] = ismember(find(directions.latent), aggloLookup(:, 1));
-directions.agglomerationSize = cellfun(@(x)sum(segmentMeta.voxelCount(gridAgglo_05{564}.axonsFinal(x))));
+axonsFinalAll = [gridAgglo_05{564}.axonsFinal,
+    num2cell(setdiff(1 : length(segmentMeta.voxelCount), cell2mat(gridAgglo_05{564}.axonsFinal)))'];
+aggloLookup = [cell2mat(axonsFinalAll), repelem(1 : length(axonsFinalAll), cellfun(@length, axonsFinalAll))'];
+
+dl = find(directions.latent); % get all segments that have been used
+[~, ourAgglos] = ismember(dl, aggloLookup(:, 1)); % for each segment get the agglo id
+agglomerationSizePre = arrayfun(@(x)sum(segmentMeta.voxelCount(axonsFinalAll{aggloLookup(x,2)})), ourAgglos); %for each segment get the size of the agglo
+directions.agglomerationSize = zeros(1, length(segmentMeta.voxelCount)); %initiate the segmentMeta like array to store the agglo size
+directions.agglomerationSize(dl) = agglomerationSizePre;
 [~, edgeposition] = ismember(sort(directions.edges, 2), graph.edges, 'rows');
 directions.edgeposition = edgeposition;
-save([p.saveFolder 'directions.mat'], 'directions')
+save([p.saveFolder 'directions.mat'], 'directions', '-v7.3')
