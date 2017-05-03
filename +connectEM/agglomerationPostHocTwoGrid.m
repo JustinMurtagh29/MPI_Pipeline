a717cc15ca7a8325056d8250e313add22409d600
KevinBoergens <kevin.boergens@gmail.com>
2017-05-04 01:10:50 +0200

    bugfixes

--- +connectEM/agglomerationPostHocTwoGrid.m
+++ +connectEM/agglomerationPostHocTwoGrid.m
@@ -1,4 +1,4 @@
-function job = aggloPostHocTwoGrid()
+function job = agglomerationPostHocTwoGrid()
 
     % axon related parameter in this run
     optionsPre.borderSizeThreshold = [30 40 50 60 70]; % border below this size are excluded from axon subgraph during inital agglomeration
@@ -12,7 +12,7 @@ function job = aggloPostHocTwoGrid()
     functionH = @connectEM.agglomerationPostHocTwo;
     inputArgumentsAxons = inputArgumentsFromParameterSets(optionsPre);
     inputArgumentFilename = arrayfun(@(x)['/gaba/scratch/mberning/aggloGridSearch/searchPostHoc01_' num2str(x, '%.5i') '.mat'], ...
-        1:size(inputArgumentsAxons,1), 'uni', 0)';
+        1:length(inputArgumentsAxons), 'uni', 0)';
     % Collect and reorder input arguments together
     inputArguments = cellfun(@(x,y)[x y], inputArgumentsAxons, inputArgumentFilename, 'uni', 0);
 
@@ -32,8 +32,8 @@ end
 function options = inputArgumentsFromParameterSets(optionsPre)
     fns = fieldnames(optionsPre);
     values = struct2cell(optionsPre);
-    eval(strrep(['[ ' num2str(1:length(fns)) ']=ndgrid(values{:});'], ' ', ' a'));
-    for idx2 = 1 : length(a1)
+    eval(strrep(['[ ' num2str(-(1:length(fns))) ']=ndgrid(values{:});'], ' -', ' a'));
+    for idx2 = 1 : prod(size(a1))
         for idx = 1 : length(fns);
             options{idx2}.(fns{idx}) = eval(['a' num2str(idx) '(' num2str(idx2) ')']);
         end
