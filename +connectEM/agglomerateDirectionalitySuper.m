50e0ff6bf536b55506ea1393972074627e6bb282
KevinBoergens <kevin.boergens@gmail.com>
2017-05-04 20:22:39 +0200

    generalize

--- +connectEM/agglomerateDirectionalitySuper.m
+++ +connectEM/agglomerateDirectionalitySuper.m
@@ -1,4 +1,4 @@
-function agglomerateDirectionalitySuper(slice, visualize)
+function agglomerateDirectionalitySuper(slice, visualize, startagglo, axonProbThreshold, outputfolder)
 
 if visualize
     borderMeta = load('/gaba/u/mberning/results/pipeline/20170217_ROI/globalBorder.mat', 'borderSize', 'borderCoM');
@@ -9,12 +9,12 @@ else
 end
 segmentMeta = load('/gaba/u/mberning/results/pipeline/20170217_ROI/segmentMeta.mat', 'voxelCount', 'centroid', 'box');
 globalSegmentPCA = load('/gaba/u/mberning/results/pipeline/20170217_ROI/globalSegmentPCA.mat', 'covMat');
-gridAgglo_05{564} = load('/gaba/scratch/mberning/aggloGridSearch/search05_00564.mat', 'axonsFinal');
+agglo_col = load(startagglo, 'axonsFinal');
 bboxDist = 1000;
-axonsFinalAll = [gridAgglo_05{564}.axonsFinal,
-    num2cell(setdiff(1 : length(segmentMeta.voxelCount), cell2mat(gridAgglo_05{564}.axonsFinal)))'];
+axonsFinalAll = [agglo_col,
+    num2cell(find(segmentMeta.axonProb > axonProbThreshold), cell2mat(agglo_col)))'];
 selection = slice:200:length(axonsFinalAll);
 y = connectEM.agglomerateDirectionality(axonsFinalAll(selection), graph, segmentMeta, borderMeta, globalSegmentPCA, bboxDist, visualize);
-Util.save(['/gaba/scratch/kboerg/cluster_directionality_run2/', num2str(slice, '%.4u') '.mat'], y);
+Util.save([outputfolder, num2str(slice, '%.4u') '.mat'], y);
 
 end
