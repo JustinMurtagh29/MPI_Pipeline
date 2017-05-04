d7b259a1b61c0f7fdf10ee584a0f02011cdc4202
KevinBoergens <kevin.boergens@gmail.com>
2017-05-04 21:03:15 +0200

    minimal spanning tree accuracy

---
+++ +connectEM/measureMSTaccuracy.m
@@ -0,0 +1,12 @@
+folder = '/gaba/u/kboerg/code/pipeline/+connectEM/evaluationData/new_axon_gt/'
+liste = dir([folder '*.nml']);
+for idx = 1 : 10
+    skel = skeleton([folder liste(idx).name]);
+    agglos = gridAgglo_05{564}.metrics.axon1.foundAgglomerates_col{idx};
+    store(idx) = 0;
+    for idx2 = 1 : length(agglos)
+        [Tree, pred] = graphminspantree(sparse(squareform(pdist(bsxfun(@times, segmentMeta.point(gridAgglo_05{564}.axonsFinal{agglos(idx2)}, :), [11.24, 11.24, 28])))));
+        store(idx) = store(idx) + sum(Tree(:));
+    end
+    store(idx) = store(idx) / full(sum(sum(skel.createWeightedAdjacencyMatrix(1))))/2 /gridAgglo_05{564}.metrics.axon1.recall_col{idx}(1)*gridAgglo_05{564}.metrics.axon1.recall_col{idx}(2);
+end
