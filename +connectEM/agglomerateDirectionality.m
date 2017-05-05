bff642eab7679226aead89259778c38f3e04394d
Manuel Berning <manuel.berning@brain.mpg.de>
2017-05-05 10:18:31 +0200

    first round of speedups, now at 42 min

--- +connectEM/agglomerateDirectionality.m
+++ +connectEM/agglomerateDirectionality.m
@@ -1,41 +1,49 @@
 function y = agglomerateDirectionality(axonsFinalAll, graph, segmentMeta, borderMeta, globalSegmentPCA, bboxDist, visualize)
-    
+
     % Preallocation
-    y.latent = sparse(1, max(cell2mat(axonsFinalAll)));
-    y.edges = [];
-    y.scores = [];
-    
-    % Keep only agglomerates (including single segment agglomerados) over 100 voxel 
-    minSize = 100;
-    axonsFinalAllSize = cellfun(@(x)sum(segmentMeta.voxelCount(x)), axonsFinalAll);
-    axonsFinalAll(axonsFinalAllSize < minSize) = [];
-    
+    y.latent = cell(numel(axonsFinalAll),1);
+    y.edges = cell(numel(axonsFinalAll),1);
+    y.scores = cell(numel(axonsFinalAll),1);
+
     % Loop over all agglomerates and all segments within each agglomerate
-    for idx = 1:length(axonsFinalAll)
-        currentAgglo = axonsFinalAll{idx};
+    for idx1 = 1:length(axonsFinalAll)
+        currentAgglo = axonsFinalAll{idx1};
+        edges = [];
+        scores = [];
         for idx2 = 1:length(currentAgglo)
             
-            % Detect local surround: All direct neighbours
-            surround = intersect(graph.neighbours{currentAgglo(idx2)}', currentAgglo);
-            thisbbox = segmentMeta.box(:, :, currentAgglo(idx2));
-            otherbboxes = segmentMeta.box(:, : , currentAgglo);
-            
-            % Add to local surround: All segments whose bouding box overlap when each bounding box is expanded
-            surround = unique([currentAgglo(idx2); surround; currentAgglo(cellfun(@(x)bboxOverlap(thisbbox, x, bboxDist), num2cell(otherbboxes, [1,2])))]);
-            covMatsIn = num2cell(reshape(globalSegmentPCA.covMat(surround, :), [length(surround), 3, 3]), [2 3]);
-            surround = surround(~cellfun(@(x)any(isnan(x(:)) | isinf(x(:))), covMatsIn));
-            
+            % If more than 2 segments in agglo -> find the surround, otherwise whole agglo is surround
+            if numel(currentAgglo) > 2
+                % Detect local surround: All direct neighbours
+                surround = intersect(graph.neighbours{currentAgglo(idx2)}', currentAgglo);
+                thisbbox = segmentMeta.box(:, :, currentAgglo(idx2));
+                otherbboxes = segmentMeta.box(:, : , currentAgglo);
+                % Add to local surround: All segments whose bouding box overlap when each bounding box is expanded
+                closeSegments = currentAgglo(bboxOverlap(thisbbox, otherbboxes, bboxDist));
+                surround = unique([currentAgglo(idx2); surround; closeSegments]);
+            else
+                surround = currentAgglo;    
+            end
+
+            % Remove segments from surround if covMatsIn is NaN (small segments)
+            covMatsIn = globalSegmentPCA.covMat(surround, :);
+            surround = surround(any(isnan(covMatsIn) | isinf(covMatsIn),2));
             % Skip current segment if surround is empty
             if isempty(surround)
                 continue
             end
-            
+
             % Calculate PCA of local surround
-            massesIn = segmentMeta.voxelCount(surround);
-            comVecsIn = bsxfun(@times, segmentMeta.centroid(:, surround)', [11.24, 11.24, 28]);
-            covMatsIn = reshape(globalSegmentPCA.covMat(surround, :), [length(surround), 3, 3]);
-            agglos = {1: length(surround)};
-            [massesOut, comVecsOut, covMatsOut] = Agglo.mergeStatisticalMoments(massesIn, comVecsIn, covMatsIn, agglos);
+            if numel(surround) > 1
+                massesIn = segmentMeta.voxelCount(surround);
+                comVecsIn = bsxfun(@times, segmentMeta.centroid(:, surround)', [11.24, 11.24, 28]);
+                covMatsIn = reshape(globalSegmentPCA.covMat(surround, :), [length(surround), 3, 3]);
+                agglos = {1: length(surround)};
+                [~, comVecsOut, covMatsOut] = Agglo.mergeStatisticalMoments(massesIn, comVecsIn, covMatsIn, agglos);
+            else
+                comVecsOut = bsxfun(@times, segmentMeta.centroid(:, surround)', [11.24, 11.24, 28]);
+                covMatsOut = squeeze(reshape(globalSegmentPCA.covMat(surround, :), [length(surround), 3, 3]));
+            end
             [mypca, latent] = eig(covMatsOut);
             assert(min(latent(:))>-1E5);
             latent = latent / sum(sum(latent));
@@ -57,12 +65,12 @@ function y = agglomerateDirectionality(axonsFinalAll, graph, segmentMeta, border
             result = borderCoMsLocalized * mypca;
             scorePre = (result(:, idxLatent1) - min(result(:, idxLatent1))) / (max(result(:, idxLatent1)) - min(result(:, idxLatent1))) * 2 - 1;
             score = scorePre(currentOutgoing(outgoing));
-            
+
             % Save output in output structure
-            y.latent(currentAgglo(idx2)) = latent1;
-            y.edges = [y.edges; repmat(currentAgglo(idx2), size(score)), borderSegId(currentOutgoing)'];
-            y.scores = [y.scores; score];
-            
+            y.latent{idx1}(idx2) = latent1;
+            edges = [edges; repmat(currentAgglo(idx2), size(score)), borderSegId(currentOutgoing)'];
+            scores = [scores; score];
+
             % Write out skeletons if flag is set
             if visualize
                 borderProb = cat(1, graph.neighProb{surround});
@@ -80,20 +88,25 @@ function y = agglomerateDirectionality(axonsFinalAll, graph, segmentMeta, border
                 connectEM.generateSkeletonFromNodes(filename, nodesHere, treenames, comments);
             end
         end
+        y.edges{idx1} = edges;
+        y.scores{idx1} = scores;
     end
 end
 
 function overlap = bboxOverlap(bbox1, bbox2, bboxDist)
     persistent cornerIdx;
-    bbox1 = bsxfun(@times, bbox1', [11.24, 11.24, 28]);
-    bbox2 = bsxfun(@times, bbox2', [11.24, 11.24, 28]);
-    bbox1 = [bbox1(1, :) - bboxDist / 2; bbox1(2, :) + bboxDist / 2];
-    bbox2 = [bbox2(1, :) - bboxDist / 2; bbox2(2, :) + bboxDist / 2];
+    bbox1 = bsxfun(@times, bbox1, [11.24; 11.24; 28]);
+    bbox2 = bsxfun(@times, bbox2, [11.24; 11.24; 28]);
+    bbox1 = bsxfun(@plus, bbox1, [-bboxDist/2, bboxDist/2]);
+    bbox2 = bsxfun(@plus, bbox2, [-bboxDist/2, bboxDist/2]);
     if isempty(cornerIdx)
-        [A,B,C] = ndgrid(1:2,1:2,1:2);
-        cornerIdx = cat(2, A(:), B(:), C(:)) +  repmat(0:2:4, 8, 1);
+        [A,B,C] = ndgrid([1 4],[2 5],[3 6]);
+        cornerIdx = cat(2, A(:), B(:), C(:));
     end
-    corners = bbox1(cornerIdx);
-    overlap = any(all(bsxfun(@gt, corners, bbox2(1,:)), 2) & all(bsxfun(@lt, corners, bbox2(2,:)), 2));
+    corners = bbox1(cornerIdx)';
+    belowUpper = all(bsxfun(@lt, corners, bbox2(:,2,:)),1);
+    aboveLower = all(bsxfun(@gt, corners, bbox2(:,1,:)),1);
+    within = any(belowUpper & aboveLower,2);
+    overlap = squeeze(within);
 end
 
