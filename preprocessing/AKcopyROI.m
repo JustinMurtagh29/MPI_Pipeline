147e03b35f299a9a76fa8e9c3549ecdd22ad4186
Ali Karimi <ali.karimi@brain.mpg.de>
2017-05-26 14:20:04 +0200

    functions to copy certain ROI(write on the knossos cube edge plus [25 25 10 ] border) write segmentation layer for nuclei

--- preprocessing/AKcopyROI.m
+++ preprocessing/AKcopyROI.m
@@ -1,4 +1,4 @@
-function raw = copyRoi()
+function [] = AKcopyRoi()
     % Copy part of 2012_09_28_ex145_07x2_new2 dataset availible on webKnossos to a new dataset to have the ROI for segmentation aligned to KNOSSOS cubes 
 
     % Define source of copy process
@@ -9,13 +9,24 @@ function raw = copyRoi()
     chosenOne.bbox = Util.convertWebknossosToMatlabBbox(chosenOne.bbox);
     
     % Define destination of copy process
-    destination.root = '/gaba/u/alik/wKcubes/2017-04-03_ppcAK99_76x79_preprocessing/color/1/';
-    destination.prefix = '2017-04-03_ppcAK99_76x79_preprocessing_mag1';
-    destination.firstCoord = 129 - [25 25 10];
-
-    % Read from one, write to wKcubes
-    raw = readKnossosRoi(chosenOne.root, chosenOne.prefix, chosenOne.bbox);
-    writeKnossosRoi(destination.root, destination.prefix, destination.firstCoord, raw);
-
+    destination.root = '/gaba/u/alik/wKcubes/2017-04-03_ppcAK99_76x79_ROI/color/1/';
+    destination.prefix = '2017-04-03_ppcAK99_76x79_ROI_mag1';
+    zCoords = chosenOne.bbox(3,1):chosenOne.bbox(3,2);
+    lastOfKnossosCube = [find(mod(zCoords,512) == 0) length(zCoords)];
+    numberValidInCube = [lastOfKnossosCube(1) diff(lastOfKnossosCube)];
+    zCoords = mat2cell(zCoords, 1, numberValidInCube);
+    zCoords(numberValidInCube == 0) = [];
+    thisSliceBbox=chosenOne.bbox; 
+     for  i=4:length(zCoords)
+        disp(num2str(i))
+        thisSliceBbox(3,:) = [zCoords{i}(1) zCoords{i}(end)];
+        disp('starting to read')
+        % Read from one, write to wKcubes
+        raw = readKnossosRoi(chosenOne.root, chosenOne.prefix, thisSliceBbox);
+        disp('finished reading, starting to write')
+        destination.firstCoord = 129 - [25 25 10];
+        destination.firstCoord(1,3) =destination.firstCoord(1,3)+zCoords{i}(1)-1;
+        writeKnossosRoi(destination.root, destination.prefix, destination.firstCoord, raw);
+    end
 end
 
