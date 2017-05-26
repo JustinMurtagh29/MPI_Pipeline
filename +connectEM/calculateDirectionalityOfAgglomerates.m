bbbade126aec018ff24b2ac58bb67c1aae0d19bc
KevinBoergens <kevin.boergens@gmail.com>
2017-05-26 23:16:09 +0200

    make robust for dimensionality

--- +connectEM/calculateDirectionalityOfAgglomerates.m
+++ +connectEM/calculateDirectionalityOfAgglomerates.m
@@ -77,7 +77,7 @@ function y = calculateDirectionalityOfAgglomerates(agglos, graph, segmentMeta, b
             % Find all outgoing edges of current surround
             % without correspondences as these have no borderIdx and thereby CoM currently
             borderIdxs = cat(1, graph.neighBorderIdx{surround{idx2}});
-            borderSegId = cat(1, graph.neighbours{surround{idx2}});
+            borderSegId = cell2mat(cellfun(@(x){x(:)}, graph.neighbours(surround{idx2})));
             borderProb = cat(1, graph.neighProb{surround{idx2}});
             % Indices to border* that are not correspondences
             outgoing = ~isnan(borderIdxs) & ~ismember(borderSegId, surround{idx2});
