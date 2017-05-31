2c097702ef0fb5bddad75d2a81555556fb1d7a4c
KevinBoergens <kevin.boergens@gmail.com>
2017-05-31 20:54:35 +0200

    dendrite query work (ongoing)

--- +connectEM/findQueries.m
+++ +connectEM/findQueries.m
@@ -1,10 +1,11 @@
-function [voxelRepresentation3, mincoord] = findQueries(agglo, segmentMeta, options)
+function [voxelRepresentation2, mincoord] = findQueries(agglo, segmentMeta, options)
 [voxelRepresentation, mincoord] = connectEM.growout_mag4(agglo, segmentMeta);
 
 ballF1 = @(x,y,z)cat(4, repmat((-x:x)',1,2*y+1,2*z+1)/x, repmat((-y:y),2*x+1,1,2*z+1)/y, repmat(reshape(-z:z,[1,1,2*z+1]),2*x+1, 2*y+1,1)/z);
 ballF2 = @(x,y,z)sqrt(sum(ballF1(x,y,z).^2,4)) <= 1;
 % remove anisotropy
 voxelRepresentation2 = voxelRepresentation(:, :, imresize(1 : size(voxelRepresentation, 3), [1, round(size(voxelRepresentation, 3) * 2.313/2)*2], 'nearest'));
+clear voxelRepresentation
 if options.debug
     connectEM.makesurf(nlfilter3(voxelRepresentation2, @mode, [2, 2, 2]), '1.issf');
 end
@@ -22,10 +23,9 @@ if options.debug
 end
 %downsample to mag8
 voxelRepresentation2 = nlfilter3(voxelRepresentation2, @mode, [2, 2, 2]);
-
-voxelRepresentation3 = imfilter(double(voxelRepresentation2), double(ballF2(6, 6, 6)));
+%voxelRepresentation2 = voxelRepresentation2(1:2:end, 1:2:end, 1:2:end);
+voxelRepresentation2 = imfilter(double(voxelRepresentation2), double(ballF2(6, 6, 6)));
 if options.debug
-    save('voxelRepresentation3', 'voxelRepresentation3', '-v7.3');
     connectEM.makesurf(voxelRepresentation2, '4.issf');
     save('voxelRepresentation2', 'voxelRepresentation2', '-v7.3');
 end
