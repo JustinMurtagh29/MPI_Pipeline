d26c30282bb3f37953c847e69fe1c7e87b6a15a2
KevinBoergens <kevin.boergens@gmail.com>
2017-06-12 15:28:44 +0200

    dendrite query work

--- +connectEM/dendriteQueryAnalysis.m
+++ +connectEM/dendriteQueryAnalysis.m
@@ -1,5 +1,5 @@
 
-outputFolder = '/gaba/scratch/mberning/dendriteQueryResults/';
+outputFolder = '/gaba/scratch/kboerg/dendriteQueryResults20170604/';
 
 % Load data
 load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameterWithSynapses.mat');
@@ -9,29 +9,37 @@ temp = load('/gaba/scratch/mberning/aggloGridSearch/search03_00514.mat');
 dendrites = temp.dendritesFinal;
 clear temp
 segmentsLeftover = setdiff(find(segmentMeta.dendriteProb > 0.5), cell2mat(dendrites));
+dendritesAll = [dendrites; num2cell(segmentsLeftover)];
+
 % Not needed so far
 %graph = load([p.saveFolder 'graphNew.mat'], 'edges', 'prob', 'borderIdx');
 
 % Where to find skeletons that were returned from the queries
-scratchFolder = '/gaba/scratch/mberning/queryResults2017/';
+scratchFolder = '/gaba/scratch/kboerg/';
 % These are the downloaded queries (see wK projects: L4_focus_flight-1 & 2, L4_focus_flight-reseed-1 & 2 (2 contains 2nd and 3rd round of reseeding now))
 % And new queries from after switching to new (agglomerate based) query analysis: L4_focus_flight-new-1
-skeletonFolders = {'MBKMB_L4_dendrite_queries_2017_a'};
+skeletonFolders = {'MBKMB_L4_dendrite_queries_2017_b_nmls'};
 skeletonFolders = cellfun(@(x)[scratchFolder x filesep], skeletonFolders, 'uni', 0); 
 % Lookup segment ids of nodes+neighbours of nmls in all folders defined above
 [ff.segIds, ff.neighbours, ff.filenames, ff.nodes, ff.startNode, ff.comments] = connectEM.lookupNmlMulti(p, skeletonFolders, false);
+ff.segIds(cellfun('isempty', ff.startNode)) = [];
+ff.neighbours(cellfun('isempty', ff.startNode)) = [];
+ff.filenames(cellfun('isempty', ff.startNode)) = [];
+ff.nodes(cellfun('isempty', ff.startNode)) = [];
+ff.comments(cellfun('isempty', ff.startNode)) = [];
+ff.startNode(cellfun('isempty', ff.startNode)) = [];
 display([num2str(sum(~cellfun(@isempty,ff.comments))) '/' num2str(numel(ff.comments)) ' queries contain comment and will not be used']);
 tabulate(cellfun(@(x)x{1}{1}, cellfun(@(x)regexp(x, 'content="(.*)"', 'tokens'), ...
     cat(1, ff.comments{~cellfun(@isempty, ff.comments)}), 'uni', 0), 'uni', 0))
 % Where to find skeletons that were returned from the queries
-scratchFolder = '/gaba/scratch/mberning/queryResults2017/';
+%scratchFolder = '/gaba/scratch/mberning/queryResults2017/';
 ff = structfun(@(x)x(cellfun(@isempty,ff.comments)), ff, 'uni', 0); 
 
 % Calculate overlap of all queries with segments
 [uniqueSegments, neighboursStartNode, nodesExcludedIdx, startNodeIdx] = cellfun(@connectEM.queryAnalysis, ...
         ff.segIds, ff.neighbours, ff.nodes', ff.startNode', 'uni', 0);
 % Determine all overlaps of agglomerations with given queries
-[partition, queryOverlap] = connectEM.queryAgglomerationOverlap(dendrites, segmentsLeftover, uniqueSegments, neighboursStartNode);
+[partition, queryOverlap] = connectEM.queryAgglomerationOverlap(dendritesAll, [], uniqueSegments, neighboursStartNode);
 % Make decision(s), here evidence/occurence threshold is applied
 % Always one (or none if evidence below 14, 1/2 node) start eqClass
 startAgglo = arrayfun(@(x)x.eqClasses(x.occurences > 13), queryOverlap.start, 'uni', 0);
@@ -67,3 +75,23 @@ temp = structfun(@(x)x(idx), ff, 'uni', 0);
 connectEM.debugQueryAttachment(segmentMeta.point', dendrites, temp, outputFolder, 'queryOpenEnd');
 clear idx temp;
 
+% Make complete list
+
+eqClassCCfull = [eqClassCC; num2cell(setdiff(1 : length(dendritesAll), cell2mat(eqClassCC)))'];
+dendritesPostQuery = cellfun(@(x){cell2mat(dendritesAll(x))}, eqClassCCfull);
+y_pre = connectEM.evaluateAggloMetaMeta(graph, [], dendritesAll, 'postQueryPre', segmentMeta);
+y_post = connectEM.evaluateAggloMetaMeta(graph, [], dendritesPostQuery, 'postQueryPost', segmentMeta);
+
+cell2mat(ff.startNode(find(cellfun(@(x)max([0,ismember(x,y_pre.dendrite1.foundAgglomerates_col{2})]), startAgglo)))')
+ff.filenames(find(cellfun(@(x)max([0,ismember(x,y_pre.dendrite1.foundAgglomerates_col{1})]), endAgglo)))'
+last=@(x)x{end};
+
+for idx = 1 : 8
+    folder = ['/gaba/scratch/kboerg/eval_agglo/postQueryPost/dendrites1/query_' num2str(idx) '_'];
+    usedAgglos = cell2mat(eqClassCC(cellfun(@(x)any(ismember(x, y_pre.dendrite1.foundAgglomerates_col{idx})),eqClassCC)));
+    filenames = ff.filenames(cellfun(@(x,y)any(ismember([x,y],usedAgglos)), startAgglo, endAgglo));
+    
+    for idx2 = 1 : length(filenames)
+        copyfile(filenames{idx2}, [folder, last(strsplit(filenames{idx2}, filesep))]);
+    end
+end
