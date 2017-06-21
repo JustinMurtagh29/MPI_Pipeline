d3227111d5b4ab8729700cd67480454601c4a99e
mbeining <marcel.beining@brain.mpg.de>
2017-06-21 17:33:05 +0200

    moved loading of mask into worker function to avoid big inputcell

--- graphConstruction/findEdgesAndBordersWrapper.m
+++ graphConstruction/findEdgesAndBordersWrapper.m
@@ -1,4 +1,4 @@
-function findEdgesAndBordersWrapper(segFile, edgeFile, borderFile, segmentFile, excludeVoxels)
+function findEdgesAndBordersWrapper(segFile, edgeFile, borderFile, segmentFile, raw, bbox)
     % Computation of edges and borders optimized for 512x512x256
 
     % Load *SMALL* cube of global segmentation IDs
@@ -6,10 +6,20 @@ function findEdgesAndBordersWrapper(segFile, edgeFile, borderFile, segmentFile, 
 
     % Use new function from SynEM for calculation of svg data
     if nargin > 4
-        if all(excludeVoxels(:)) % skip calculation if completely in mirrorPaded region
-            return
-        end
-        [edges, borders, segments] = SynEM.Svg.findEdgesAndBorders(seg,excludeVoxels);
+	mask = readKnossosRoi(regexprep(raw.root,'/color/','/mask/'),[raw.prefix,'_mask'],bbox);
+        if  all(mask(:))
+		[edges, borders, segments] = SynEM.Svg.findEdgesAndBorders(seg);
+	elseif ~any(mask(:))
+		edges =  zeros(0,2,'uint32');
+		borders = struct('PixelIdxList',{},'Area',{},'Centroid',{});
+		segments = struct('PixelIdxList',{},'Id',{});
+	else
+		[edges, borders, segments] = SynEM.Svg.findEdgesAndBorders(seg,~mask);
+	end
+%        if all(excludeVoxels(:)) % skip calculation if completely in mirrorPaded region
+%            return
+%        end
+%        [edges, borders, segments] = SynEM.Svg.findEdgesAndBorders(seg,excludeVoxels);
     else
         [edges, borders, segments] = SynEM.Svg.findEdgesAndBorders(seg);
     end
--- pipeline/graphConstruction.m
+++ pipeline/graphConstruction.m
@@ -1,5 +1,6 @@
 function job = graphConstruction(parameter)
     % collect parameters
+    edges = [];borders = [];segments = [];
     cubeCount = numel(parameter.local);
     inputCell = cell(cubeCount, 1);
     toDelete = false(cubeCount,1);
@@ -7,34 +8,47 @@ function job = graphConstruction(parameter)
         curCube = parameter.local(curIdx);
         curSegFile = [curCube.saveFolder, 'segGlobal.mat'];
         if exist(regexprep(parameter.raw.root,'/color/','/mask/'),'dir')  % check for mask data
-            mask = readKnossosRoi(regexprep(parameter.raw.root,'/color/','/mask/'),[parameter.raw.prefix,'_mask'],curCube.bboxSmall);
-        else
-            mask = 1;
-        end
-        if all(mask(:))  % ignore mask if everything is one (no padded area)
-            inputCell{curIdx} = { ...
-                curSegFile, ...
-                curCube.edgeFile, ...
-                curCube.borderFile, ...
-                curCube.segmentFile};
-        elseif ~any(mask(:))
-            toDelete(curIdx) = 1;
-	    edges =  zeros(0,2,'uint32');
-            Util.save(curCube.edgeFile, edges);
-	    borders = struct('PixelIdxList',{},'Area',{},'Centroid',{});
-            Util.save(curCube.borderFile, borders);
-	    segments = struct('PixelIdxList',{},'Id',{});
-            Util.save(curCube.segmentFile, segments);
-        else
-            inputCell{curIdx} = { ...
+            %mask = readKnossosRoi(regexprep(parameter.raw.root,'/color/','/mask/'),[parameter.raw.prefix,'_mask'],curCube.bboxSmall);
+	   inputCell{curIdx} = { ...
                 curSegFile, ...
                 curCube.edgeFile, ...
                 curCube.borderFile, ...
                 curCube.segmentFile, ...
-                ~mask};
+                parameter.raw,curCube.bboxSmall};
+ 
+        else
+            %mask = 1;
+	    inputCell{curIdx} = { ...
+                curSegFile, ...
+                curCube.edgeFile, ...
+                curCube.borderFile, ...
+                curCube.segmentFile};
+
         end
+%        if all(mask(:))  % ignore mask if everything is one (no padded area)
+%            inputCell{curIdx} = { ...
+%                curSegFile, ...
+%                curCube.edgeFile, ...
+%                curCube.borderFile, ...
+%                curCube.segmentFile};
+%        elseif ~any(mask(:))
+%            toDelete(curIdx) = 1;
+%	    edges =  zeros(0,2,'uint32');
+%            save(curCube.edgeFile, 'edges');
+%	    borders = struct('PixelIdxList',{},'Area',{},'Centroid',{});
+%            save(curCube.borderFile, 'borders');
+%	    segments = struct('PixelIdxList',{},'Id',{});
+%            save(curCube.segmentFile, 'segments');
+%        else
+%            inputCell{curIdx} = { ...
+%                curSegFile, ...
+%                curCube.edgeFile, ...
+%                curCube.borderFile, ...
+%                curCube.segmentFile, ...
+%                ~mask};
+%        end
     end
-    inputCell(toDelete) = [];
+ %   inputCell(toDelete) = [];
     functionH = @findEdgesAndBordersWrapper;
     job = startCPU(functionH, inputCell, 'graphConstruction', 12); % 30 min should suffice
 end
