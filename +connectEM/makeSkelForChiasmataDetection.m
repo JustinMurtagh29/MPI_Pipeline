95ddc07cb998e793e41f7e5815cf38a1fe8ac1b6
KevinBoergens <kevin.boergens@gmail.com>
2017-07-12 13:50:19 +0200

    uncomment

--- +connectEM/makeSkelForChiasmataDetection.m
+++ +connectEM/makeSkelForChiasmataDetection.m
@@ -1,30 +1,30 @@
-% borderGT = []
-% for idx = 1 : 10
-%     temp = load(['/tmpscratch/kboerg/aggloGridSearch6/6_01_00046/' sprintf('%0.2u',idx)]);
-%     bordersGT = [bordersGT; cell2mat(temp.edgesToStore)];
-% end
-% edgesGT=find(ismember(graph.borderIdx,bordersGT));
-% startAgglo = load('/gaba/scratch/mberning/aggloGridSearch/search05_00564.mat');
-% edgesGTall = [graph.edges(edgesGT,:);startAgglo.axonEdges];
-% % edgesGTall is bigger than the edges used for the final agglos, because some excluded edges from agglomerateMerge.m made it through (see code there)
-% 
-% result1 = load('/tmpscratch/kboerg/axonQueryAnalysisResult1.mat');
-% result2 = load('/tmpscratch/kboerg/axonQueryAnalysisResult2.mat');
-% result1ends = cell2mat(result1.endAgglo(cellfun('length',result1.endAgglo)>0)');
-% result1ends(result1ends>length(axons)) = [];
-% a = cellfun(@(x)max([-1,find(ismember(result1ends,x))]),result2.startAgglo);
-% result2ends = cell2mat(result2.endAgglo(cellfun('length',result2.endAgglo)>0)');
-% result2ends(result2ends>length(axons)) = [];
-% 
-% edges1 = cellfun(@(x,y)combnk([-1, x(x<=length(axons)) y(y<=length(axons))], 2), result1.startAgglo(result1.idxGood), result1.endAgglo(result1.idxGood), 'uni', 0);
-% edges2 = cellfun(@(x,y)combnk([-1, x(x<=length(axons)) y(y<=length(axons))], 2), result2.startAgglo(result2.idxGood), result2.endAgglo(result2.idxGood), 'uni', 0);
-% edges=[edges1; edges2];
-% edges = cell2mat(edges);
-% edges(edges(:,1) == edges(:,2),:) = [];
-% edges(any(edges==-1,2),:)=[];
-% eqClassCC = Graph.findConnectedComponents(edges, true, true);
-% 
-% eqClassCCfull = [eqClassCC; num2cell(setdiff(1 : length(axons), cell2mat(eqClassCC)))'];
+borderGT = []
+for idx = 1 : 10
+    temp = load(['/tmpscratch/kboerg/aggloGridSearch6/6_01_00046/' sprintf('%0.2u',idx)]);
+    bordersGT = [bordersGT; cell2mat(temp.edgesToStore)];
+end
+edgesGT=find(ismember(graph.borderIdx,bordersGT));
+startAgglo = load('/gaba/scratch/mberning/aggloGridSearch/search05_00564.mat');
+edgesGTall = [graph.edges(edgesGT,:);startAgglo.axonEdges];
+% edgesGTall is bigger than the edges used for the final agglos, because some excluded edges from agglomerateMerge.m made it through (see code there)
+
+result1 = load('/tmpscratch/kboerg/axonQueryAnalysisResult1.mat');
+result2 = load('/tmpscratch/kboerg/axonQueryAnalysisResult2.mat');
+result1ends = cell2mat(result1.endAgglo(cellfun('length',result1.endAgglo)>0)');
+result1ends(result1ends>length(axons)) = [];
+a = cellfun(@(x)max([-1,find(ismember(result1ends,x))]),result2.startAgglo);
+result2ends = cell2mat(result2.endAgglo(cellfun('length',result2.endAgglo)>0)');
+result2ends(result2ends>length(axons)) = [];
+
+edges1 = cellfun(@(x,y)combnk([-1, x(x<=length(axons)) y(y<=length(axons))], 2), result1.startAgglo(result1.idxGood), result1.endAgglo(result1.idxGood), 'uni', 0);
+edges2 = cellfun(@(x,y)combnk([-1, x(x<=length(axons)) y(y<=length(axons))], 2), result2.startAgglo(result2.idxGood), result2.endAgglo(result2.idxGood), 'uni', 0);
+edges=[edges1; edges2];
+edges = cell2mat(edges);
+edges(edges(:,1) == edges(:,2),:) = [];
+edges(any(edges==-1,2),:)=[];
+eqClassCC = Graph.findConnectedComponents(edges, true, true);
+
+eqClassCCfull = [eqClassCC; num2cell(setdiff(1 : length(axons), cell2mat(eqClassCC)))'];
 for idx_agglo = randperm(length(eqClassCCfull),300)
     currentEC =eqClassCCfull{idx_agglo};
     nodes = [];
