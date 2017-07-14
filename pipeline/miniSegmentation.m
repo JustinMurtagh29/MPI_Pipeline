a2e9d41ef2a360e1bd2cf49eb3fc2f9de6800300
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-07-14 18:52:43 +0200

    Simplify pipeline/miniSegmentation.m and make it WKW ready

--- pipeline/miniSegmentation.m
+++ pipeline/miniSegmentation.m
@@ -1,35 +1,8 @@
 function job = miniSegmentation(p)
+inputCellBuilder = @(idx) {{ ...
+    p.class, p.local(idx).bboxBig, ...
+    p.seg.func, p.local(idx).tempSegFile}};
 
-% allocate task inputs
-taskCount = numel(p.local);
-inputCell = cell(taskCount, 1);
-
-for idx = 1:taskCount
-    % create cube folder, if needed
-    if ~exist(p.local(idx).saveFolder, 'dir')
-        mkdir(p.local(idx).saveFolder);
-    end
-    
-    inputCell{idx} = { ...
-        p.class, p.local(idx).bboxBig, ...
-        p.seg.func, p.local(idx).tempSegFile};
+inputCell = arrayfun(inputCellBuilder, 1:numel(p.local));
+job = startCPU(@segmentForPipeline, inputCell, 'segmentation', 36);
 end
-
-functionH = @miniSegmentationJobWrapper;
-job = startCPU(functionH, inputCell, 'segmentation', 36);
-
-end
-
-function miniSegmentationJobWrapper(saveFolder, idx)
-% INPUT saveFolder: string
-%           Path to segmentation main folder containing the
-%           'allParameter.mat' file.
-%       idx: int
-%           Linear index of the current local cube.
-
-m = load(fullfile(saveFolder, 'allParameter.mat'));
-p = m.p;
-segmentForPipeline(p.class.root, p.class.prefix, p.local(idx).bboxBig, ...
-    p.seg.func, p.local(idx).tempSegFile);
-end
-
