7a310b814f3f9bd084a1ba2a3515e05525a8c9f5
KevinBoergens <kevin.boergens@gmail.com>
2017-07-17 18:19:48 +0200

    speed optim + parallel

--- +connectEM/makeSkelForChiasmataDetection.m
+++ +connectEM/makeSkelForChiasmataDetection.m
@@ -1,13 +1,9 @@
-borderGT = []
-for idx = 1 : 10
-    temp = load(['/tmpscratch/kboerg/aggloGridSearch6/6_01_00046/' sprintf('%0.2u',idx)]);
-    bordersGT = [bordersGT; cell2mat(temp.edgesToStore)];
-end
-edgesGT=find(ismember(graph.borderIdx,bordersGT));
-startAgglo = load('/gaba/scratch/mberning/aggloGridSearch/search05_00564.mat');
-edgesGTall = [graph.edges(edgesGT,:);startAgglo.axonEdges];
-% edgesGTall is bigger than the edges used for the final agglos, because some excluded edges from agglomerateMerge.m made it through (see code there)
-
+function makeSkelForChiasmataDetection(startingidx)
+temp = load('/gaba/scratch/mberning/axonQueryGeneration/beforeQueryGeneration.mat', 'axonsNew');
+axons = temp.axonsNew;
+clear temp
+segmentMeta = load('/gaba/u/mberning/results/pipeline/20170217_ROI/segmentMeta.mat', 'point');
+load('/gaba/scratch/mberning/edgesGTall.mat');
 result1 = load('/tmpscratch/kboerg/axonQueryAnalysisResult1.mat');
 result2 = load('/tmpscratch/kboerg/axonQueryAnalysisResult2.mat');
 result1ends = cell2mat(result1.endAgglo(cellfun('length',result1.endAgglo)>0)');
@@ -28,8 +24,8 @@ eqClassCCfull = [eqClassCC; num2cell(setdiff(1 : length(axons), cell2mat(eqClass
 last = @(x)x{end};
 fifth = @(x)x{5}
 getTask = @(x)fifth(strsplit(last(strsplit(x,'/')),'_'));
-iterate over super agglos
-for idx_agglo = randperm(length(eqClassCCfull),1)
+% iterate over super agglos
+for idx_agglo = startingidx : 500 : length(eqClassCCfull)
     currentEC =eqClassCCfull{idx_agglo};
     nodes = [];
     edges = [];
@@ -52,7 +48,9 @@ for idx_agglo = randperm(length(eqClassCCfull),1)
     % create nodes and edges for queries
     for runidx = 1 : 2
         for idx = 1 : length(resultCol{runidx}.startAgglo)
-            idx
+            if mod(idx,100)==0
+                disp(idx);
+            end
             if ~isempty(resultCol{runidx}.startAgglo{idx}) && resultCol{runidx}.startAgglo{idx}<=length(axons) && ismember(resultCol{runidx}.startAgglo{idx},currentEC)
                 if size(resultCol{runidx}.ff.nodes{idx},1)<2 ||any(ismember(getTask(resultCol{runidx}.ff.filenames{idx}),usedTasks))
                     continue;
@@ -64,12 +62,14 @@ for idx_agglo = randperm(length(eqClassCCfull),1)
                 %somehow we lost the node order for the query, here reconstructed with MSP
                 Tree = graphminspantree(sparse(squareform(pdist(resultCol{runidx}.ff.nodes{idx}))));
                 [X,Y]=find(Tree);
-                nodes2=[nodes2;resultCol{runidx}.ff.nodes{idx}];
-                [~, Locb] = ismember(tempids(:),lookup);
+                [~, Locb] = ismember(tempids(:),lookup2);
                 [I, ~] = ind2sub(size(tempids),find(Locb));
-                edges2=[edges2;[X,Y]+size(nodes2,1); I+size(nodes2,1)-size(resultCol{runidx}.ff.nodes{idx},1), Locb(Locb>0)];
+                edges2=[edges2;[X,Y]+size(nodes2,1); I+size(nodes2,1), Locb(Locb>0)];
+                nodes2=[nodes2;resultCol{runidx}.ff.nodes{idx}];
+                assert(size(nodes2,1)>=max(edges2(:)));
             end
         end
     end
-    detectChiasmataKMB2([],nodes2,edges2,true,['/tmpscratch/kboerg/visX7_' num2str(idx_agglo) '/'])
+    mkdir(['/tmpscratch/kboerg/visX11_' num2str(floor(idx_agglo/100)) '/']);
+    detectChiasmataKMB2([],nodes2,edges2,true,['/tmpscratch/kboerg/visX11_' num2str(floor(idx_agglo/100)) '/visX11_' num2str(idx_agglo) '/'])
 end
