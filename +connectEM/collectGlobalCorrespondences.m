7bd5d4356b2e71e672de67ddaf5546eb24774c4e
Benedikt Staffler <benedikt.staffler@brain.mpg.de>
2017-08-08 14:16:33 +0200

    collect coms as well

--- +connectEM/collectGlobalCorrespondences.m
+++ +connectEM/collectGlobalCorrespondences.m
@@ -1,14 +1,19 @@
-function corrEdges = collectGlobalCorrespondences(p)
+function [corrEdges, corrCom] = collectGlobalCorrespondences(p)
 
     saveFolder = [p.saveFolder 'correspondencesNew' filesep];
     files = dir([saveFolder '*.mat']);
     
     corrEdges = zeros(0,2,'uint32');
+    corrCom = zeros(0,3,'uint32');
     for i=1:length(files)
         load([saveFolder files(i).name]);
         countsCnormMin = countsC ./ min(arrayfun(@(x)countsS(uniqueSegments == x), uniqueCorrespondences),[],2);
         theseEdges = uniqueCorrespondences(countsC > 10 & countsCnormMin > 0.9,:);
+        if nargout > 1
+            theseComs = com(countsC > 10 & countsCnormMin > 0.9, :);
+        end
         corrEdges(end+1:end+size(theseEdges,1),:) = theseEdges;
+        corrCom(end+1:end+size(theseEdges,1),:) = theseComs;
     end
 end
 
