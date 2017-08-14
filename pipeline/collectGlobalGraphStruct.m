4fe791d277114f3c54494f9e54be6f7105b948b1
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-08-14 18:50:09 +0200

    Fix bug in pipeline/collectGlobalGraphStruct

--- pipeline/collectGlobalGraphStruct.m
+++ pipeline/collectGlobalGraphStruct.m
@@ -2,13 +2,14 @@ function collectGlobalGraphStruct(p)
     edgeFile = fullfile(p.saveFolder, 'globalEdges.mat');
     probFile = fullfile(p.saveFolder, 'globalNeuriteContinuityProb.mat');
     corrFile = fullfile(p.saveFolder, 'mapping.mat');
-
+    
+    % Load edges and their probabilities
+    load(edgeFile, 'edges');
+    load(probFile, 'prob');
+    
     % The edge list only contains edges between segments in the same
     % segmentation cube. Edges between different cubes are called
     % 'correspondences' and must be added separately here.
-    edges = load(edgeFile, 'edges');
-    prob = load(probFile, 'prob');
-    
     corr = load(corrFile, 'correspondences');
     corr = corr.correspondences;
     
