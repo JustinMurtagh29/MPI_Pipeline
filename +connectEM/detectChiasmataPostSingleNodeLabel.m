2fb6e95bbfa1cfdd118bbe65f2c4249e4d1c573d
KevinBoergens <kevin.boergens@gmail.com>
2017-08-16 00:32:52 +0200

    only do the crazy stuff if necessary

--- +connectEM/detectChiasmataPostSingleNodeLabel.m
+++ +connectEM/detectChiasmataPostSingleNodeLabel.m
@@ -1,27 +1,31 @@
 function [output, queryIdx] = detectChiasmataPostSingleNodeLabel(edges, isIntersection, nrExits, nodes, p, nodesV, prob)
 if sum(isIntersection) > 1
     N = sum(isIntersection);
-    distances = pdist(nodes(isIntersection,:));
-    connections = find(distances < 2000);
-    clear distances
-    % pdist lists the distances in a special way (see documentation)
-    % the next two lines define the borders of the right column of those replies
-    idxs_end = cumsum(N-1:-1:1);
-    idxs_start = [1, cumsum(N-1:-1:2)+1];
-    % sort the connections within these boundaries
-    [B, I] = sort([connections,(idxs_end + 0.5)]); % B = A(I);
-    I_reverse(I) = 1:length(I);
-    % find the separators again in the sorted list
-    separators = find(B ~= round(B));
-    % make a lookup to find for each entry in which box it is
-    bins = repelem(1:(N-1),diff([1,separators]));
-    %with that we get the right side for each connection
-    rightside = bins(I_reverse(1 : end-1));
-    rightside = rightside(1:length(connections));
-    %the left side depends where it is in its box
-    leftside_pre = idxs_start(rightside);
-    leftside = connections-leftside_pre+rightside+1;
-    [cccount,ccpre] =graphconncomp(sparse(leftside, rightside,ones(1,length(connections)),N,N),'Directed',false);
+    if N > 10000
+        distances = pdist(nodes(isIntersection,:));
+        connections = find(distances < 2000);
+        clear distances
+        % pdist lists the distances in a special way (see documentation)
+        % the next two lines define the borders of the right column of those replies
+        idxs_end = cumsum(N-1:-1:1);
+        idxs_start = [1, cumsum(N-1:-1:2)+1];
+        % sort the connections within these boundaries
+        [B, I] = sort([connections,(idxs_end + 0.5)]); % B = A(I);
+        I_reverse(I) = 1:length(I);
+        % find the separators again in the sorted list
+        separators = find(B ~= round(B));
+        % make a lookup to find for each entry in which box it is
+        bins = repelem(1:(N-1),diff([1,separators]));
+        %with that we get the right side for each connection
+        rightside = bins(I_reverse(1 : end-1));
+        rightside = rightside(1:length(connections));
+        %the left side depends where ipwdt is in its box
+        leftside_pre = idxs_start(rightside);
+        leftside = connections-leftside_pre+rightside+1;
+        [cccount,ccpre] =graphconncomp(sparse(leftside, rightside,ones(1,length(connections)),N,N),'Directed',false);
+    else
+        [cccount,ccpre] =graphconncomp(sparse(squareform(pdist(nodes(isIntersection,:)))<2000));
+    end
     lookup = find(isIntersection);
     cc = arrayfun(@(x){lookup(ccpre==x)},1:cccount);
     [~, centerOfCC] = cellfun(@(x)min(pdist2(bsxfun(@minus, nodes(x,:), mean(nodes(x,:),1)), [0 0 0])), cc);
