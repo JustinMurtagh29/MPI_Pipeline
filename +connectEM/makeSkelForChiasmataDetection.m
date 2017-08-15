cf0693a8d985b125ef6db132221a97d870e33f23
KevinBoergens <kevin.boergens@gmail.com>
2017-08-16 00:48:51 +0200

    use new superagglos

--- +connectEM/makeSkelForChiasmataDetection.m
+++ +connectEM/makeSkelForChiasmataDetection.m
@@ -1,26 +1,13 @@
 function makeSkelForChiasmataDetection(startingidx)
-temp = load('/gaba/scratch/mberning/axonQueryGeneration/beforeQueryGeneration.mat', 'axonsNew');
-axons = temp.axonsNew;
-clear temp
+load('/tmpscratch/kboerg/axonsBorderNew');
 segmentMeta = load('/gaba/u/mberning/results/pipeline/20170217_ROI/segmentMeta.mat', 'point');
 load('/gaba/scratch/mberning/edgesGTall.mat');
-result1 = load('/tmpscratch/kboerg/axonQueryAnalysisResult1.mat');
-result2 = load('/tmpscratch/kboerg/axonQueryAnalysisResult2.mat');
-filterDoubles = false;
-if filterDoubles
-    result1ends = cell2mat(result1.endAgglo(cellfun('length',result1.endAgglo)>0)');
-    result1ends(result1ends>length(axons)) = [];
-    a = find(cellfun(@(x)max([-1,find(ismember(result1ends,x))]),result2.startAgglo)~=-1);
-    result2.endAgglo(a) = [];
-    result2.startAgglo(a) = [];
-    result2.idxGood(a) = [];
-end
+result3 = load('/tmpscratch/kboerg/axonQueryAnalysisResult3.mat');
 
-resultCol = {result1, result2};
 
-edges1 = cellfun(@(x,y)combnk([-1, x(x<=length(axons)) y(y<=length(axons))], length(x(x<=length(axons)))*2), result1.startAgglo(result1.idxGood), result1.endAgglo(result1.idxGood), 'uni', 0);
-edges2 = cellfun(@(x,y)combnk([-1, x(x<=length(axons)) y(y<=length(axons))], length(x(x<=length(axons)))*2), result2.startAgglo(result2.idxGood), result2.endAgglo(result2.idxGood), 'uni', 0);
-edges=[edges1; edges2];
+
+
+edges = cellfun(@(x,y)combnk([-1, x(x<=length(axons)) y(y<=length(axons))], length(x(x<=length(axons)))*2), result3.startAgglo(result3.idxGood), result3.endAgglo(result3.idxGood), 'uni', 0);
 edges(cellfun(@isempty,edges))=[];
 edges = cell2mat(edges);
 edges(edges(:,1) == edges(:,2),:) = [];
@@ -30,15 +17,11 @@ eqClassCC = Graph.findConnectedComponents(edges, true, true);
 eqClassCCfull = [eqClassCC; num2cell(setdiff(1 : length(axons), cell2mat(eqClassCC)))'];
 % iterate over super agglos
 numstr = '19';
+
 for idx_agglo = startingidx : 500 : length(eqClassCCfull);
     currentEC =eqClassCCfull{idx_agglo};
     mkdir(['/tmpscratch/kboerg/visX', numstr, '_' num2str(floor(idx_agglo/100)) '/']);
-    
-    if idx_agglo == 1
-        load('backup','nodes2','edges2');
-    else
-        [nodes2, edges2] = connectEM.makeSkelForChiasmataDetectionSub(currentEC, axons, edgesGTall, resultCol, segmentMeta, false);
-    end
+    [nodes2, edges2] = connectEM.makeSkelForChiasmataDetectionSub(currentEC, axons, edgesGTall, {result3}, segmentMeta, false);
     assert(length(Graph.findConnectedComponents(edges2))<=1);
     connectEM.detectChiasmata([],nodes2,edges2,idx_agglo ~= 1,['/tmpscratch/kboerg/visX',numstr ,'_' num2str(floor(idx_agglo/100)) '/visX', numstr, '_' num2str(idx_agglo) '/'])
 end
