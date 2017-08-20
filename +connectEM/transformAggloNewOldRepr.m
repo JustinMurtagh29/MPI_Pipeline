4230c0e248d7a7174bb80d3cfbbd8ff2fc12b112
Marcel Beining <marcel.beining@brain.mpg.de>
2017-08-20 19:34:53 +0200

    brackets..

--- +connectEM/transformAggloNewOldRepr.m
+++ +connectEM/transformAggloNewOldRepr.m
@@ -4,7 +4,7 @@ function aggloOld = transformAggloNewOldRepr(aggloNew)
 % author marcel.beining@brain.mpg.de
 
 if isfield(aggloNew,'nodes')  % new representation
-    aggloOld = cellfun(@(x) x(~isnan(x(:,4),4)),{aggloNew.nodes},'uni',0);
+    aggloOld = cellfun(@(x) x(~isnan(x(:,4)),4),{aggloNew.nodes},'uni',0);
     aggloOld = aggloOld(:);
 else
     aggloOld = aggloNew(:);
