4b3c7d930941c47acbd8d2498e2dea12df4fac4d
Marcel Beining <marcel.beining@brain.mpg.de>
2017-08-20 19:31:08 +0200

    git fix

---
+++ +connectEM/transformAggloNewOldRepr.m
@@ -0,0 +1,13 @@
+function aggloOld = transformAggloNewOldRepr(aggloNew)
+% transforms the new agglo represenation into the old representation
+%
+% author marcel.beining@brain.mpg.de
+
+if isfield(agglos,'nodes')  % new representation
+    aggloOld = cellfun(@(x) x(~isnan(x(:,4),4)),{aggloNew.nodes},'uni',0);
+    aggloOld = aggloOld(:);
+else
+    aggloOld = aggloNew(:);
+end
+
+
