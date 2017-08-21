e2d1e6067350d3fc017a60fba107560115b42ef0
Marcel Beining <marcel.beining@brain.mpg.de>
2017-08-21 10:55:57 +0200

    added function to remove duplicate seg ids in agglo (new or old representation)

---
+++ +connectEM/removeDuplicateSegIdsInAgglo.m
@@ -0,0 +1,17 @@
+function [ newAgglos ] = removeDuplicateSegIdsInAgglo( agglos )
+% this function removes duplicate segment ids in agglos (new or old
+% representation)
+%
+% author: marcel.beining@brain.mpg.de
+
+if ~isfield(agglos,'nodes') % old representation
+    newAgglos = cellfun(@(x) unique(x),agglos,'uni',0);
+else
+    for n = 1:numel(agglos)
+        [~, idx] = unique(agglos(n).nodes(:,4),'first');  % this also works for NaNs (which are no duplicates
+        idx = ~ismember(1:size(agglos(n).nodes,1),idx);  % get index to duplicates
+        agglos(n).nodes(idx,:) = []; % remove overlapping nodes
+        agglos(n).edges = unique(connectEM.changem(agglos(n).edges, 1:numel(allOverlapIdx)-cumsum(idx),1:numel(idx)),'rows');% correct edges accordingly
+    end
+end
+
