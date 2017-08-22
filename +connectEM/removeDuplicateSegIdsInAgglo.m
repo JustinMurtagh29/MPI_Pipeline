5715c7eee0cee5fec3ee25ee8be34915445709a4
Marcel Beining <marcel.beining@brain.mpg.de>
2017-08-22 22:20:25 +0200

    fix

--- +connectEM/removeDuplicateSegIdsInAgglo.m
+++ +connectEM/removeDuplicateSegIdsInAgglo.m
@@ -12,9 +12,9 @@ else
     totalDupl = 0;
     for n = 1:numel(agglos)
         [~, idx,idx2] = unique(agglos(n).nodes(:,4),'first');  % this also works for NaNs (which are no duplicates
+        agglos(n).edges = unique(connectEM.changem(agglos(n).edges, idx2,1:numel(idx2)),'rows');% correct edges accordingly
         agglos(n).nodes = agglos(n).nodes(idx,:); % keep only unique nodes
-        agglos(n).edges = unique(connectEM.changem(agglos(n).edges, idx2,1:size(agglos(n).nodes,1)),'rows');% correct edges accordingly
-        totalDupl = totalDupl + size(agglos(n).nodes,1) - numel(idx);
+        totalDupl = totalDupl + numel(idx2) - numel(idx);
     end
 end
 
