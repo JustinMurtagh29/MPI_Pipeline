d5c1466fb88913af1e43f74f4bd9b7db3c15090c
Marcel Beining <marcel.beining@brain.mpg.de>
2017-08-22 10:18:21 +0200

    performance improvement

--- +connectEM/removeDuplicateSegIdsInAgglo.m
+++ +connectEM/removeDuplicateSegIdsInAgglo.m
@@ -13,9 +13,11 @@ else
     for n = 1:numel(agglos)
         [~, idx] = unique(agglos(n).nodes(:,4),'first');  % this also works for NaNs (which are no duplicates
         idx = ~ismember(1:size(agglos(n).nodes,1),idx);  % get index to duplicates
-        totalDupl = totalDupl + sum(idx);
-        agglos(n).nodes(idx,:) = []; % remove overlapping nodes
-        agglos(n).edges = unique(connectEM.changem(agglos(n).edges, 1:numel(idx)-cumsum(idx),1:numel(idx)),'rows');% correct edges accordingly
+        if any(idx)
+            totalDupl = totalDupl + sum(idx);
+            agglos(n).nodes(idx,:) = []; % remove overlapping nodes
+            agglos(n).edges = unique(connectEM.changem(agglos(n).edges, 1:numel(idx)-cumsum(idx),1:numel(idx)),'rows');% correct edges accordingly
+        end
     end
 end
 
