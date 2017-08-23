35a59076db576fa3f64484bcf934013b9f1def1f
Manuel Berning <manuel.berning@brain.mpg.de>
2017-08-23 18:44:05 +0200

    update start position correction to always provide full node evidence

--- +connectEM/correctQueryLocationToEndOfSegment.m
+++ +connectEM/correctQueryLocationToEndOfSegment.m
@@ -1,38 +1,60 @@
-function newPos = correctQueryLocationToEndOfSegment(p, cc, pos, dir, nmStepback)
-% Correct query location to center of mass of last ~1000nm of cc along direction vector
+function newPos = correctQueryLocationToEndOfSegment(p, cc, pos, dir, nmStepback, visualize)
+% Correct query location to point with largest distance to segment surface
+% within some bounding box, NOTE: dir and nmStepback not needed anymore, kept
+% for compability
 
-    % Define area to load
-    sizeBBox = [100 100 40];
-    scale = p.raw.voxelSize;
-    bboxToLoad(:,1) = pos - sizeBBox;
-    bboxToLoad(:,2) = pos + sizeBBox;
+    distance = 500;
 
-    % Load data and set logical indicating whether voxel is in cc
-    seg = readKnossosRoi(p.seg.root, p.seg.prefix, bboxToLoad, 'uint32', '', 'raw');
-    segAgglo = imdilate(ismember(seg, cc), ones(3,3,3));
-    clear seg;
+    % Visualiyation for debug purposes
+    if nargin < 6
+        visualize = false;
+    end
 
-    % Get all true voxel postions, offset to global coordinates
-    [pixelList(:,1), pixelList(:,2), pixelList(:,3)] = ind2sub(size(segAgglo), find(segAgglo));
-    pixelList = bsxfun(@plus, pixelList, bboxToLoad(:,1)' - [1 1 1]);
-    clear props;
+    % Define area to load around border in both directions in each dimension
+    surroundToLoad = ceil(repmat(distance,1,3)./p.raw.voxelSize);
+    centerOfBbox = surroundToLoad + 1;
+    % Bounding box to load from KNOSSOS hierachy centered on position that
+    % we want to correct (usually border center of mass right now)
+    bboxToLoad(:,1) = pos - surroundToLoad;
+    bboxToLoad(:,2) = pos + surroundToLoad;
 
-    % Project onto direction vector and keep only voxel in last 10 frames
-    dirNm = (dir .* scale)' ./ norm(dir .* scale);
-    prOnDir = bsxfun(@times, pixelList, scale) * dirNm;
-    lastVoxelPosAlongDir = max(prOnDir);
-    withinStartRegion = prOnDir > (lastVoxelPosAlongDir - nmStepback - 50) ...
-                      & prOnDir < (lastVoxelPosAlongDir - nmStepback + 50);
+    % Load segmentation data 
+    seg = readKnossosRoi(p.seg.root, p.seg.prefix, bboxToLoad, ...
+        'uint32', '', 'raw');
+    % Calculate distance to watershed border of each voxel in loaded box
+    segAggloDistToSurface = bwdist(~ismember(seg, cc));
 
-    % New position is center of mass of those pixel
-    newPos = round(mean(pixelList(withinStartRegion,:)));
-
-    % Check whether start is in right process
-    %testPos = newPos - bboxToLoad(:,1)' + [1 1 1];
-    %if ~segAgglo(testPos(1),testPos(2),testPos(3))
-    %    testPos
-    %    error('Start position not in agglomeration');
-    %end
+    % Find all voxel in bbox which would provide maximal node evidence
+    voxelIds = find(segAggloDistToSurface(:) > 2);
+    [voxelPos(:,1), voxelPos(:,2), voxelPos(:,3)] = ...
+        ind2sub(size(segAggloDistToSurface), voxelIds);
+    % Find the one closest to original position
+    distances = pdist2(voxelPos, centerOfBbox);
+    [~,idx] = min(distances);
+    
+    % New start position in local coordinates
+    newPosLocal = voxelPos(idx,:);
+    
+    if visualize
+        close all;
+        figure;
+        % Dark grey for agglo segments
+        temp = (segAggloDistToSurface > 0) * 0.5;
+        % Brighter gray for old start position
+        temp(centerOfBbox(1),centerOfBbox(2),centerOfBbox(3)) = 0.75;
+        % White for new start position
+        temp(newPosLocal(1),newPosLocal(2),newPosLocal(3)) = 1;
+        % Play video and pause execution
+        implay(temp);
+    end
+    
+    % This should always yield full node evidence at start position
+    if maxVal < 2 
+        error('could not determine a voxel with full node evidence');
+    end
+    
+    % New position scale to global offset, also transfer from Matlab
+    % coordinates as used here to webKnossos coordinates for API call
+    newPos = newPosLocal + bboxToLoad(:,1)' - [2 2 2];
 
 end
-
