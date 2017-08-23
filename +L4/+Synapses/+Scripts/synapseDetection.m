5196ac776b5d31027daaed8aa3cd76f844acbfb9
Benedikt Staffler <benedikt.staffler@brain.mpg.de>
2017-08-23 16:56:32 +0200

    remove redundant files

--- +L4/+Synapses/+Scripts/synapseDetection.m
+++
@@ -1,8 +0,0 @@
-%% synapse detection pipeline from SynEM predictions
-% Author: Benedikt Staffler <benedikt.staffler@brain.mpg.de>
-
-%% load data
-
-p = Gaba.getSegParameters('ex145_ROI2017');
-p = L4.updateParamsToNewestFiles(p);
-graph = Seg.IO.loadGraph(p, true, 'end');
--- +L4/+Util/getSkel.m
+++
@@ -1,9 +0,0 @@
-function skel = getSkel()
-%GETSKEL Empty skeleton for ex145_07x2.
-% OUTPUT skel: skeleton object
-% Author: Benedikt Staffler <benedikt.staffler@brain.mpg.de>
-
-skel = Skeleton.setParams4Dataset([], 'ex145_ROI2017');
-
-end
-
