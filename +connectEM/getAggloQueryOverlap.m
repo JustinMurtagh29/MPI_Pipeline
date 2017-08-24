8be194e76092b8e619bbda1ff39a4ffd0b8f00ad
ChristianS <christian.schramm@brain.mgp.de>
2017-08-24 17:03:03 +0200

    add several statistics

--- +connectEM/getAggloQueryOverlap.m
+++ +connectEM/getAggloQueryOverlap.m
@@ -1,6 +1,5 @@
 % Calculate the overlap between given agglomerate state and all queries
 % done so far
-
 load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameterWithSynapses.mat');
 
 scratchFolder = '/tmpscratch/mberning/axonQueryResults/';
@@ -20,15 +19,17 @@ axons = cellfun(@(x)x(~isnan(x)),axons,'uni',0);
 % Lookup segment ids of nodes+neighbours of nmls in all folders defined above
 [ff.segIds, ff.neighbours, ff.filenames, ff.nodes, ff.startNode, ff.comments] = connectEM.lookupNmlMulti(p, skeletonFolders, false);
 
-display([num2str(sum(~cellfun(@isempty,ff.comments))) '/' num2str(numel(ff.comments)) ' queries contain comment and will not be used']);
 tabulate(cellfun(@(x)x{1}{1}, cellfun(@(x)regexp(x, 'content="(.*)"', 'tokens'), ...
     cat(1, ff.comments{~cellfun(@isempty, ff.comments)}), 'uni', 0), 'uni', 0))
 
+
 % queries commented by the HIWI in terms of unsolvability
 idx_comment = ~cellfun(@isempty,ff.comments);
+display([num2str(sum(idx_comment)) '/' num2str(numel(idx_comment)) ' queries contain comment and will not be used']);
 
 % ~600 queries do not have a start node, not sure why (maybe the ones with more than one tree), maybe check later
 idx_startEmpty = cellfun(@isempty, ff.startNode);
+display([num2str(sum(idx_startEmpty)) '/' num2str(numel(idx_startEmpty)) ' queries do not have a start node']);
 
 save(fullfile(param.saveFolder, 'aggloState/', 'AxonFlightPaths.mat'), 'ff', 'idx_comment', 'idx_startEmpty')
 % load(fullfile(param.saveFolder, 'aggloState/', 'AxonFlightPaths.mat'))
@@ -36,11 +37,11 @@ save(fullfile(param.saveFolder, 'aggloState/', 'AxonFlightPaths.mat'), 'ff', 'id
 ff = structfun(@(x)x(cellfun(@isempty, ff.comments)), ff, 'uni', 0);
 ff = structfun(@(x)x(~cellfun(@isempty, ff.startNode)), ff, 'uni', 0);
 %%    
-segmentsLeftover = [];
 
 % Calculate overlap of all queries with segments
 [uniqueSegments, neighboursStartNode, nodesExcludedIdx, startNodeIdx] = cellfun(@connectEM.queryAnalysis, ...
         ff.segIds, ff.neighbours, ff.nodes', ff.startNode', 'uni', 0);
+segmentsLeftover = [];
 % Determine all overlaps of agglomerations with given queries
 [partition, queryOverlap] = connectEM.queryAgglomerationOverlap(axons, segmentsLeftover, uniqueSegments, neighboursStartNode);
 % Make decision(s), here evidence/occurence threshold is applied
@@ -58,8 +59,11 @@ idxNoClearEnd = cellfun('isempty', endAgglo);
 idxGood = ~(idxNoClearStart | idxNoClearEnd);
 % Display some statistics
 display([num2str(sum(idxNoClearStart)./numel(idxNoClearStart)*100, '%.2f') '% of remaining queries have no clear start']);
+display([num2str(sum(idxNoClearStart)) ' in total']);
 display([num2str(sum(idxNoClearEnd)./numel(idxNoClearEnd)*100, '%.2f') '% of remaining queries have no clear end']);
+display([num2str(sum(idxNoClearEnd)) ' in total']);
 display([num2str(sum(idxGood)./numel(idxGood)*100, '%.2f') '% of remaining queries have clear start and ending']);
+display([num2str(sum(idxGood)) ' in total']);
 display([num2str(numel(cat(2, endAgglo{idxGood}))) ' attachments made by ' num2str(sum(idxGood)) ' queries']);
 % Find CC of eqClasses to be joined including single eqClasses with or
 % without dangling query
