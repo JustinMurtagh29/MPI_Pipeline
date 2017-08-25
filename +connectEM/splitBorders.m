c6edd03cb37121d835b9c5960990070101553fb8
KevinBoergens <kevin.boergens@gmail.com>
2017-08-25 12:34:59 +0200

    split borders with prob

--- +connectEM/splitBorders.m
+++ +connectEM/splitBorders.m
@@ -1,45 +1,71 @@
-% load needed data
-load(fullfile(outputFolder,'superagglos.mat'), 'superagglos');
-load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameterWithSynapses.mat','p');
-[graph, segmentMeta] = connectEM.loadAllSegmentationData(p);
-borderMeta = load([p.saveFolder 'globalBorder.mat'], 'borderCoM');
+function splitBorders(startidx)
+    % load needed data
+    load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameterWithSynapses.mat','p');
+    temp = load(fullfile(p.saveFolder,'aggloState/axons_03.mat'));
+    superagglos = temp.axons(temp.indBigAxons);
+    graph = connectEM.loadAllSegmentationData(p);
+    borderMeta = load([p.saveFolder 'globalBorder.mat'], 'borderCoM');
 
-% create bbox
-options.border = [2000; -2000];
-borderNm = repmat(options.border, 1, 3);
-borderVoxel = round(bsxfun(@times, 1./p.raw.voxelSize, borderNm));
-bboxSmall = p.bbox + borderVoxel';
+    % create bbox
+    options.border = [2000; -2000];
+    borderNm = repmat(options.border, 1, 3);
+    borderVoxel = round(bsxfun(@times, 1./p.raw.voxelSize, borderNm));
+    bboxSmall = p.bbox + borderVoxel';
 
-% create fake center of dataset field in borderCoM list for correspondences
-borderMeta.borderCoM(end+1,:)=round(mean(bboxSmall,2)');
+    % create fake center of dataset field in borderCoM list for correspondences
+    borderMeta.borderCoM(end+1,:)=round(mean(bboxSmall,2)');
 
-edgesCol = [];
 
-for idx = 1 : length(superagglos)
-    if mod(idx,100) == 0
-        idx
+    for idx = startidx : 50 : length(superagglos)
+        nodesHere = superagglos(idx).nodes;
+        segIdsHere = nodesHere(:, 4);
+        flatten = @(x)x(:);
+        edgesCandidates = sort([flatten(repelem(segIdsHere,cellfun('length',graph.neighbours(segIdsHere)))), cell2mat(graph.neighbours(segIdsHere))],2);
+        edgesCandidatesProb = cell2mat(graph.neighProb(segIdsHere));
+        edgesCandidatesBorderIdx = cell2mat(graph.neighBorderIdx(segIdsHere));
+        only12 = @(x)x(:,1:2);
+        edgeOverview = flipud(sortrows([double(edgesCandidates),edgesCandidatesProb,double(edgesCandidatesBorderIdx)],3));
+        [~, filter] = unique(only12(edgeOverview),'rows');
+        edgesCandidates=edgeOverview(filter,1:2);
+        edgesCandidatesProb = edgeOverview(filter,3);
+        edgesCandidatesBorderIdx = edgeOverview(filter,4);
+        
+        [~, usededges] = ismember(edgesCandidates,sort(reshape(segIdsHere(superagglos(idx).edges),[],2),2),'rows');
+        % find whether edge is outside of bbox
+        borderidxs = edgesCandidatesBorderIdx(usededges>0);
+        borderidxs(isnan(borderidxs))=length(borderMeta.borderCoM); % correspondences
+        borderPositions = double(borderMeta.borderCoM(borderidxs,:));
+        outsideBbox = ~(all(bsxfun(@gt, borderPositions, bboxSmall(:, 1)'), 2) & ...
+            all(bsxfun(@lt, borderPositions, bboxSmall(:, 2)'), 2));
+        % remove all insulting edges
+        edgesFinal = superagglos(idx).edges(usededges(usededges>0),:);
+        edgesFinal(outsideBbox&edgesCandidatesProb(usededges>0)<0.98, :) = [];
+        C = Graph.findConnectedComponents(edgesFinal);
+        superagglosBorderSplit{idx} = struct('nodes', {},'edges', {});
+        for idx2 = 1 : length(C)
+            superagglosBorderSplit{idx}(idx2).nodes = superagglos(idx).nodes(C{idx2},:);
+            clear lookup
+            lookup(C{idx2})=1:length(C{idx2});
+            superagglosBorderSplit{idx}(idx2).edges = sort(reshape(lookup(edgesFinal(any(ismember(edgesFinal,C{idx2}),2),:)),[],2),2);
+        end
+        missingNodes = setdiff(1:size(superagglos(idx).nodes,1), cell2mat(C));
+        superagglosBorderSplit{idx} = [superagglosBorderSplit{idx}, struct('edges',cell(1,length(missingNodes)),'nodes', arrayfun(@(x){superagglos(idx).nodes(x,:)},missingNodes))];
+        calculateLength = @(x)max(pdist(bsxfun(@times, double(borderMeta.borderCoM(x, :)), p.raw.voxelSize)));
+        filternan = @(x)x(~isnan(x));
+        for idx2 = 1 : length(superagglosBorderSplit{idx})
+            if length(superagglosBorderSplit{idx}(idx2).nodes(:,4)) < 5000
+                axonLength{idx}(idx2) = max([-1, calculateLength(filternan(cell2mat(graph.neighBorderIdx(superagglosBorderSplit{idx}(idx2).nodes(:,4)))))]);
+            else
+                axonLength{idx}(idx2) = Inf;
+            end
+        end
+        %comments = cellfun(@(x){cell(size(x,1), 1)},{superagglosBorderSplit{idx}.nodes});
+        %writeNml(['a.nml'], writeSkeletonFromNodesAndEdges({superagglosBorderSplit{idx}.nodes}, {superagglosBorderSplit{idx}.edges},comments, repmat({'axon'},size(superagglosBorderSplit{idx})), repmat({[0 0 1 1]},size(superagglosBorderSplit{idx}))));
+        %comments = cellfun(@(x){cell(size(x,1), 1)},{superagglos(idx).nodes});
+        %writeNml(['b.nml'], writeSkeletonFromNodesAndEdges({superagglos(idx).nodes}, {superagglos(idx).edges},comments, repmat({'axon'},1,1), repmat({[0 0 1 1]},1,1)));
+
     end
-    nodesHere = superagglos(idx).nodes;
-    segIdsHere = nodesHere(:, 4);
-    % find edge idxs of used hot edges
-    [~, usededges] = ismember(graph.edges, sort(segIdsHere(superagglos(idx).edges),2),'rows');
-    % find whether edge is outside of bbox
-    borderidxs = graph.borderIdx(usededges>0);
-    borderidxs(isnan(borderidxs))=length(borderMeta.borderCoM); % correspondences
-    borderPositions = double(borderMeta.borderCoM(borderidxs,:));
-    outsideBbox = ~(all(bsxfun(@gt, borderPositions, bboxSmall(:, 1)'), 2) & ...
-        all(bsxfun(@lt, borderPositions, bboxSmall(:, 2)'), 2));
-    % remove all insulting edges
-    todelete = graph.prob(usededges>0)<0.98&outsideBbox;
-    usededges(usededges==0) = [];
-    edgesHere = superagglos(idx).edges ...
-        + sum(arrayfun(@(x)size(x.nodes,1),superagglos(1:idx-1))); % offset so that all nodes and edges go into one struct
-    edgesHere(usededges(todelete), :) = [];
-    edgesCol = [edgesCol; edgesHere];
+    save(['/tmpscratch/kboerg/splitBorder/splitBorder_' num2str(startidx)], 'superagglosBorderSplit','axonLength')
 end
-nodesCol = cat(1, superagglos(:).nodes);
-C = Graph.findConnectedComponents(edgesCol);
-superagglosBorderSplit = cellfun(@(x)struct('nodes',nodesCol(x, :), 'edges', edgesCol(all(ismember(edgesCol,x),2),:)), C);
-
-% add lonely nodes
-superagglosBorderSplit = [superagglosBorderSplit; struct('nodes', arrayfun(@(x){nodesCol(x,:)},setdiff(1:size(nodesCol,1), cell2mat(C))),1)];
+function dummy()
+end
---
+++ +connectEM/splitBordersMeta.m
@@ -0,0 +1,25 @@
+functionH = @connectEM.splitBorders;
+inputCell = cellfun(@(x){x}, num2cell(1:50), 'uni', 0);
+cluster = Cluster.getCluster( ...
+    '-pe openmp 1', ...
+    '-p 0', ...
+    '-l h_vmem=64G', ...
+    '-l s_rt=23:50:00', ...
+    '-l h_rt=24:00:00');
+job = Cluster.startJob( functionH, inputCell, ...
+    'name', 'splitBorders', ...
+    'cluster', cluster);
+superagglosBorderSplit = {};
+axonLength = {};
+for idx = 1:50
+    idx
+    temp = load(['/tmpscratch/kboerg/splitBorder/splitBorder_' num2str(idx)])
+    
+    superagglosBorderSplit = [superagglosBorderSplit, temp.superagglosBorderSplit(idx:50:end)];
+    axonLength = [axonLength, temp.axonLength(idx:50:end)];
+end
+superagglosBorderSplit = cell2mat(superagglosBorderSplit);
+axonLength=cell2mat(axonLength);
+axons = superagglosBorderSplit;
+indBigAxons = axonLength > 5000;
+save(fullfile(p.saveFolder,'aggloState/axons_04.mat'), 'axons','indBigAxons');
