97757b3b19fb0e94cfb6fd2959972e0c737cdc16
Manuel Berning <manuel.berning@brain.mpg.de>
2017-08-28 11:59:27 +0200

    4. ending function checked

--- +connectEM/generateAxonEndings.m
+++ +connectEM/generateAxonEndings.m
@@ -2,26 +2,26 @@ function generateAxonEndings(param)
     % Written by
     %   Manuel Berning <manuel.berning@brain.mpg.de>
     %   Christian Schramm <christian.schramm@brain.mpg.de>
-    
+
     options = struct;
     options.latentScore = 0.5;
     options.segDirScore = 0.8;
     options.distanceCutoff = 600; % in nm
-    
+
     % Directory with input / output data
     dataDir = fullfile(param.saveFolder, 'aggloState');
-    
+
     % load directionality information
     endingInput = fullfile(dataDir, 'axonEndingInputData.mat');
     endingInput = load(endingInput, 'axonIds', 'directionality');
     directionality = endingInput.directionality;
     axonIds = endingInput.axonIds;
-    
+
     % load border CoMs
     borderCoM = fullfile(param.saveFolder, 'globalBorder.mat');
     borderCoM = load(borderCoM, 'borderCoM');
     borderCoM = borderCoM.borderCoM;
-    
+
     % Find all borders for valid endings
     idxDirectional = cellfun( ...
         @(x) x(:, 1) > options.latentScore, ...
@@ -32,14 +32,14 @@ function generateAxonEndings(param)
     idxAll = cellfun( ...
         @(x, y) find(x & y), ...
         idxDirectional, idxEnding, 'UniformOutput', false);
-    
+
     % Keep only those agglomerates with at least one ending candidate
     nrCanidates = cellfun(@numel, idxAll);
     axonMask = nrCanidates > 0;
     axonIds = axonIds(axonMask);
-   
-    display([num2str(numel(axonMask)) ' agglomerates in total']);
-    display([num2str(1 - (sum(axonMask)./numel(axonMask))*100, '%.2f') '% of agglomerates have not a single ending']);
+
+    display([num2str(numel(axonMask)) ' agglomerates > 5 micron in total']);
+    display([num2str(100 - (sum(axonMask)./numel(axonMask))*100, '%.2f') '% of > 5 micron agglomerates have not a single ending']);
     display([num2str(numel(axonMask) - sum(axonMask)) ' in total']);
 
     % clustering on left candidates
@@ -52,13 +52,12 @@ function generateAxonEndings(param)
     borderClusters = cellfun( ...
         @(x) clusterBorders(param, options, x), ...
         borderPositions, 'UniformOutput', false);
-    
+
     clusterSizes = cellfun(@max, borderClusters);
     singleEnding = sum(clusterSizes == 1);
     display([num2str(singleEnding./numel(clusterSizes)*100, '%.2f') '% of agglomerates have just one single ending']);
     display([num2str(singleEnding) ' in total']);
-   
-    
+
     % save result
     out = struct;
     out.axonIds = axonIds;
@@ -67,7 +66,7 @@ function generateAxonEndings(param)
     out.borderPositions = borderPositions;
     out.borderClusters = borderClusters;
     out.gitInfo = Util.gitInfo();
-    
+
     Util.saveStruct(fullfile(dataDir, 'axonEndings.mat'), out);
 end
 
@@ -76,7 +75,7 @@ function clusterIds = clusterBorders(param, options, borderCoM)
         % convert borderCoM to nm space
         borderCoM = double(borderCoM);
         borderCoM = bsxfun(@times, borderCoM, param.raw.voxelSize);
-        
+
         clusterIds = clusterdata( ...
             borderCoM, ...
             'linkage', 'single', ...
