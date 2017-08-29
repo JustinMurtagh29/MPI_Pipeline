70bd7f26892bba71fc047723e3f2eb055bdd336b
Manuel Berning <manuel.berning@brain.mpg.de>
2017-08-29 23:50:36 +0200

    tested whether positions are still the ones from open endings with another approach, all seems fine

--- +connectEM/someTests.m
+++ +connectEM/someTests.m
@@ -1,5 +1,4 @@
 param = load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameterWithSynapses.mat');
- = arrayfun(@(x)x.eqClasses(x.occurences > 53), queryOverlap.ends, 'uni', 0);
 param = param.p;
 dataDir = fullfile(param.saveFolder, 'aggloState');
 
@@ -58,7 +57,48 @@ startAgglo = arrayfun(@(x)x.eqClasses(x.occurences > 13), m.queryOverlap.start, 
 endAgglo = arrayfun(@(x)x.eqClasses(x.occurences > 53), m.queryOverlap.ends, 'uni', 0);
 sum(cellfun(@(x,y)numel(intersect(x,y)), endAgglo, startAgglo, 'uni', 0));
 
+% Check that border positions of new tasks make sense (needs linearized version of thisborderPositions from generateAxonQueries)
 
+% Determine all endings hit by at least one flight path
+endingOverlap = load(fullfile(dataDir, 'axonEndingOverlaps.mat'));
+startEndings = unique(cell2mat(endingOverlap.startEndingOverlaps));
+endEndings = unique(cell2mat(endingOverlap.endEndingOverlaps));
+totalEndings = union(startEndings,endEndings);
 
+% Determine open endings
+endings = load(fullfile(dataDir, 'axonEndings.mat'));
+openEndings = setdiff(1:sum(cellfun(@max, endings.borderClusters)), totalEndings);
+numel(openEndings)
 
+% Create lookup which agglo and which cluster within agglo an ending belongs to
+clusterSizes = cellfun(@max, endings.borderClusters);
+clusterLookup = repelem(1:numel(clusterSizes), clusterSizes);
+withinClusterLookup = arrayfun(@(x)(1:x)', clusterSizes, 'uni', 0);
+withinClusterLookup = cat(1, withinClusterLookup{:});
+
+% Get positions of queried locations in cluster
+theseBorderPositions = cellfun(@(x)cat(1, x{:}), thisborderPositions, 'uni', 0);
+theseBorderPositions = cat(1, theseBorderPositions{:});
+
+% Get positions of all members of each ending cluster not hit
+positions = arrayfun(@(x)endings.borderPositions{clusterLookup(x)}...
+    (endings.borderClusters{clusterLookup(x)} == withinClusterLookup(x),:), ...
+    openEndings, 'uni', 0);
+clusterSizes = cellfun(@(x)size(x,1), positions);
+positions = cat(1, positions{:});
+
+% Define exclusion zone near dataset center
+options.border = [3000; -3000];
+borderNm = repmat(options.border, 1, 3);
+borderVoxel = round(bsxfun(@times, 1./param.raw.voxelSize, borderNm));
+bboxSmall = param.bbox + borderVoxel';
+
+% Exclude positions outside
+idxWithin = all(bsxfun(@gt, positions, bboxSmall(:, 1)'),2) ...
+    & all(bsxfun(@lt, positions, bboxSmall(:, 2)'),2);
+positions = positions(idxWithin,:);
+
+% Test whether positions are the same
+suprise = setdiff(theseBorderPositions, positions, 'rows');
+% None found, hence all seems fine
 
