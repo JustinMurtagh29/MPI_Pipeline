5f67a341d2e3d880a0ffb7eeca6f51312ecaffc2
Manuel Berning <manuel.berning@brain.mpg.de>
2017-08-30 15:02:35 +0200

    create copy of files that only keeps case 3 (Moritz Case 6)

---
+++ +connectEM/generateAxonQueries_keep3only.m
@@ -0,0 +1,139 @@
+function generateAxonQueries_keep3only(param)
+    % Written by
+    %   Manuel Berning <manuel.berning@brain.mpg.de>
+    %   Christian Schramm <christian.schramm@brain.mpg.de>
+
+
+    dataDir = fullfile(param.saveFolder, 'aggloState');
+
+    % Load data from ending generation
+    endingData = fullfile(dataDir, 'axonEndingsAllData.mat');
+    endingData = load(endingData);
+    % Extract some variables
+    idxAll = endingData.idxAll;
+    idxCanidateFound = endingData.axonMask;
+    borderIds = endingData.borderIds;
+    borderClusters = endingData.borderClusters ;
+
+    % Load directionality information
+    directionality = fullfile(dataDir, 'axonEndingInputData.mat');
+    directionality = load(directionality, 'directionality');
+    directionality = directionality.directionality;
+
+    % Load border CoMs
+    borderCoM = fullfile(param.saveFolder, 'globalBorder.mat');
+    borderCoM = load(borderCoM, 'borderCoM');
+    borderCoM = borderCoM.borderCoM;
+    borderPositions = cellfun(@(x) borderCoM(x,:), directionality.borderIdx(idxCanidateFound),'uni',0);
+
+    % Load larger 5 micron agglomerates
+    m = load(fullfile(dataDir, 'axons_04.mat'));
+    axons = m.axons(m.indBigAxons);
+    axons = arrayfun(@Agglo.fromSuperAgglo, axons, 'UniformOutput', false);
+
+    % Determine endings which are not redundant(already attached by flight path)
+    m = load(fullfile(dataDir, 'attachedEndingsKeepCaseThree.mat'), 'attachedEndings');
+    attachedEndings = m.attachedEndings;
+    clear m
+
+    % Find indices of ending candidates in directionality lists (scores,
+    % pca...) and exclude redundant endings
+    idxUse = idxAll(idxCanidateFound);
+    counter = 1;
+    idxCluster = [];
+    for j=1:length(borderClusters)
+        idxCluster{j,1} = [];
+        for k=1:max(borderClusters{j})
+            if ismember(counter,attachedEndings)
+                idxCluster{j,1}{k,1} = [];
+            else
+                idxCluster{j,1}{k,1} = idxUse{j,1}(find(borderClusters{j}==k));
+            end
+            counter = counter + 1;
+        end
+        idxCluster{j,1} = idxCluster{j,1}(~cellfun('isempty',idxCluster{j,1}));
+    end
+
+    % Exclude axons without a single ending left
+    redundant = cellfun('isempty',idxCluster);
+    idxCluster = idxCluster(~redundant);
+
+    % Write out absolut values of seg direction scores
+    SegDirScores = cellfun(@(x)abs(x),directionality.scores(idxCanidateFound),'uni',0);
+    SegDirScores = SegDirScores(~redundant);
+    borderPositions = borderPositions(~redundant);
+
+    % Dataset border conditions
+    options.border = [3000; -3000];
+    borderNm = repmat(options.border, 1, 3);
+    borderVoxel = round(bsxfun(@times, 1./param.raw.voxelSize, borderNm));
+    bboxSmall = param.bbox + borderVoxel';
+
+    % Keep only candidate with highest score for each cluster and exclude
+    % candidate outside border cutoff
+    candidateUse = {};
+    for j=1:length(idxCluster)
+        candidateUse{j,1} = [];
+        for k=1:length(idxCluster{j})
+            scoreClusters = SegDirScores{j,1}(idxCluster{j,1}{k,1});
+            [~, sortIdx] = sort(scoreClusters, 'descend');
+            candidate = idxCluster{j,1}{k,1}(sortIdx(1));
+            if all(bsxfun(@gt, borderPositions{j}(candidate,:), bboxSmall(:, 1)'),2)...
+                    & all(bsxfun(@lt, borderPositions{j}(candidate,:), bboxSmall(:, 2)'),2)
+                candidateUse{j,1}(end+1,1) = candidate;
+            end
+        end
+    end
+
+    % Write out scores and pca, apply all masks
+    outside = cellfun('isempty',candidateUse);
+    axons = axons(idxCanidateFound);
+    axons = axons(~redundant);
+    axons = axons(~outside);
+
+    pcaFound = directionality.pca(idxCanidateFound);
+    pcaFound = pcaFound(~redundant);
+    pcaFound = pcaFound(~outside);
+    scoresFound = directionality.scores(idxCanidateFound);
+    scoresFound = scoresFound(~redundant);
+    scoresFound = scoresFound(~outside);
+    borderIdxFound = directionality.borderIdx(idxCanidateFound);
+    borderIdxFound = borderIdxFound(~redundant);
+    borderIdxFound = borderIdxFound(~outside);
+    candidateUse = candidateUse(~outside);
+    borderPositions = borderPositions(~outside);
+
+    % Extract some values for borders we want to query
+    thisborderPositions = {};
+    directions = {};
+    for j=1:sum(~outside)
+        thisPca{j,1} = [];
+        thisScores{j,1} = [];
+        thisBorderIdx{j,1} = [];
+        thisborderPositions{j,1} = {};
+        directions{j,1} = {};
+        for k=1:length(candidateUse{j})
+            thisPca{j,1}(end+1,:) = pcaFound{j}(:,1,candidateUse{j}(k))';
+            thisScores{j,1}(end+1,1) = scoresFound{j}(candidateUse{j}(k));
+            thisBorderIdx{j,1}(end+1,1) = borderIdxFound{j}(candidateUse{j}(k));
+            thisborderPositions{j,1}{end+1,1} = double(borderPositions{j,1}(candidateUse{j}(k),:));
+            directions{j,1}{end+1,1} = bsxfun(@times, bsxfun(@times, sign(thisScores{j,1}(end,1)), thisPca{j,1}(end,:)), 1 ./ param.raw.voxelSize);
+        end
+    end
+
+    outputFolder = fullfile(dataDir, 'queriesCase3/');
+    if ~exist(outputFolder, 'dir')
+        mkdir(outputFolder)
+    end
+
+    batchBoundaries = round(linspace(1, numel(axons), 101));
+    for i=1:length(batchBoundaries)-1
+        inputList{i,1} = {i};
+    end
+
+    sharedInputs = {batchBoundaries,axons,thisborderPositions,directions,param,outputFolder};
+    sharedInputsLocation = 2:7;
+    cluster = Cluster.getCluster('-p -100','-tc 20','-l h_vmem=64G','-l s_rt=5:28:00','-l h_rt=5:29:00');
+    job = Cluster.startJob(@connectEM.processQueryTasks, inputList, 'name', 'queryGeneration', 'cluster', cluster, ...
+        'sharedInputs', sharedInputs, 'sharedInputsLocation', sharedInputsLocation);
+
---
+++ +connectEM/makeEndingCaseDistinctions_keep3only.m
@@ -0,0 +1,100 @@
+function makeEndingCaseDistinctions_keep3only(param)
+
+    dataDir = fullfile(param.saveFolder, 'aggloState');
+
+    superAgglos = load(fullfile(dataDir, 'axons_04.mat'));
+    origAgglos = arrayfun(@Agglo.fromSuperAgglo, superAgglos.axons, 'uni', 0);
+    superAgglos = superAgglos.axons(superAgglos.indBigAxons);
+
+    endings = load(fullfile(dataDir, 'axonEndings.mat'));
+    endingOverlap = load(fullfile(dataDir, 'axonEndingOverlaps.mat'));
+    flightPaths = load(fullfile(dataDir, 'axonPostQueryAnalysisState.mat'), 'ff', 'startAgglo', 'endAgglo');
+    clusterSizes = cellfun(@max, endings.borderClusters);
+
+    % Which endings are linked together
+    [linkages, idxMultipleHits] = cellfun(@(x,y)connectEM.edgeCreator(x,y), endingOverlap.startEndingOverlaps, endingOverlap.endEndingOverlaps, 'uni', 0);
+    linkagesFlat = cat(1, linkages{:});
+    idxMultipleHits = cat(1, idxMultipleHits{:});
+    flightPaths.ff = structfun(@(x)x(~idxMultipleHits), flightPaths.ff, 'uni', 0);
+    flightPaths.startAgglo(idxMultipleHits) = [];
+    flightPaths.endAgglo(idxMultipleHits) = [];
+
+    % Which agglos are linked together
+    linkagesAgglos = cellfun(@(x,y)connectEM.edgeCreator(x,y), flightPaths.startAgglo, flightPaths.endAgglo, 'uni', 0);
+    linkagesAgglos = cat(1, linkagesAgglos{:});
+
+    % Which endings belong to which agglo
+    clusterSizes = cellfun(@max, endings.borderClusters);
+    clusterLookup = repelem(1:numel(clusterSizes), clusterSizes);
+
+    caseDistinctions = zeros(size(linkagesFlat,1),1);
+    % Choose 1 for no attachment
+    caseDistinctions(isnan(linkagesFlat(:,1)) & isnan(linkagesFlat(:,2))) = 1;
+    % Choose 2 for attachment at end but not at start
+    caseDistinctions(isnan(linkagesFlat(:,1)) & ~isnan(linkagesFlat(:,2))) = 2;
+    % Choose 3 for attachment to start but not at end
+    caseDistinctions(~isnan(linkagesFlat(:,1)) & isnan(linkagesFlat(:,2))) = 3;
+    % Find indices did match criteria so far
+    idxMatched = any(isnan(linkagesFlat),2);
+    % Choose 4 for attachted to start not at ending and to end not at ending
+    caseDistinctions(linkagesFlat(:,1) == 0 & linkagesFlat(:,2) == 0 & ~idxMatched) = 4;
+    % Choose 5 for attachted to start at ending and to end not at ending
+    caseDistinctions(linkagesFlat(:,1) ~= 0 & linkagesFlat(:,2) == 0 & ~idxMatched) = 5;
+    % Choose 6 for attachted to start not at ending and to end at ending
+    caseDistinctions(linkagesFlat(:,1) == 0 & linkagesFlat(:,2) ~= 0 & ~idxMatched) = 6;
+    % Find indices did not match criteria so far
+    idxMatched = idxMatched | any(linkagesFlat == 0,2);
+    % Choose 7 for self attachment (within ending)
+    caseDistinctions((linkagesFlat(:,1) == linkagesFlat(:,2)) & ~idxMatched) = 7;
+    % Find indices did not match criteria so far
+    idxMatched = idxMatched | (linkagesFlat(:,1) == linkagesFlat(:,2));
+    % Choose 8 for self attachment (within agglo)
+    caseDistinctions((linkagesAgglos(:,1) == linkagesAgglos(:,2)) & ~idxMatched) = 8;
+    % Find indices did not match criteria so far
+    idxMatched = idxMatched | (linkagesAgglos(:,1) == linkagesAgglos(:,2));
+    % Choose 9 for normal attachment
+    caseDistinctions(~idxMatched) = 9;
+
+    % Define exclusion zone near dataset center
+    options.border = [3000; -3000];
+    borderNm = repmat(options.border, 1, 3);
+    borderVoxel = round(bsxfun(@times, 1./param.raw.voxelSize, borderNm));
+    bboxSmall = param.bbox + borderVoxel';
+
+    % Check which flight path passes through exclusion zone of the dataset
+    idxWithin = cellfun(@(x)all(all(bsxfun(@gt, x, bboxSmall(:, 1)'),2) ...
+        & all(bsxfun(@lt, x, bboxSmall(:, 2)'),2),1), flightPaths.ff.nodes);
+
+    % Choose 10 for dangling edge that reaches EODS
+    caseDistinctions(~idxWithin & caseDistinctions' == 3) = 10;
+
+    % Check case 5 & 9 for inconsitencies and give them special case
+    % Move all triangles in case 5 to case 11
+    idx5 = caseDistinctions == 5;
+    uniqueLinkages5 = unique(cat(2, linkagesFlat(idx5,1), linkagesAgglos(idx5,2)), 'rows');
+    uniqueEndings5 = unique(uniqueLinkages5(:,1));
+    count = histc(uniqueLinkages5(:,1), uniqueEndings5);
+    idxFlight5 = any(ismember(linkagesFlat, uniqueEndings5(count > 1)),2);
+    caseDistinctions(idx5 & idxFlight5) = 11;
+
+    % Move all triangles in case 9 to case 12
+    idx9 = caseDistinctions == 9;
+    uniqueLinkages9 = unique(sort(linkagesFlat(caseDistinctions == 9,:),2), 'rows');
+    uniqueEndings9 = unique(uniqueLinkages9(:));
+    count = histc(uniqueLinkages9(:), uniqueEndings9);
+    idxFlight9 = any(ismember(linkagesFlat, uniqueEndings9(count > 1)),2);
+    caseDistinctions(idx9 & idxFlight9) = 12;
+
+    % Output count & frequency of each case
+    tabulate(caseDistinctions)
+
+    % Chose cases that are defined as attachted
+    casesToCountAttached = setdiff(1:12, 3);
+    attachedQueries = linkagesFlat(ismember(caseDistinctions, casesToCountAttached),:);
+    attachedEndings = unique(attachedQueries(:));
+
+    % Save
+    save(fullfile(dataDir, 'attachedEndingsKeepCaseThree.mat'));
+
+end
+
