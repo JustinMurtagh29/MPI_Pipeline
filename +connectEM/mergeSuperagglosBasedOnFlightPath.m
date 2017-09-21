c056b9ed729d6e82a7e6928312ad88e1fdba75e1
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-09-21 17:37:58 +0200

    Add comments to +connectEM/mergeSuperagglosBasedOnFlightPath

--- +connectEM/mergeSuperagglosBasedOnFlightPath.m
+++ +connectEM/mergeSuperagglosBasedOnFlightPath.m
@@ -1,36 +1,57 @@
-function superagglos_new = mergeSuperagglosBasedOnFlightPath(superagglos, eqClassCCfull, startAgglo, endAgglo, ff)
-
+function superagglos_new = mergeSuperagglosBasedOnFlightPath( ...
+        superagglos, eqClassCCfull, startAgglo, endAgglo, ff)
+    % Written by
+    %   Manuel Berning <manuel.berning@brain.mpg.de>
+    %   Christian Schramm <christian.schramm@brain.mpg.de>
+    
+    % find connected component for each flight path
     attachments = cellfun(@(x,y)[x y], startAgglo, endAgglo, 'uni', 0);
     ccLookup = buildLUT(max(cell2mat(eqClassCCfull)), eqClassCCfull);
     attachmentsCC = cellfun(@(x)unique(ccLookup(x)), attachments);
+    
     % Generate new superagglo
     for i=1:length(eqClassCCfull)
         % Concatenate superagglos of this equivalence class
         superagglos_new(i,1).nodes = cat(1, superagglos(eqClassCCfull{i}).nodes);
+        
+        % collect and renumber edges
         nrNodes = cumsum(arrayfun(@(x)size(x.nodes,1), superagglos(eqClassCCfull{i})));
         nodeOffset = cat(1, 0, nrNodes);
         edgeCell = arrayfun(@(x,y)x.edges+y, superagglos(eqClassCCfull{i}), nodeOffset(1:end-1), 'uni', 0);
         superagglos_new(i,1).edges = cat(1, edgeCell{:});
+        
         % Find all queries that link to current superagglo (start or end)
         queryIdx = attachmentsCC == i;
+        
         if any(queryIdx)
-            segIdsThisSuperagglo = superagglos_new(i).nodes(:,4);
-            % Add in flight path of current query that connects to current eqClass
+            
+            % concatenate nodes from flight paths
+            % by definition, flight paths nodes have `nan` as segment ID
             newNodes = cat(1, ff.nodes{queryIdx});
             newNodes(:,4) = NaN(size(newNodes,1),1);
+            
+            % reconstruct flight path edges using MST
             edgeCell = cellfun(@minimalSpanningTree, ff.nodes(queryIdx), 'uni', 0);
+            
+            % collect and renumber edges
             nrNodes = cumsum(cellfun(@(x)size(x,1), ff.nodes(queryIdx)));
             nodeOffset = cat(2, 0, nrNodes);
             newEdges = cellfun(@(x,y)x+y, edgeCell, num2cell(nodeOffset(1:end-1)), 'uni', 0);
             newEdges = cat(1, newEdges{:});
+            
             % Add in edges that connect current query to current superagglo
+            % This means: connect to all segments in neighbourhood
+            segIdsThisSuperagglo = superagglos_new(i).nodes(:,4);
             segIdsHitByNewNodes = cat(2, cat(1, ff.segIds{queryIdx}), cat(1, ff.neighbours{queryIdx}));
             [idxNewNodes, idxOldNodes] = ismember(segIdsHitByNewNodes, segIdsThisSuperagglo);
             newNodesBeingAttached = find(any(idxNewNodes,2));
             flatten = @(x)x(:);
+            
+            % Build segment-to-query edges
             connectingEdges = cat(2, flatten(idxOldNodes(newNodesBeingAttached,:)), repmat(newNodesBeingAttached + size(superagglos_new(i).nodes,1),27,1));
             connectingEdges = unique(connectingEdges, 'rows');
             connectingEdges(any(connectingEdges == 0,2),:) = [];
+            
             % Add to output structure
             superagglos_new(i,1).edges = cat(1, superagglos_new(i).edges, newEdges + size(superagglos_new(i).nodes,1), connectingEdges);
             superagglos_new(i,1).nodes = cat(1, superagglos_new(i).nodes, newNodes);
