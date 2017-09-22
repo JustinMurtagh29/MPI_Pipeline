0a3b7c51d85d0d6f8e60376469a924bc39b3bcfe
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-09-22 11:48:06 +0200

    Add comments and sanity checks to +connectEM/createNewSuperagglos

--- +connectEM/createNewSuperagglos.m
+++ +connectEM/createNewSuperagglos.m
@@ -10,41 +10,93 @@ function createNewSuperagglos(param,state)
     % Load current state of agglomerates
     agglos = load(fullfile(dataDir, strcat('axons_',num2str(axonVersion,'%.2i'),'.mat')));
     superAgglos = agglos.axons(agglos.indBigAxons);
+    
+    % NOTE(amotta): `agglos` contains all super-agglomerates. `superAgglos`
+    % is restricted to the large-enough ones.
 
     % Load linkages and cases for execusion
     load(fullfile(dataDir, strcat('caseDistinctions',suffix,'.mat')),...
         'linkagesAgglos','flightPaths','linkagesFlat');
     load(fullfile(dataDir, strcat('attachedEndings',suffix,'.mat')),'executedFlightPaths');
+    
+    % sanity checks
+    assert(isequal(size(linkagesAgglos), size(linkagesFlat)));
+    assert(size(linkagesAgglos, 1) == numel(flightPaths.startAgglo));
+    assert(size(linkagesAgglos, 1) == numel(flightPaths.endAgglo));
+    assert(size(linkagesAgglos, 1) == numel(flightPaths.ff.segIds));
+    assert(~any(diff(structfun(@numel, flightPaths.ff))));
+    assert(max(executedFlightPaths) < size(linkagesAgglos, 1));
+    
     execusion = false(size(linkagesAgglos,1),1);
     execusion(executedFlightPaths) = true;
+    
     executedFlightPaths = execusion;
+    clear execusion;
+
+    % NOTE(amotta):
+    % Q: `superAgglos` contains only the large-enough axons. Is it also
+    %    true that the agglomerate IDs in `linkagesAgglos` refer to the
+    %    large-enough axons?
+    % A: The `linkagesAgglos` are directly generated from `startAgglo` and
+    %    `endAgglo` of `flightPaths`. It seems like these fields also only
+    %    refer to the large-enough axons.
+    % See:
+    %    https://gitlab.mpcdf.mpg.de/connectomics/pipeline/blob/master/+connectEM/getAggloQueryOverlapB.m#L18
 
     % Find connected components of equivalent classes
+    % NOTE(amotta): Only use flights which attach at both end to
+    % agglomerates. This makes sense.
     edgesCC = linkagesAgglos(executedFlightPaths,:);
-    nans = isnan(edgesCC(:,1)) | isnan(edgesCC(:,2));
-    edgesCC = edgesCC(~nans,:);
+    edgesCC(any(isnan(edgesCC), 2), :) = [];
     edgesCC = sort(edgesCC, 2);
+    
     eqClassCC = Graph.findConnectedComponents(edgesCC, true, true);
-    % Generate new merged classes
     eqClassCCfull = [eqClassCC; num2cell(setdiff(1 : length(superAgglos), cell2mat(eqClassCC)))'];
 
+    % NOTE(amotta): Add flight path ID for tracking
+    flightPaths.id = reshape(1:numel(flightPaths.startAgglo), [], 1);
+    
     % Use only those flight paths for execusion with the chosen case IDs
+    % NOTE(amotta): Dimensions agree!
+    flightPaths.id(~executedFlightPaths) = [];
     flightPaths.startAgglo(~executedFlightPaths,:) = [];
     flightPaths.endAgglo(~executedFlightPaths,:) = [];
     flightPaths.ff = structfun(@(x)x(executedFlightPaths), flightPaths.ff, 'uni', 0);
     linkagesFlat = linkagesFlat(executedFlightPaths);
 
     % Eliminate duplicate flight paths
+    % NOTE(amotta): Flight paths do **not** require to attach to
+    % agglomerates at both ends. This is true for dangling flight queries,
+    % for example.
     [~, positionUniques] = unique(sort(linkagesFlat,2), 'rows');
     duplicates = true(size(linkagesFlat,1),1);
     duplicates(positionUniques) = 0;
+    
+    % NOTE(amotta): Dimensions agree!
+    flightPaths.id(duplicates) = [];
     flightPaths.startAgglo(duplicates,:) = [];
     flightPaths.endAgglo(duplicates,:) = [];
     flightPaths.ff = structfun(@(x)x(~duplicates), flightPaths.ff, 'uni', 0);
+    
+    % NOTE(amotta): At this point each agglomerate should still be a
+    % single connected component. That's what Marcel checked. Can confirm.
+    
+    % sanity check
+    % NOTE(amotta): passes!
+    % assert(all(arrayfun(@(x) ...
+    %     numel(Graph.findConnectedComponents(x.edges)) == 1, ...
+    %     superAgglos(arrayfun(@(x) size(x.nodes, 1), superAgglos) > 1))));
 
     % Merging to super agglos
-    axonsNew = connectEM.mergeSuperagglosBasedOnFlightPath(superAgglos, eqClassCCfull,...
-        flightPaths.startAgglo, flightPaths.endAgglo, flightPaths.ff);
+    axonsNew = connectEM.mergeSuperagglosBasedOnFlightPath( ...
+        superAgglos, eqClassCCfull, flightPaths.startAgglo, ...
+        flightPaths.endAgglo, flightPaths.ff);
+    
+    % sanity check
+    assert(all(arrayfun(@(x) ...
+        numel(Graph.findConnectedComponents(x.edges)) == 1, ...
+        axonsNew(arrayfun(@(x) size(x.nodes, 1), axonsNew) > 1))));
+    
     % Concatenate small axons below 5 um
     axons = cat(1,axonsNew, agglos.axons(~agglos.indBigAxons));
     indBigAxons = false(length(axons),1);
@@ -53,6 +105,5 @@ function createNewSuperagglos(param,state)
     % Save super agglos and deprive writing permission
     saveFile = fullfile(dataDir, strcat('axons_',num2str(axonVersion+1,'%.2i'),'.mat'));
     save(saveFile, 'axons','indBigAxons');
-    system(['chmod -w ' saveFile])
-
+    system(['chmod -w ' saveFile]);
 end
