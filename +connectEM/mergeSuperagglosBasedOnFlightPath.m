8cab02f42ec1c7477bbd767b3260e14e5ab593b7
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-09-22 12:48:08 +0200

    Revert "Simplify +connectEM/mergeSuperagglosBasedOnFlightPath"
    This reverts commit 473f5248a55c2448dbf48b461fe54ed60c6e8254 because,
    yes, it was too slow.

--- +connectEM/mergeSuperagglosBasedOnFlightPath.m
+++ +connectEM/mergeSuperagglosBasedOnFlightPath.m
@@ -1,4 +1,4 @@
-function superagglosNew = mergeSuperagglosBasedOnFlightPath( ...
+function superagglos_new = mergeSuperagglosBasedOnFlightPath( ...
         superagglos, eqClassCCfull, startAgglo, endAgglo, ff)
     % Written by
     %   Manuel Berning <manuel.berning@brain.mpg.de>
@@ -33,80 +33,68 @@ function superagglosNew = mergeSuperagglosBasedOnFlightPath( ...
     ccLookup = Agglo.buildLUT(max(cell2mat(eqClassCCfull)), eqClassCCfull);
     attachmentsCC = cellfun(@(x)unique(ccLookup(x)), attachments);
     
-    % Preallocate output
-    superagglosNew = struct( ...
-        'nodes', cell(numel(eqClassCCfull), 1), ...
-        'edges', cell(numel(eqClassCCfull), 1));
-    
     % Generate new superagglo
-    for i = 1:numel(eqClassCCfull)
+    for i=1:length(eqClassCCfull)
         % Concatenate superagglos of this equivalence class
-        superagglosNew(i).nodes = cat( ...
-            1, superagglos(eqClassCCfull{i}).nodes);
-        segmentIds = superagglosNew(i).nodes(:, 4);
+        superagglos_new(i,1).nodes = cat(1, superagglos(eqClassCCfull{i}).nodes);
         
         % collect and renumber edges
-        nrNodes = cumsum(arrayfun(@(x) ...
-            size(x.nodes, 1), superagglos(eqClassCCfull{i})));
+        nrNodes = cumsum(arrayfun(@(x)size(x.nodes,1), superagglos(eqClassCCfull{i})));
         nodeOffset = cat(1, 0, nrNodes);
-        
-        edgeCell = arrayfun( ...
-            @(x, y) x.edges + y, ...
-            superagglos(eqClassCCfull{i}), nodeOffset(1:end-1), 'uni', 0);
-        superagglosNew(i).edges = cat(1, edgeCell{:});
+        edgeCell = arrayfun(@(x,y)x.edges+y, superagglos(eqClassCCfull{i}), nodeOffset(1:end-1), 'uni', 0);
+        superagglos_new(i,1).edges = cat(1, edgeCell{:});
         
         % Find all queries that link to current superagglo (start or end)
-        queryIds = find(attachmentsCC == i);
+        queryIdx = attachmentsCC == i;
         
-        for curFfIdx = queryIds
-            % build nodes by adding non-segment ID
-            curFfNodes = ff.nodes{curFfIdx};
-            curFfNodes(:, 4) = nan;
+        if any(queryIdx)
+            % concatenate nodes from flight paths
+            % by definition, flight paths nodes have `nan` as segment ID
+            newNodes = cat(1, ff.nodes{queryIdx});
+            newNodes(:,4) = NaN(size(newNodes,1),1);
             
-            curFfEdges = Graph.getMST(bsxfun( ...
-                @times, curFfNodes(:, 1:3), voxelSize));
+            % reconstruct flight path edges using MST
+            edgeCell = cellfun(@(n) ...
+                Graph.getMST(bsxfun(@times, n(:, 1:3), voxelSize)), ...
+                ff.nodes(queryIdx), 'uni', 0);
             
-            % fix node IDs in edge list
-            curNodeCount = size(superagglosNew(i).nodes, 1);
-            curFfEdges = curFfEdges + curNodeCount;
+            % collect and renumber edges
+            nrNodes = cumsum(cellfun(@(x)size(x,1), ff.nodes(queryIdx)));
+            nodeOffset = cat(2, 0, nrNodes);
+            newEdges = cellfun(@(x,y)x+y, edgeCell, num2cell(nodeOffset(1:end-1)), 'uni', 0);
+            newEdges = cat(1, newEdges{:});
             
-            % build edges connecting query to segment-based agglomerates
-            curFfSegIdsHit = cat( ...
-                2, ff.segIds{curFfIdx}, ff.neighbours{curFfIdx});
-           [~, curFfNodeIdsHit] = ismember(curFfSegIdsHit, segmentIds);
+            % Add in edges that connect current query to current superagglo
+            % This means: connect to all segments in neighbourhood
+            segIdsThisSuperagglo = superagglos_new(i).nodes(:,4);
+            segIdsHitByNewNodes = cat(2, cat(1, ff.segIds{queryIdx}), cat(1, ff.neighbours{queryIdx}));
+            [idxNewNodes, idxOldNodes] = ismember(segIdsHitByNewNodes, segIdsThisSuperagglo);
+            newNodesBeingAttached = find(any(idxNewNodes,2));
+            flatten = @(x)x(:);
             
-            curFfConnEdges = 1:size(curFfNodeIdsHit, 1);
-            curFfConnEdges = curFfConnEdges + curNodeCount;
+            % Build segment-to-query edges
+            connectingEdges = cat(2, flatten(idxOldNodes(newNodesBeingAttached,:)), repmat(newNodesBeingAttached + size(superagglos_new(i).nodes,1),27,1));
+            connectingEdges = unique(connectingEdges, 'rows');
+            connectingEdges(any(connectingEdges == 0,2),:) = [];
             
-            curFfConnEdges = repmat(curFfConnEdges(:), 1, 27);
-            curFfConnEdges = cat(2, curFfConnEdges(:), curFfNodeIdsHit(:));
-            
-            curFfConnEdges = curFfConnEdges(all(curFfConnEdges, 2), :);
-            curFfConnEdges = unique(curFfConnEdges, 'rows');
-            
-            % add to output
-            superagglosNew(i).edges = cat( ...
-                1, superagglosNew(i).edges, curFfEdges, curFfConnEdges);
-            superagglosNew(i).nodes = cat( ...
-                1, superagglosNew(i).nodes, curFfNodes);
+            % Add to output structure
+            superagglos_new(i,1).edges = cat(1, superagglos_new(i).edges, newEdges + size(superagglos_new(i).nodes,1), connectingEdges);
+            superagglos_new(i,1).nodes = cat(1, superagglos_new(i).nodes, newNodes);
         end
         
         % sanity checks
         % * edges are correctly sorted
-        assert(all(diff(superagglosNew(i,1).edges, 1, 2) > 0));
+        assert(all(diff(superagglos_new(i,1).edges, 1, 2) > 0));
         
         % * single connected component
-        switch size(superagglosNew(i,1).nodes, 1)
+        switch size(superagglos_new(i,1).nodes, 1)
             case 0
                 assert(false);
             case 1
-                assert(isempty(superagglosNew(i,1).edges));
+                assert(isempty(superagglos_new(i,1).edges));
             otherwise
                 assert(numel(Graph.findConnectedComponents( ...
-                    superagglosNew(i).edges)) == 1);
+                    superagglos_new(i).edges)) == 1);
         end
     end
-    
-    % make it a column vector
-    superagglosNew = reshape(superagglosNew, [], 1);
 end
