06d319342604a8700632aac3ce22eee4515bb8e7
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-09-28 17:45:04 +0200

    Make `newAgglos` a structure column vector

--- +connectEM/splitChiasmataMulti.m
+++ +connectEM/splitChiasmataMulti.m
@@ -218,6 +218,8 @@ function [newAgglos, summary] = ...
         newAgglos(curIdx).endings = curEndings;
     end
     
+    newAgglos = reshape(newAgglos, [], 1);
+    
     % NOTE(amotta): The rest is only executed if `outputFile` is set
     if ~exist('outputFile', 'var') || isempty(outputFile); return; end
     Util.save(strcat(outputFile, '.mat'), agglo, tasks, newAgglos, summary);
