2a1b406d0437853ae59c2a7bdfa86e01dd5c9e5f
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-09-28 11:25:20 +0200

    Start to clean up +connectEM/splitChiasmataMultiSuper

--- +connectEM/splitChiasmataMultiSuper.m
+++ +connectEM/splitChiasmataMultiSuper.m
@@ -7,33 +7,53 @@ temp.axons = temp.axons(temp.indBigAxons);
 queryDir = fullfile(p.saveFolder, 'chiasmata');
 queries = connectEM.generateQueriesFromChiasmata(queryDir, temp);
 
-% get taskdata from Heiko
-fid = fopen('/tmpscratch/kboerg/IDs_MBKMB_L4_chiasma_axon_queries_26_09_2017.txt');
-taskdata = fread(fid);
-fclose(fid);
-taskdata2 = char(taskdata)';
-taskdata3 = strsplit(taskdata2,'\n');
-taskdata4 = taskdata3(2:end);
-taskdata5 = cellfun(@(x){strsplit(x,',')},taskdata4);
+% load task IDs corresponding to queries
+taskIdFile = '/tmpscratch/kboerg/IDs_MBKMB_L4_chiasma_axon_queries_26_09_2017.txt';
+taskIds = loadTaskIds(taskIdFile);
+
+% sanity check
+assert(size(queries, 1) == numel(taskIds));
+
 % get FF structure
 [ff.segIds, ff.neighbours, ff.filenames, ff.nodes, ff.startNode, ff.comments] = connectEM.lookupNmlMulti(p, {'/tmpscratch/kboerg/L4_chiasma_axon_queries_26_09_2017_nmls/'}, false);
 second = @(x)x(2);
 ff.filenamesShort = cellfun(@(x)second(strsplit(x,'__')), ff.filenames);
 
-for idx = 1 : length(temp.axons)
-    idx
-    temp.axons(idx).nodesScaled = bsxfun(@times, temp.axons(idx).nodes(:, 1 : 3), [11.24,11.24,28]);
-    % find idx in queries for tasks
-    taskids = find(queries(:,1)==idx);
-    if isempty(taskids)
-        continue
-    end
-    % find taskids in wK for tasks
-    taskids2 = arrayfun(@(x){taskdata5{x}{1}},taskids);
-    % find idxs in ff for tasks
-    taskids3 = cellfun(@(x)find(strcmp(ff.filenamesShort,x)),taskids2);
+for curAxonIdx = 1 : length(temp.axons)
+    curAxonIdx
+    
+    % find all queries in current axon
+    curQueryIds = find(queries(:, 1) == curAxonIdx);
+    if isempty(curQueryIds); continue; end
+    
+    % find task IDs and ff indices
+    curTaskIds = taskIds(curQueryIds);
+    curFfIds = cellfun(@(x) find(strcmp(ff.filenamesShort, x)), curTaskIds);
+    
     % create task defintions
-    tasks = arrayfun(@(x,y)struct('tracings',struct('segids',ff.segIds(x),'nodes',ff.nodes(x)),'centeridx',queries(y,7)),taskids3,taskids);
+    tasks = arrayfun(@(x,y)struct('tracings',struct('segids',ff.segIds(x),'nodes',ff.nodes(x)),'centeridx',queries(y,7)),curFfIds,curQueryIds);
+    
     % run splitting
-    connectEM.splitChiasmataMulti(p,temp.axons(idx),tasks,['/tmpscratch/kboerg/chiasmaSplit26/' num2str(idx) '.mat'])
+    temp.axons(curAxonIdx).nodesScaled = bsxfun( ...
+        @times, temp.axons(curAxonIdx).nodes(:, 1:3), [11.24, 11.24, 28]);
+    connectEM.splitChiasmataMulti( ...
+        p,temp.axons(curAxonIdx),tasks,['/tmpscratch/kboerg/chiasmaSplit26/' num2str(curAxonIdx) '.mat'])
+end
+
+function taskIds = loadTaskIds(taskFile)
+    fid = fopen(taskFile, 'rt');
+    data = fread(fid, 'char=>char');
+    fclose(fid);
+    
+    % split into lines
+    data = reshape(data, 1, []);
+    data = strsplit(data, '\n');
+    
+    % remove header
+    data(1) = [];
+    
+    % extract first column with task IDs
+    taskIds = cellfun( ...
+        @(x) x(1:(find(x == ',', 1) - 1)), ...
+        data(:), 'UniformOutput', false);
 end
