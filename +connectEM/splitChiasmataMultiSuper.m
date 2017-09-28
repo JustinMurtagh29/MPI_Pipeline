d4ce95212e6d4056c89e26cda4099c9965b41252
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-09-28 18:40:44 +0200

    Remove old code and fix bug in `clearvars`

--- +connectEM/splitChiasmataMultiSuper.m
+++ +connectEM/splitChiasmataMultiSuper.m
@@ -8,31 +8,7 @@ function splitChiasmataMultiSuper(p)
     
     agglos = oldAgglos;
     agglos.axons = agglos.axons(agglos.indBigAxons);
-
-    %{
-    % get query data, i.e. agglo idx, center idx
-    queryDir = fullfile(p.saveFolder, 'chiasmata');
-    queries = connectEM.generateQueriesFromChiasmata(queryDir, temp);
-
-    % load task IDs corresponding to queries
-    taskIdFile = '/tmpscratch/kboerg/IDs_MBKMB_L4_chiasma_axon_queries_26_09_2017.txt';
-    taskIds = loadTaskIds(taskIdFile);
-
-    % sanity check
-    assert(size(queries, 1) == numel(taskIds));
-
-    % get FF structure
-    nmlDirs = {'/tmpscratch/kboerg/L4_chiasma_axon_queries_26_09_2017_nmls/'};
-    [ff.segIds, ff.neighbours, ff.filenames, ff.nodes, ff.startNode, ff.comments] = connectEM.lookupNmlMulti(p, nmlDirs, false);
-    second = @(x)x(2);
-    ff.filenamesShort = cellfun(@(x)second(strsplit(x,'__')), ff.filenames);
-
-    outputDir = '/tmpscratch/kboerg/chiasmaSplit26';
-    %}
     
-    %% NOTE(amotta): Run this code to
-    clearvars -except p agglos;
-
     curDir = fullfile( ...
         p.saveFolder, 'chiasmataSplitting', ...
         '20170928T132211-projects-a-and-b');
