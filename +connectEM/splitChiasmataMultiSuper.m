241668f2351ff883dd12c7f7d206c7f7d7c9e2f6
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-09-28 16:29:57 +0200

    Make +connectEM/splitChiasmataMultiSuper a function

--- +connectEM/splitChiasmataMultiSuper.m
+++ +connectEM/splitChiasmataMultiSuper.m
@@ -1,92 +1,96 @@
-load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameter.mat', 'p');
-
-temp=load(fullfile(p.saveFolder,'aggloState/axons_06_b.mat'));
-temp.axons = temp.axons(temp.indBigAxons);
-
-%{
-% get query data, i.e. agglo idx, center idx
-queryDir = fullfile(p.saveFolder, 'chiasmata');
-queries = connectEM.generateQueriesFromChiasmata(queryDir, temp);
-
-% load task IDs corresponding to queries
-taskIdFile = '/tmpscratch/kboerg/IDs_MBKMB_L4_chiasma_axon_queries_26_09_2017.txt';
-taskIds = loadTaskIds(taskIdFile);
-
-% sanity check
-assert(size(queries, 1) == numel(taskIds));
-
-% get FF structure
-nmlDirs = {'/tmpscratch/kboerg/L4_chiasma_axon_queries_26_09_2017_nmls/'};
-[ff.segIds, ff.neighbours, ff.filenames, ff.nodes, ff.startNode, ff.comments] = connectEM.lookupNmlMulti(p, nmlDirs, false);
-second = @(x)x(2);
-ff.filenamesShort = cellfun(@(x)second(strsplit(x,'__')), ff.filenames);
-
-outputDir = '/tmpscratch/kboerg/chiasmaSplit26';
-%}
-%% NOTE(amotta): Run this code to
-
-clearvars -except p temp;
-
-curDir = fullfile( ...
-    p.saveFolder, 'chiasmataSplitting', ...
-    '20170928T132211-projects-a-and-b');
-outputDir = fullfile(curDir, 'outputs');
-
-load(fullfile(curDir, 'input-data.mat'));
-clear curDir;
-
-%%
-
-% NOTE(amotta): Sort queries to make processing easier
-[~, sortedRows] = sortrows(queries(:, 1:3));
-queries = queries(sortedRows, :);
-taskIds = taskIds(sortedRows);
-clear sortedRows;
-
-% NOTE(amotta): Fix dimensions of `ff` fields
-ff = structfun(@(x) x(:), ff, 'UniformOutput', false);
-
-axonCount = numel(temp.axons);
-inputArgs = cell(axonCount, 1);
-sharedInputArgs = {p};
-
-for curAxonIdx = 1:axonCount
-    % find all queries in current axon
-    curQueryIds = find(queries(:, 1) == curAxonIdx);
-    curQueries = queries(curQueryIds, :);
-    assert(issorted(curQueries(:, 1:3), 'rows'));
+function splitChiasmataMultiSuper(p)
+    % Written by
+    %   Kevin Boergens <kevin.boergens@brain.mpg.de>
+    %   Alessandro Motta <alessandro.motta@brain.mpg.de>
     
-    % find task IDs and ff indices
-    curTaskIds = taskIds(curQueryIds);
-    curFfIds = cellfun(@(x) find(strcmp(ff.filenamesShort, x)), curTaskIds);
+    agglos = load(fullfile(p.saveFolder,'aggloState/axons_06_b.mat'));
+    agglos.axons = agglos.axons(agglos.indBigAxons);
+
+    %{
+    % get query data, i.e. agglo idx, center idx
+    queryDir = fullfile(p.saveFolder, 'chiasmata');
+    queries = connectEM.generateQueriesFromChiasmata(queryDir, temp);
+
+    % load task IDs corresponding to queries
+    taskIdFile = '/tmpscratch/kboerg/IDs_MBKMB_L4_chiasma_axon_queries_26_09_2017.txt';
+    taskIds = loadTaskIds(taskIdFile);
+
+    % sanity check
+    assert(size(queries, 1) == numel(taskIds));
+
+    % get FF structure
+    nmlDirs = {'/tmpscratch/kboerg/L4_chiasma_axon_queries_26_09_2017_nmls/'};
+    [ff.segIds, ff.neighbours, ff.filenames, ff.nodes, ff.startNode, ff.comments] = connectEM.lookupNmlMulti(p, nmlDirs, false);
+    second = @(x)x(2);
+    ff.filenamesShort = cellfun(@(x)second(strsplit(x,'__')), ff.filenames);
+
+    outputDir = '/tmpscratch/kboerg/chiasmaSplit26';
+    %}
     
-    assert(numel(curFfIds) == size(curQueries, 1));
-    curChiasmaCount = max(cat(1, 0, curQueries(:, 2)));
-    
-    % group flights by chiasma
-    curFfGroups = arrayfun( ...
-        @(idx) curFfIds(curQueries(:, 2) == idx), ...
-        (1:curChiasmaCount)', 'UniformOutput', false);
-    curCenterIds = unique(curQueries(:, 7), 'stable');
-    
-    % create task defintions
-    tasks = struct( ...
-        'tracings', cellfun(@(ffIds) struct( ...
-            'segids', ff.segIds(ffIds), ...
-            'nodes', ff.nodes(ffIds)), ...
-            curFfGroups, 'UniformOutput', false), ...
-        'centeridx', num2cell(curCenterIds));
-    
-    temp.axons(curAxonIdx).nodesScaled = bsxfun( ...
-        @times, temp.axons(curAxonIdx).nodes(:, 1:3), p.raw.voxelSize);
-    inputArgs{curAxonIdx} = {temp.axons(curAxonIdx), tasks};
+    %% NOTE(amotta): Run this code to
+    clearvars -except p temp;
+
+    curDir = fullfile( ...
+        p.saveFolder, 'chiasmataSplitting', ...
+        '20170928T132211-projects-a-and-b');
+    outputDir = fullfile(curDir, 'outputs');
+
+    load(fullfile(curDir, 'input-data.mat'));
+    clear curDir;
+
+    %%
+    % NOTE(amotta): Sort queries to make processing easier
+    [~, sortedRows] = sortrows(queries(:, 1:3));
+    queries = queries(sortedRows, :);
+    taskIds = taskIds(sortedRows);
+    clear sortedRows;
+
+    % NOTE(amotta): Fix dimensions of `ff` fields
+    ff = structfun(@(x) x(:), ff, 'UniformOutput', false);
+
+    axonCount = numel(agglos.axons);
+    inputArgs = cell(axonCount, 1);
+    sharedInputArgs = {p};
+
+    for curAxonIdx = 1:axonCount
+        % find all queries in current axon
+        curQueryIds = find(queries(:, 1) == curAxonIdx);
+        curQueries = queries(curQueryIds, :);
+        assert(issorted(curQueries(:, 1:3), 'rows'));
+
+        % find task IDs and ff indices
+        curTaskIds = taskIds(curQueryIds);
+        curFfIds = cellfun(@(x) ...
+            find(strcmp(ff.filenamesShort, x)), curTaskIds);
+
+        assert(numel(curFfIds) == size(curQueries, 1));
+        curChiasmaCount = max(cat(1, 0, curQueries(:, 2)));
+
+        % group flights by chiasma
+        curFfGroups = arrayfun( ...
+            @(idx) curFfIds(curQueries(:, 2) == idx), ...
+            (1:curChiasmaCount)', 'UniformOutput', false);
+        curCenterIds = unique(curQueries(:, 7), 'stable');
+
+        % create task defintions
+        tasks = struct( ...
+            'tracings', cellfun(@(ffIds) struct( ...
+                'segids', ff.segIds(ffIds), ...
+                'nodes', ff.nodes(ffIds)), ...
+                curFfGroups, 'UniformOutput', false), ...
+            'centeridx', num2cell(curCenterIds));
+
+        agglos.axons(curAxonIdx).nodesScaled = bsxfun(@times, ...
+            agglos.axons(curAxonIdx).nodes(:, 1:3), p.raw.voxelSize);
+        inputArgs{curAxonIdx} = {agglos.axons(curAxonIdx), tasks};
+    end
+
+    job = Cluster.startJob( ...
+        @connectEM.splitChiasmataMulti, inputArgs, ...
+        'sharedInputs', sharedInputArgs, ...
+        'taskGroupSize', ceil(axonCount / 500), ...
+        'numOutputs', 2);
 end
-
-job = Cluster.startJob( ...
-    @connectEM.splitChiasmataMulti, inputArgs, ...
-    'sharedInputs', sharedInputArgs, ...
-    'taskGroupSize', ceil(axonCount / 500), ...
-    'numOutputs', 2);
 
 %%
 function taskIds = loadTaskIds(taskFile)
