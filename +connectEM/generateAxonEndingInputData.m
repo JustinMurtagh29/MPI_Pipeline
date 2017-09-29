24d44a9046dba8af4c9b2443ba8700825d31ea7d
Marcel Beining <marcel.beining@brain.mpg.de>
2017-09-29 18:57:31 +0200

    added optional definition of agglo input and output file name

--- +connectEM/generateAxonEndingInputData.m
+++ +connectEM/generateAxonEndingInputData.m
@@ -1,4 +1,4 @@
-function generateAxonEndingInputData(param)
+function generateAxonEndingInputData(param,stateFile,suffix)
     % generateAxonEndingInputData(param)
     %   Generates a `endingInputData.mat` file in the pipeline directory
     %   which contains all the data necessary for the ending detection.
@@ -10,18 +10,24 @@ function generateAxonEndingInputData(param)
     % Parameters
     minAxonLength = 5000; % in nm
     bboxDist = 1000; % in nm
-
+    if ~exist('stateFile','var')
+        stateFile = 'axons_04.mat';
+    end
+    if ~exist('suffix','var')
+        suffix = '';
+    end
     % Directory with input / output data
     dataDir = fullfile(param.saveFolder, 'aggloState');
-    intermediateFile = fullfile(dataDir, 'axonDirectionality.mat');
-    outFile = fullfile(dataDir, 'axonEndingInputData.mat');
+    intermediateFile = fullfile(dataDir, sprintf('axonDirectionality%s.mat',suffix));
+    outFile = fullfile(dataDir, sprintf('axonEndingInputData%s.mat',suffix));
 
     % Load data
    [graph, segmentMeta, borderMeta, globalSegmentPCA] = ...
        connectEM.loadAllSegmentationData(param);
 
-    % Load state of axon agglomeration
-    axons = load(fullfile(dataDir, 'axons_04.mat'));
+    % Load state of axon agglomeration and load big indices as loaded by Kevin
+    axons = load(fullfile(dataDir, stateFile));
+    axonIds = find(axons.indBigAxons);
     axons = axons.axons;
 
     % Convert to old-school agglomerates
@@ -36,10 +42,6 @@ function generateAxonEndingInputData(param)
     directionality = connectEM.calculateDirectionalityOfAgglomerates( ...
         axonAgglos, graph, segmentMeta, borderMeta, globalSegmentPCA, options);
     save(intermediateFile, 'directionality', '-v7.3');
-
-    % Load big indices as loaded by Kevin
-    m = load(fullfile(dataDir, 'axons_04.mat'), 'indBigAxons');
-    axonIds = find(m.indBigAxons);
 
     % Save results
     out = struct;
