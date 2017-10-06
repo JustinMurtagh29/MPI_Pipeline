b2892fc084cc2c8db21ea1230d53104ea1035a6d
Benedikt Staffler <benedikt.staffler@brain.mpg.de>
2017-10-06 18:20:22 +0200

    store results for now

--- +connectEM/+eval/+Scripts/newAxonGTEval.m
+++ +connectEM/+eval/+Scripts/newAxonGTEval.m
@@ -1,6 +1,8 @@
 % script to run the evaluation on the new_axon_gt axons
 % Author: Benedikt Staffler <benedikt.staffler@brain.mpg.de>
 
+info = Util.runInfo();
+
 %% get the overlaps between gt skeletons and agglos
 
 p = Gaba.getSegParameters('ex145_ROI2017');
@@ -12,7 +14,20 @@ m = load(p.agglo.axonAggloFile);
 agglos = m.axons(m.indBigAxons);
 ov = connectEM.eval.getNewAxonGTAggloOverlap(segIds, agglos);
 
+outFile = fullfile(p.agglo.saveFolder, 'eval', ...
+    sprintf('axon_gt_eval_%s.mat', datestr(clock, now)));
+
+if ~exist(outFile, 'file')
+    save(outFile, 'info', 'ov', 'skels', 'agglos');
+end
+
 %% everything to nml
 
 ovSkels1 = connectEM.eval.newAxonGTOverlapsToSkel(skels, agglos, ov);
 ovSkels2 = connectEM.eval.newAxonGTOverlapsToSkel(skels, agglos, ov, 2);
+
+% % write to tracings
+% for i = 1:10
+%     ovSkels1{i}.write(sprintf('Axon%02d_ov1.nml', i));
+%     ovSkels2{i}.write(sprintf('Axon%02d_ov2.nml', i));
+% end
