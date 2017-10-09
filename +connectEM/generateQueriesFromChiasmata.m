8134b503c1df673d05519685f2082b88835899ac
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-10-09 15:29:41 +0200

    Clean up +connectEM/generateQueriesFromChiasmata

--- +connectEM/generateQueriesFromChiasmata.m
+++ +connectEM/generateQueriesFromChiasmata.m
@@ -1,18 +1,27 @@
 function output = generateQueriesFromChiasmata( ...
         aggloFile, chiasmataId, outputFolder)
+    % Written by
+    %   Kevin Boergens <kevin.boergens@brain.mpg.de>
+    %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    
+    % TODO(amotta): Use parameter structure
+    voxelSize = [11.24, 11.24, 28];
+    curDateStr = datestr(clock, 30);
+    
     % load axon agglomerates
     agglos = load(aggloFile, 'axons', 'indBigAxons');
     
-    curDateStr = datestr(clock, 30);
-    fid = sprintf('%s_flightTasks.txt', curDateStr);
-    fid = fopen(fullfile(outputFolder, fid), 'w');
-    
     output = [];
-    for agglo_idx = find(agglos.indBigAxons)'
+    taskDef = table;
+    taskDef.position = zeros(0, 3);
+    taskDef.rotation = zeros(0, 3);
+    taskDef.bbox = zeros(0, 6);
+    
+    for aggloIdx = find(agglos.indBigAxons)'
         temp = load(fullfile( ...
             '/tmpscratch/kboerg/chiasmata', ...
-            sprintf('chiasmataX%s_%d', chiasmataId, floor(agglo_idx / 100)), ...
-            sprintf('visX%s_%d', chiasmataId, agglo_idx), ...
+            sprintf('chiasmataX%s_%d', chiasmataId, floor(aggloIdx / 100)), ...
+            sprintf('visX%s_%d', chiasmataId, aggloIdx), ...
             'result.mat'));
         
         for i = 1:numel(temp.output.position)
@@ -30,27 +39,30 @@ function output = generateQueriesFromChiasmata( ...
             end
             
             for j = 1:curNrQueries
-                extend = round(4000 ./[11.24,11.24,28]);
-                dd = sqrt(sum((temp.output.direction{i}(j,:) .* [11.24,11.24,28]).^2));
-                if dd>2000
-                    extend = round(2*dd ./[11.24,11.24,28]);
+                extend = round(4000 ./ voxelSize);
+                dd = sqrt(sum((voxelSize ...
+                    .* temp.output.direction{i}(j,:)) .^ 2));
+                if dd > 2000
+                    extend = round(2 * dd ./ voxelSize);
                 end
-                [phi, theta, psi] = calculateEulerAngles(-temp.output.direction{i}(j,:), [11.24,11.24,28]);
-                q.angles{i}(j,:) = [phi theta psi];
-                minPos = temp.output.position{i}(j,:) - extend;
+                
+                % determine size of bounding box
+                minPos = temp.output.position{i}(j, :) - extend;
                 sizeBbox = 2*extend;
-                taskString = ['2012-09-28_ex145_07x2_ROI2017,56d6a7c6140000d81030701e,queriesMHchiasma,1,' ...
-                    num2str(temp.output.position{i}(j,1)-1) ',' num2str(temp.output.position{i}(j,2)-1) ',' num2str(temp.output.position{i}(j,3)-1) ',' ...
-                    num2str(phi) ',' num2str(theta) ',' num2str(psi) ',1,Connectomics department,' ...
-                    num2str(minPos(1)) ',' num2str(minPos(2)) ',' num2str(minPos(3)) ',' ...
-                    num2str(sizeBbox(1)) ',' num2str(sizeBbox(2)) ',' num2str(sizeBbox(3)) ',' 'queriesMHchiasma'];
-                fprintf(fid, '%s\n', taskString);
-                output(end+1,:) = [agglo_idx, i, j, temp.output.position{i}(j,:), temp.output.ccCenterIdx(i)];
+                
+                % determine Euler angles
+                [phi, theta, psi] = calculateEulerAngles( ...
+                    -temp.output.direction{i}(j, :), voxelSize);
+                output(end+1, :) = [ ...
+                    aggloIdx, i, j, ...
+                    temp.output.position{i}(j, :), ...
+                    temp.output.ccCenterIdx(i)]; %#ok
+                taskDef(end + 1, {'position', 'rotation', 'bbox'}) = { ...
+                    temp.output.position{i}(j, :) - 1, ...
+                   [phi, theta, psi], [minPos, sizeBbox]}; %#ok
             end
         end
     end
-    
-    fclose(fid);
     
     % NOTE(amotta): Store `output` on disk so that we can avoid running all
     % of the above stuff in the future (e.g., when splitting chiasmata).
@@ -62,8 +74,10 @@ function output = generateQueriesFromChiasmata( ...
     out.gitInfo = Util.gitInfo();
     
     saveFile = sprintf('%s_data.mat', curDateStr);
-    saveFile = fullfile(outputFolder, saveFile);
-    Util.saveStruct(saveFile, out);
+    Util.saveStruct(fullfile(outputFolder, saveFile), out);
+    
+    taskDefFile = sprintf('%s_flightTasks.txt', curDateStr);
+    writeTaskDefinition(taskDef, fullfile(outputFolder, taskDefFile));
 end
 
 function [phi, thetha, psi] = calculateEulerAngles(di, voxelSize)
@@ -77,5 +91,24 @@ function [phi, thetha, psi] = calculateEulerAngles(di, voxelSize)
     phi = round(eulerAngles(1));
     thetha = round(eulerAngles(2));
     psi = round(eulerAngles(3));
+end
 
+function writeTaskDefinition(taskDef, outFile)
+    fullTaskDef = taskDef;
+    fullTaskDef.dataSet(:) = {'2012-09-28_ex145_07x2_ROI2017'};
+    fullTaskDef.taskTypeId(:) = {'56d6a7c6140000d81030701e'};
+    fullTaskDef.expDomain(:) = {'queriesMHchiasma'};
+    fullTaskDef.expMinVal(:) = 1;
+    fullTaskDef.instances(:) = 1;
+    fullTaskDef.team(:) = {'Connectomics department'};
+    fullTaskDef.project(:) = {'queriesMHchiasma'};
+    
+    % reorder-columns
+    columnOrder = { ...
+        'dataSet', 'taskTypeId', 'expDomain', 'expMinVal', ...
+        'position', 'rotation', 'instances', 'team', 'bbox', 'project'};
+    fullTaskDef = fullTaskDef(:, columnOrder);
+    
+    % write CSV file
+    writetable(fullTaskDef, outFile, 'WriteVariableNames', false);
 end
