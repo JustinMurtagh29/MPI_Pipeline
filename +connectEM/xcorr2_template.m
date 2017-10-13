b3f362ccfd9e36b41ec979cbe9f68eba72c4ebbf
Manuel Berning <manuel.berning@brain.mpg.de>
2017-10-13 19:12:40 +0200

    updates for determining image alignment issues based on normalized cross correlation

---
+++ +connectEM/xcorr2_template.m
@@ -0,0 +1,26 @@
+function [x_shiftA, y_shiftA, x_shiftB, y_shiftB] = xcorr2_template(A, B)
+    % Normalized cross-correlation for 2 dimensional arrays
+    % A and B should habe uneven number of elements in each dim and be square
+
+    templateHalfSize = 20;
+
+    % Do template matching in both directions to check for symetry
+    midA = (size(A,1)-1)/2+1;
+    midB = (size(B,1)-1)/2+1;
+    % 11x11 templates centered in the large image
+    templateA = A(midA-templateHalfSize:midA+templateHalfSize,midA-templateHalfSize:midA+templateHalfSize);
+    templateB = B(midB-templateHalfSize:midB+templateHalfSize,midB-templateHalfSize:midB+templateHalfSize);
+    xcorrA = normxcorr2(templateB, A);
+    xcorrB = normxcorr2(templateA, B);
+
+    [x_max, y_max] = find(xcorrA == max(xcorrA(:)));
+    x_shiftA = x_max-midA-templateHalfSize;
+    y_shiftA = y_max-midA-templateHalfSize;
+
+    [x_max, y_max] = find(xcorrB == max(xcorrB(:)));
+    % Minus to have shifts in the same direction
+    x_shiftB = -(x_max-midB-templateHalfSize);
+    y_shiftB = -(y_max-midB-templateHalfSize);
+
+end
+
