88f07c19f7b79e61675d42654c33baaa0e254cf1
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-10-19 15:34:04 +0200

    Remove excess function argument

--- +connectEM/exportAggloOverlapToAmira.m
+++ +connectEM/exportAggloOverlapToAmira.m
@@ -13,8 +13,7 @@ function exportAggloOverlapToAmira( ...
         cellfun(@numel, skelToAgglos));
     allAgglos = agglos(cell2mat(skelToAgglos));
     
-    isoSurfs = buildIsoSurfaces( ...
-        param, allAgglos, outDir, varargin{:});
+    isoSurfs = buildIsoSurfaces(param, allAgglos, varargin{:});
     
     % write PLY files
     for curCellIdx = 1:max(cellIds)
