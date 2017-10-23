68f306028e095ff20b9b6dd0464c3946cf6eed83
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-10-23 11:37:59 +0200

    Use `MBKMB_L4_chiasma_axon_queries_21_10_2017_requery_round_2` results

--- +connectEM/splitChiasmataMultiSuper.m
+++ +connectEM/splitChiasmataMultiSuper.m
@@ -19,7 +19,7 @@ outputDir = fullfile(curDir, 'outputs');
 % List with input files **in decreasing order of dominance**. Add new
 % requery rounds to the top of this list.
 dataFiles = { ...
-    'requeries/20171020T132537_input-data.mat', ...
+    'requeries/20171023T102000_input-data.mat', ...
     '20171018T104038_input-data.mat'};
 dataFiles = fullfile(curDir, dataFiles);
 clear curDir;
@@ -33,25 +33,16 @@ for curFileIdx = 1:numel(dataFiles)
     curDataFile = dataFiles{curFileIdx};
     curData = load(curDataFile);
     
-    if istable(curData.queries)
-        % requeries
-        curQueries = curData.queries(:, { ...
-            'axonId', 'chiasmaId', 'exitId', ...
-            'exitNodeId', 'seedPos', 'centerNodeId'});
-        curQueries.taskId = curData.taskIds;
-        curFlights = curData.ff;
-    else
-        % original queries
-        curQueries = table;
-        curQueries.axonId = curData.queries(:, 1);
-        curQueries.chiasmaId = curData.queries(:, 2);
-        curQueries.exitId = curData.queries(:, 3);
-        curQueries.exitNodeId = curData.queries(:, 8);
-        curQueries.seedPos = curData.queries(:, 4:6);
-        curQueries.centerNodeId = curData.queries(:, 7);
-        curQueries.taskId = curData.taskIds;
-        curFlights = curData.ff;
-    end
+    % original queries
+    curQueries = table;
+    curQueries.axonId = curData.queries(:, 1);
+    curQueries.chiasmaId = curData.queries(:, 2);
+    curQueries.exitId = curData.queries(:, 3);
+    curQueries.exitNodeId = curData.queries(:, 8);
+    curQueries.seedPos = curData.queries(:, 4:6);
+    curQueries.centerNodeId = curData.queries(:, 7);
+    curQueries.taskId = curData.taskIds;
+    curFlights = curData.ff;
     
     queries{curFileIdx} = curQueries;
     flights{curFileIdx} = curFlights;
