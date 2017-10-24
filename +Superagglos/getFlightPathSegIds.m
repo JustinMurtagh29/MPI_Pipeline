2b6dd896d5da8b0e4ae40ba84955760d1b451614
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-10-24 11:19:32 +0200

    Add `Superagglos.getFlightPathSegIds`. Not very RAM efficient...

---
+++ +Superagglos/getFlightPathSegIds.m
@@ -0,0 +1,75 @@
+function flights = getFlightPathSegIds(param, sagglos, nhood)
+    % Written by
+    %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    
+    if ~exist('nhood', 'var') || isempty(nhood)
+        nhood = 3;
+    else
+        assert(mod(nhood, 2) == 1);
+    end
+    
+    %% reconstruct flights
+    aggloCount = numel(sagglos);
+    flights = cell(aggloCount, 1);
+    flights(:) = {zeros(0, 6)};
+
+    for curIdx = 1:aggloCount
+        curSagglos = sagglos(curIdx);
+
+        % work around empty edges
+        curSagglos.edges = reshape(curSagglos.edges, [], 2);
+
+        % extract flight nodes and edges
+        curNodeIds = find(isnan(curSagglos.nodes(:, 4)));
+       [~, curEdges] = ismember(curSagglos.edges, curNodeIds);
+        curEdges = curEdges(all(curEdges, 2), :);
+        curEdges = sort(curEdges, 2);
+
+        % group into paths
+        curAdj = sparse( ...
+            curEdges(:, 2), curEdges(:, 1), ...
+            true, numel(curNodeIds), numel(curNodeIds));
+       [~, curLUT] = graphconncomp(curAdj, 'Directed', false);
+
+        flights{curIdx} = horzcat( ...
+           repelem(curIdx, numel(curNodeIds), 1), curNodeIds(:), ...
+           curLUT(:), curSagglos.nodes(curNodeIds, 1:3));
+    end
+
+    flights = cell2mat(flights);
+    flights = transpose(flights);
+
+    %% add 26-neighbours
+    if nhood > 1
+        nhoodVec = (nhood - 1) / 2;
+        nhoodVec = (-nhoodVec):nhoodVec;
+        
+        posOff = cell(3, 1);
+       [posOff{:}] = ndgrid(nhoodVec, nhoodVec, nhoodVec);
+       
+        posOff = cat(4, posOff{:});
+        posOff = reshape(posOff, [], 3);
+        posOff = transpose(posOff);
+
+        posOff = cat(1, zeros( ...
+            size(flights, 1) - size(posOff, 1), ...
+            size(posOff, 2)), posOff);
+        
+        flights = reshape(flights, 6, 1, []);
+        flights = bsxfun(@plus, flights, posOff);
+        flights = reshape(flights, 6, []);
+        flights = transpose(flights);
+    end
+    
+    %% convert into table
+    flights = array2table( ...
+        flights, 'VariableNames', ...
+       {'axonId', 'nodeId', 'flightId', 'posX', 'posY', 'posZ'});
+    flights.coord = table2array( ...
+        flights(:, {'posX', 'posY', 'posZ'}));
+    flights(:, {'posX', 'posY', 'posZ'}) = [];
+
+    %% look up segment IDs
+    flights.segId = Seg.Global.getSegIds( ...
+        param, flights.coord, [1024, 1024, 1024]);
+end
