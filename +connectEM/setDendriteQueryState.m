ec434899c5fcdf0fe9e0f65bd33aa7816d9d3c5d
ChristianS <christian.schramm@brain.mgp.de>
2017-10-30 10:14:06 +0100

    changes on dendrite query analysis

--- +connectEM/setDendriteQueryState.m
+++ +connectEM/setDendriteQueryState.m
@@ -14,22 +14,22 @@ function [skeletonFolders, versionSuffix, dendriteVersion,...
     % First 'clean' run
     if strcmp(state,'1.0') 
         % source folders for flight paths
-        scratchFolder = '/u/mberning/results/pipeline/20170217_ROI/aggloState/dendQueryAnswers/';
-        skeletonFolders = {'dend_queries_18_10_2017'};
+        scratchFolder = '/u/scchr/aggloState/dendQueryAnswers/';
+        skeletonFolders = {'L4_dendrite_queries_27_10_2017'};
         skeletonFolders = cellfun(@(x)[scratchFolder x filesep], skeletonFolders, 'uni', 0);
         % filename additionals for getAggloQueryOverlapB and flightEndingOverlapRun
         versionSuffix = '1.0';
-        dendriteVersion = '03_v2';%'04';
+        dendriteVersion = 'flight_01';
        
     % Commented queries of first clean run    
     elseif strcmp(state,'1.1')
         % source folders for flight paths
-        scratchFolder = '/u/mberning/results/pipeline/20170217_ROI/aggloState/dendriteQueryAnswers/';
-        skeletonFolders = {'L4_dendrite_queries_18_10_2017'};
+        scratchFolder = '/u/scchr/aggloState/dendQueryAnswers/';
+        skeletonFolders = {'L4_dendrite_queries_27_10_2017'};
         skeletonFolders = cellfun(@(x)[scratchFolder x filesep], skeletonFolders, 'uni', 0);
         % filename additionals for getAggloQueryOverlapB and flightEndingOverlapRun
         versionSuffix = '_1.1';
-        dendriteVersion = '04';
+        dendriteVersion = 'flight_01';
           
 
         
