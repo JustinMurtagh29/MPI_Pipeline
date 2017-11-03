905aae5b52518f837de73e6ffbf1215a5dc5a7a2
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-03 11:30:54 +0100

    Document and streamline +connectEM/+border/borderCalc

--- +connectEM/+borders/borderCalc.m
+++ +connectEM/+borders/borderCalc.m
@@ -1,42 +1,84 @@
 function borderCalc(idx)
-load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameterWithSynapses.mat');
-%load('/gaba/u/mberning/results/pipeline/20170217_ROI/aggloState/axons_08_a.mat');
+blockSize = [32, 32, 16];
+
+% TODO(amotta): Pass in as shared parameters (or at least paths to file)
+load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameterWithSynapses.mat', 'p');
 load('/gaba/u/mberning/results/pipeline/20170217_ROI/connectomeState/connectome.mat','axons', 'dendrites');
 
-seg = readKnossosRoi('/gaba/u/mberning/results/pipeline/20170217_ROI/globalSeg/','2012-09-28_ex145_07x2_ROI2016_corrected_mag1', p.local(idx).bboxSmall, 'uint32');
+maxSegId = Seg.Global.getMaxSegId(p);
+axonLookup = Agglo.buildLUT(maxSegId, axons);
+dendriteLookup = Agglo.buildLUT(maxSegId, dendrites);
 
-dendriteIDs = cat(1,dendrites{:});
-dendriteLookup(dendriteIDs)=repelem(1:length(dendrites),cellfun(@length, dendrites));
+% TODO(amotta):
+% What about overlaps between axon and dendrite agglomerates? For now,
+% axons dominate over dendrites.
 
-axonIDs = cat(1,axons{:});
-axonLookup(axonIDs)=repelem(1:length(axons),cellfun(@length, axons));
-
-%axon ids will be negative
-axonLookup=-axonLookup;
-
+% axon ids will be negative
 generalLookup = dendriteLookup;
-generalLookup(axonLookup~=0)=axonLookup(axonLookup~=0);
-if max(seg(:)) > length(generalLookup)
-    generalLookup(max(seg(:))) = 0;
-end
-seg2 = double(seg);
-seg2(seg2~=0)=generalLookup(seg2(seg2~=0));
-[edges, ind] = connectEM.borders.codeBenedikt(seg2);
-[X,Y,Z] = ind2sub(size(seg2)+2,ind); 
+generalLookup(axonLookup ~= 0) = -axonLookup(axonLookup ~= 0);
+
+% load segmentation and convert do double
+seg = loadSegDataGlobal(p.seg, p.local(idx).bboxSmall);
+blockCount = ceil(size(seg) ./ blockSize);
+
+% Segments which are not part of either the axon or dendrite agglomerates
+% should not be counted as border voxels (despite being mapped to zero in
+% the lines below).
+segExclude = seg > 0;
+segExclude(segExclude) = ~generalLookup(seg(segExclude));
+
+% apply equivalence class mapping
+seg = double(seg);
+seg(seg ~= 0) = generalLookup(seg(seg ~= 0));
+
+% find edges and borders
+% TODO(amotta): Cube borders are not properly handled yet.
+[edges, ind] = connectEM.borders.codeBenedikt(seg, segExclude);
+
+corrSegId = max(seg(:)) + 1;
+seg = padarray(seg, [1, 1, 1], corrSegId);
+[X, Y, Z] = ind2sub(size(seg) , ind);
+
+% correct for padding
+X = X - 1;
+Y = Y - 1;
+Z = Z - 1;
+
 %the +2 and -1 is for fixing the padding
-indSmall = sub2ind(ceil(size(seg2)./[32,32,16]),ceil((X-1)/32),ceil((Y-1)/32),ceil((Z-1)/16));
-for idx2 = 1 : prod(ceil(size(seg2)./[32,32,16]))
-    [C,ia,ic] = unique(edges(indSmall==idx2,:), 'rows');
-    freqs = tabulate(ic);
-    if ~isempty(freqs)
-        findings{idx2} = [C, freqs(:,2)];
-        indSelect = ind(indSmall==idx2);
-        areaM{idx2} = Seg.Local.physicalBorderArea2(arrayfun(@(v){indSelect(ic==v)},1:length(ia)), C, padarray(seg2,[1,1,1]), [11.24,11.24,26], false);
-    else
-        findings{idx2} = [];
-        areaM{idx2} = [];
+blkIdx = sub2ind( ...
+    blockCount, ...
+    ceil(X / blockSize(1)), ...
+    ceil(Y / blockSize(2)), ...
+    ceil(Z / blockSize(3)));
+
+findings = cell(prod(blockCount), 1);
+areaM = cell(prod(blockCount), 1);
+
+for curBlkIdx = 1:prod(blockCount)
+    curBlkMask = (blkIdx == curBlkIdx);
+    curEdges = edges(curBlkMask, :);
+    
+    if isempty(curEdges)
+        findings{curBlkIdx} = zeros(0, 2);
+        areaM{curBlkIdx} = zeros(0, 1);
+        continue;
     end
+    
+    % count voxels per edges
+   [curEdges, ~, curEdgeId] = unique(curEdges, 'rows');
+    curVxCount = accumarray(curEdgeId, 1);
+    
+    findings{curBlkIdx} = [curEdges, curVxCount];
+    
+    % calculate physical area
+    curVxIds = ind(curBlkMask);
+    curVxIds = arrayfun( ...
+        @(idx) curVxIds(curEdgeId == idx), ...
+        1:size(curEdges, 1), 'UniformOutput', false);
+    
+    areaM{curBlkIdx} = Seg.Local.physicalBorderArea2( ...
+        curVxIds, curEdges, seg, p.raw.voxelSize, true);
 end
 
 save(['/tmpscratch/kboerg/borders3/borders_' num2str(idx)],'findings','areaM');
-
+end
