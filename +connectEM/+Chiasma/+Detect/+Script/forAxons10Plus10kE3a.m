eb6049f0e1829f2f699469785d8ad71ccb3fa169
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-04 18:24:30 +0100

    Run chiasma detection on axons 10a with 10k of E3a flights

---
+++ +connectEM/+Chiasma/+Detect/+Script/forAxons10Plus10kE3a.m
@@ -0,0 +1,53 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+
+%% configuration
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+axonFile = fullfile(rootDir, 'aggloState', 'axons_10_a_plus_10kE3a.mat');
+
+outputDir = fullfile( ...
+    rootDir, 'chiasmataSplitting', ...
+    '20171104T181213-on-axons-10a-plus-10kE3a');
+tempOutputDir = '/tmpscratch/amotta/l4/chiasma-detection';
+
+chiasmaParam = struct;
+chiasmaParam.sphereRadiusOuter = 10000;
+chiasmaParam.sphereRadiusInner = 1000;
+chiasmaParam.minNodeDist = 2000;
+chiasmaParam.clusterSize = 2000;
+
+info = Util.runInfo();
+
+%% load data
+param = load(fullfile(rootDir, 'allParameter.mat'), 'p');
+param = param.p;
+
+agglos = load(axonFile, 'axons', 'indBigAxons');
+agglos = agglos.axons(agglos.indBigAxons);
+
+%% run chiasma detection
+[job, runId] = connectEM.detectChiasmata( ...
+    param, chiasmaParam, agglos, tempOutputDir);
+fprintf('Running chiasma detection %s... ', runId);
+
+wait(job);
+fprintf('done!\n');
+
+%% collecting results
+fprintf('Collecting results... ')
+chiasmata = connectEM.Chiasma.Detect.collectResults( ...
+    tempOutputDir, runId, numel(agglos));
+fprintf('done!\n');
+
+%% building output
+out = struct;
+out.info = info;
+out.runId = runId;
+out.chiasmata = chiasmata;
+
+outFile = sprintf('%s_chiasmata.mat', runId);
+outFile = fullfile(outputDir, outFile);
+
+Util.saveStruct(outFile, out);
+system(sprintf('chmod a-w "%s"', outFile));
