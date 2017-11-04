0a3109ec1becb6bd9dc6a4a484ffce2237f3db6b
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-04 16:23:40 +0100

    Limit the number of applied E3a flights to avoid percolation

--- +connectEM/applyE3aFlights.m
+++ +connectEM/applyE3aFlights.m
@@ -13,7 +13,7 @@ clear;
 
 %% configuration
 minNodeEvidence = 2 * 27;
-maxNumFlights = inf;
+maxNumFlights = 10000;
 
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
 workingDir = fullfile(rootDir, 'aggloState');
