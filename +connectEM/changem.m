a39421eb4996947a889d47bd4b8b8fe4bd3e725b
Marcel Beining <marcel.beining@brain.mpg.de>
2017-11-06 15:05:09 +0100

    2 bugfixes

--- +connectEM/changem.m
+++ +connectEM/changem.m
@@ -2,5 +2,7 @@ function newmap = changem(map, newcode, oldcode)
 
     [~, idx] = ismember(map, oldcode);
     newmap = newcode(idx);
-
+    if all(size(map)~=size(newmap))  % fix only-1-edge problem
+        newmap = newmap';
+    end
 end
