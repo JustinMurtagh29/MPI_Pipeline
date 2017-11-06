ce8752bead2475faccb9ae4bece1ead917bde149
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-06 19:25:38 +0100

    Switch order of loops to reduced calls to `graphconncomp`

--- +connectEM/+Chiasma/+Bridge/findBridges.m
+++ +connectEM/+Chiasma/+Bridge/findBridges.m
@@ -19,43 +19,41 @@ function findBridges(param, chiasmaParam, axon, chiasmata, chiasmaId)
     exitNodeIds = sort(reshape(exitNodeIds, 1, 4));
     assert(all(exitNodeIds));
     
-    %%
-    curIdA = exitNodeIds(1);
-    for curIdB = exitNodeIds(2:end)
-        curIdsAB = horzcat(curIdA, curIdB);
-        curIdsCD = setdiff(exitNodeIds, curIdsAB);
+    %% find bridges
+    edges = axon.edges;
+    assert(issorted(edges, 2));
+    
+    adjMat = sparse( ...
+        edges(:, 2), edges(:, 1), ...
+        true, nodeCount, nodeCount);
+    assert(graphconncomp(adjMat, 'Directed', false) == 1);
+    clear adjMat;
+    
+    % search for bridges
+    for curEdgeId = 1:size(edges, 1)
+        curEdges = edges;
+        curEdges(curEdgeId, :) = [];
         
-        % add loops
-        curEdges = axon.edges;
-        curEdges = cat(1, curEdges, [curIdA, curIdB; curIdsCD]);
-        assert(issorted(curEdges, 2));
-        
-        curAdj = sparse( ...
+        curAdjMat = sparse( ...
             curEdges(:, 2), curEdges(:, 1), ...
             true, nodeCount, nodeCount);
-        assert(graphconncomp(curAdj, 'Directed', false) == 1);
-
-        % search for loops
-        for curEdgeId = 1:size(curEdges, 1)
-            curAdj = curEdges;
-            curAdj(curEdgeId, :) = [];
-            assert(issorted(curAdj, 2));
+        [~, curComps] = graphconncomp(curAdjMat, 'Directed', false);
         
-            curAdj = sparse( ...
-                curAdj(:, 2), curAdj(:, 1), ...
-                true, nodeCount, nodeCount);
-           [~, curComps] = graphconncomp(curAdj, 'Directed', false);
-           
+        % check for 2-2 partition
+        for curIdB = exitNodeIds(2:end)
+            curIdsAB = horzcat(exitNodeIds(1), curIdB);
+            curIdsCD = setdiff(exitNodeIds, curIdsAB);
+
             curCompAB = unique(curComps(curIdsAB));
-            curCompCD = unique(curComps(curIdsCD));
+            if ~isscalar(curCompAB); continue; end
             
-            assert(isscalar(curCompAB));
-            assert(isscalar(curCompCD));
+            curCompCD = unique(curComps(curIdsCD));
+            if ~isscalar(curCompCD); continue; end
             
             if curCompAB ~= curCompCD
                 fprintf('Found bridge:\n');
                 fprintf('(%d, %d) and (%d, %d)\n', curIdsAB, curIdsCD);
-                fprintf('Edge: (%d, %d)\n', curEdges(curEdgeId, :));
+                fprintf('Edge: (%d, %d)\n', edges(curEdgeId, :));
             end
         end
     end
