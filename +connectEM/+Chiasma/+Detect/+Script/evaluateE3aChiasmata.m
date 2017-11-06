ad6a38d1413990fd23f65cf3dce19d0dd5b2f464
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-06 09:49:14 +0100

    Evaluate random E3a chiasmata

---
+++ +connectEM/+Chiasma/+Detect/+Script/evaluateE3aChiasmata.m
@@ -0,0 +1,54 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+
+%% configuration
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+chiasmataFile = fullfile( ...
+    rootDir, 'chiasmataSplitting', ...
+    '20171104T181213-on-axons-10a-plus-10kE3a', ...
+    '20171104T184018_chiasmata.mat');
+
+outputDir = '/home/amotta/Desktop/random-chiasmata';
+
+%% load data
+param = load(fullfile(rootDir, 'allParameter.mat'));
+param = param.p;
+
+data = load(chiasmataFile);
+axonFile = data.info.param.axonFile;
+chiasmata = data.chiasmata;
+clear data;
+
+axons = load(axonFile);
+axonIds = find(axons.indBigAxons);
+axons = axons.axons(axonIds);
+
+%% select random chiasmata
+chiasma = table;
+chiasma.axonId = repelem((1:numel(chiasmata))', ...
+    cellfun(@(c) numel(c.ccCenterIdx), chiasmata));
+chiasma.chiasmaId = cell2mat(cellfun(@(c) ...
+    (1:numel(c.ccCenterIdx))', chiasmata, 'UniformOutput', false));
+
+
+%% export axons
+rng(0);
+randIds = randperm(size(chiasma, 1));
+
+if ~exist(outputDir, 'dir')
+    mkdir(outputDir);
+end
+
+for curIdx = 1:20
+    curChiasma = chiasma(randIds(curIdx), :);
+    
+    curSkel = connectEM.Chiasma.Detect.buildSkeleton( ...
+        axons(curChiasma.axonId), chiasmata{curChiasma.axonId});
+    curSkel = Skeleton.setParams4Pipeline(curSkel, param);
+    
+    curSkelFile = sprintf( ...
+        '%03d_axon-%d_chiasma-%d.nml', ...
+        curIdx, curChiasma.axonId, curChiasma.chiasmaId);
+    curSkel.write(fullfile(outputDir, curSkelFile));
+end
