67d6ea6cc43e2879e6d339b4ab11174b64372e3f
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-06 19:46:05 +0100

    Collect data and show quantitative results

--- +connectEM/+Chiasma/+Bridge/evaluate.m
+++ +connectEM/+Chiasma/+Bridge/evaluate.m
@@ -32,11 +32,18 @@ chiasma = [
 chiasma = array2table( ...
     chiasma, 'VariableNames', {'axonId', 'chiasmaId'});
 
+chiasmaCount = size(chiasma, 1);
+
 %%
 paramForChiasma = transpose(horzcat( ...
     fieldnames(chiasmaParam), struct2cell(chiasmaParam)));
 paramForChiasma = Util.modifyStruct(param, paramForChiasma{:});
 
+results = table;
+results.nrPartitions = nan(chiasmaCount, 1);
+results.nrBridges = nan(chiasmaCount, 1);
+
+tic
 for curIdx = 1:size(chiasma, 1)
     curAxon = axons(chiasma.axonId(curIdx));
     curChiasmata = chiasmata{chiasma.axonId(curIdx)};
@@ -44,4 +51,14 @@ for curIdx = 1:size(chiasma, 1)
     
    [isBridge, edges] = connectEM.Chiasma.Bridge.findBridges( ...
         param, chiasmaParam, curAxon, curChiasmata, curChiasmaId);
+    
+    results.nrPartitions(curIdx) = sum(any(isBridge, 1));
+    results.nrBridges(curIdx) = sum(isBridge(:));
 end
+toc
+
+%% show results
+fprintf('\n');
+fprintf('# chiasmata: %d\n', chiasmaCount);
+fprintf('# splittable chiasmata: %d\n', sum(results.nrPartitions > 0));
+fprintf('# chiasmata with unique split: %d\n', sum(results.nrPartitions == 1));
