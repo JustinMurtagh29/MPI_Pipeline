b28992143d5d03bda095374d59a8e3e81a273f36
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-08 20:29:57 +0100

    Do extensive evaluation of dynamic querying approach

--- +connectEM/+Chiasma/+Flight/+Dynamic/+Simulate/runDynamicQueries.m
+++ +connectEM/+Chiasma/+Flight/+Dynamic/+Simulate/runDynamicQueries.m
@@ -60,7 +60,10 @@ chiasmaT.dynNrQueries = cellfun( ...
     @(o) sum(o(:, 1) > 0), chiasmaT.dynOverlaps);
 
 chiasmaT.dynLUT = cell(size(chiasmaT, 1), 1);
+chiasmaT.dynCorrect = false(size(chiasmaT, 1), 1);
+
 chiasmaT.statLUT = cell(size(chiasmaT, 1), 1);
+chiasmaT.statPart = cell(size(chiasmaT, 1), 1);
 
 for curIdx = 1:size(chiasmaT, 1)
     curOverlaps = chiasmaT.dynOverlaps{curIdx};
@@ -86,11 +89,85 @@ for curIdx = 1:size(chiasmaT, 1)
     %% retrieve LUT from static querying
     curAggloId = chiasmaT.aggloId(curIdx);
     curChiasmaId = chiasmaT.chiasmaId(curIdx);
-    curStatLUT = summaries(curAggloId).tracings{curChiasmaId}.lut;
+    curTracings = summaries(curAggloId).tracings{curChiasmaId};
+    
+    curStatLUT = curTracings.lut;
+    curStatPart = reshape(curTracings.partition, 1, []);
+    curStatPartValid = curTracings.partitionIsValid;
+    
+    %% build partition string for static solution
+    curStatPartStr = arrayfun( ...
+        @num2str, curStatPart, 'UniformOutput', false);
+    curStatPartStr = strjoin(curStatPartStr, '-');
+    
+    curStatPartValidStr = {'invalid', 'valid'};
+    curStatPartValidStr = curStatPartValidStr{1 + curStatPartValid};
+    curStatPartStr = strcat(curStatPartStr, '-', curStatPartValidStr);
     
     %% collect outputs
     chiasmaT.dynLUT{curIdx} = curDynLUT;
     chiasmaT.statLUT{curIdx} = curStatLUT;
+    chiasmaT.statPart{curIdx} = curStatPartStr;
 end
 
+chiasmaT.dynCorrect = cellfun( ...
+    @isequal, chiasmaT.dynLUT, chiasmaT.statLUT);
+chiasmaT.dynOverlaps = [];
 clear cur*;
+
+%% show results
+chiasma4T = chiasmaT;
+chiasma4T = chiasma4T(chiasma4T.nrExits == 4, :);
+
+fprintf('\n');
+fprintf('Total number of chiasmata: %d\n', size(chiasmaT, 1));
+fprintf('Total number of chiasma exits: %d\n', sum(chiasmaT.nrExits));
+
+fprintf('\n');
+fprintf( ...
+    'Total number of dynamic queries: %d (%.1f %%)\n', ...
+    sum(chiasmaT.dynNrQueries), 100 * ...
+    sum(chiasmaT.dynNrQueries) / sum(chiasmaT.nrExits));
+fprintf( ...
+    'Total number of dynamic query rounds: %d\n', ...
+    max(chiasmaT.dynNrQueries));
+
+fprintf('\n');
+fprintf( ...
+    'Dynamic queries correct: %.1f %%\n', ...
+    100 * mean(chiasmaT.dynCorrect));
+fprintf( ...
+    'Dynamic queries correct (4-fold only): %1.f %%\n', ...
+    100 * mean(chiasma4T.dynCorrect));
+
+temp = table;
+[temp.nrExits, ~, tempUniRows] = unique(chiasmaT.nrExits);
+temp.nrChiasmata = accumarray(tempUniRows, 1);
+temp.nrCorrect = accumarray(tempUniRows, chiasmaT.dynCorrect);
+temp.percCorrect = round(100 .* temp.nrCorrect ./ temp.nrChiasmata, 1);
+
+fprintf('\n');
+fprintf('Error rate vs. number of chiasma exits:\n\n');
+disp(temp);
+
+temp = table;
+[temp.partition, ~ , tempUniRows] = unique(chiasma4T.statPart);
+temp.nrChiasmata = accumarray(tempUniRows, 1);
+temp.nrCorrect = accumarray(tempUniRows, chiasma4T.dynCorrect);
+temp.percCorrect = round(100 .* temp.nrCorrect ./ temp.nrChiasmata, 1);
+
+fprintf('\n');
+fprintf('Error rate vs. chiasma partition (4-fold only):\n\n');
+disp(temp);
+
+temp = table;
+[temp.partition, ~ , tempUniRows] = unique(chiasmaT.statPart);
+temp.nrChiasmata = accumarray(tempUniRows, 1);
+temp.nrCorrect = accumarray(tempUniRows, chiasmaT.dynCorrect);
+temp.percCorrect = round(100 .* temp.nrCorrect ./ temp.nrChiasmata, 1);
+
+fprintf('\n');
+fprintf('Error rate vs. chiasma partition:\n\n');
+disp(temp);
+
+clear temp*;
