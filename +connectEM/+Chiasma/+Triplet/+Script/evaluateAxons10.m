44ebbbe36ecda1a2df03c010e0747a473d9f9672
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-10 15:49:31 +0100

    Overlap with agglomerates after application of all E3a flights

--- +connectEM/+Chiasma/+Triplet/+Script/evaluateAxons10.m
+++ +connectEM/+Chiasma/+Triplet/+Script/evaluateAxons10.m
@@ -10,6 +10,9 @@ chiasmataFile = fullfile( ...
     rootDir, 'tripletDetection', ...
     '20171109T133421-on-axons-10a', ...
     '20171109T133849_chiasmata.mat');
+axonE3aFile = fullfile( ...
+    '/mnt/mpibr/data/Personal/mottaa/L4', ...
+    '2017-11-09-Finding-Triplets/data/axons_10_a_plus_allE3a.mat');
 
 %% loading data
 param = load(fullfile(rootDir, 'allParameter.mat'), 'p');
@@ -20,7 +23,13 @@ axonFile = chiasmata.info.param.axonFile;
 chiasmata = chiasmata.chiasmata;
 
 axons = load(axonFile, 'axons', 'indBigAxons');
-axons = axons.axons(axons.indBigAxons);
+axonIds = find(axons.indBigAxons);
+axons = axons.axons(axonIds);
+
+% load axons with all E3a flights
+axonsE3a = load(axonE3aFile);
+axonsE3aChildIds = axonsE3a.childIds;
+axonsE3a = axonsE3a.axons;
 
 %% build chiasmata table
 chiasmaT = connectEM.Chiasma.Detect.buildTable(chiasmata, axons);
@@ -42,8 +51,31 @@ for curIdx = 1:20
     curAggloId = chiasmaT.aggloId(curRandId);
     curChiasmaId = chiasmaT.chiasmaId(curRandId);
     
+    curChiasmata = chiasmata{curAggloId};
+    curCenterPos = curChiasmata.nodes( ...
+        curChiasmata.ccCenterIdx(curChiasmaId), :);
+    
+    % axon as before E3a
+    curAxon = axons(curAggloId);
     curSkel = connectEM.Chiasma.Detect.buildSkeleton( ...
-        axons(curAggloId), chiasmata{curAggloId}, curChiasmaId);
+        curAxon, chiasmata{curAggloId}, curChiasmaId);
+    
+    % axon as after E3a
+    curAxonE3a = axonsE3a(axonsE3aChildIds(axonIds(curAggloId)));
+    
+    if size(curAxonE3a.nodes, 1) > 1E5
+        % restrict to sphere
+       [~, curCenterNodeId] = min(pdist2( ...
+           curAxonE3a.nodes(:, 1:3) .* param.raw.voxelSize, ...
+           curCenterPos .* param.raw.voxelSize, 'squaredeuclidean'));
+        curAxonE3a = ...
+            connectEM.Chiasma.restrictToSphere( ...
+                param, curAxonE3a, curCenterNodeId, 20000);
+    end
+    
+    curSkel = curSkel.addTree( ...
+        'After applying all E3a flights', ...
+        curAxonE3a.nodes(:, 1:3), curAxonE3a.edges);
     curSkel = Skeleton.setParams4Pipeline(curSkel, param);
     
     curFileName = sprintf( ...
