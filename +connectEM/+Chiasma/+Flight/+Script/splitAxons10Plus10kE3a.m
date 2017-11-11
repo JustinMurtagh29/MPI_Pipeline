20e7116515a124e2ad428cf3dce59bc2c2f6a65e
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-11 16:16:50 +0100

    Build or load all data necessary for chiasma splitting

--- +connectEM/+Chiasma/+Flight/+Script/splitAxons10Plus10kE3a.m
+++ +connectEM/+Chiasma/+Flight/+Script/splitAxons10Plus10kE3a.m
@@ -25,19 +25,43 @@ nmlDir = fullfile( ...
     chiasmaDir, 'taskAnswers', ...
     sprintf('%s_flightTasks', taskGenId));
 
+splitFile = fullfile( ...
+    chiasmaDir, sprintf('%s_splitData.mat', taskGenId));
+splitFileBuild = false;
+
 info = Util.runInfo();
 
 %% loading data
 param = load(fullfile(rootDir, 'allParameter.mat'));
 param = param.p;
 
-taskGenData = load(taskGenFile);
+%% build or load split file
+if splitFileBuild || ~exist(splitFile, 'file')
+    % task definitions
+    taskGenData = load(taskGenFile);
+    taskDefs = taskGenData.taskDefs;
+    exits = taskGenData.exits;
 
-% task IDs
-taskIds = loadTaskIds(taskIdFile);
-taskIds = taskIds.id;
+    % chiasmata
+    chiasmata = load(taskGenData.info.param.chiasmataFile);
+    axonFile = chiasmata.info.param.axonFile;
+    chiasmata = chiasmata.chiasmata;
 
-ff = loadFlightPaths(param, nmlDir);
+    % task IDs
+    taskIds = loadTaskIds(taskIdFile);
+    taskIds = taskIds.id;
 
-% sanity checks
-assert(size(taskGenData.exits, 1) == numel(taskIds));
+    % flight paths
+    flights = loadFlightPaths(param, nmlDir);
+
+    % build split file
+    splitData = connectEM.Chiasma.Flight.prepareSplit( ...
+        chiasmata, taskDefs, exits, taskIds,flights);
+    splitData.axonFile = axonFile;
+    splitData.info = info;
+    
+    % save
+    Util.saveStruct(splitFile, splitData);
+else
+    splitData = load(splitFile);
+end
