750cd087a55e9c37fd954c6ab6ec7d9f490bd2aa
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-11 14:53:10 +0100

    Start working on chiasma splitting for axons 10 with 10k E3a flights

---
+++ +connectEM/+Chiasma/+Flight/+Script/splitAxons10Plus10kE3a.m
@@ -0,0 +1,39 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+import connectEM.Chiasma.Util.loadFlightPaths;
+import connectEM.Chiasma.Util.loadTaskIds;
+
+clear;
+
+%% configuration
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+
+chiasmaDir = fullfile( ...
+    rootDir, 'chiasmataSplitting', ...
+    '20171104T181213-on-axons-10a-plus-10kE3a');
+
+% task specifics
+taskGenId = '20171108T105021';
+
+taskGenFile = fullfile( ...
+    chiasmaDir, 'taskGeneration', ...
+    sprintf('%s_taskGeneration.mat', taskGenId));
+taskIdFile = fullfile( ...
+    chiasmaDir, 'taskAnswers', ...
+    sprintf('%s_flightTaskIDs.mat', taskGenId));
+nmlDir = fullfile( ...
+    chiasmaDir, 'taskAnswers', ...
+    sprintf('%s_flightTasks', taskGenId));
+
+info = Util.runInfo();
+
+%% loading data
+param = load(fullfile(rootDir, 'allParameter.mat'));
+param = param.p;
+
+taskGenData = load(taskGenFile);
+taskIds = loadTaskIds(taskIdFile);
+ff = loadFlightPaths(param, nmlDir);
+
+% sanity checks
+assert(size(taskGenData.exits, 1) == numel(taskIds));
