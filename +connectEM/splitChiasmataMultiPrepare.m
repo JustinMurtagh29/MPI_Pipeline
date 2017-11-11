3d3d86528cbdeb1c194d5c864063af09b24f9fe5
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-11 13:47:58 +0100

    Make `loadTaskIds` a common utility

--- +connectEM/splitChiasmataMultiPrepare.m
+++ +connectEM/splitChiasmataMultiPrepare.m
@@ -2,15 +2,19 @@ function out = splitChiasmataMultiPrepare(p, taskGenDir, taskGenId)
     % Written by
     %   Kevin Boergens <kevin.boergens@brain.mpg.de>
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    import connectEM.Chiasma.Util.loadTaskIds;
     
     % load results from query generation
     dataFile = sprintf('%s_data.mat', taskGenId);
     data = load(fullfile(taskGenDir, dataFile));
-    assert(size(data.queries, 1) == size(data.taskDef, 1));
     
     % load webKNOSSOS task IDs
     taskFile = sprintf('%s_flightTaskIDs.txt', taskGenId);
     taskIds = loadTaskIds(fullfile(taskGenDir, taskFile));
+    taskIds = taskIds.id;
+    
+    % sanity checks
+    assert(size(data.queries, 1) == size(data.taskDef, 1));
     assert(size(data.queries, 1) == numel(taskIds));
     
     % load flight path NMLs
@@ -27,24 +31,6 @@ function out = splitChiasmataMultiPrepare(p, taskGenDir, taskGenId)
     out.taskDef = data.taskDef;
     out.taskIds = taskIds;
     out.ff = ff;
-end
-
-function taskIds = loadTaskIds(taskFile)
-    fid = fopen(taskFile, 'rt');
-    data = fread(fid, 'char=>char');
-    fclose(fid);
-    
-    % split into lines
-    data = reshape(data, 1, []);
-    data = strsplit(data, '\n');
-    
-    % remove header
-    data(1) = [];
-    
-    % extract first column with task IDs
-    taskIds = cellfun( ...
-        @(x) x(1:(find(x == ',', 1) - 1)), ...
-        data(:), 'UniformOutput', false);
 end
 
 function ff = loadFlightPaths(p, nmlDirs)
