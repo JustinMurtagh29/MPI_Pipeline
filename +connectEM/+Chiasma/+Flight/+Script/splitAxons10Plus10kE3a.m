2c10d553d4fa1ee7e13b17daab62ee5b051f4f76
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-12 12:38:38 +0100

    Requery of chiasmata with inconsistent partitions and of commented tasks

--- +connectEM/+Chiasma/+Flight/+Script/splitAxons10Plus10kE3a.m
+++ +connectEM/+Chiasma/+Flight/+Script/splitAxons10Plus10kE3a.m
@@ -1,10 +1,12 @@
 % Written by
 %   Alessandro Motta <alessandro.motta@brain.mpg.de>
 import connectEM.Chiasma.Flight.loadSplitData;
+import connectEM.Chiasma.Flight.shuffleExits;
 import connectEM.Chiasma.Util.loadFlightPaths;
 import connectEM.Chiasma.Util.loadTaskIds;
 
 clear;
+runId = datestr(now, 30);
 
 %% configuration
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
@@ -15,9 +17,10 @@ chiasmaDir = fullfile( ...
 
 % task specifics
 taskGenId = '20171108T105021';
+taskGenDir = fullfile(chiasmaDir, 'taskGeneration');
 
 taskGenFile = fullfile( ...
-    chiasmaDir, 'taskGeneration', ...
+    taskGenDir, ...
     sprintf('%s_taskGeneration.mat', taskGenId));
 taskIdFile = fullfile( ...
     chiasmaDir, 'taskAnswers', ...
@@ -61,3 +64,47 @@ splitAxons.info = info;
 %% saving result
 Util.saveStruct(splitAxonsFile, splitAxons);
 system(sprintf('chmod a-w "%s"', splitAxonsFile));
+
+%% requerying
+taskGen = load(taskGenFile);
+exits = taskGen.exits;
+taskDefs = taskGen.taskDefs;
+clear taskGen;
+
+% find unsolved chiasmata
+unsolvedT = table;
+unsolvedT.aggloId = repelem( ...
+    cat(1, splitAxons.summary.axonId), ...
+    cat(1, splitAxons.summary.nrChiasmata));
+unsolvedT.chiasmaId = cat(1, splitAxons.summary.chiasmaId);
+
+unsolvedT.isSolved = cat(1, splitAxons.summary.solved);
+unsolvedT(unsolvedT.isSolved, :) = [];
+unsolvedT.isSolved = [];
+
+% mark tasks to requery
+requeryMask =  ...
+    ismember(exits, openExits, 'rows') | ismember( ...
+    exits(:, {'aggloId', 'chiasmaId'}), unsolvedT, 'rows');
+clear unsolvedT;
+
+requery = struct;
+requery.exits = exits(requeryMask, :);
+clear requeryMask;
+
+[requery.exits, shuffledRows] = shuffleExits(requery.exits, 500);
+requery.taskDefs = taskDefs(shuffledRows, :);
+clear shuffledRows;
+
+requeryTaskDefFile = fullfile( ...
+    taskGenDir, sprintf('%s_flightTasks.txt', runId));
+writetable( ...
+    requery.taskDefs, requeryTaskDefFile, 'WriteVariableNames', false);
+
+%% build output
+requery.info = info;
+requery.taskDefFile = requeryTaskDefFile;
+
+requeryTaskGenFile = fullfile( ...
+    taskGenDir, sprintf('%s_taskGeneration.mat', runId));
+Util.saveStruct(requeryTaskGenFile, requery);
