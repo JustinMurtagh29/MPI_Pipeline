e74bdf46816384cedf95fc150f8cd358468cb466
Benedikt Staffler <benedikt.staffler@brain.mpg.de>
2017-11-13 17:51:05 +0100

    updated synapse version

--- +Soma/somaSynapses.m
+++ +Soma/somaSynapses.m
@@ -3,7 +3,7 @@
 
 info = Util.runInfo();
 doPlot = false; %#ok<*UNRCH>
-ver = 'v1';
+ver = 'v2';
 
 
 %% load data
@@ -39,18 +39,6 @@ postsynSomaLUT = L4.Agglo.buildLUT(synapses.postsynId, maxSomaId, true);
 somaSynIdxAll = cellfun(@(x)setdiff(cell2mat(postsynSomaLUT(x)), 0), ...
     somaAgglos(:,1), 'uni', 0); % not that setdiff also makes the idx unique
 somaSynIdx = somaSynIdxAll;
-
-% exclude synapses with presyn in soma for now (quick fix, should be done
-% in a newer synapse version at some point)
-isSomaSeg = false(max(graph.edges(:)), 1);
-isSomaSeg(cell2mat(somaAgglos)) = true;
-toDelIdx = cell(numSomas, 1);
-for i = 1:numSomas
-    toDelIdx{i} = arrayfun(@(x)any(isSomaSeg(synapses.presynId{x})), ...
-        somaSynIdx{i});
-end
-insideSomaSyn = cellfun(@(x, y)x(y), somaSynIdx, toDelIdx, 'uni', 0);
-somaSynIdx = cellfun(@(x, y)x(~y), somaSynIdx, toDelIdx, 'uni', 0);
 
 % soma syn coms (use only first edge for each synapse)
 somaSynComs = cell(numSomas, 1);
