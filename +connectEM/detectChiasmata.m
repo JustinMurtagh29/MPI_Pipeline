598c859c149147a26f2a25b177ad5a037241dc98
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-14 15:32:14 +0100

    Show progress during chiasma detection

--- +connectEM/detectChiasmata.m
+++ +connectEM/detectChiasmata.m
@@ -20,9 +20,11 @@ edges = unique(edges, 'rows');
 
 % for each node ("marching sphere" approach to merger detection)
 nrExits = zeros(size(nodes, 1), 1);
+showProgress = @(n) fprintf('%d of %d nodes done\n', n, size(nodes, 1));
 
 for i = 1:size(nodes, 1)
     nrExits(i) = connectEM.detectChiasmataNodes(p, nodes, edges, i);
+    if ~mod(i, 10000); showProgress(i); end
 end
 
 % Mark intersections
