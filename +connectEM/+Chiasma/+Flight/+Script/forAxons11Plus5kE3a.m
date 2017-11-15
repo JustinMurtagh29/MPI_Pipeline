e841e8efde40588576841a375050b3d7e7cc361f
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-15 11:46:26 +0100

    Generate tasks for axons 11 plus 5k E3a flights

---
+++ +connectEM/+Chiasma/+Flight/+Script/forAxons11Plus5kE3a.m
@@ -0,0 +1,56 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+import connectEM.Chiasma.Detect.buildTable;
+import connectEM.Chiasma.Flight.selectExits;
+import connectEM.Chiasma.Flight.shuffleExits;
+import connectEM.Chiasma.Flight.generateTasks;
+
+%%
+clear;
+runId = datestr(now, 30);
+
+%% configuration
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+
+chiasmaDir = fullfile( ...
+    rootDir, 'chiasmataSplitting', ...
+    '20171113T133413-on-axons-11a-plus-5kE3a');
+
+chiasmataFile = fullfile(chiasmaDir, '20171113T134102_chiasmata.mat');
+outputDir = fullfile(chiasmaDir, 'taskGeneration');
+clear workingDir;
+
+info = Util.runInfo();
+
+%% load data
+param = load(fullfile(rootDir, 'allParameter.mat'));
+param = param.p;
+
+chiasmata = load(chiasmataFile);
+axonFile = chiasmata.info.param.axonFile;
+chiasmata = chiasmata.chiasmata;
+
+axons = load(axonFile);
+axonIds = find(axons.indBigAxons);
+axons = axons.axons(axonIds);
+
+%% find exits to query
+chiasmaT = buildTable(chiasmata, axons);
+chiasmaT(chiasmaT.isSolved, :) = [];
+
+exits = selectExits(chiasmaT, [], inf);
+exits = shuffleExits(exits, 500);
+
+taskDefFile = fullfile(outputDir, sprintf('%s_flightTasks.txt', runId));
+taskDefs = generateTasks(param, chiasmata, exits, taskDefFile);
+
+%% build output
+out = struct;
+out.info = info;
+out.exits = exits;
+out.taskDefs = taskDefs;
+out.taskDefFile = taskDefFile;
+out.chiasmataFile = chiasmataFile;
+
+outFile = sprintf('%s_taskGeneration.mat', runId);
+Util.saveStruct(fullfile(outputDir, outFile), out);
