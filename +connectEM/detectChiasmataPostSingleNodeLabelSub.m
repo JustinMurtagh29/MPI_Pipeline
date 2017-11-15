08e7020787e9c0c7a1a8782fa8bf5825cdab56aa
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-15 17:52:51 +0100

    Remove obsolete function

--- +connectEM/detectChiasmataPostSingleNodeLabelSub.m
+++
@@ -1,8 +0,0 @@
-function detectChiasmataPostSingleNodeLabelSub(startidx, outputFolder)
-load([outputFolder 'prep_singlenodelabel.mat']);
-for i=startidx:500:length(cc)
-    [~, pos{i}, dir{i}, queryIdx{i}] = connectEM.detectChiasmataNodes( ...
-        nodes,edges,prob,p,cc{i}(centerOfCC(i))); %#ok
-end
-save([outputFolder, 'temp_singlenodelabel_' num2str(startidx)],'pos', 'dir', 'queryIdx', 'centerOfCC', 'cc','-v7.3');
-end
