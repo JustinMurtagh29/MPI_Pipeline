c76407324430d7b15727511e5c6c22e2f13d57c8
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-15 17:53:21 +0100

    Remove yet another obsolete function

--- +connectEM/detectChiasmataSub.m
+++
@@ -1,9 +0,0 @@
-function nrExits = detectChiasmataSub(startidx, outputFolder)
-    load(fullfile(outputFolder, 'prep.mat'));
-    nrExits = zeros(size(nodes, 1), 1);
-    for i=startidx:5000:size(nodes,1)
-        nrExits(i) = connectEM.detectChiasmataNodes( ...
-            nodes, edges, ones(size(edges,1), 1), p, i);
-    end
-    save([outputFolder 'temp_' num2str(startidx)], 'nrExits', '-v7.3');
-end
