14bf2adad5b91d468e199fd0a5881532899bc979
Marcel Beining <marcel.beining@brain.mpg.de>
2017-11-15 16:46:07 +0100

    added function to calculate mitochondrium score and total volume for each segment

---
+++ +connectEM/getMitoScoreAndVolume.m
@@ -0,0 +1,33 @@
+function getMitoScoreAndVolume(p)
+
+% Get mito scores
+labelMap(1).root = '/tmpscratch/bstaffle/data/2012-09-28_ex145_07x2_ROI2017/SVM/cnn17_2';
+labelMap(1).backend = 'wkw';
+labelMap(1).segId = [0.5 1];
+
+inputCell = cell(numel(p.local),1);
+for cube=1:numel(p.local)
+    bbox = p.local(cube).bboxSmall;
+    inputCell{cube} = {bbox};
+end
+functionH = @connectEM.getSegmentHeuristicsScore;
+
+% Execute and retrieve results
+seg = p.seg;
+cluster = Cluster.getCluster('-l h_vmem=12G');
+job = Cluster.startJob(functionH, inputCell, ...
+    'name', 'mitoLookup', 'sharedInputs', {seg labelMap}, ...
+    'sharedInputsLocation', [1 2], 'cluster', cluster, ...
+    'numOutputs', 3);
+Cluster.waitForJob(job);
+
+scores = fetchOutputs(job);
+
+% Reformatting for save file
+segIds = cat(1, scores{:,2});
+sc = cat(1, scores{:,1});
+mitoScore = cat(1, sc{:,1});
+sc = cat(1, scores{:,3});
+voxelCounts = cat(1, sc{:,1});
+
+Util.save([p.saveFolder 'mitoResult.mat'], segIds, mitoScore, voxelCounts);
--- +connectEM/getSegmentHeuristicsScore.m
+++ +connectEM/getSegmentHeuristicsScore.m
@@ -1,13 +1,25 @@
-function [segmentOverlap, uniqueSegId] = getSegmentHeuristicsScore(seg, binaryMap, bbox)
+function [segmentOverlap, uniqueSegId,voxelCount] = getSegmentHeuristicsScore(seg, labelMap, bbox)
     % Takes segmentation struct (root, prefix, bbox) and looks up the fraction 
-    % of each segment overlapping with binaryMap (specified by root, prefix and segId to lookup)
+    % of each segment overlapping with labelMap (specified by root, prefix/backend and segId to lookup)
+    % if segId has to values, the function uses all values between these
+    % values (including the left side)
+    % if the third output is defined, it also returns the voxel count of
+    % each connected component in the labelMap which is associated with the
+    % segment that overlaps the most with the connected component.
 
     seg = loadSegDataGlobal(seg, bbox);
     uniqueSegId = unique(seg(seg ~= 0));
-    segmentOverlap = cell(length(binaryMap),1);
-    for i=1:length(binaryMap)
-        class = loadSegDataGlobal(binaryMap(i), bbox);
-        class = class == binaryMap(i).segId;
+    segmentOverlap = cell(length(labelMap),1);
+    if nargout > 2
+        voxelCount = cell(length(labelMap),1);
+    end
+    for i=1:length(labelMap)
+        class = loadSegDataGlobal(labelMap(i), bbox);
+        if numel(labelMap(i).segId) == 2
+            class = class >= labelMap(i).segId(1) & class < labelMap(i).segId(2); % make logical map with all values between segId(1) and segId(2)
+        else
+            class = class == labelMap(i).segId; % make logical map with all values equal to segId
+        end
         segOccurences = histc(seg(seg~=0), uniqueSegId); 
         classVoxels = histc(seg(seg~=0 & class), uniqueSegId);
         if size(classVoxels,2) > 1
@@ -15,6 +27,16 @@ function [segmentOverlap, uniqueSegId] = getSegmentHeuristicsScore(seg, binaryMa
             segmentOverlap{i} = classVoxels' ./ segOccurences;
         else
             segmentOverlap{i} = classVoxels ./ segOccurences;
+        end
+        if nargout > 2
+            % calculate the voxelCount of each labeled connected component (CC) and associate it
+            % with the segment it overlaps the most
+            class = bwlabeln(class); % label each CC with a different color
+            voxelCounts = histc(class(:),1:max(class(:))); % calculate the voxelCount of each CC
+            mostOverlapSegs = accumarray(class(class~=0),seg(class~=0),[],@mode); % calculate which segIds overlap the most with each CC
+            voxelCount{i} = zeros(numel(uniqueSegId),1);
+            [~,ind] = ismember(mostOverlapSegs,uniqueSegId);
+            voxelCount{i}(ind) = voxelCounts;  % associate these segIds with the voxelCount
         end
     end
 
