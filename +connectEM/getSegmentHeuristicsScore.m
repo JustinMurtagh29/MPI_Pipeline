cfac64f40c32ba0537caf46e954133cfb0cff665
Marcel Beining <marcel.beining@brain.mpg.de>
2017-11-16 11:49:21 +0100

    add possibility of applying function to label map

--- +connectEM/getSegmentHeuristicsScore.m
+++ +connectEM/getSegmentHeuristicsScore.m
@@ -1,11 +1,15 @@
 function [segmentOverlap, uniqueSegId,voxelCount] = getSegmentHeuristicsScore(seg, labelMap, bbox)
     % Takes segmentation struct (root, prefix, bbox) and looks up the fraction 
     % of each segment overlapping with labelMap (specified by root, prefix/backend and segId to lookup)
-    % if segId has to values, the function uses all values between these
+    % if segId has two values, the function uses all values between these
     % values (including the left side)
+    % if label map has the field "function", this function is applied to
+    % the label map after applying the threshold or segId on it, e.g.
+    % bwareaopen
     % if the third output is defined, it also returns the voxel count of
     % each connected component in the labelMap which is associated with the
     % segment that overlaps the most with the connected component.
+    
 
     seg = loadSegDataGlobal(seg, bbox);
     uniqueSegId = unique(seg(seg ~= 0));
@@ -19,6 +23,9 @@ function [segmentOverlap, uniqueSegId,voxelCount] = getSegmentHeuristicsScore(se
             class = class >= labelMap(i).segId(1) & class < labelMap(i).segId(2); % make logical map with all values between segId(1) and segId(2)
         else
             class = class == labelMap(i).segId; % make logical map with all values equal to segId
+        end
+        if isfield(labelMap(i),'function')
+            class = labelMap(i).function(class);
         end
         segOccurences = histc(seg(seg~=0), uniqueSegId); 
         classVoxels = histc(seg(seg~=0 & class), uniqueSegId);
