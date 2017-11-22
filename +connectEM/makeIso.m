d7d59145667f6c692b1a1bdf6ecd6fc97502e5df
Marcel Beining <marcel.beining@brain.mpg.de>
2017-11-22 10:53:00 +0100

    add some logs

--- +connectEM/makeIso.m
+++ +connectEM/makeIso.m
@@ -12,11 +12,11 @@ if isfield(agglos,'nodes')
 end
 if ~exist('p','var') || isempty(p)
     load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameterWithSynapses.mat');
-    p.seg.root = ['/gabaghi/wKcubes_archive/2012-09-28_ex145_07x2_ROI2017/segmentation/1/'];
+    p.seg.root = '/gabaghi/wKcubes_archive/2012-09-28_ex145_07x2_ROI2017/segmentation/1/';
     p.seg.backend = 'wkwrap';
 end
 Util.log('Now running main viz')
 Visualization.exportAggloToAmira(p,agglos,outDir,'reduce',reduce,'smoothSizeHalf',4,'smoothWidth',8);
-
+Util.log('PLYs complete. Finished')
 info=Util.runInfo(false);
 save(fullfile(outDir,'info.mat'),'info','agglos');
