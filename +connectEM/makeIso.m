06a74e22496b5a04ed4e3aad1af42cc24a0cbdb0
Marcel Beining <marcel.beining@brain.mpg.de>
2017-11-22 10:57:12 +0100

    removing a pitfall..

--- +connectEM/makeIso.m
+++ +connectEM/makeIso.m
@@ -1,4 +1,4 @@
-function makeIso(agglos,outDir,p,reduce)
+function makeIso(agglos,outDir,reduce)
 
 Util.log('initialize stuff')
 if ~exist('reduce','var')
@@ -10,11 +10,11 @@ end
 if isfield(agglos,'nodes')
     agglos = Superagglos.transformAggloNewOldRepr(agglos);
 end
-if ~exist('p','var') || isempty(p)
+% if ~exist('p','var') || isempty(p)
     load('/gaba/u/mberning/results/pipeline/20170217_ROI/allParameterWithSynapses.mat');
     p.seg.root = '/gabaghi/wKcubes_archive/2012-09-28_ex145_07x2_ROI2017/segmentation/1/';
     p.seg.backend = 'wkwrap';
-end
+% end
 Util.log('Now running main viz')
 Visualization.exportAggloToAmira(p,agglos,outDir,'reduce',reduce,'smoothSizeHalf',4,'smoothWidth',8);
 Util.log('PLYs complete. Finished')
