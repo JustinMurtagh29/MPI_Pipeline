3017fc8fa85294894c3966082796138d5b6ce13a
ChristianS <christian.schramm@brain.mgp.de>
2017-12-05 13:22:01 +0100

    Add comments

--- +connectEM/processQueryTasks.m
+++ +connectEM/processQueryTasks.m
@@ -1,5 +1,27 @@
 function processQueryTasks(i,batchBoundaries,axons,borderPositions,directions,p,outputFolder)
 
+ % Written by
+    %   Manuel Berning <manuel.berning@brain.mpg.de>
+    %   Christian Schramm <christian.schramm@brain.mpg.de>
+    % Collects the information for querying agglo endings. The number of 
+    % agglos is presumly (connectEM.generateAxonQueries) divided into 100
+    % batches for parallelisation.
+    % Input parameters:
+    %   i 
+    %     index of batch
+    %   batchBoundaries
+    %     (1x100) array defining the borders of the batches
+    %   axons
+    %     super agglomerates
+    %   borderPositions
+    %     border positions of chosen endings
+    %   directions
+    %     pca of border ID
+    %   p
+    %     parameter file
+    %   outputFolder
+    %     output directory
+
     theseIdx = batchBoundaries(i):batchBoundaries(i+1)-1;
     theseAxons = axons(theseIdx);
     theseBorderPositions = borderPositions(theseIdx);
