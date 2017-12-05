950d9c9441a95c9117f2c9db56749d4353681ec2
Benedikt Staffler <benedikt.staffler@brain.mpg.de>
2017-12-05 16:48:40 +0100

    additional inputs

--- +connectEM/generateSegmentationForNewQueries.m
+++ +connectEM/generateSegmentationForNewQueries.m
@@ -1,4 +1,4 @@
-function generateSegmentationForNewQueries(param)
+function generateSegmentationForNewQueries(param, seg, axFile)
 
     % Define where to load and store results
     dataDir = fullfile(param.saveFolder, 'aggloState');
@@ -7,14 +7,20 @@ function generateSegmentationForNewQueries(param)
     [~, segmentMeta] = connectEM.loadAllSegmentationData(param);
 
     % Load larger 5 micron agglomerates
-    m = load(fullfile(dataDir, 'axons_04.mat'));
+    if ~exist('axFile', 'var') || isempty(axFile)
+        m = load(fullfile(dataDir, 'axons_04.mat'));
+    else
+        m = load(axFile);
+    end
     axons = m.axons(m.indBigAxons);
     axons = arrayfun(@Agglo.fromSuperAgglo, axons, 'UniformOutput', false);
     clear m;
     
-    seg = struct;
-    seg.root = fullfile(param.saveFolder, 'aggloState', '20170829_segmentationForQueries', '1');
-    seg.prefix = param.seg.prefix;
+    if ~exist('seg', 'var') || isempty(seg)
+        seg = struct;
+        seg.root = fullfile(param.saveFolder, 'aggloState', '20170829_segmentationForQueries', '1');
+        seg.prefix = param.seg.prefix;
+    end
 
     % Write new segmentation based on axon queries
     mapping = connectEM.createLookup(segmentMeta, axons);
