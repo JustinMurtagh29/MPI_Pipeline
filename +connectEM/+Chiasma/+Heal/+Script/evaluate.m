00dab43fb9d5fbd90c26285b7438b77aebe6abfe
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-12-06 13:48:12 +0100

    Start to look at endings from chiasma splitting

---
+++ +connectEM/+Chiasma/+Heal/+Script/evaluate.m
@@ -0,0 +1,69 @@
+% Look at random examples of endings which were created by the
+% splitting of a chiasma. Questions to answer:
+% 
+%   1) How many of these are there?
+%   2) How many of these are splits?
+%   3) How can we query them?
+%      a) Seeding: Position and direction?
+%      b) Attachment
+%
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+outputDir = '/home/amotta/Desktop/chiasma-healing';
+
+%% loading parameters
+paramFile = fullfile(rootDir, 'allParameter.mat');
+param = load(paramFile, 'p');
+param = param.p;
+
+%% loading axons
+axonFile = fullfile(rootDir, 'aggloState', 'axons_13_a.mat');
+axons = load(axonFile, 'axons', 'indBigAxons');
+
+axonIds = find(axons.indBigAxons);
+axons = axons.axons(axonIds);
+
+%% collecting and counting endings
+endings = table;
+endings.axonId = repelem( ...
+    reshape(1:numel(axons), [], 1), ...
+    arrayfun(@(a) numel(a.endings), axons));
+endings.nodeId = cat(1, axons.endings);
+
+numEndings = size(endings, 1);
+fprintf('Total number of endings: %d\n', numEndings);
+
+%% exporting random examples
+numExamples = 20;
+
+rng(0);
+randIds = randperm(size(endings, 1), numExamples);
+randEndings = endings(randIds, :);
+
+mkdir(outputDir);
+
+for curIdx = 1:numExamples
+    curSkel = skeleton();
+    
+    curAxonId = randEndings.axonId(curIdx);
+    curNodeId = randEndings.nodeId(curIdx);
+    
+    curAxon = axons(curAxonId);
+    curNodeCount = size(curAxon.nodes, 1);
+    
+    curComments = repmat({''}, [curNodeCount, 1]);
+    curComments{curNodeId} = 'Ending';
+    
+    curSkel = curSkel.addTree( ...
+        'Axon', curAxon.nodes(:, 1:3), ...
+        curAxon.edges, [], [], curComments);
+    curSkel = curSkel.addBranchpoint(curNodeId);
+    curSkel = Skeleton.setParams4Pipeline(curSkel, param);
+    
+    curSkelFile = sprintf( ...
+        '%d__axon-%d__node-%d.nml', ....
+        curIdx, curAxonId, curNodeId);
+    curSkel.write(fullfile(outputDir, curSkelFile));
+end
