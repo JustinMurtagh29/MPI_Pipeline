01408993d77af149d90b40cf417cee08cc5d909e
Benedikt Staffler <benedikt.staffler@brain.mpg.de>
2017-12-06 10:49:38 +0100

    also output the original border position

--- +connectEM/processQueryTasks.m
+++ +connectEM/processQueryTasks.m
@@ -1,26 +1,29 @@
-function processQueryTasks(i,batchBoundaries,axons,borderPositions,directions,p,outputFolder)
+function processQueryTasks(i, batchBoundaries, axons, borderPositions, ...
+    directions, p, outputFolder)
 
- % Written by
+    % Written by
     %   Manuel Berning <manuel.berning@brain.mpg.de>
     %   Christian Schramm <christian.schramm@brain.mpg.de>
     % Collects the information for querying agglo endings. The number of 
     % agglos is presumly (connectEM.generateAxonQueries) divided into 100
     % batches for parallelisation.
     % Input parameters:
-    %   i 
-    %     index of batch
+    %   i: int
+    %     index of current batch
     %   batchBoundaries
-    %     (1x100) array defining the borders of the batches
+    %     (1xN) array containing the start indices for each batch. The last
+    %       index for a batch is given by batchBoundaries(i+1)-1, i.e.
+    %       batchBoundaries(end+1) should be length(axons) + 1
     %   axons
-    %     super agglomerates
+    %     axon agglomerates in the old format (i.e. list of segment ids)
     %   borderPositions
     %     border positions of chosen endings
     %   directions
     %     pca of border ID
-    %   p
-    %     parameter file
-    %   outputFolder
-    %     output directory
+    %   p: struct
+    %     segmentation parameter file
+    %   outputFolder: string
+    %     output directory. output files are of the format 'batch_\d.mat'
 
     theseIdx = batchBoundaries(i):batchBoundaries(i+1)-1;
     theseAxons = axons(theseIdx);
@@ -28,16 +31,24 @@ function processQueryTasks(i,batchBoundaries,axons,borderPositions,directions,p,
     theseDirections = directions(theseIdx);
 
     q.pos = {};
+    q.posBorder = {};
     q.dir = {};
     q.angles = {};
     for j=1:length(theseAxons)
         tic;
-        thesePositions = cellfun(@(x,y,z)connectEM.correctQueryLocationToEndOfSegment(p, x, y, z, 200), ...
-            repmat(theseAxons(j),size(theseBorderPositions{j},1),1), theseBorderPositions{j}, theseDirections{j}, 'uni', 0);
+        q.posBorder{end+1} = theseBorderPositions{j};
+        thesePositions = cellfun(@(x,y,z) ...
+            connectEM.correctQueryLocationToEndOfSegment(p, x, y, z, 200), ...
+            repmat(theseAxons(j),size(theseBorderPositions{j},1),1), ...
+            theseBorderPositions{j}, ...
+            theseDirections{j}, 'uni', 0);
         q.pos{end+1} = thesePositions;
         q.dir{end+1} = theseDirections{j};
-        [phi, theta, psi] = cellfun(@(x)connectEM.calculateEulerAngles(x, p.raw.voxelSize), q.dir{end});
-        q.angles{end+1} = mat2cell(cat(2, phi, theta, psi), ones(numel(phi),1), 3);
+        [phi, theta, psi] = cellfun(@(x) ...
+            connectEM.calculateEulerAngles(x, p.raw.voxelSize), ...
+            q.dir{end});
+        q.angles{end+1} = mat2cell(cat(2, phi, theta, psi), ...
+            ones(numel(phi),1), 3);
         toc;
     end
 
@@ -46,6 +57,5 @@ function processQueryTasks(i,batchBoundaries,axons,borderPositions,directions,p,
         'q', 'theseAxons');
     display(['Batch ' num2str(i, '%.4i') ' done']);
     clear these* q phi theta psi;
-
 end
 
