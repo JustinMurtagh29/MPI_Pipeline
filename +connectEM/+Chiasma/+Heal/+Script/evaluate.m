be4ca5b883f94c62321ea8a635fed31645f4dd11
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-12-07 10:39:07 +0100

    Evaluate direction for query generation

--- +connectEM/+Chiasma/+Heal/+Script/evaluate.m
+++ +connectEM/+Chiasma/+Heal/+Script/evaluate.m
@@ -26,11 +26,7 @@ axonIds = find(axons.indBigAxons);
 axons = axons.axons(axonIds);
 
 %% collecting and counting endings
-endings = table;
-endings.axonId = repelem( ...
-    reshape(1:numel(axons), [], 1), ...
-    arrayfun(@(a) numel(a.endings), axons));
-endings.nodeId = cat(1, axons.endings);
+endings = connectEM.Chiasma.Heal.buildTaskDefs(param, axons);
 
 numEndings = size(endings, 1);
 fprintf('Total number of endings: %d\n', numEndings);
@@ -61,6 +57,14 @@ for curIdx = 1:numExamples
         curAxon.edges, [], [], curComments);
     curSkel = curSkel.addBranchpoint(curNodeId);
     curSkel = Skeleton.setParams4Pipeline(curSkel, param);
+    
+    curQueryStart = randEndings.position(curIdx, :);
+    curQueryEnd = randEndings.direction(curIdx, :);
+    curQueryEnd = curQueryEnd ./ norm(curQueryEnd);
+    curQueryEnd = curQueryStart + 10 .* curQueryEnd;
+    
+    curSkel = curSkel.addTree( ...
+        'Query', [curQueryStart; curQueryEnd], [1, 2], [0, 0, 1, 1]);
     
     curSkelFile = sprintf( ...
         '%d__axon-%d__node-%d.nml', ....
