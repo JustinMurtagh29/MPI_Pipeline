c61173f9f157b554d3c378667ef5099c2f73947a
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-12-07 11:51:57 +0100

    Write out tasks to heal chiasma splits in axons 13

---
+++ +connectEM/+Chiasma/+Heal/+Script/queryAxons13.m
@@ -0,0 +1,59 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+runId = datestr(now, 30);
+
+%% configuration
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+
+thisDir = fullfile( ...
+    rootDir, 'chiasmaSplitHealing', ...
+    '20171207T112927-healing-axons-13');
+assert(logical(exist(thisDir, 'dir')));
+
+% segmentation for dynamic stopping of queries
+segRoot = '/tmpscratch/amotta/l4/2017-12-07-segmentation-for-axons-13';
+
+info = Util.runInfo();
+
+%% loading parameters
+paramFile = fullfile(rootDir, 'allParameter.mat');
+param = load(paramFile, 'p');
+param = param.p;
+
+%% loading axons
+axonFile = fullfile(rootDir, 'aggloState', 'axons_13_a.mat');
+axons = load(axonFile, 'axons', 'indBigAxons');
+
+axonIds = find(axons.indBigAxons);
+axons = axons.axons(axonIds);
+
+%% generate task and webKNOSSOS task definitions
+taskDefFile = fullfile(thisDir, sprintf('%s_flightTasks.txt', runId));
+taskDefs = connectEM.Chiasma.Heal.generateTasks(param, axons, taskDefFile);
+
+%%
+out = struct;
+out.info = info;
+out.taskDefs = taskDefs;
+out.taskDefFile = taskDefFile;
+
+outFile = sprintf('%s_taskGeneration.mat', runId);
+Util.saveStruct(fullfile(outputDir, outFile), out);
+
+%% generate segmentation for dynamic stopping
+%{
+maxSegId = Seg.Global.getMaxSegId(param);
+agglos = Superagglos.getSegIds(axons);
+mapping = Agglo.buildLUT(maxSegId, agglos);
+
+seg = struct;
+seg.root = fullfile(segRoot, '1');
+seg.prefix = param.seg.prefix;
+seg.backend = 'wkwrap';
+
+% Initialize WKW dataset
+wkwInit('new', seg.root, 32, 32, 'uint32', 1);
+Seg.Global.applyMappingToSegmentation(param, mapping, seg);
+compressSegmentation(seg);
+%}
