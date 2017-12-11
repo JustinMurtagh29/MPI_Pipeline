0680ecf35e40894277fd969f1fb8d7ef1ecda565
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-12-11 13:29:21 +0100

    Start working on chiasma split healing

---
+++ +connectEM/+Chiasma/+Heal/loadData.m
@@ -0,0 +1,14 @@
+function data = loadData(param, taskGenFile, taskIdFile, nmlDir)
+    % Written by
+    %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    taskGenData = load(taskGenFile);
+    taskIds = connectEM.Chiasma.Util.loadTaskIds(taskIdFile);
+    flights = connectEM.Chiasma.Util.loadFlightPaths(param, nmlDir);
+    
+    data = struct;
+    data.axonFile = taskGenData.info.param.axonFile;
+    data.endings  = taskGenData.endings;
+    data.taskDef  = taskGenData.taskDefs;
+    data.taskIds  = taskIds.id;
+    data.flights  = flights;
+end
