9f15d1ae491d366f3faf5db8b38e0c9052cb8a14
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-12-11 18:00:09 +0100

    Make `dropIfCommented` work for two-dimensional fields

--- +connectEM/+Flight/dropIfCommented.m
+++ +connectEM/+Flight/dropIfCommented.m
@@ -2,5 +2,5 @@ function [flights, mask] = dropIfCommented(flights)
     % Written by
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
     mask = cellfun(@isempty, flights.comments);
-    flights = structfun(@(f) f(mask), flights, 'UniformOutput', false);
+    flights = structfun(@(f) f(mask, :), flights, 'UniformOutput', false);
 end
