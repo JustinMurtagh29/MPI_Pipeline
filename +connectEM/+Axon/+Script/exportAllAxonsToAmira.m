8c9630ed229444595df47313dc4eb8345e283a86
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-01-09 14:51:52 +0100

    Export current axon state to Amira

---
+++ +connectEM/+Axon/+Script/exportAllAxonsToAmira.m
@@ -0,0 +1,20 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+
+%% configuration
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+axonFile = fullfile(rootDir, 'aggloState', 'axons_16_b.mat');
+outDir = '/tmpscratch/amotta/l4/2018-01-09-all-axons-for-amira';
+
+%% loading data
+fprintf('Loading data... ');
+param = load(fullfile(rootDir, 'allParameter.mat'), 'p');
+param = param.p;
+
+axons = load(axonFile, 'axons', 'indBigAxons');
+axons = axons.axons(axons.indBigAxons);
+fprintf('done!\n');
+
+%% exporting to amira
+Superagglos.exportToAmira(param, axons, outDir);
