37e27f07da674b6236ede6268ec4c371718d8f35
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-01-19 10:40:43 +0100

    Summary statistics over axon path length

--- +connectEM/+Axon/+Script/calculatePathLengths.m
+++ +connectEM/+Axon/+Script/calculatePathLengths.m
@@ -9,7 +9,6 @@ clear;
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
 axonFile = fullfile(rootDir, 'aggloState', 'axons_16_b.mat');
 
-
 [outDir, outFile] = fileparts(axonFile);
 outFile = sprintf('%s_meta.mat', outFile);
 outFile = fullfile(outDir, outFile);
@@ -46,17 +45,31 @@ save(outFile, '-struct', 'out', append{:});
 % load(outFile, 'pathLen');
 pathLenUm = pathLen ./ 1E3;
 
+percUm = prctile(pathLenUm, [10, 90]);
+medianUm = median(pathLenUm);
+meanUm = mean(pathLenUm);
+maxUm = max(pathLenUm);
+totalM = sum(pathLenUm) / 1E6;
+
+fprintf('Axon path length distribution:\n');
+fprintf('  10th percentile: %.1f µm\n', percUm(1));
+fprintf('           Median: %.1f µm\n', medianUm);
+fprintf('             Mean: %.1f µm\n', meanUm);
+fprintf('  90th percentile: %.1f µm\n', percUm(2));
+fprintf('          Maximum: %.1f µm\n', maxUm);
+fprintf('\n');
+fprintf('Total path length: %.3f m\n', totalM);
+fprintf('\n');
+
 figure;
-histogram(pathLenUm, linspace(0, 500, 100));
+histogram(pathLenUm, 100);
 title('Axonal path length distribution');
 xlabel('Path length (MST) [µm]');
-xlim([0, 500]);
 
 fig = figure;
 ax = axes(fig);
-histogram(ax, pathLenUm, linspace(0, 500, 100));
+histogram(ax, pathLenUm, 100);
 title(ax, 'Axonal path length distribution');
 xlabel(ax, 'Path length (MST) [µm]');
-xlim(ax, [0, 500]);
 ax.YScale = 'log';
-gca.YScale = 'log';
+
