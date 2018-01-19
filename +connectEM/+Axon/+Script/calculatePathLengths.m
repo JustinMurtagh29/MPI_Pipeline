47821c6e40d7d59af11bef234e25cf1ab7a15d1b
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-01-19 10:17:06 +0100

    Plot axonal path length histograms

--- +connectEM/+Axon/+Script/calculatePathLengths.m
+++ +connectEM/+Axon/+Script/calculatePathLengths.m
@@ -9,6 +9,12 @@ clear;
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
 axonFile = fullfile(rootDir, 'aggloState', 'axons_16_b.mat');
 
+
+[outDir, outFile] = fileparts(axonFile);
+outFile = sprintf('%s_meta.mat', outFile);
+outFile = fullfile(outDir, outFile);
+clear outDir;
+
 info = Util.runInfo();
 
 %% loading data
@@ -26,10 +32,6 @@ pathLen = Superagglos.mstLength(agglos, param.raw.voxelSize);
 fprintf('done!\n');
 
 %% write output
-[outDir, outFile] = fileparts(axonFile);
-outFile = sprintf('%s_meta.mat', outFile);
-outFile = fullfile(outDir, outFile);
-
 append = {{}, {'-append'}};
 append = append{1 + (exist(outFile, 'file') ~= 0)};
 
@@ -39,3 +41,22 @@ out.pathLenInfo = info;
 
 % write result
 save(outFile, '-struct', 'out', append{:});
+
+%% plotting
+% load(outFile, 'pathLen');
+pathLenUm = pathLen ./ 1E3;
+
+figure;
+histogram(pathLenUm, linspace(0, 500, 100));
+title('Axonal path length distribution');
+xlabel('Path length (MST) [µm]');
+xlim([0, 500]);
+
+fig = figure;
+ax = axes(fig);
+histogram(ax, pathLenUm, linspace(0, 500, 100));
+title(ax, 'Axonal path length distribution');
+xlabel(ax, 'Path length (MST) [µm]');
+xlim(ax, [0, 500]);
+ax.YScale = 'log';
+gca.YScale = 'log';
