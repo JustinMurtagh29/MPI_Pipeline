c0f94ef2f77e132d12edc8a1c83fa181929517c9
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-02-01 09:43:01 +0100

    Specifically look at axons targeting the somata of interneurons

--- +connectEM/+Connectome/analyseSomata.m
+++ +connectEM/+Connectome/analyseSomata.m
@@ -10,8 +10,15 @@ connFile = fullfile( ...
     'connectome_axons_18_a_with_den_meta.mat');
 
 minSynPre = 10;
-minSynPost = 0;
 spineFracThresh = 0.5;
+
+% IDs of interneuron somata
+% relative to all the 'Somata' agglomerates
+inSomaIdsRel = [13, 56];
+minInSomaSynCount = 1;
+minSpineFracExc = 0.5;
+
+info = Util.runInfo();
 
 %% loading data
 param = load(fullfile(rootDir, 'allParameter.mat'), 'p');
@@ -22,13 +29,15 @@ sizes = Seg.Global.getSegToSizeMap(param);
 
 conn = load(connFile);
 
+%% find soma agglomerates
+somaIds = find(conn.denMeta.targetClass == 'Somata');
+
 %% soma synapse analysis
 % show number of synapses per soma
 fig = figure;
 ax = axes(fig);
 
-somaMask = (conn.denMeta.targetClass == 'Somata');
-somaSynCount = conn.denMeta.synCount(somaMask);
+somaSynCount = conn.denMeta.synCount(somaIds);
 
 histogram(ax, somaSynCount, 21);
 
@@ -41,7 +50,7 @@ ax.YAxis.TickDirection = 'out';
 fig.Position(3:4) = [570, 350];
 
 %% calculate per-soma position
-somaAgglos = conn.dendrites(somaMask);
+somaAgglos = conn.dendrites(somaIds);
 somaSize = cellfun(@(segIds) ...
     sum(sizes(segIds)), somaAgglos);
 somaPos = cellfun(@(segIds) ...
@@ -64,3 +73,106 @@ skel = skel.addNodesAsTrees( ...
     somaPos(somaIds, :), treeNames);
 skel = Skeleton.setParams4Pipeline(skel, param);
 skel.write('/home/amotta/Desktop/somata-with-synapse-count.nml');
+
+%% find axons which make synapses onto IN somata
+inSomaConn = conn.connectome;
+
+inSomaIds = find(conn.denMeta.targetClass == 'Somata');
+inSomaIds = inSomaIds(inSomaIdsRel);
+
+[~, inSomaConn.edges(:, 2)] = ...
+    ismember(inSomaConn.edges(:, 2), inSomaIds);
+inSomaConn(~inSomaConn.edges(:, 2), :)  = [];
+
+inSomaAxonIds = find(conn.axonMeta.synCount >= minSynPre);
+inSomaAxonIds = intersect(inSomaAxonIds, inSomaConn.edges(:, 1));
+
+[~, inSomaConn.edges(:, 1)] = ...
+    ismember(inSomaConn.edges(:, 1), inSomaAxonIds);
+inSomaConn(~inSomaConn.edges(:, 1), :) = [];
+
+% count synapses
+inSomaConn.synCount = cellfun(@numel, inSomaConn.synIdx);
+
+% count IN soma synapses per axon
+axonInSomaSynCount = accumarray( ...
+    inSomaConn.edges(:, 1), ...
+    inSomaConn.synCount, ...
+   [numel(inSomaAxonIds), 1]);
+
+%% plot spine synapse fraction for these axons
+spineSynFracAll = conn.axonMeta.spineSynCount ./ conn.axonMeta.synCount;
+spineSynFrac = spineSynFracAll(inSomaAxonIds);
+
+fig = figure();
+ax = axes(fig);
+
+histogram(ax, spineSynFrac, linspace(0, 1, 21));
+xlim(ax, [0, 1]); xticks(ax, [0, 1]);
+xlabel(ax, 'Spine synapse fraction');
+ylabel(ax, {'Axons with'; '≥ 1 synapse onto IN somata'});
+
+annotation( ...
+    fig, ...
+    'textbox', [0, 0.9, 1, 0.1], ...
+	'String', { ...
+        'Spine synapse fraction of IN soma targeting axons'; ...
+        info.git_repos{1}.hash}, ...
+    'EdgeColor', 'none', 'HorizontalAlignment', 'center');
+
+fig.Position(3:4) = [678, 475];
+
+%% plot specificities for these axons vs. all excitatory axons
+excAxonMask = ...
+    conn.axonMeta.synCount >= minSynPre ...
+  & spineSynFracAll >= minSpineFracExc;
+axonSpecsAll = conn.classConnectome ./ sum(conn.classConnectome, 2);
+
+axonClasses = { ...
+   {'Axons with'; '≥ 1 synapse onto IN somata'}, axonSpecsAll(inSomaAxonIds, :); ...
+   {'Axons with'; sprintf('spine synapse fraction ≥ %.1f', minSpineFracExc)}, axonSpecsAll(excAxonMask, :)};
+axonClassCount = size(axonClasses, 1);
+
+fig = figure();
+
+targetClassNames = conn.denClasses;
+targetClassCount = numel(targetClassNames);
+
+for curTargetClassIdx = 1:targetClassCount
+    for curAxonClassIdx = 1:axonClassCount
+        curAx = subplot( ...
+            axonClassCount, targetClassCount, ...
+            curTargetClassIdx + (curAxonClassIdx - 1) * targetClassCount);
+        
+        curAxonSpecs = axonClasses{curAxonClassIdx, 2};
+        curAxonSpecs = curAxonSpecs(:, curTargetClassIdx);
+        histogram(curAx, curAxonSpecs, linspace(0, 1, 21));
+        
+        curAx.TickDir = 'out';
+        
+        xlim(curAx, [0, 1]);
+        xticks(curAx, [0, 1]);
+        xticklabels(curAx, {});
+        
+        if curAxonClassIdx == axonClassCount
+            xticklabels(curAx, {'0', '1'});
+            xlabel(curAx, sprintf('S(%s)', ...
+                targetClassNames{curTargetClassIdx}));
+        end
+        
+        if curTargetClassIdx == 1
+            curAxonClassName = axonClasses{curAxonClassIdx, 1};
+            ylabel(curAx, curAxonClassName);
+        end
+    end
+end
+
+annotation( ...
+    fig, ...
+    'textbox', [0, 0.9, 1, 0.1], ...
+	'String', { ...
+        'IN soma targeting axons versus all spine-targeting axons'; ...
+        info.git_repos{1}.hash}, ...
+    'EdgeColor', 'none', 'HorizontalAlignment', 'center');
+
+fig.Position(3:4) = [1758, 631];
