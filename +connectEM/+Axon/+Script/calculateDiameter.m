7d0e268ca46e25f013a34dabf0ccdbbc48d3f8ee
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-02-06 13:37:39 +0100

    Calculate local diameter for all axons and store result

--- +connectEM/+Axon/+Script/calculateDiameter.m
+++ +connectEM/+Axon/+Script/calculateDiameter.m
@@ -6,6 +6,13 @@ clear;
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
 connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons_18_a.mat');
 
+[outFile, outName] = fileparts(connFile);
+outName = sprintf('%s_diameter.mat', outName);
+outFile = fullfile(outFile, outName);
+clear outName;
+
+info = Util.runInfo();
+
 %% loading data
 conn = load(connFile);
 
@@ -21,5 +28,10 @@ segCov = load(fullfile(rootDir, 'globalSegmentPCA.mat'), 'covMat');
 segCov = reshape(segCov.covMat, [], 3, 3);
 
 %% calculate diameters
+tic; fprintf('Calculating axon diameters... ');
 [segIds, coms, diams] = Agglo.calculateDiameter( ...
     segSizes, segCentroids, segCov, conn.axons, 'nhoodThresh', 750);
+fprintf('done!\n'); toc;
+
+%% save result
+Util.save(outFile, info, segIds, coms, diams);
