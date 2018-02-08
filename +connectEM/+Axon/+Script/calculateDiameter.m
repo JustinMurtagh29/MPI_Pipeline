71d60b1aaabdbb897d9b8ac7335043438ff74faa
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-02-08 14:03:27 +0100

    Also calculate node weights

--- +connectEM/+Axon/+Script/calculateDiameter.m
+++ +connectEM/+Axon/+Script/calculateDiameter.m
@@ -33,5 +33,31 @@ tic; fprintf('Calculating axon diameters... ');
     segSizes, segCentroids, segCov, conn.axons, 'nhoodThresh', 750);
 fprintf('done!\n'); toc;
 
+%% calculate node weights
+% NOTE(amotta): To calculate the mean or median diameter of an axon, we
+% need to know how much each measurement (i.e., node) contributes to the
+% whole axon. Let's do this by calculating the sum of all half-edges for
+% each node...
+weights = cell(size(coms));
+for curIdx = 1:numel(coms)
+    curComs = coms{curIdx};
+    
+    if size(curComs, 1) == 1
+        weights{curIdx} = 1;
+    else
+        curAdj = pdist(curComs);
+        curAdj = sparse(squareform(curAdj));
+        curAdj = graphminspantree(curAdj);
+        
+        % sanity check
+        assert(size(curAdj, 1) == size(curComs, 1));
+        
+        curWeights = sum(curAdj, 1) + sum(curAdj, 2)';
+        curWeights = full(curWeights(:)) / 2;
+        
+        weights{curIdx} = curWeights;
+    end
+end
+
 %% save result
-Util.save(outFile, info, segIds, coms, diams);
+Util.save(outFile, info, segIds, coms, diams, weights);
