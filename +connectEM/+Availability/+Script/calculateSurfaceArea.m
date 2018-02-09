617f374e8e32f05fa0356d97ec5efb0887aa5ac6
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-02-09 14:55:02 +0100

    Calculate per-target-class surface areas over whole dataset

--- +connectEM/+Availability/+Script/calculateSurfaceArea.m
+++ +connectEM/+Availability/+Script/calculateSurfaceArea.m
@@ -6,7 +6,7 @@ clear;
 %% configuration
 blockSize = [32, 32, 16];
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
-connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons_18_a.mat');
+connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons_18_a_with_den_meta.mat');
 outDir = '/tmpscratch/amotta/l4/2018-02-09-surface-availability-connectome-axons-18-a';
 
 %% loading data
@@ -15,7 +15,7 @@ param = param.p;
 
 %% actually start job
 inputArgs = arrayfun(@(i) {{i}}, 1:numel(param.local));
-sharedInputArgs = {param, blockSize, connFile, outDir};
+sharedInputArgs = {param, connFile, blockSize};
 mkdir(outDir);
 
 cluster = Cluster.getCluster( ...
@@ -25,6 +25,24 @@ cluster = Cluster.getCluster( ...
     '-l s_rt=0:59:00');
 
 job = Cluster.startJob( ...
-    @connectEM.Availability.calculateSurfaceArea, ...
-    inputArgs, 'sharedInputs', sharedInputArgs, ...
-    'cluster', cluster, 'name', mfilename);
+    @jobFunction, inputArgs, ...
+    'sharedInputs', sharedInputArgs, ...
+    'numOutputs', 2, ...
+    'cluster', cluster, ...
+    'name', mfilename);
+
+%% function running on workers
+function [classSurfAreas, classes] = ...
+        jobFunction(param, connFile, blockSize, cubeIdx)
+    import connectEM.Availability.*;
+    
+    %% loading data
+    conn = load(connFile);
+    
+    box = param.local(cubeIdx).bboxSmall;
+    seg = loadSegDataGlobal(param.seg, box);
+    
+    %% compute
+   [classSurfAreas, classes] = ...
+        calculateSurfaceAreaOfTargetClasses(param, conn, seg, blockSize);
+end
