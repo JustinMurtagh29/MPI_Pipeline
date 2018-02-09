df72b6593bdd7802009b16dc64dfe0045ca4ebf1
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-02-09 16:27:20 +0100

    Simplify script for launching precomputations

---
+++ +connectEM/+Availability/+Script/precompute.m
@@ -0,0 +1,19 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+
+%% configuration
+blockSize = [32, 32, 16];
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons_18_a_with_den_meta.mat');
+outDir = '/tmpscratch/amotta/l4/2018-02-09-surface-availability-connectome-axons-18-a';
+
+info = Util.runInfo();
+
+%% load parameter
+param = load(fullfile(rootDir, 'allParameter.mat'), 'p');
+param = param.p;
+
+%% precompute
+[axonBlocks, targetClassAreas, targetClasses] = ...
+	connectEM.Availability.precompute(param, connFile, blockSize);
