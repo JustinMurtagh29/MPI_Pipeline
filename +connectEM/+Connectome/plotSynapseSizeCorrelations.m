6fd03b3eb34b97c21bb6c4c17bcd412ae1f539c9
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-02-12 23:28:58 +0100

    Add possibility to only consider spine synapses

--- +connectEM/+Connectome/plotSynapseSizeCorrelations.m
+++ +connectEM/+Connectome/plotSynapseSizeCorrelations.m
@@ -1,30 +1,55 @@
-% Written by
+% TODO(amotta)
+% * `contactArea` in `connectomeMeta` ranges up to ~120. That's true even
+%   for spine-head filtered synapses. Find out what's going on: Do I
+%   misunderstand the data structure or the physical units? Is there a bug?
+%   Is synapse agglomeration off? Soma segments classified as spine?
+%
+% Written by`
 %   Alessandro Motta <alessandro.motta@brain.mpg.de>
 clear;
 
 %% configuration
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+synFile = fullfile(rootDir, 'connectomeState', 'SynapseAgglos_v3.mat');
 connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons_18_a_with_den_meta.mat');
+
+%{
+synFile = '/home/amotta/Desktop/SynapseAgglos_v3.mat';
+connFile = '/home/amotta/Desktop/connectome_axons_18_a_with_den_meta.mat';
+%}
+
+synCountFilter = @(n) (n == 2); % couplings with exactly two synapses, only
+spineSynFilter = @(s) s; % spine synapses only
 
 %% loading data
 % NOTE(amotta): Synapses sizes are contained in the `contactArea` field of 
 % `conn.connectomeMeta`. Each cell contains the synapses sizes of the
 % correponding entries in `conn.connectome`.
 conn = load(connFile);
+syn = load(synFile);
 
 %% find doubly coupled neurites
+% filter based on spine / non-spine
+spineSynMask = spineSynFilter(syn.isSpineSyn);
+
+conn.connectomeMeta.contactArea = cellfun( ...
+    @(ids, areas) areas(spineSynMask(ids)), ...
+    conn.connectome.synIdx, conn.connectomeMeta.contactArea, ...
+    'UniformOutput', false);
+conn.connectome.synIdx = cellfun( ...
+    @(ids) ids(spineSynMask(ids)), ...
+    conn.connectome.synIdx, 'UniformOutput', false);
+
+% filter based on synapse count
 conn.connectome.synCount = ...
     cellfun(@numel, conn.connectome.synIdx);
+synCountMask = synCountFilter(conn.connectome.synCount);
 
-prePostPairMask = (conn.connectome.synCount == 2);
-nrPrePostPairs = sum(prePostPairMask);
-
-jointPreIds = unique(conn.connectome.edges(prePostPairMask, 1));
-jointPostIds = unique(conn.connectome.edges(prePostPairMask, 2));
+conn.connectome(~synCountMask, :) = [];
+conn.connectomeMeta.contactArea(~synCountMask) = [];
 
 %% plot correlation for pre- & post-coupled synapses
-prePostPairs = conn.connectome.edges(prePostPairMask, :);
-prePostPairSynAreas = conn.connectomeMeta.contactArea(prePostPairMask);
+prePostPairSynAreas = conn.connectomeMeta.contactArea;
 
 % sort areas within
 prePostPairSynAreas = cellfun( ...
