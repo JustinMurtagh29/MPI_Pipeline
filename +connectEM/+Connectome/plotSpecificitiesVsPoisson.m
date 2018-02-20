d15c850740008ed36ebdcb3d3d5bf9448bd19d9b
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-02-20 14:40:01 +0100

    Poisson comparison not only for INH but also EXC axons

--- +connectEM/+Connectome/plotSpecificitiesVsPoisson.m
+++ +connectEM/+Connectome/plotSpecificitiesVsPoisson.m
@@ -11,20 +11,10 @@ targetClasses = { ...
     'SmoothDendrite', 'AxonInitialSegment', 'OtherDendrite'};
 
 minSynPre = 10;
-maxSpineSynFracPoiss = 0.5;
 info = Util.runInfo();
 
 %% loading data
 conn = load(connFile);
-
-%% find inhibitory axons
-conn.axonMeta.spineSynFrac = ...
-    conn.axonMeta.spineSynCount ...
-    ./ conn.axonMeta.synCount;
-
-axonIds = find(conn.axonMeta.synCount >= minSynPre);
-poissAxonIds = find(conn.axonMeta.spineSynFrac < maxSpineSynFracPoiss);
-poissAxonIds = intersect(axonIds, poissAxonIds);
 
 %% build class connectome
 % try to replicate cass connectome
@@ -45,79 +35,115 @@ classConnectome = accumarray( ...
     cat(2, connectome.edges(:, 1), connectome.targetClassId), ...
     connectome.synCount, [numel(conn.axons), numel(targetClasses)]);
 
-%% calculate probabilities for Poisson
-targetClassSyns = sum(classConnectome(poissAxonIds, :), 1);
-targetClassProbs = targetClassSyns ./ sum(targetClassSyns);
+%% build axon classes
+conn.axonMeta.spineSynFrac = ...
+    conn.axonMeta.spineSynCount ...
+    ./ conn.axonMeta.synCount;
 
-%% restrict to axons with enough synapses
-[synCounts, ~, synCountAxons] = unique(conn.axonMeta.synCount(axonIds));
-synCountAxons = accumarray(synCountAxons, 1);
+axonClasses = struct;
+axonClasses(1).axonIds = find( ...
+    conn.axonMeta.synCount >= minSynPre ...
+  & conn.axonMeta.spineSynFrac > 0.7);
+axonClasses(1).title = 'axons with spine synapse fraction > 0.7';
 
-specificities = classConnectome ./ sum(classConnectome, 2);
-specificities = specificities(axonIds, :);
+axonClasses(2).axonIds = find( ...
+    conn.axonMeta.synCount >= minSynPre ...
+  & conn.axonMeta.spineSynFrac > 0.5);
+axonClasses(2).title = 'axons with spine synapse fraction > 0.5';
 
-%% plotting
-fig = figure;
-binEdges = linspace(0, 1, 26);
-axes = cell(size(targetClasses));
+axonClasses(3).axonIds = find( ...
+    conn.axonMeta.synCount >= minSynPre ...
+  & conn.axonMeta.spineSynFrac < 0.5);
+axonClasses(3).title = 'axons with spine synapse fraction < 0.5';
 
-for classIdx = 1:numel(targetClasses)
-    className = targetClasses{classIdx};
-    classProb = targetClassProbs(classIdx);
-    
-    % Poissons
-    poiss = table;
-    poiss.prob = cell2mat(arrayfun(@(nSyn, nAxons) ...
-        nAxons * poisspdf((0:nSyn)', nSyn * classProb), ...
-        synCounts, synCountAxons, 'UniformOutput', false));
-    poiss.spec = cell2mat(arrayfun( ...
-        @(nSyn) (0:nSyn)' ./ nSyn, ...
-        synCounts, 'UniformOutput', false));
-    
-    poiss.binId = discretize(poiss.spec, binEdges);
-    poissBinCount = accumarray(poiss.binId, poiss.prob);
-    
-    % Measured
-    
-    ax = subplot(1, numel(targetClasses), classIdx);
-    hold(ax, 'on');
-    
-    histogram(ax, ...
-        'BinEdges', binEdges, ...
-        'BinCounts', poissBinCount, ...
-        'DisplayStyle', 'stairs', ...
-        'LineWidth', 2);
-    
-    histogram(ax, ...
-        specificities(:, classIdx), binEdges, ...
-        'DisplayStyle', 'stairs', ...
-        'LineWidth', 2);
+axonClasses(4).axonIds = find( ...
+    conn.axonMeta.synCount >= minSynPre ...
+  & conn.axonMeta.spineSynFrac < 0.3);
+axonClasses(4).title = 'axons with spine synapse fraction < 0.3';
 
-    xlabel(ax, sprintf('S(%s)', className));
-    ax.XAxis.TickDirection = 'out';
-    ax.XAxis.Limits = [0, 1];
-    
-    ax.YAxis.TickDirection = 'out';
-    ax.YAxis.Limits(1) = 10 ^ (-0.1);
-    ax.YAxis.Scale = 'log';
-    
-    if classIdx == numel(targetClasses)
-        legend(ax, ...
-            'Expected (Poisson)', 'Observed', ...
-            'Location', 'NorthEast');
-    end
-    
-    axes{classIdx} = ax;
+%% plot
+for curIdx = 1:numel(axonClasses)
+    plotAxonClass( ...
+        info, conn.axonMeta, classConnectome, ...
+        targetClasses, axonClasses(curIdx));
 end
 
-axes = horzcat(axes{:});
-yMax = max(arrayfun(@(a) a.YAxis.Limits(end), axes));
-for ax = axes; ax.YAxis.Limits(end) = yMax; end
+%% plotting
+function plotAxonClass(info, axonMeta, classConn, targetClasses, axonClass)
+    %% preparations
+    % calculate probabilities for Poisson
+    targetClassSyns = sum(classConn(axonClass.axonIds, :), 1);
+    targetClassProbs = targetClassSyns / sum(targetClassSyns);
+    
+    specificities = classConn(axonClass.axonIds, :);
+    specificities = specificities ./ sum(specificities, 2);
 
-annotation(...
-    'textbox', [0, 0.9, 1, 0.1], ...
-    'EdgeColor', 'none', 'HorizontalAlignment', 'center', ...
-    'String', { ...
-        'Observed specificities vs. Poisson model';
-        sprintf('Based on axons with spine synapse fraction < %.1f', maxSpineSynFracPoiss);
-        info.git_repos{1}.hash});
+    % restrict to axons with enough synapses
+   [synCounts, ~, synCountAxons] = unique( ...
+        axonMeta.synCount(axonClass.axonIds));
+    synCountAxons = accumarray(synCountAxons, 1);
+    
+    %% plotting
+    fig = figure;
+    binEdges = linspace(0, 1, 26);
+    axes = cell(size(targetClasses));
+
+    for classIdx = 1:numel(targetClasses)
+        className = targetClasses{classIdx};
+        classProb = targetClassProbs(classIdx);
+
+        % Poissons
+        poiss = table;
+        poiss.prob = cell2mat(arrayfun(@(nSyn, nAxons) ...
+            nAxons * poisspdf((0:nSyn)', nSyn * classProb), ...
+            synCounts, synCountAxons, 'UniformOutput', false));
+        poiss.spec = cell2mat(arrayfun( ...
+            @(nSyn) (0:nSyn)' ./ nSyn, ...
+            synCounts, 'UniformOutput', false));
+
+        poiss.binId = discretize(poiss.spec, binEdges);
+        poissBinCount = accumarray(poiss.binId, poiss.prob);
+
+        % Measured
+        ax = subplot(1, numel(targetClasses), classIdx);
+        hold(ax, 'on');
+
+        histogram(ax, ...
+            'BinEdges', binEdges, ...
+            'BinCounts', poissBinCount, ...
+            'DisplayStyle', 'stairs', ...
+            'LineWidth', 2);
+
+        histogram(ax, ...
+            specificities(:, classIdx), binEdges, ...
+            'DisplayStyle', 'stairs', ...
+            'LineWidth', 2);
+
+        xlabel(ax, sprintf('S(%s)', className));
+        ax.XAxis.TickDirection = 'out';
+        ax.XAxis.Limits = [0, 1];
+
+        ax.YAxis.TickDirection = 'out';
+        ax.YAxis.Limits(1) = 10 ^ (-0.1);
+        ax.YAxis.Scale = 'log';
+
+        if classIdx == numel(targetClasses)
+            legend(ax, ...
+                'Expected (Poisson)', 'Observed', ...
+                'Location', 'NorthEast');
+        end
+
+        axes{classIdx} = ax;
+    end
+
+    axes = horzcat(axes{:});
+    yMax = max(arrayfun(@(a) a.YAxis.Limits(end), axes));
+    for ax = axes; ax.YAxis.Limits(end) = yMax; end
+
+    annotation(...
+        'textbox', [0, 0.9, 1, 0.1], ...
+        'EdgeColor', 'none', 'HorizontalAlignment', 'center', ...
+        'String', ...
+           {'Observed specificities vs. Poisson model'; ...
+            axonClass.title; info.git_repos{1}.hash});
+end
