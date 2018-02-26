d3437e304f555d9c02e0f801717e364e02221b2a
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-02-26 22:04:11 +0100

    Export more trees

--- +connectEM/+Connectome/exportMultiplyCoupledNeurites.m
+++ +connectEM/+Connectome/exportMultiplyCoupledNeurites.m
@@ -64,7 +64,7 @@ skelDesc = sprintf('%s (%s)', mfilename, info.git_repos{1}.hash);
 skel = skel.setDescription(skelDesc);
 
 rng(0);
-randIds = randperm(size(prePostPair, 1), 10);
+randIds = randperm(size(prePostPair, 1), 25);
 
 for curIdx = 1:numel(randIds)
     curId = randIds(curIdx);
