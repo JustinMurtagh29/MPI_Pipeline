570550b033dce3fddb65c03914ac7746ec4d9d22
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-02-28 14:30:23 +0100

    Plot specificity vs. distance for all target classes

--- +connectEM/+Connectome/plotSpecificitiesVsAvailabilities.m
+++ +connectEM/+Connectome/plotSpecificitiesVsAvailabilities.m
@@ -26,18 +26,73 @@ connectome.synIdx = [];
 [~, denMetaRow] = ismember(connectome.edges(:, 2), conn.denMeta.id);
 connectome.targetClass = conn.denMeta.targetClass(denMetaRow);
 
+% renumber classes alphabetically
+conn.denMeta.targetClass = reordercats(conn.denMeta.targetClass);
 targetClasses = unique(conn.denMeta.targetClass);
+
 [~, connectome.targetClassId] = ...
     ismember(connectome.targetClass, targetClasses);
 
 classConnectome = accumarray( ...
     cat(2, connectome.edges(:, 1), connectome.targetClassId), ...
     connectome.synCount, [numel(conn.axons), numel(targetClasses)]);
+specificities = classConnectome ./ sum(classConnectome, 2);
 
 %% build availabilities
 [~, classIds] = ismember(targetClasses, avail.targetClasses);
 availabilities = avail.axonAvail(classIds, :, :);
 availabilities = availabilities ./ sum(availabilities, 1);
+
+%% plot availability vs. distance
+axonIds = find(conn.axonMeta.synCount >= 10);
+cmap = parula(101);
+
+fig = figure();
+fig.Color = 'white';
+
+for classIdx = 1:numel(targetClasses)
+    className = char(targetClasses(classIdx));
+    
+   [~, sortIds] = sort(specificities(axonIds, classIdx), 'ascend');
+    axonIds = axonIds(sortIds);
+    
+    ax = subplot(1, numel(targetClasses), classIdx);
+    
+    ax.Color = 'black';
+    ax.TickDir = 'out';
+    
+    colormap(ax, cmap);
+    hold(ax, 'on');
+
+    for curAxonId = reshape(axonIds, 1, [])
+        curAvails = shiftdim(availabilities(classIdx, :, curAxonId));
+
+        curColor = specificities(curAxonId, classIdx);
+        curColor = cmap(round(100 * curColor) + 1, :);
+
+        plot(ax, avail.dists / 1E3, curAvails, 'Color', curColor);
+    end
+
+    xlabel(ax, {className; 'r_{pred} (µm)'});
+    xlim(ax, [0, avail.dists(end)] / 1E3);
+end
+
+% Y axis
+yMax = max(arrayfun(@(a) a.YLim(end), fig.Children));
+[fig.Children.YLim] = deal([0, yMax]);
+ylabel(fig.Children(end), 'Availability');
+
+% colorbar
+oldPos = ax.Position;
+cbar = colorbar(ax);
+cbar.Label.String = 'Specificity';
+ax.Position = oldPos;
+
+annotation( ...
+    fig, ...
+    'textbox', [0, 0.9, 1, 0.1], ...
+    'EdgeColor', 'none', 'HorizontalAlignment', 'center', ...
+    'String', {info.filename; info.git_repos{1}.hash});
 
 %% likelihoods
 axonTargetLik = cellfun(@numel, {avail.dists, conn.axons, targetClasses});
