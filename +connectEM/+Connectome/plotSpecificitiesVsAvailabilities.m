d5e77473f0eab628cc1e398abe9cca55e784d5a1
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-02-28 18:30:03 +0100

    Plot cosine similarity between availability and specificity

--- +connectEM/+Connectome/plotSpecificitiesVsAvailabilities.m
+++ +connectEM/+Connectome/plotSpecificitiesVsAvailabilities.m
@@ -223,21 +223,6 @@ annotation( ...
     'EdgeColor', 'none', 'HorizontalAlignment', 'center', ...
     'String', {info.filename; info.git_repos{1}.hash});
 
-%% likelihoods
-axonTargetLik = cellfun(@numel, {avail.dists, conn.axons, targetClasses});
-axonTargetLik = nan(axonTargetLik);
-
-for curAxonId = 1:numel(conn.axons)
-    curTargetSyns = classConnectome(curAxonId, :);
-    curSynCount = conn.axonMeta.synCount(curAxonId);
-    curLambdas = curSynCount * availabilities(:, :, curAxonId);
-    
-    for curClassIdx = 1:numel(targetClasses)
-        axonTargetLik(:, curAxonId, curClassIdx) = poisspdf( ...
-            curTargetSyns(curClassIdx), curLambdas(curClassIdx, :));
-    end
-end
-
 %% build axon classes
 conn.axonMeta.spineSynFrac = ...
     conn.axonMeta.spineSynCount ...
@@ -250,29 +235,15 @@ axonClasses(1).axonIds = find( ...
 axonClasses(1).title = sprintf( ...
    ['axons with ≥ %d synapses and ', ...
     'at least 70 %% onto spines'], minSynPre);
-
+    
 axonClasses(2).axonIds = find( ...
     conn.axonMeta.synCount >= minSynPre ...
-  & conn.axonMeta.spineSynFrac > 0.5);
-axonClasses(2).title = sprintf( ...
-   ['axons with ≥ %d synapses and ', ...
-    'at least 50 %% onto spines'], minSynPre);
-
-axonClasses(3).axonIds = find( ...
-    conn.axonMeta.synCount >= minSynPre ...
-  & conn.axonMeta.spineSynFrac < 0.5);
-axonClasses(3).title = sprintf( ...
-   ['axons with ≥ %d synapses and ', ...
-    'at most 50 %% onto spines'], minSynPre);
-    
-axonClasses(4).axonIds = find( ...
-    conn.axonMeta.synCount >= minSynPre ...
   & conn.axonMeta.spineSynFrac < 0.3);
-axonClasses(4).title = sprintf( ...
+axonClasses(2).title = sprintf( ...
    ['axons with ≥ %d synapses and ', ...
     'at most 30 %% onto spines'], minSynPre);
 
-%% plot likelihoods
+%% plot cosine similarity
 fig = figure();
 for curTargetClassIdx = fliplr(1:numel(targetClasses))
     curTargetClass = targetClasses(curTargetClassIdx);
@@ -282,10 +253,14 @@ for curTargetClassIdx = fliplr(1:numel(targetClasses))
     
     for curAxonClassIdx = 1:numel(axonClasses)
         curAxonIds = axonClasses(curAxonClassIdx).axonIds;
-        curMedianLik = axonTargetLik(:, curAxonIds, curTargetClassIdx);
-        curMedianLik = median(curMedianLik, 2);
         
-        plot(curAx, avail.dists / 1E3, curMedianLik, 'LineWidth', 2);
+        curAvail = squeeze(availabilities( ...
+            curTargetClassIdx, :, curAxonIds));
+        curSpecs = transpose(specificities( ...
+            curAxonIds, curTargetClassIdx));
+        
+        curCos = 1 - pdist2(curSpecs, curAvail, 'cosine');
+        plot(curAx, avail.dists / 1E3, curCos, 'LineWidth', 2);
     end
     
     ylim(curAx, [0, 1]);
@@ -301,63 +276,10 @@ for curTargetClassIdx = fliplr(1:numel(targetClasses))
 end
 
 ylabel(curAx, { ...
-    'Likelihood of target synapses';
-    'under availability-based Poisson model'});
+    'Cosine similarity between';
+    'availability and specificity'});
 legend(curAx, {axonClasses.title}, 'Location', 'NorthWest');
-annotation( ...
-    'textbox', [0, 0.9, 1, 0.1], ...
-    'String', {info.filename; info.git_repos{1}.hash}, ...
-    'EdgeColor', 'none', 'HorizontalAlignment', 'center');
 
-%% plot correlations
-fig = figure();
-for curTargetClassIdx = fliplr(1:numel(targetClasses))
-    curTargetClass = targetClasses(curTargetClassIdx);
-    
-    curAx = subplot(1, numel(targetClasses), curTargetClassIdx);
-    hold(curAx, 'on');
-    
-    for curAxonClassIdx = 1:numel(axonClasses)
-        curAxonIds = axonClasses(curAxonClassIdx).axonIds;
-        
-        curAvails = availabilities(curTargetClassIdx, :, curAxonIds);
-        curAvails = transpose(squeeze(curAvails));
-        
-        curSpecs = classConnectome(curAxonIds, :);
-        curSpecs = curSpecs ./ sum(curSpecs, 2);
-        
-        curCorrVec = nan(numel(avail.dists), 1);
-        for curDistIdx = 1:numel(avail.dists)
-            curCorrMat = corrcoef( ...
-                curAvails(:, curDistIdx), ...
-                curSpecs(:, curTargetClassIdx));
-            curCorrVec(curDistIdx) = curCorrMat(2);
-        end
-        
-        plot(curAx, avail.dists / 1E3, curCorrVec, 'LineWidth', 2);
-    end
-    
-    plot(curAx, avail.dists([1, end]) / 1E3, [0, 0], 'k--');
-    
-    title(curAx, char(curTargetClass));
-    xlabel(curAx, 'r_{pred} (µm)');
-    
-    if curTargetClass == targetClasses(1)
-        ylabel(curAx, { ...
-            'Correlation between'; ...
-            'availability and specificity'});
-    else
-        curAx.YTickLabel = {};
-    end
-    
-    curAx.TickDir = 'out';
-end
-
-yMin = min(arrayfun(@(a) a.YLim(1), fig.Children));
-yMax = max(arrayfun(@(a) a.YLim(2), fig.Children));
-[fig.Children.YLim] = deal([yMin, yMax]);
-
-legend(curAx, {axonClasses.title}, 'Location', 'NorthWest');
 annotation( ...
     'textbox', [0, 0.9, 1, 0.1], ...
     'String', {info.filename; info.git_repos{1}.hash}, ...
