1ca6233d91e5b1bd50afb8019b4a5a50db1d2d44
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-03-16 15:02:06 +0100

    Add axon classes to `axonMeta` of connectome

--- +connectEM/+Connectome/load.m
+++ +connectEM/+Connectome/load.m
@@ -6,6 +6,10 @@ function conn = load(param, connName)
     %     Localgical column vector which indicates axons that were
     %     identified as being thalamocortical.
     %
+    %   * conn.axonMeta.axonClass
+    %     A categorical column vector which marks excitatory, inhibitory,
+    %     thalamocortical and other axons.
+    %
     % Written by
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
     connDir = fullfile(param.saveFolder, 'connectomeState');
@@ -22,4 +26,14 @@ function conn = load(param, connName)
     
     conn.axonMeta.isThalamocortical = ...
         connectEM.Axon.detectThalamocorticals(conn, interSyn);
+    
+    %% label all axon classes
+    axonClasses = ...
+        connectEM.Connectome.buildAxonClasses(conn, 'minSynPre', 10);
+    
+    conn.axonMeta.axonClass(:) = {'Other'};
+    conn.axonMeta.axonClass(axonClasses(1).axonIds) = {'Excitatory'};
+    conn.axonMeta.axonClass(axonClasses(2).axonIds) = {'Inhibitory'};
+    conn.axonMeta.axonClass(axonClasses(3).axonIds) = {'Thalamocortical'};
+    conn.axonMeta.axonClass = categorical(conn.axonMeta.axonClass);
 end
