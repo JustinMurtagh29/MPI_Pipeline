c350897d1d817f534582372a2a99aa6a733c3834
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-03-16 19:58:53 +0100

    Clean up code and figure. Also plot observed ratios.

--- +connectEM/+Connectome/plotInputRatiosVsNullHypothesis.m
+++ +connectEM/+Connectome/plotInputRatiosVsNullHypothesis.m
@@ -33,24 +33,6 @@ dendClasses(1).nullIds = dendClasses(1).ids;
 dendClasses(1).title = sprintf( ...
     'Dendrites with ≥ %d synapses (n = %d)', ...
     minSynPost, numel(dendClasses(1).ids));
-
-%{
-dendClasses(2).ids = find( ...
-    conn.denMeta.targetClass == 'Somata' ...
-  & conn.denMeta.synCount >= minSynPost);
-dendClasses(2).nullIds = dendClasses(2).ids;
-dendClasses(2).title = sprintf( ...
-    'Somata with ≥ %d synapses (n = %d)', ...
-    minSynPost, numel(dendClasses(2).ids));
-
-dendClasses(3).ids = find( ...
-    conn.denMeta.targetClass == 'WholeCell' ...
-  & conn.denMeta.synCount >= minSynPost);
-dendClasses(3).nullIds = dendClasses(3).ids;
-dendClasses(3).title = sprintf( ...
-    'Whole cells with ≥ %d synapses (n = %d)', ...
-    minSynPost, numel(dendClasses(3).ids));
-%}
     
 %% plot
 for curIdx = 1:numel(dendClasses)
@@ -61,21 +43,37 @@ end
 
 %% plotting
 function plotAxonClass(info, classConn, axonClasses, dendClass)
+    import connectEM.Specificity.calcExpectedFractionDist;
+    classConn = classConn(dendClass.nullIds, :);
+    
+    % Plotting
     fig = figure();
+    fig.Color = 'white';
     binEdges = linspace(0, 1, 21);
     
     % Plot expected e / (e + i)
     excId = find(axonClasses == 'Excitatory');
     inhId = find(axonClasses == 'Inhibitory');
     
+    obsFrac = classConn(:, [excId, inhId]);
+    obsFrac = obsFrac(:, 1) ./ sum(obsFrac, 2);
+    obsFrac(isnan(obsFrac)) = 0;
+    
    [expFrac, expCount] = ...
-        connectEM.Specificity.calcExpectedFractionDist( ...
-            classConn(dendClass.nullIds, :), excId, inhId); %#ok
+        calcExpectedFractionDist(classConn, excId, inhId);
         
 	binId = discretize(expFrac, binEdges);
     binCount = accumarray(binId, expCount);
     
     ax = subplot(1, 2, 1);
+    axis(ax, 'square');
+    hold(ax, 'on');
+    
+    histogram(ax, ...
+        obsFrac, ...
+        'BinEdges', binEdges, ...
+        'DisplayStyle', 'stairs', ...
+        'LineWidth', 2);
     histogram(ax, ...
         'BinEdges', binEdges, ...
         'BinCounts', binCount, ...
@@ -84,20 +82,34 @@ function plotAxonClass(info, classConn, axonClasses, dendClass)
     
     xlabel(ax, 'exc / (exc + inh)');
     xlim(ax, binEdges([1, end]));
+    
+    ax.YAxis.Limits(1) = 10 ^ (-0.1);
+    ax.YScale = 'log';
     ax.TickDir = 'out';
     
     % Plot expected tc / (tc + e)
     tcId = find(axonClasses == 'Thalamocortical');
     excId = find(axonClasses == 'Excitatory');
     
+    obsFrac = classConn(:, [tcId, excId]);
+    obsFrac = obsFrac(:, 1) ./ sum(obsFrac, 2);
+    obsFrac(isnan(obsFrac)) = 0;
+    
    [expFrac, expCount] = ...
-        connectEM.Specificity.calcExpectedFractionDist( ...
-            classConn(dendClass.nullIds, :), tcId, excId); %#ok
+        calcExpectedFractionDist(classConn, tcId, excId);
         
 	binId = discretize(expFrac, binEdges);
     binCount = accumarray(binId, expCount);
     
     ax = subplot(1, 2, 2);
+    axis(ax, 'square');
+    hold(ax, 'on');
+    
+    histogram(ax, ...
+        obsFrac, ...
+        'BinEdges', binEdges, ...
+        'DisplayStyle', 'stairs', ...
+        'LineWidth', 2);
     histogram(ax, ...
         'BinEdges', binEdges, ...
         'BinCounts', binCount, ...
@@ -106,5 +118,25 @@ function plotAxonClass(info, classConn, axonClasses, dendClass)
     
     xlabel(ax, 'tc / (tc + exc)');
     xlim(ax, binEdges([1, end]));
+    
+    ax.YAxis.Limits(1) = 10 ^ (-0.1);
+    ax.YScale = 'log';
     ax.TickDir = 'out';
+    
+    % Legend
+    axPos = ax.Position;
+    leg = legend(ax, ...
+        'Observed', ...
+        'Expected (multinomial)', ...
+        'Location', 'NorthEast');
+    ax.Position = axPos;
+
+    annotation(fig, ...
+        'textbox', [0, 0.9, 1, 0.1], ...
+        'EdgeColor', 'none', ...
+        'HorizontalAlignment', 'center', ...
+        'String', { ...
+            info.filename; ...
+            info.git_repos{1}.hash; ...
+            dendClass.title});
 end
