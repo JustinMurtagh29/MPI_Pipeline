cb724222367ac2e5d0c22d2e6f7a65666a957ce6
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-03-17 15:20:24 +0100

    Make function more general and add documentation.

--- +connectEM/+Specificity/calcExpectedFractionDist.m
+++ +connectEM/+Specificity/calcExpectedFractionDist.m
@@ -1,14 +1,27 @@
 function [expFrac, expCount] = ...
-        calcExpectedFractionDist(classConn, numClassId, otherClassId)
+        calcExpectedFractionDist(classConn, numClassIds, denumClassIds)
+    % calcExpectedFractionDist(classConn, numClassIds, denumClassIds)
+    %   Given a class connectome and two sets of classes (A and B, with A
+    %   being a subset of B), this function calculates the expected
+    %   distribution of A / B.
+    %
     % Written by
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    
+    % Sanity check
+    assert(all(ismember(numClassIds, denumClassIds)));
+    
+    otherClassIds = setdiff(1:size(classConn, 2), denumClassIds);
+    denumClassIds = setdiff(denumClassIds, numClassIds);
     
     % Calculate probabilities for multinomial
     classProbs = sum(classConn, 1);
     classProbs = classProbs / sum(classProbs);
     
-    mnProbs = classProbs([numClassId, otherClassId]);
-    mnProbs = horzcat(mnProbs, 1 - sum(mnProbs));
+    mnProbs = [ ...
+        sum(classProbs(numClassIds)), ...
+        sum(classProbs(denumClassIds)), ...
+        sum(classProbs(otherClassIds))];
     
     % Determine minimal set of synapse counts
     synCounts = sum(classConn, 2);
