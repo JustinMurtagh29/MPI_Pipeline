98c6e0588c813d9003da5ab67bda24d6711d7713
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-03-20 09:04:07 +0100

    Significantly clean up code and generated plot

--- +connectEM/+Connectome/plotGeometricPedictability.m
+++ +connectEM/+Connectome/plotGeometricPedictability.m
@@ -18,138 +18,80 @@ avail = load(availFile);
 %% Prepare data
 [classConn, targetClasses] = ...
     connectEM.Connectome.buildClassConnectome(conn);
-
 axonClasses = ...
     connectEM.Connectome.buildAxonClasses(conn, 'minSynPre', minSynPre);
-
-synCounts = sum(classConn, 2);
-axonsWithSynapses = find(synCounts);
-
-axonClasses(1).axonIds = intersect( ...
-    axonClasses(1).axonIds, axonsWithSynapses);
-axonClasses(2).axonIds = intersect( ...
-    axonClasses(2).axonIds, axonsWithSynapses);
 
 % Determine relative availabilities of target classes
 [~, classIds] = ismember(targetClasses, avail.targetClasses);
 availabilities = avail.axonAvail(classIds, :, :);
 availabilities = availabilities ./ sum(availabilities, 1);
 
-%% Calculate predictability
-axonCount = size(classConn, 1);
-distCount = numel(avail.dists);
-classCount = numel(targetClasses);
+% Count synapses per axon
+% NOTE(amotta): Depending on the crazy stuff we're doing (e.g., excluding
+% 'OtherDendrites') this value might be different from what's stored in
+% conn.axonMeta.synCount.
+synCounts = sum(classConn, 2);
 
-bhattaDist = nan(axonCount, distCount);
-distProbs = nan(axonCount, distCount);
-distClassProbs = nan(axonCount, classCount, distCount);
-
-for curDistIdx = 1:distCount
-    curAvail = availabilities(:, curDistIdx, :);
-    curAvail = transpose(squeeze(curAvail));
-    
-    % Calculate per-axon probabilities
-    curProbs = mnpdf(classConn, curAvail);
-    
-    % Calculate per-synapse probabilities
-    curProbs = curProbs .^ (1 ./ synCounts);
-    distProbs(:, curDistIdx) = curProbs;
-    
-    % Calculate Bhattacharyya distance
-    curBhattaDist = classConn ./ sum(classConn, 2);
-    curBhattaDist = sqrt(curBhattaDist .* curAvail);
-    curBhattaDist = -log(sum(curBhattaDist, 2));
-    bhattaDist(:, curDistIdx) = curBhattaDist;
-    
-    % Calculate per-class probabilities
-    for curClassIdx = 1:classCount
-        curClassAvail = curAvail(:, curClassIdx);
-        curClassAvail = [curClassAvail, (1 - curClassAvail)];
-        
-        curClassSyns = classConn(:, curClassIdx);
-        curClassSyns = [curClassSyns, (synCounts - curClassSyns)];
-        
-        curClassProbs = mnpdf(curClassSyns, curClassAvail);
-        curClassProbs = curClassProbs .^ (1 ./ synCounts);
-        distClassProbs(:, curClassIdx, curDistIdx) = curClassProbs;
-    end
-end
-
-%% Coefficient of determination
-curAxonIds = axonClasses(2).axonIds;
-
-curClassConn = classConn(curAxonIds, :);
-curClassConn = curClassConn ./ sum(curClassConn, 2);
-
-curSsTot = mean(curClassConn, 1);
-curSsTot = sum((curClassConn - curSsTot) .^ 2, 1);
-
-curSsRes = availabilities(:, :, curAxonIds);
-curSsRes = permute(curSsRes, [3, 1, 2]);
-curSsRes = sum((curClassConn - curSsRes) .^ 2, 1);
-curSsRes = transpose(squeeze(curSsRes));
-
-curRsq = 1 - (curSsRes ./ curSsTot);
-
-% Plotting
-fig = figure();
-ax = axes(fig);
-hold(ax, 'on');
-
-for curIdx = 1:size(curRsq, 2)
-    plot(ax, curRsq(:, curIdx), 'LineWidth', 2);
-end
-
-legend(arrayfun(@char, targetClasses, 'Uni', false));
-% ylim([0, 0.5]);
-
-%% Plotting
-excProbs = distProbs(axonClasses(1).axonIds, :);
-inhProbs = distProbs(axonClasses(2).axonIds, :);
+%% Plot R² per axon and dendrite class
+plotAxonClasses = 1:2;
+plotTargetClasses = targetClasses;
 
 fig = figure();
-ax = axes(fig);
-hold(ax, 'on');
+fig.Color = 'white';
+fig.Position(3:4) = [1100, 850];
 
-plot(avail.dists, median(excProbs, 1), 'LineWidth', 2);
-plot(avail.dists, median(inhProbs, 1), 'LineWidth', 2);
-
-legend('Excitatory axons', 'Inhibitory axons');
-
-ylim(ax, [0, 1]);
-
-%% Plotting Bhattacharyya distance
-excDists = bhattaDist(axonClasses(1).axonIds, :);
-inhDists = bhattaDist(axonClasses(2).axonIds, :);
-
-fig = figure();
-ax = axes(fig);
-hold(ax, 'on');
-
-plot(avail.dists, median(excDists, 1), 'LineWidth', 2);
-plot(avail.dists, median(inhDists, 1), 'LineWidth', 2);
-ax.YLim(1) = 0;
-
-%% Per-class plotting
-fig = figure();
-
-for curClassIdx = 1:classCount
-    curAx = subplot(1, classCount, curClassIdx);
+for curAxonClassIdx = 1:numel(plotAxonClasses)
+    curAxonClassId = plotAxonClasses(curAxonClassIdx);
+    curAxonClass = axonClasses(curAxonClassId);
+    curAxonIds = curAxonClass.axonIds;
+    
+    % Fractional class connectome
+    curClassConn = classConn(curAxonIds, :);
+    curClassConn = curClassConn ./ sum(curClassConn, 2);
+    
+    % Variance
+    curSsTot = mean(curClassConn, 1);
+    curSsTot = sum((curClassConn - curSsTot) .^ 2, 1);
+    
+    % Residuals
+    curSsRes = availabilities(:, :, curAxonIds);
+    curSsRes = permute(curSsRes, [3, 1, 2]);
+    curSsRes = sum((curClassConn - curSsRes) .^ 2, 1);
+    curSsRes = transpose(squeeze(curSsRes));
+    
+    % R²
+    curRsq = 1 - (curSsRes ./ curSsTot);
+    
+    % Plotting
+    curAx = subplot(1, numel(plotAxonClasses), curAxonClassIdx);
     hold(curAx, 'on');
     
-    curExcProbs = axonClasses(1).axonIds;
-    curExcProbs = distClassProbs(curExcProbs, curClassIdx, :);
-    curExcProbs = median(squeeze(curExcProbs), 1);
-    plot(curAx, avail.dists, curExcProbs, 'LineWidth', 2);
+    for curTargetClassIdx = 1:numel(plotTargetClasses)
+        plot( ...
+            curAx, avail.dists / 1E3, ...
+            curRsq(:, curTargetClassIdx), ...
+            'LineWidth', 2);
+    end
     
-    curInhProbs = axonClasses(2).axonIds;
-    curInhProbs = distClassProbs(curInhProbs, curClassIdx, :);
-    curInhProbs = median(squeeze(curInhProbs), 1);
-    plot(curAx, avail.dists, curInhProbs, 'LineWidth', 2);
+    ylim(curAx, [-0.2, 0.5]);
+    xlabel('Radius (µm)');
     
-    ylim(curAx, [0, 1]);
-    
-    title(curAx, ...
-        char(targetClasses(curClassIdx)), ...
+    title( ...
+        curAx, curAxonClass.title, ...
         'FontWeight', 'normal', 'FontSize', 10);
 end
+
+ylabel(fig.Children(end), 'R²');
+legend(fig.Children(1), ...
+    arrayfun( ...
+        @char, plotTargetClasses, ...
+        'UniformOutput', false), ...
+	'Location', 'NorthEast');
+
+annotation(fig, ...
+    'textbox', [0, 0.9, 1, 0.1], ...
+    'EdgeColor', 'none', ...
+    'HorizontalAlignment', 'center', ...
+    'String', { ...
+        info.filename; ...
+        info.git_repos{1}.hash});
