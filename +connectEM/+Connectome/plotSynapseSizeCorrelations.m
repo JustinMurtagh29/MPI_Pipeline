8abb2e8686065baab1400a82593de6f1404d588f
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-03-22 11:01:13 +0100

    Factor out construction of synapse table

--- +connectEM/+Connectome/plotSynapseSizeCorrelations.m
+++ +connectEM/+Connectome/plotSynapseSizeCorrelations.m
@@ -29,32 +29,7 @@ conn = connectEM.Connectome.load( ...
     struct('saveFolder', rootDir), connName);
 
 %% limit synapses
-synT = table;
-synT.id = cell2mat(conn.connectome.synIdx);
-synT.area = cell2mat(conn.connectomeMeta.contactArea);
-
-synT.preAggloId = repelem( ...
-    conn.connectome.edges(:, 1), ...
-    cellfun(@numel, conn.connectome.synIdx));
-
-synT.postAggloId = repelem( ...
-    conn.connectome.edges(:, 2), ...
-    cellfun(@numel, conn.connectome.synIdx));
-
-synT.isSpine = syn.isSpineSyn(synT.id);
-synT.isSoma = ismember(synT.postAggloId, ...
-    find(conn.denMeta.targetClass == 'Somata'));
-
-% remove duplicate entries
-[~, uniRows, uniCount] = unique(synT.id);
-synT = synT(uniRows, :);
-
-% remove synapses occuring multiple times
-% (i.e., between two or more pairs of neurites)
-synT.occurences = accumarray(uniCount, 1);
-synT(synT.occurences > 1, :) = [];
-synT.occurences = [];
-
+synT = connectEM.Connectome.buildSynapseTable(conn, syn);
 synAreaLim = prctile(synT.area, 99.9);
 
 %% define axon classes
