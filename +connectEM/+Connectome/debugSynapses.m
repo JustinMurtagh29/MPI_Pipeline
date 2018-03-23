2c8a24062ce06ef80f3819b1a7b45d45cf01be97
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-03-23 09:11:50 +0100

    Cross-check with spine head agglomerates

--- +connectEM/+Connectome/debugSynapses.m
+++ +connectEM/+Connectome/debugSynapses.m
@@ -21,15 +21,37 @@ conn = connectEM.Connectome.load(param, connName);
 shAgglos = load(shFile, 'shAgglos');
 shAgglos = shAgglos.shAgglos;
 
-%% Build spine head look-up table
-shLUT = Agglo.buildLUT(maxSegId, shAgglos);
-
 %% Absolute numbers
 allSynCount = numel(syn.isSpineSyn);
 allSpineSynCount = sum(syn.isSpineSyn);
 allSpineSynFrac = mean(syn.isSpineSyn);
 
+shLUT = Agglo.buildLUT(maxSegId, shAgglos);
+
 % Reproduce spine flag
 syn.myIsSpine = cellfun( ...
     @(ids) any(shLUT(ids)), syn.synapses.postsynId);
 assert(isequal(syn.myIsSpine, syn.isSpineSyn));
+
+fprintf( ...
+    '# synapses: %d\n', allSynCount);
+fprintf( ...
+    '# synapses onto spines: %d (%.0f %%)\n', ...
+    allSpineSynCount, 100 * allSpineSynFrac);
+
+%% Analyse spine heads
+shCount = numel(shAgglos);
+
+postSynSegIds = cell2mat(syn.synapses.postsynId);
+postSynSegLUT = logical(Agglo.buildLUT(maxSegId, {postSynSegIds}));
+
+shWithSyn = cellfun( ...
+    @(ids) any(postSynSegLUT(ids)), shAgglos);
+shWithSynCount = sum(shWithSyn);
+shWithSynFrac = mean(shWithSyn);
+
+fprintf( ...
+    '# spine heads: %d\n', shCount);
+fprintf( ...
+    '# spine heads with synapse: %d (%.0f %%)\n', ...
+    shWithSynCount, 100 * shWithSynFrac);
