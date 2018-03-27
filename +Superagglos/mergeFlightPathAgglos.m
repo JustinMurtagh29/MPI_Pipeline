6254c75f60d3a874971146e14caf49c45311fa06
Benedikt Staffler <benedikt.staffler@brain.mpg.de>
2018-03-27 14:06:33 +0200

    doc & change default

--- +Superagglos/mergeFlightPathAgglos.m
+++ +Superagglos/mergeFlightPathAgglos.m
@@ -14,24 +14,29 @@ function axons = mergeFlightPathAgglos( ov, axonsBig, axonsSmall, toAgglo )
 %           Flag indicating that the output should be in the agglo format,
 %           i.e. a list of segmentation ids for each agglo. This is
 %           typically much faster.
-%           (Default: false)
+%           Note that Superagglos.mergeOnOverlaps results in different
+%           output agglos (should currently not be used anymore).
+%           (Default: true)
 % OUTPUT axons: [Nx1] struct
 %           Resulting axons in the superagglo format.
 % Author: Benedikt Staffler <benedikt.staffler@brain.mpg.de>
 
 if ~exist('toAgglo', 'var') || isempty(toAgglo)
-    toAgglo = false;
+    toAgglo = true;
 end
 
 axons = axonsBig;
 for i = 1:length(axonsBig)
     if toAgglo
         if ~isempty(ov{i}) % simply concatenate nodes
+            % assumes axons seg ids are mutually exclusive
             axons(i).nodes = cat(1, axons(i).nodes, ...
                 cell2mat({axonsSmall(ov{i}).nodes}'));
         end
     else
         for j = 1:length(ov{i})
+            % axonsBig are used as second input such that the flight path
+            % from the big axon is removed and replaced with the small axon
             axonsBig(i) = Superagglos.mergeOnOverlaps( ...
                 axonsSmall(ov{i}(j)), axonsBig(i), ...
                 'scale', [11.24, 11.24, 28], ...
