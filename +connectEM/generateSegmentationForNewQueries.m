aed0ac6c2de907bc47f2c950df8dd8e9263032b9
Benedikt Staffler <benedikt.staffler@brain.mpg.de>
2018-03-27 14:06:44 +0200

    allow to pass in axons directly

--- +connectEM/generateSegmentationForNewQueries.m
+++ +connectEM/generateSegmentationForNewQueries.m
@@ -2,23 +2,27 @@ function generateSegmentationForNewQueries(param, seg, axFile)
 
     % Define where to load and store results
     dataDir = fullfile(param.saveFolder, 'aggloState');
+    if ~exist('axFile', 'var') || isempty(axFile)
+        axFile = fullfile(dataDir, 'axons_04.mat');
+    end
 
     % Load segmentMeta
     [~, segmentMeta] = connectEM.loadAllSegmentationData(param);
 
     % Load larger 5 micron agglomerates
-    if ~exist('axFile', 'var') || isempty(axFile)
-        m = load(fullfile(dataDir, 'axons_04.mat'));
-    else
+    if ischar(axFile)
         m = load(axFile);
+        axons = m.axons(m.indBigAxons);
+        axons = arrayfun(@Agglo.fromSuperAgglo, axons, 'uni', 0);
+    else
+        axons = axFile;
     end
-    axons = m.axons(m.indBigAxons);
-    axons = arrayfun(@Agglo.fromSuperAgglo, axons, 'UniformOutput', false);
     clear m;
     
     if ~exist('seg', 'var') || isempty(seg)
         seg = struct;
-        seg.root = fullfile(param.saveFolder, 'aggloState', '20170829_segmentationForQueries', '1');
+        seg.root = fullfile(param.saveFolder, 'aggloState', ...
+            '20170829_segmentationForQueries', '1');
         seg.prefix = param.seg.prefix;
     end
 
