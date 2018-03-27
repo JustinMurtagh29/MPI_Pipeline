81e1308a573ca32c87ba2d5a86f7e9558f7f14c3
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-03-27 17:02:28 +0200

    Calculate synapse areas based on merger mode tracings

--- +connectEM/+Consistency/runAnalysis.m
+++ +connectEM/+Consistency/runAnalysis.m
@@ -12,9 +12,23 @@ ctrlDir = fullfile(thisDir, 'annotations', 'random-spine-synapses');
 param = load(fullfile(rootDir, 'allParameter.mat'));
 param = param.p;
 
+%% Loading augmented graph
+graph = Graph.load(rootDir);
+graph(~graph.borderIdx, :) = [];
+
+borderAreas = fullfile(rootDir, 'globalBorder.mat');
+borderAreas = load(borderAreas, 'borderArea2');
+borderAreas = borderAreas.borderArea2;
+
+graph.borderArea = borderAreas(graph.borderIdx);
+clear borderAreas;
+
 %% Loading control data
 ctrlFiles = dir(fullfile(ctrlDir, '*.nml'));
 ctrlFiles = fullfile(ctrlDir, {ctrlFiles.name});
 
 ctrlSynT = connectEM.Consistency.loadAnnotations(param, ctrlFiles);
 ctrlSynT = vertcat(ctrlSynT{:});
+
+% Calculate synapse areas
+ctrlSynT.area = connectEM.Consistency.calcSynapseAreas(param, graph, ctrlSynT);
