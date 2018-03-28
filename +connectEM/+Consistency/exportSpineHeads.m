49dfad55f452818972d56b720424b2a2a420010a
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-03-28 09:54:50 +0200

    Generate second set of random spine heads

--- +connectEM/+Consistency/exportSpineHeads.m
+++ +connectEM/+Consistency/exportSpineHeads.m
@@ -25,8 +25,20 @@ shAgglos = load(shFile, 'shAgglos');
 shAgglos = shAgglos.shAgglos;
 
 %% Export random examples
+% NOTE(amotta): I've messed up `5115760af3200096c7847f016ce2dd3dd5b6a140`
+% by not passing a seed value to `rng`. As a result, I cannot reproduce the
+% spine heads used for the first set. Let's manually remove these spine
+% heads based on their IDs (recovered from the NML file).
+openShIds = [ ...
+     70266, 304247, 269329, 187491, 227442, 123208, 309636,  78566, ...
+    285553, 153211, 260123, 324407,  33731, 386422, 202399, 181221, ...
+    185763, 127374, 211426, 212367, 330470];
+openShIds = setdiff(1:numel(shAgglos), openShIds);
+
 rng(0);
-randIds = randperm(numel(shAgglos), 25);
+randIds = randperm(numel(openShIds));
+randIds = openShIds(randIds(1:25));
+
 randShAgglos = cellfun( ...
     @(segIds) segPoints(segIds, :), ...
     shAgglos(randIds), 'UniformOutput', false);
