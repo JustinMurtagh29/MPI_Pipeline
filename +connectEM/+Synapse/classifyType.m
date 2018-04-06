d694816c7d76bd3a93ff15dc2a05156edaef9736
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-04-06 16:03:44 +0200

    Look at spine heads with primary and secondary synapses in webKONSSOS

--- +connectEM/+Synapse/classifyType.m
+++ +connectEM/+Synapse/classifyType.m
@@ -32,9 +32,8 @@ param.svg.synScoreFile = synScoreFile;
 % Synapse scores
 
 maxSegId = Seg.Global.getMaxSegId(param);
+segPoints = Seg.Global.getSegToPointMap(param);
 graph = Seg.IO.loadGraph(param, false);
-
-%%
 
 % Spine heads
 shAgglos = load(shFile, 'shAgglos');
@@ -107,3 +106,47 @@ shT.secSynIds(mask) = cellfun( ...
 clear mask;
 
 %% Look at examples in webKNOSSOS
+rng(0);
+
+randIds = find(~cellfun(@isempty, shT.secSynIds));
+randIds = randIds(randperm(numel(randIds)));
+randIds = randIds(1:25);
+
+skel = skeleton();
+skel = Skeleton.setParams4Pipeline(skel, param);
+skel = skel.setDescription(sprintf( ...
+    '%s (%s)', info.filename, info.git_repos{1}.hash));
+
+for curIdx = 1:numel(randIds)
+    curShT = shT(randIds(curIdx), :);
+    curShAgglo = shAgglos(curShT.id);
+    
+    curSynIds = [ ...
+        curShT.priSynId; ...
+        curShT.secSynIds{1}];
+    curSynAgglos = cellfun( ...
+        @vertcat, ...
+        synapses.presynId(curSynIds), ...
+        synapses.postsynId(curSynIds), ...
+        'UniformOutput', false);
+    
+    curAgglos = cellfun( ...
+        @(segIds) segPoints(segIds, :), ...
+        [curShAgglo; curSynAgglos], ...
+        'UniformOutput', false);
+    
+    curNames = [ ...
+       {sprintf( ...
+            'Spine head %d', curShT.id)}; ...
+        arrayfun( ...
+            @(id) sprintf('Synapse %d', id), ...
+            curSynIds, 'UniformOutput', false)];
+	curNames(2) = strcat(curNames(2), {' (Primary)'});
+    curNames(3:end) = strcat(curNames(3:end), {' (Secondary)'});
+    
+    curPrefix = sprintf('%0*d.', ceil(log10(1 + numel(randIds))), curIdx);
+    curNames = strcat(curPrefix, {' '}, curNames);
+    
+    skel = Skeleton.fromMST(curAgglos, param.raw.voxelSize, skel);
+    skel.names((end - numel(curNames) + 1):end) = curNames;
+end
