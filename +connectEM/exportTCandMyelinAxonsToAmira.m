80ae0a1153cc5cb2035be86e6e5b27e5e31873d0
Marcel Beining <marcel.beining@brain.mpg.de>
2018-04-16 11:20:18 +0200

    use the axon_18 state for iso calculation

--- +connectEM/exportTCandMyelinAxonsToAmira.m
+++ +connectEM/exportTCandMyelinAxonsToAmira.m
@@ -25,20 +25,30 @@ if ~exist('heuristics','var')
 end
 
 conn = connectEM.Connectome.load(p,connName);
+load(fullfile(p.saveFolder,'aggloState','axons_18_a.mat'))
+
+% map axons from connectome state to the ones from axon state in order to
+% be able to apply the thalamocortical ids
+[axonLUT,axonSegIds] = Superagglos.buildLUT(axons);
+connAxonSegIds = cell2mat(conn.axons);
+connAxonLUT = Agglo.buildLUT(numel(axonLUT),conn.axons);
+[ismem,ind] = ismember(connAxonSegIds,axonSegIds);
+connAxonIds = (accumarray(connAxonLUT(connAxonSegIds(ismem))',axonLUT(axonSegIds(ind(ismem)))',[],@mode));
+axons = axons(connAxonIds);
 
 disp('all data loaded')
 %%
 % get classification thalamocortical and myelinated
 isThalamocortical = conn.axonMeta.isThalamocortical;
-[ myelinFracAx,~,~,myelinNeighSegments] = connectEM.calculateSurfaceMyelinScore( conn.axons, graph, borderMeta, heuristics ); % calculate myelin score for the dendrite class
+[ myelinFracAx,~,~,myelinNeighSegments] = connectEM.calculateSurfaceMyelinScore( axons, graph, borderMeta, heuristics ); % calculate myelin score for the dendrite class
 
 disp('Done calculating TC/myelinated axons, creating isos')
 %% export TC axons including myelin iso
-Visualization.exportAggloToAmira(p, conn.axons(isThalamocortical), fullfile(mainOutDir,'TC'), isoParamAx{:})
+Visualization.exportAggloToAmira(p, axons(isThalamocortical), fullfile(mainOutDir,'TC'), isoParamAx{:})
 Visualization.exportAggloToAmira(p, myelinNeighSegments(isThalamocortical), fullfile(mainOutDir,'TCmyelin'), isoParamMy{:})
 
 
 %% export myelinated axons including myelin iso, excluding tc axons
-myAxNoTC = ~isThalamocortical & myelinFracAx > 0.2;
-Visualization.exportAggloToAmira(p, conn.axons(myAxNoTC), fullfile(mainOutDir,'AxMy'), isoParamAx{:})
-Visualization.exportAggloToAmira(p, myelinNeighSegments(myAxNoTC), fullfile(mainOutDir,'AxMymyelin'), isoParamMy{:})
+myAxNoTC = ~isThalamocortical & myelinFracAx > 0.1;
+Visualization.exportAggloToAmira(p, axons(myAxNoTC), fullfile(mainOutDir,'AxMy_0.1b'), isoParamAx{:})
+Visualization.exportAggloToAmira(p, myelinNeighSegments(myAxNoTC), fullfile(mainOutDir,'AxMymyelin_0.1b'), isoParamMy{:})
