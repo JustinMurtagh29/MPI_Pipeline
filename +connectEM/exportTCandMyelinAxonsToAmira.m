2c28a64c8f20dca1861eb9cf0f9dd3e6a49c852c
Marcel Beining <marcel.beining@brain.mpg.de>
2018-04-16 13:30:37 +0200

    use different approach to map connectome axons to axon18a

--- +connectEM/exportTCandMyelinAxonsToAmira.m
+++ +connectEM/exportTCandMyelinAxonsToAmira.m
@@ -1,4 +1,4 @@
-connName = 'connectome_ax18a_deWC01wSp';
+connName = 'connectome_ax18a_deWC01wSp_v4.mat';
 mainOutDir = ['/tmpscratch/mbeining/isos/',connName];
 isoParamAx = {'smoothWidth',5,'smoothSizeHalf',5,'reduce',0.1};  % heiko suggests 9 , 9 , 0.2
 isoParamMy = {'smoothWidth',2,'smoothSizeHalf',2,'reduce',0.15};
@@ -24,21 +24,35 @@ if ~exist('heuristics','var')
     heuristics = load(fullfile(p.saveFolder, 'heuristicResult.mat'),'myelinScore');
 end
 
-conn = connectEM.Connectome.load(p,connName);
+conn = load(fullfile(p.saveFolder,'connectomeState',connName));
+interSyn = load(fullfile(p.saveFolder,'connectomeState', sprintf('%s_intersynapse.mat', connName(1:end-4))));
+conn.axonMeta.isThalamocortical = ...
+    connectEM.Axon.detectThalamocorticals(conn, interSyn);
+
+load(fullfile(p.saveFolder,'aggloState','axons_18_a.mat'))
+
+% map axons from connectome state to the ones from axon state in order to
+% be able to apply the thalamocortical ids
+[axonLUT,axonSegIds] = Superagglos.buildLUT(axons);
+connAxonSegIds = cell2mat(conn.axons);
+connAxonLUT = Agglo.buildLUT(numel(axonLUT),conn.axons);
+[ismem,ind] = ismember(connAxonSegIds,axonSegIds);
+connAxonIds = (accumarray(connAxonLUT(connAxonSegIds(ismem))',axonLUT(axonSegIds(ind(ismem)))',[],@mode));
+axons = axons(connAxonIds);
 
 disp('all data loaded')
 %%
 % get classification thalamocortical and myelinated
 isThalamocortical = conn.axonMeta.isThalamocortical;
-[ myelinFracAx,~,~,myelinNeighSegments] = connectEM.calculateSurfaceMyelinScore( conn.axons, graph, borderMeta, heuristics ); % calculate myelin score for the dendrite class
+[ myelinFracAx,~,~,myelinNeighSegments] = connectEM.calculateSurfaceMyelinScore( axons, graph, borderMeta, heuristics ); % calculate myelin score for the dendrite class
 
 disp('Done calculating TC/myelinated axons, creating isos')
 %% export TC axons including myelin iso
-Visualization.exportAggloToAmira(p, conn.axons(isThalamocortical), fullfile(mainOutDir,'TC'), isoParamAx{:})
+Visualization.exportAggloToAmira(p, axons(isThalamocortical), fullfile(mainOutDir,'TC'), isoParamAx{:})
 Visualization.exportAggloToAmira(p, myelinNeighSegments(isThalamocortical), fullfile(mainOutDir,'TCmyelin'), isoParamMy{:})
 
 
 %% export myelinated axons including myelin iso, excluding tc axons
 myAxNoTC = ~isThalamocortical & myelinFracAx > 0.2;
-Visualization.exportAggloToAmira(p, conn.axons(myAxNoTC), fullfile(mainOutDir,'AxMy'), isoParamAx{:})
+Visualization.exportAggloToAmira(p, axons(myAxNoTC), fullfile(mainOutDir,'AxMy'), isoParamAx{:})
 Visualization.exportAggloToAmira(p, myelinNeighSegments(myAxNoTC), fullfile(mainOutDir,'AxMymyelin'), isoParamMy{:})
