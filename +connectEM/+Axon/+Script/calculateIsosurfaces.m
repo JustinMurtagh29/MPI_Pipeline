d418916e27b36c0e02ab9709f38530e9126ac35f
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-04-18 16:22:02 +0200

    Generate MAT file with PLY files of different axon classes

--- +connectEM/+Axon/+Script/calculateIsosurfaces.m
+++ +connectEM/+Axon/+Script/calculateIsosurfaces.m
@@ -4,15 +4,21 @@ clear;
 
 %% configuration
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
-axonFile = fullfile(rootDir, 'connectomeState', 'connectome_axons_18_a_ax_spine_syn_clust.mat');
-outDir = '/tmpscratch/amotta/l4/2018-01-24-axons-18a-isosurfaces';
+connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons_18_a_ax_spine_syn_clust.mat');
+synFile = fullfile(rootDir, 'connectomeState', 'SynapseAgglos_v3_ax_spine_clustered_classified.mat');
+
+isoDir = '';
+axonClassFile = '/tmpscratch/amotta/l4/2018-01-24-axons-18a-isosurfaces/axon-classes_v1.mat';
+
+info = Util.runInfo();
 
 %% load parameters
 param = load(fullfile(rootDir, 'allParameter.mat'), 'p');
 param = param.p;
 
-axons = load(axonFile, 'axons');
-axons = axons.axons;
+[conn, ~, axonClasses] = ...
+    connectEM.Connectome.load(param, connFile, synFile);
+axons = conn.axons;
 
 % for speed
 param.seg = struct;
@@ -20,6 +26,31 @@ param.seg.root = '/tmpscratch/amotta/l4/2012-09-28_ex145_07x2_ROI2017/segmentati
 param.seg.backend = 'wkwrap';
 
 %% generate isosurfaces
-Visualization.exportAggloToAmira( ...
-    param, axons, outDir, 'reduce', 0.05, ...
-    'smoothSizeHalf', 4, 'smoothWidth', 8);
+if ~isempty(isoDir)
+    Visualization.exportAggloToAmira( ...
+        param, axons, isoDir, 'reduce', 0.05, ...
+        'smoothSizeHalf', 4, 'smoothWidth', 8);
+end
+
+%% generate axon classes
+if ~isempty(axonClassFile)
+    % Build paths
+    plyFiles = arrayfun( ...
+        @(axonId) sprintf('iso-%d.ply', axonId), ...
+        transpose(1:numel(axons)), 'UniformOutput', false);
+    plyFiles = fullfile(fileparts(axonClassFile), 'ply', plyFiles);
+    
+    % Build output
+    out = struct;
+    out.info = info;
+    
+    for axonClass = reshape(axonClasses, 1, [])
+        axonClassName = strsplit(axonClass.title);
+        axonClassName = axonClassName{1};
+        
+        out.(axonClassName) = plyFiles(axonClass.axonIds);
+    end
+    
+    Util.saveStruct(axonClassFile, out);
+    Util.protect(axonClassFile);
+end
