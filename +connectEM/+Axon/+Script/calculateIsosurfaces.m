3b1a834e6cf84e237ffa57744ea09373f3cf2ffc
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-04-18 15:47:42 +0200

    Simplify script for rendering of axon isosurfaces
    Both sets of segment equivalence classes are indeed identical.

--- +connectEM/+Axon/+Script/calculateIsosurfaces.m
+++ +connectEM/+Axon/+Script/calculateIsosurfaces.m
@@ -4,41 +4,20 @@ clear;
 
 %% configuration
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
-axonFile = fullfile(rootDir, 'aggloState', 'axons_18_a.mat');
+axonFile = fullfile(rootDir, 'connectomeState', 'connectome_axons_18_a_ax_spine_syn_clust.mat');
 outDir = '/tmpscratch/amotta/l4/2018-01-24-axons-18a-isosurfaces';
-
-% temporarily add Benedikt's repo to path
-beneDir = '/gaba/u/amotta/code/benedikt';
-oldPath = addpath(genpath(beneDir), '-begin');
-restorePath = onCleanup(@() path(oldPath));
-clear oldPath;
-
-info = Util.runInfo();
 
 %% load parameters
 param = load(fullfile(rootDir, 'allParameter.mat'), 'p');
 param = param.p;
 
-% for Benedikt's code
-param.agglo = struct;
-param.agglo.axonAggloFile = axonFile;
+axons = load(axonFile, 'axons');
+axons = axons.axons;
 
 % for speed
 param.seg = struct;
 param.seg.root = '/tmpscratch/amotta/l4/2012-09-28_ex145_07x2_ROI2017/segmentation/1';
 param.seg.backend = 'wkwrap';
-
-%% load and complete agglomerates
-cacheFile = fullfile(outDir, 'axons.mat');
-
-if ~exist(cacheFile, 'file')
-    disp('Generating axon agglomerates and cache');
-    axons = L4.Axons.getLargeAxons(param, true, true);
-    save(cacheFile, 'info', 'axons');
-else
-    disp('Loading axon agglomerates from cache');
-    load(cacheFile, 'axons');
-end
 
 %% generate isosurfaces
 Visualization.exportAggloToAmira( ...
