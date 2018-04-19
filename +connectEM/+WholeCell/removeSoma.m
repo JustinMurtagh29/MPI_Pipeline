c06e99a294d7865495f57e29d1e97894b3d97e68
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-04-19 11:43:08 +0200

    Outline of what's going to happen

---
+++ +connectEM/+WholeCell/removeSoma.m
@@ -0,0 +1,10 @@
+function agglo = removeSoma(agglo, soma)
+    % agglo = removeSoma(agglo, soma)
+    %   Removes all soma-overlapping segments from a super-agglomerate. If
+    %   the super-agglomerate is split into multiple connected components,
+    %   the components are joined via a non-segment node placed at the
+    %   center-of-mass of the soma.
+    %
+    % Written by
+    %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+end
