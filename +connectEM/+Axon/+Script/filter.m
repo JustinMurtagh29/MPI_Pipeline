d178b64b0754eb5f99cf48bbe6ff3b0f6b011d70
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-04-24 11:53:22 +0200

    Plot histogram of median glia probability over axon super-agglomerates

---
+++ +connectEM/+Axon/+Script/filter.m
@@ -0,0 +1,45 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+
+%% Configuration
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+axonFile = fullfile(rootDir, 'aggloState', 'axons_18_b.mat');
+
+info = Util.runInfo();
+
+%% Loading data
+param = load(fullfile(rootDir, 'allParameter.mat'));
+param = param.p;
+
+segmentMeta = struct;
+segmentMeta.maxSegId = Seg.Global.getMaxSegId(param);
+segmentMeta = connectEM.addSegmentClassInformation(param, segmentMeta);
+
+axon = load(axonFile);
+
+%% Calculate per-axon glia score
+axons = axon.axons(axon.indBigAxons);
+axons = Agglo.fromSuperAgglo(axons);
+
+gliaScore = segmentMeta.gliaProb;
+gliaScore = cellfun(@(ids) median(gliaScore(ids), 'omitnan'), axons);
+
+%% Histogram of glia score
+binEdges = linspace(0, 1, 51);
+
+fig = figure();
+fig.Color = 'white';
+
+ax = axes(fig);
+histogram(ax, ...
+    gliaScore, binEdges, ...
+    'DisplayStyle', 'stairs', ...
+    'LineWidth', 2);
+
+ax.Box = 'off';
+ax.TickDir = 'out';
+ax.YScale = 'log';
+
+ax.XLim = binEdges([1, end]);
+ax.YLim(1) = 0;
