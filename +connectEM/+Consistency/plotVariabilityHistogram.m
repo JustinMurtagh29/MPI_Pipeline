928b037f48961310102b6bd9a3ecc2d812e303b8
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-05-03 20:31:44 +0200

    That was maybe slightly over the top in terms of generality...

--- +connectEM/+Consistency/plotVariabilityHistogram.m
+++ +connectEM/+Consistency/plotVariabilityHistogram.m
@@ -1,40 +1,73 @@
 function fig = plotVariabilityHistogram( ...
-        info, synT, plotConfig, pairConfigs, varargin)
+        info, synT, plotConfigs, pairConfigs, varargin)
     % Written by
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
     opt = struct;
     opt.binEdges = linspace(0, 1.5, 21);
     opt = Util.modifyStruct(opt, varargin{:});
     
+    % Sanity check
+    plotConfigSize = size(plotConfigs);
+    pairConfigSize = size(pairConfigs);
+    
+    assert(isequal( ...
+        plotConfigSize(2:end), ...
+        pairConfigSize(2:end)));
+    
+    % Homogenize representation
+    plotSize = size(plotConfigs);
+    plotSize((end + 1):3) = 1;
+    
+    plotConfigs = reshape( ...
+        plotConfigs, [], plotSize(2), plotSize(3));
+    pairConfigs = reshape( ...
+        pairConfigs, [], plotSize(2), plotSize(3));
+    
     fig = figure();
     fig.Color = 'white';
-    fig.Position(3:4) = [750, 420];
     
-    ax = axes(fig);
-    ax.TickDir = 'out';
-    axis(ax, 'square');
-    hold(ax, 'on');
-    
-    for curConfig = reshape(pairConfigs, 1, [])
-        curCvs = synT.area(curConfig.synIdPairs);
-        curCvs = std(curCvs, 0, 2) ./ mean(curCvs, 2);
-        
-        histogram( ...
-            ax, curCvs, ...
-            'BinEdges', opt.binEdges, ...
-            'Normalization', 'probability', ...
-            'DisplayStyle', 'stairs', ...
-            'LineWidth', 2);
+    for curRow = 1:plotSize(3)
+        for curCol = 1:plotSize(2)
+            curPlotConfig = plotConfigs(1, curCol, curRow);
+            curPairConfigs = pairConfigs(:, curCol, curRow);
+            
+            curAx = curCol + (curRow - 1) * plotSize(2);
+            curAx = subplot(plotSize(3), plotSize(2), curAx);
+            
+            curAx.TickDir = 'out';
+            axis(curAx, 'square');
+            hold(curAx, 'on');
+            
+            for curPairConfig = reshape(curPairConfigs, 1, [])
+                curCvs = synT.area(curPairConfig.synIdPairs);
+                curCvs = std(curCvs, 0, 2) ./ mean(curCvs, 2);
+
+                histogram( ...
+                    curAx, curCvs, ...
+                    'BinEdges', opt.binEdges, ...
+                    'Normalization', 'probability', ...
+                    'DisplayStyle', 'stairs', ...
+                    'LineWidth', 2);
+            end
+            
+            xlim(curAx, opt.binEdges([1, end]));
+            xlabel(curAx, 'Coefficient of variation');
+            ylabel(curAx, 'Probability');
+            
+            legend( ...
+                curAx, {curPairConfigs.title}, ...
+                'Location', 'SouthOutside', ...
+                'Box', 'off');
+            
+            title( ...
+                curAx, curPlotConfig.title, ...
+                'FontWeight', 'normal', ...
+                'FontSize', 10);
+        end
     end
     
-    xlim(ax, opt.binEdges([1, end]));
-    xlabel(ax, 'Coefficient of variation');
-    ylabel(ax, 'Probability');
-    
-    legend( ...
-        ax, {pairConfigs.title}, ...
-        'Location', 'EastOutside', 'Box', 'off');
-    title( ...
-        ax, {info.filename; info.git_repos{1}.hash; plotConfig.title}, ...
-        'FontWeight', 'normal', 'FontSize', 10);
+    annotation(fig, ...
+        'textbox', [0, 0.9, 1, 0.1], ...
+        'String', {info.filename; info.git_repos{1}.hash}, ...
+        'EdgeColor', 'none', 'HorizontalAlignment', 'center');
 end
