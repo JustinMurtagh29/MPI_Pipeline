8c6808aadb9878618215f93d01a40b55c6035d3c
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-05-18 10:18:46 +0200

    Generate meta data to color isosurfaces according to target class

--- +connectEM/+Availability/+Script/calculateIsosurfaces.m
+++ +connectEM/+Availability/+Script/calculateIsosurfaces.m
@@ -4,7 +4,8 @@ clear;
 
 %% Configuration
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
-outDir = '/tmpscratch/amotta/l4/2018-04-30-availabilities-soma-axon/iso';
+outDir = '/tmpscratch/amotta/l4/2018-04-30-availabilities-soma-axon';
+isoDir = fullfile(outDir, 'iso');
 
 % NOTE(amotta): This is an older connectome. It has the same dendrite
 % agglomerates as `connectome_axons_18_a_ax_spine_syn_clust`, but different
@@ -17,6 +18,11 @@ outDir = '/tmpscratch/amotta/l4/2018-04-30-availabilities-soma-axon/iso';
 connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons_18_a.mat');
 minSynCount = 10;
 
+% NOTE(amotta): For historic reasons the postsynaptic agglomerates are
+% rendered from an other version of the connectome. But for target class
+% identification we will use the latest results.
+dendMetaFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-19-a_dendrites-wholeCells-03-v2-classified_spine-syn-clust.mat');
+
 distVol = struct;
 distVol.rootDir = '/tmpscratch/amotta/l4/2018-04-30-availabilities-soma-axon/wkw-lz4';
 distVol.thresh = 5000;
@@ -27,12 +33,19 @@ info = Util.runInfo();
 param = load(fullfile(rootDir, 'allParameter.mat'));
 param = param.p;
 
+maxSegId = Seg.Global.getMaxSegId(param);
+
 % for speed
 param.seg = struct;
 param.seg.root = '/tmpscratch/amotta/l4/2012-09-28_ex145_07x2_ROI2017/segmentation/1';
 param.seg.backend = 'wkwrap';
 
 conn = load(connFile);
+
+% Load traget classes
+dendMeta = connectEM.Connectome.load(param, dendMetaFile);
+dendMeta.denMeta.agglo = dendMeta.dendrites;
+dendMeta = dendMeta.denMeta;
 
 %% Select dendrite subset
 postSynCount = accumarray( ...
@@ -43,8 +56,34 @@ postSynCount = accumarray( ...
 postSynIds = find(postSynCount >= minSynCount);
 postSynAgglos = conn.dendrites(postSynIds);
 
+%% Build meta data
+% Find target class for each postsynaptic agglomerate
+targetClasses = Agglo.buildLUT(maxSegId, dendMeta.agglo);
+targetClasses = cellfun(@(segIds) mode( ...
+    nonzeros(targetClasses(segIds))), postSynAgglos);
+targetClasses = dendMeta.targetClass(targetClasses);
+
+[targetClasses, ~, targetClassFiles] = unique(targetClasses);
+targetClasses = categories(targetClasses);
+
+targetClassFiles = accumarray( ...
+    targetClassFiles, reshape(1:numel(targetClassFiles), [], 1), [], ...
+    @(aggloIds) {arrayfun(@(id) ...
+        fullfile(isoDir, 'ply', sprintf('iso-%d.ply', id)), ...
+        aggloIds, 'UniformOutput', false)});
+
+%% Save meta data
+out = struct;
+out.info = info;
+out.targetClasses = targetClasses;
+out.targetClassFiles = targetClassFiles;
+
+infoFile = fullfile(outDir, 'info.mat');
+Util.saveStruct(infoFile, out);
+Util.protect(infoFile);
+
 %% Building isosurface
 Visualization.exportAggloToAmira( ...
-    param, postSynAgglos, outDir, ...
+    param, postSynAgglos, isoDir, ...
     'distVol', distVol, 'reduce', 0.05, ...
     'smoothSizeHalf', 4, 'smoothWidth', 8);
