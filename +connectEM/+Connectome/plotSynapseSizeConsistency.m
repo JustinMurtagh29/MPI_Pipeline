cf54660df3bddf2862197c7a9c2afcafe023dc1d
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-06-05 13:23:40 +0200

    Analyse distance-dependence on control version of connectome v5

--- +connectEM/+Connectome/plotSynapseSizeConsistency.m
+++ +connectEM/+Connectome/plotSynapseSizeConsistency.m
@@ -5,10 +5,7 @@ clear;
 %% Configuration
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
 connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-19-a_dendrites-wholeCells-03-v2-classified_spine-syn-clust.mat');
-
-% Control with linearized axons
-ctrlConnFile = fullfile(rootDir, 'connectomeState', 'connectome_axons_18_b_linearized_ax_spine_syn_clust.mat');
-ctrlSynFile = fullfile(rootDir, 'connectomeState', 'SynapseAgglos_v3_ax_spine_clustered.mat');
+ctrlConnFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-19-a-linearized_dendrites-wholeCells-03-v2-classified_spine-syn-clust.mat');
 
 info = Util.runInfo();
 
@@ -18,14 +15,18 @@ param = param.p;
 
 [conn, syn, axonClasses] = ...
     connectEM.Connectome.load(param, connFile);
+[ctrlConn, ctrlSyn] = ...
+    connectEM.Connectome.load(param, ctrlConnFile);
 
 [~, synToSynFile] = fileparts(connFile);
 synToSynFile = sprintf('%s_synToSynDists.mat', synToSynFile);
 synToSynFile = fullfile(fileparts(connFile), synToSynFile);
 synToSyn = load(synToSynFile);
 
-ctrlConn = load(ctrlConnFile);
-ctrlSyn = load(ctrlSynFile);
+[~, ctrlSynToSynFile] = fileparts(ctrlConnFile);
+ctrlSynToSynFile = sprintf('%s_synToSynDists.mat', ctrlSynToSynFile);
+ctrlSynToSynFile = fullfile(fileparts(connFile), ctrlSynToSynFile);
+ctrlSynToSyn = load(ctrlSynToSynFile);
 
 %% Prepare data
 synT = connectEM.Connectome.buildSynapseTable(conn, syn);
@@ -233,245 +234,110 @@ connectEM.Consistency.plotVariabilityHistogram( ...
     info, synT, curPlotConfigs, curPairConfigs);
 
 %% Variability vs. distance
-curMaxDistUm = 20;
+clear cur*;
+curConfigs = [ ...
+    struct( ...
+        'synT', synT, 'plotConfig', plotConfigs(1), ...
+        'synToSyn', synToSyn, 'maxDistUm', []), ...
+    struct( ...
+        'synT', synT, 'plotConfig', plotConfigs(1), ...
+        'synToSyn', synToSyn, 'maxDistUm', 20), ...
+    struct( ...
+        'synT', ctrlSynT, 'plotConfig', ctrlPlotConfigs(1), ...
+        'synToSyn', ctrlSynToSyn, 'maxDistUm', []), ...
+    struct( ...
+        'synT', ctrlSynT, 'plotConfig', ctrlPlotConfigs(1), ...
+        'synToSyn', ctrlSynToSyn, 'maxDistUm', 20)];
 
-curPlotConfig = plotConfigs(1);
-curPairConfig = connectEM.Consistency.buildPairConfigs(synT, curPlotConfig);
-curPairConfig = curPairConfig(1);
-
-curT = struct2table(rmfield(curPairConfig, 'title'));
-curT.preAggloId = synT.preAggloId(curT.synIdPairs(:, 1));
-curT.postAggloId = synT.postAggloId(curT.synIdPairs(:, 1));
-
-curT.preDist = zeros(size(curT.preAggloId));
-curT.postDist = zeros(size(curT.postAggloId));
-for curIdx = 1:numel(curT.preDist)
-    curPreAggloId = curT.preAggloId(curIdx);
-    curPostAggloId = curT.postAggloId(curIdx);
-    curSynIds = synT.id(curT.synIdPairs(curIdx, :));
+for curConfig = curConfigs
+    curSynT = curConfig.synT;
+    curPlotConfig = curConfig.plotConfig;
+    curSynToSyn = curConfig.synToSyn;
     
-    % Distance along axonal side
-    curPreSynIds = synToSyn.axonSynIds{curPreAggloId};
-   [~, curPreSynIds] = ismember(curSynIds, curPreSynIds);
-    curPreDist = synToSyn.axonSynToSynDists{curPreAggloId};
-    curPreDist = curPreDist(curPreSynIds(1), curPreSynIds(2));
-    
-    % Distance along axonal side
-    curPostSynIds = synToSyn.dendSynIds{curPostAggloId};
-   [~, curPostSynIds] = ismember(curSynIds, curPostSynIds);
-    curPostDist = synToSyn.dendSynToSynDists{curPostAggloId};
-    curPostDist = curPostDist(curPostSynIds(1), curPostSynIds(2));
-    
-    curT.preDist(curIdx) = curPreDist;
-    curT.postDist(curIdx) = curPostDist;
-end
+    curPairConfig = ...
+        connectEM.Consistency.buildPairConfigs(curSynT, curPlotConfig);
+    curPairConfig = curPairConfig(1);
 
-curT.cv = synT.area(curT.synIdPairs);
-curT.cv = std(curT.cv, 0, 2) ./ mean(curT.cv, 2);
+    curT = struct2table(rmfield(curPairConfig, 'title'));
+    curT.preAggloId = curSynT.preAggloId(curT.synIdPairs(:, 1));
+    curT.postAggloId = curSynT.postAggloId(curT.synIdPairs(:, 1));
 
-curPreT = curT(curT.preDist / 1E3 < curMaxDistUm, :);
-curFitPre = fit(curPreT.preDist / 1E3, curPreT.cv, 'poly1');
+    curT.preDist = zeros(size(curT.preAggloId));
+    curT.postDist = zeros(size(curT.postAggloId));
+    for curIdx = 1:numel(curT.preDist)
+        curPreAggloId = curT.preAggloId(curIdx);
+        curPostAggloId = curT.postAggloId(curIdx);
+        curSynIds = curSynT.id(curT.synIdPairs(curIdx, :));
 
-curPostT = curT(curT.postDist / 1E3 < curMaxDistUm, :);
-curFitPost = fit(curPostT.postDist / 1E3, curPostT.cv, 'poly1');
+        % Distance along axonal side
+        curPreSynIds = curSynToSyn.axonSynIds{curPreAggloId};
+       [~, curPreSynIds] = ismember(curSynIds, curPreSynIds);
+        curPreDist = curSynToSyn.axonSynToSynDists{curPreAggloId};
+        curPreDist = curPreDist(curPreSynIds(1), curPreSynIds(2));
 
-% Plot
-fig = figure();
-fig.Color = 'white';
-fig.Position(3:4) = [560, 360];
+        % Distance along axonal side
+        curPostSynIds = curSynToSyn.dendSynIds{curPostAggloId};
+       [~, curPostSynIds] = ismember(curSynIds, curPostSynIds);
+        curPostDist = curSynToSyn.dendSynToSynDists{curPostAggloId};
+        curPostDist = curPostDist(curPostSynIds(1), curPostSynIds(2));
 
-% Presynaptic side
-ax = subplot(1, 2, 1);
-axis(ax, 'square');
-hold(ax, 'on');
-
-plot(ax, curPreT.preDist / 1E3, curPreT.cv, '.');
-plot(ax, ax.XLim, curFitPre(ax.XLim), 'Color', 'black');
-title(ax, {'Presynaptic', sprintf( ...
-    'CV = %.2f + %.4fd', curFitPre.p2, curFitPre.p1)}, ...
-    'FontWeight', 'normal', 'FontSize', 10);
-
-% Postsynaptic side
-ax = subplot(1, 2, 2);
-axis(ax, 'square');
-hold(ax, 'on');
-
-plot(ax, curPostT.postDist / 1E3, curPostT.cv, '.');
-plot(ax, ax.XLim, curFitPost(ax.XLim), 'Color', 'black');
-title(ax, {'Postsynaptic', sprintf( ...
-    'CV = %.2f + %.4fd', curFitPost.p2, curFitPost.p1)}, ...
-    'FontWeight', 'normal', 'FontSize', 10);
-
-axes = fig.Children;
-[ax.TickDir] = deal('out');
-
-ax = axes(end);
-xlabel(ax, 'Intersynapse distance (µm)');
-ylabel(ax, 'Coefficient of variation');
-
-annotation(fig, ...
-    'textbox', [0, 0.9, 1, 0.1], ...
-    'String', { ...
-        info.filename; ...
-        info.git_repos{1}.hash; ...
-        curPairConfig.title}, ...
-    'EdgeColor', 'none', 'HorizontalAlignment', 'center');
-
-%% ASI sizes (in log scale)
-plotCouplings = 2:5;
-fig = figure();
-fig.Color = 'white';
-fig.Position(3:4) = [960, 1090];
-
-for curCouplingIdx = 1:numel(plotCouplings)
-    curCoupling = plotCouplings(curCouplingIdx);
-    
-    synAreas = zeros(0, 1);
-    synGroups = zeros(0, 1);
-
-    for curClassIdx = 1:numel(plotConfigs)
-        curAxonIds = plotConfigs(curClassIdx).axonIds;
-        curSynIds = plotConfigs(curClassIdx).synIds;
-        
-        curSynGroups = synT(curSynIds, {'preAggloId', 'postAggloId'});
-       [~, ~, curSynGroups] = unique(curSynGroups, 'rows');
-       
-        curSynAreas = accumarray( ...
-            curSynGroups, synT.area(curSynIds), ...
-            [], @(areas) {sort(areas, 'descend')});
-        
-        curSynGroups = accumarray(curSynGroups, 1);
-        curSynAreas(curSynGroups ~= curCoupling) = [];
-        
-        curSynGroups = transpose(repmat( ...
-            1:curCoupling, 1, numel(curSynAreas)));
-        curSynGroups = (curSynGroups - 1) ...
-            * numel(plotConfigs) + curClassIdx;
-        curSynAreas = cell2mat(curSynAreas);
-        
-        synAreas = [synAreas; curSynAreas(:)]; %#ok
-        synGroups = [synGroups; curSynGroups(:)]; %#ok
+        curT.preDist(curIdx) = curPreDist;
+        curT.postDist(curIdx) = curPostDist;
     end
+
+    curT.cv = synT.area(curT.synIdPairs);
+    curT.cv = std(curT.cv, 0, 2) ./ mean(curT.cv, 2);
     
-    curGroupLabels = arrayfun(@(i) sprintf( ...
-        '(%d) (%s)', ...
-        ceil(i / numel(plotConfigs)), ...
-        plotConfigs(1 + mod(i - 1, numel(plotConfigs))).tag), ...
-        unique(synGroups), 'UniformOutput', false);
-    curGroupIds = mod(synGroups - 1, numel(plotConfigs));
-    
-    figure(fig);
-    curAx = subplot(numel(plotCouplings), 1, curCouplingIdx);
-    synAreasLog = log10(synAreas);
-    
-    boxplot( ...
-        curAx, synAreasLog, synGroups, ...
-        'Labels', curGroupLabels, ...
-        'Colors', curAx.ColorOrder, ...
-        'ColorGroup', curGroupIds, ...
-        'Symbol', '.');
-    
-    curAx.TickDir = 'out';
-    curAx.XTickLabelRotation = 30;
-end
-
-xlabel(curAx, 'Synapse');
-ylabel(curAx, 'log_{10}(ASI)');
-
-xMax = max(arrayfun(@(a) a.XLim(end), fig.Children));
-yLims = cell2mat(arrayfun( ...
-    @(a) a.YLim, fig.Children(:), ...
-    'UniformOutput', false));
-yLims = prctile(yLims(:), [0, 100]);
-
-[fig.Children.XLim] = deal([0, xMax]);
-[fig.Children.YLim] = deal(yLims);
-
-annotation( ...
-    fig, ...
-    'textbox', [0, 0.9, 1, 0.1], ...
-	'String', {info.filename; info.git_repos{1}.hash}, ...
-    'EdgeColor', 'none', 'HorizontalAlignment', 'center');
-
-%% ASI variability for all combinations
-plotCouplings = 2:5;
-cvOf = @(d) std(d, 0, 2) ./ mean(d, 2);
-
-fig = figure();
-fig.Color = 'white';
-fig.Position(3:4) = [960, 1090];
-
-for curCouplingIdx = 1:numel(plotCouplings)
-    curCoupling = plotCouplings(curCouplingIdx);
-    
-    curPairs = sortrows(combnk(1:curCoupling, 2));
-   [~, curSortIds] = sort(diff(curPairs, 1, 2), 'ascend');
-    curPairs = curPairs(curSortIds, :);
-    
-    cvVals = zeros(0, 1);
-    cvGroups = zeros(0, 1);
-
-    for curClassIdx = 1:numel(plotConfigs)
-        curAxonIds = plotConfigs(curClassIdx).axonIds;
-        curSynIds = plotConfigs(curClassIdx).synIds;
-
-       [~, ~, curCvGroups] = unique(synT( ...
-            curSynIds, {'preAggloId', 'postAggloId'}), 'rows');
-        curSynAreas = accumarray( ...
-            curCvGroups, synT.area(curSynIds), ...
-            [], @(areas) {sort(areas, 'descend')});
-        
-        curCvGroups = accumarray(curCvGroups, 1);
-        curSynAreas(curCvGroups ~= curCoupling) = [];
-        
-        curCvVals = cellfun( ...
-            @(asis) cvOf(reshape(asis(curPairs), [], 2))', ...
-            curSynAreas, 'UniformOutput', false);
-        curCvVals = cell2mat(curCvVals);
-        
-        curCvGroups = repelem( ...
-            1:size(curPairs, 1), size(curCvVals, 1));
-        curCvGroups = (curCvGroups - 1) ...
-            * numel(plotConfigs) + curClassIdx;
-        
-        cvVals = [cvVals; curCvVals(:)]; %#ok
-        cvGroups = [cvGroups; curCvGroups(:)]; %#ok
+    if ~isempty(curConfig.maxDistUm)
+        curPreT = curT(curT.preDist / 1E3 < curConfig.maxDistUm, :);
+        curPostT = curT(curT.postDist / 1E3 < curConfig.maxDistUm, :);
+    else
+        curPreT = curT;
+        curPostT = curT;
     end
-    
-    curGroupLabels = arrayfun(@(i) sprintf( ...
-        '(%d, %d) (%s)', ...
-        curPairs(ceil(i / numel(plotConfigs)), :), ...
-        plotConfigs(1 + mod(i - 1, numel(plotConfigs))).tag), ...
-        unique(cvGroups), 'UniformOutput', false);
-    curGroupIds = mod(cvGroups - 1, numel(plotConfigs));
-    
-    figure(fig);
-    curAx = subplot(numel(plotCouplings), 1, curCouplingIdx);
-    
-    boxplot( ...
-        curAx, cvVals, cvGroups, ...
-        'Labels', curGroupLabels, ...
-        'Colors', curAx.ColorOrder, ...
-        'ColorGroup', curGroupIds, ...
-        'Symbol', '.');
-    
-    curAx.TickDir = 'out';
-    curAx.XTickLabelRotation = 30;
+
+    curFitPre = fit(curPreT.preDist / 1E3, curPreT.cv, 'poly1');
+    curFitPost = fit(curPostT.postDist / 1E3, curPostT.cv, 'poly1');
+
+    % Plot
+    fig = figure();
+    fig.Color = 'white';
+    fig.Position(3:4) = [560, 390];
+
+    % Presynaptic side
+    ax = subplot(1, 2, 1);
+    axis(ax, 'square');
+    hold(ax, 'on');
+
+    plot(ax, curPreT.preDist / 1E3, curPreT.cv, '.');
+    plot(ax, ax.XLim, curFitPre(ax.XLim), 'Color', 'black');
+    title(ax, {'Presynaptic', sprintf( ...
+        'CV = %.2f + %.4fd', curFitPre.p2, curFitPre.p1)}, ...
+        'FontWeight', 'normal', 'FontSize', 10);
+
+    % Postsynaptic side
+    ax = subplot(1, 2, 2);
+    axis(ax, 'square');
+    hold(ax, 'on');
+
+    plot(ax, curPostT.postDist / 1E3, curPostT.cv, '.');
+    plot(ax, ax.XLim, curFitPost(ax.XLim), 'Color', 'black');
+    title(ax, {'Postsynaptic', sprintf( ...
+        'CV = %.2f + %.4fd', curFitPost.p2, curFitPost.p1)}, ...
+        'FontWeight', 'normal', 'FontSize', 10);
+
+    axes = fig.Children;
+    [ax.TickDir] = deal('out');
+
+    ax = axes(end);
+    xlabel(ax, 'Intersynapse distance (µm)');
+    ylabel(ax, 'Coefficient of variation');
+
+    annotation(fig, ...
+        'textbox', [0, 0.9, 1, 0.1], ...
+        'String', { ...
+            info.filename; ...
+            info.git_repos{1}.hash; ...
+            curPairConfig.title}, ...
+        'EdgeColor', 'none', 'HorizontalAlignment', 'center');
 end
-
-xlabel(curAx, 'Synapse pair');
-ylabel(curAx, 'Synapse variability (CV)');
-
-xMax = max(arrayfun(@(a) a.XLim(end), fig.Children));
-yMax = max(arrayfun(@(a) a.YLim(end), fig.Children));
-
-[fig.Children.XLim] = deal([0, xMax]);
-[fig.Children.YLim] = deal([0, yMax]);
-
-annotation( ...
-    fig, ...
-    'textbox', [0, 0.9, 1, 0.1], ...
-	'String', { ...
-        'Variability for all synapse combinations'; ...
-        info.git_repos{1}.hash}, ...
-    'EdgeColor', 'none', 'HorizontalAlignment', 'center');
