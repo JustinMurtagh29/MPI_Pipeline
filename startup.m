dff2e398ddeb95da6dfbb945b01aef351ce0c16a
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-06-11 10:58:04 +0200

    Add Benedikt's repository as submodule and tail to MATLAB path

--- startup.m
+++ startup.m
@@ -1,7 +1,16 @@
+thisDir = fileparts(mfilename('fullpath'));
+beneDir = fullfile(thisDir, 'benedikt');
+
 % Add current directory and its sub-directories to path
-addpath(genpathGit(fileparts(mfilename('fullpath'))));
+addpath(genpathGit(thisDir));
+
+% Make sure that Benedikt's repository doesn't shadow anything
+benePath = genpath(beneDir);
+
+rmpath(benePath);
+addpath(benePath, '-end');
 
 % Mark as ready
 global PIPELINE_READY;
 PIPELINE_READY = true;
-clear PIPELINE_READY
+clear;
