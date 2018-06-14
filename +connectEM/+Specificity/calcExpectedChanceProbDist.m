71915dc57e207536851d0486ab3eca6bc69bddb3
Benedikt Staffler <benedikt.staffler@brain.mpg.de>
2018-06-14 16:59:29 +0200

    add option to specify pdf to allow for arbitrary models

--- +connectEM/+Specificity/calcExpectedChanceProbDist.m
+++ +connectEM/+Specificity/calcExpectedChanceProbDist.m
@@ -1,7 +1,23 @@
 function [pVals, expAxonCount] = ...
-        calcExpectedChanceProbDist(axonSynCounts, synProb)
+        calcExpectedChanceProbDist(axonSynCounts, synProb, varargin)
+    % Calculates the expected distribution of righ-tail p-values
+    % (upper CDF), i.e. P(X >= p), for a binomial model with success rate
+    % syn prob and axons with varying total synapse numbers.
+    %
+    % calcExpectedChanceProbDist(axonSynCounts, [], 'pdf', pdf_func)
+    %   calculates the p-value distribution using the pdf given by
+    %   pdf_func, where pdf_func is a function handle that accepts the
+    %   total number of synapses n and must return a distribution over the
+    %   0:n as a vector of probabilities.
+    %
     % Written by
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    %   Benedikt Staffler <benedikt.staffler@brain.mpg.de>
+    
+    opts = struct;
+    opts.pdf = @(n)binopdf(0:n, n, synProb);
+    opts = Util.modifyStruct(opts, varargin{:});
+    
    [synCounts, ~, axonCounts] = unique(axonSynCounts);
     axonCounts = accumarray(axonCounts, 1);
     
@@ -12,7 +28,7 @@ function [pVals, expAxonCount] = ...
         curSynCount = synCounts(curIdx);
         curAxonCount = axonCounts(curIdx);
         
-        curProbs = binopdf(0:curSynCount, curSynCount, synProb);
+        curProbs = opts.pdf(curSynCount);
         curPVals = flip(cumsum(flip(curProbs)));
         
         pVals{curIdx} = curPVals(:);
