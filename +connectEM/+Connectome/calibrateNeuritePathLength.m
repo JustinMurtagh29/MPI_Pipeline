4397537e608a53268ab940e500551b3ad7cc33f2
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-06-16 12:15:16 +0200

    Calibrate MST-based path length calculation for dendritic trunks

---
+++ +connectEM/+Connectome/calibrateNeuritePathLength.m
@@ -0,0 +1,75 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+
+%% Configuration
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-19-a_dendrites-wholeCells-03-v2-classified_spine-syn-clust.mat');
+trunkFile = fullfile(rootDir, 'aggloState', 'dendrites_wholeCells_02_v3.mat');
+
+[~, lengthFile] = fileparts(connFile);
+lengthFile = sprintf('%s_pathLengths.mat', lengthFile);
+lengthFile = fullfile(fileparts(connFile), lengthFile);
+
+info = Util.runInfo();
+
+%% Loading data
+param = load(fullfile(rootDir, 'allParameter.mat'));
+param = param.p;
+
+conn = connectEM.Connectome.load(param, connFile);
+
+trunks = load(trunkFile);
+trunks = trunks.dendrites(trunks.indBigDends);
+trunks = Agglo.fromSuperAgglo(trunks);
+
+lengths = load(lengthFile);
+
+maxSegId = Seg.Global.getMaxSegId(param);
+trunkLUT = Agglo.buildLUT(maxSegId, trunks);
+
+%% Loading dendrite calibration data
+dendCalibT = connectEM.Dendrite.Data.loadPathLengthCalibration(param);
+
+dendCalibT.trunkId = cellfun( ...
+    @(segIds) mode(nonzeros(trunkLUT(segIds))), ...
+    conn.dendrites(dendCalibT.id));
+dendCalibT.trunkPathLength = ...
+    lengths.trunkPathLengths(dendCalibT.trunkId);
+
+%% Calculating correction coefficient
+corrCoeff = mean(dendCalibT.pathLength ./ dendCalibT.trunkPathLength);
+
+%% Plot
+curFig = figure();
+curFig.Color = 'white';
+curFig.Position(3:4) = [420, 420];
+
+curAx = axes(curFig);
+axis(curAx, 'equal');
+hold(curAx, 'on');
+
+scatter(curAx, ...
+    dendCalibT.trunkPathLength / 1E3, ...
+    dendCalibT.pathLength / 1E3, 72, '.');
+
+curLim = max(curAx.XLim(2), curAx.YLim(2));
+curAx.XLim = [0, curLim];
+curAx.YLim = [0, curLim];
+
+plot(curAx, curAx.XLim, corrCoeff * curAx.XLim);
+plot(curAx, curAx.XLim, curAx.YLim, 'Color', 'black', 'LineStyle', '--');
+
+curAx.TickDir = 'out';
+xlabel(curAx, 'MST-based trunk length (µm)');
+ylabel(curAx, 'Tracing-based trunk length (µm)');
+
+curLeg = legend(curAx, { ...
+    'Calibration points', ...
+    sprintf('Correction (coefficient = %.3f)', corrCoeff)}, ...
+    'Location', 'NorthWest');
+curLeg.Box = 'off';
+
+title(curAx,  ...
+    {info.filename; info.git_repos{1}.hash}, ...
+    'FontWeight', 'normal', 'FontSize', 10);
---
+++ +connectEM/+Dendrite/+Data/loadPathLengthCalibration.m
@@ -0,0 +1,54 @@
+function calibT = loadPathLengthCalibration(param)
+    % Written by
+    %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    thisDir = fileparts(mfilename('fullpath'));
+    nmlDir = fullfile(thisDir, 'pathLengthCalibration');
+    
+    nmlFiles = dir(fullfile(nmlDir, '*.nml'));
+    nmlFiles = reshape({nmlFiles.name}, [], 1);
+    nmlFiles = fullfile(nmlDir, nmlFiles);
+    
+    calibT = table;
+    calibT.nmlFile = nmlFiles;
+    
+    calibT.id = regexp( ...
+        calibT.nmlFile, '.*-(?<id>\d+)\.nml$', 'tokens', 'once');
+    calibT(cellfun(@isempty, calibT.id), :) = [];
+    
+    calibT.id = cat(1, calibT.id{:});
+    calibT.id = cellfun(@str2double, calibT.id);
+    
+    % Calculate ground truth path length
+    calibT.pathLength = cellfun( ...
+        @(nmlFile) calculatePathLength(param, nmlFile), calibT.nmlFile);
+    
+    % Move file path to the right
+    % It's so long that it clutters the display output
+    calibT = calibT(:, [2:end, 1]);
+end
+
+function pathLength = calculatePathLength(param, nmlFile)
+    nml = slurpNml(nmlFile);
+    trees = NML.buildTreeTable(nml);
+    
+    nodes = NML.buildNodeTable(nml);
+    nodes.coord = nodes.coord .* param.raw.voxelSize;
+    
+    % Find tree with oldest node
+    % This way we identify the template tracing
+   [~, templateTreeId] = min(nodes.time);
+    templateTreeId = nodes.treeId(templateTreeId);
+    
+    calibTreeId = setdiff(nodes.treeId, templateTreeId);
+    assert(isscalar(calibTreeId));
+    
+    calibEdges = trees.edges{trees.id == calibTreeId};
+    calibEdges = cat(2, calibEdges.source, calibEdges.target);
+   [~, calibEdges] = ismember(calibEdges, nodes.id);
+    
+    pathLength = ...
+        nodes.coord(calibEdges(:, 1), :) ...
+      - nodes.coord(calibEdges(:, 2), :);
+    pathLength = sqrt(sum(pathLength .^ 2, 2));
+    pathLength = sum(pathLength);
+end
