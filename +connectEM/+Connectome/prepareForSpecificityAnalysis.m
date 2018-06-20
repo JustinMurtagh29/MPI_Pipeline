668e914ae60b334ace1de20fbf770545b528a1a3
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-06-20 18:07:33 +0200

    Drop excitatory synapses onto excitatory somata for specificity analysis

--- +connectEM/+Connectome/prepareForSpecificityAnalysis.m
+++ +connectEM/+Connectome/prepareForSpecificityAnalysis.m
@@ -1,6 +1,10 @@
-function conn = prepareForSpecificityAnalysis(conn)
+function [conn, axonClasses] = ...
+        prepareForSpecificityAnalysis(conn, axonClasses, varargin)
     % Written by
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    opt = struct;
+    opt.minSynPre = 1;
+    opt = Util.modifyStruct(opt, varargin{:});
     
     %% Configuration
     targetClassOrder = { ...
@@ -27,4 +31,75 @@ function conn = prepareForSpecificityAnalysis(conn)
         conn.denMeta.targetClass, cats);
     conn.denMeta.targetClass = reordercats( ...
         conn.denMeta.targetClass, targetClassOrder);
+    
+    %% Remove excitatory synapses onto excitatory somata
+    % As discussed with MH in a meeting on 19.06.2018.
+    conn = removeExcSynapsesOntoExcSomata(conn);
+    
+    %% Update axon classes, if desired
+    if ~exist('axonClasses', 'var'); return; end
+    
+    validAxonIds = conn.axonMeta.synCount >= opt.minSynPre;
+    validAxonIds = conn.axonMeta.id(validAxonIds);
+    
+    for curIdx = 1:numel(axonClasses)
+        axonClasses(curIdx).axonIds = intersect( ...
+            axonClasses(curIdx).axonIds, validAxonIds);
+        axonClasses(curIdx).nullAxonIds = intersect( ...
+            axonClasses(curIdx).nullAxonIds, validAxonIds);
+        axonClasses(curIdx).title = regexprep( ...
+            axonClasses(curIdx).title, 'n = \d+', ...
+            sprintf('n = %d', numel(axonClasses(curIdx).axonIds)));
+    end
+end
+
+function conn = removeExcSynapsesOntoExcSomata(conn)
+    excAxonIds = ismember( ...
+        conn.axonMeta.axonClass, ...
+        {'Corticocortical', 'Thalamocortical'});
+    excAxonIds = conn.axonMeta.id(excAxonIds);
+    
+    excSomaIds = ...
+        ~conn.denMeta.isInterneuron ...
+       & conn.denMeta.targetClass == 'Somata';
+    excSomaIds = conn.denMeta.id(excSomaIds);
+    
+    excSomaConn = conn.connectome;
+    excSomaConn.row(:) = 1:height(excSomaConn);
+    excSomaConn = excSomaConn(...
+        ismember(excSomaConn.edges(:, 1), excAxonIds) ...
+      & ismember(excSomaConn.edges(:, 2), excSomaIds), :);
+    
+    % Remove synapses from connectome
+    conn.connectome(excSomaConn.row, :) = [];
+    
+    % Decrease synapse count
+    synT = table;
+    synT.id = cell2mat(excSomaConn.synIdx);
+    synT.axonId(:) = repelem( ...
+        excSomaConn.edges(:, 1), ...
+        cellfun(@numel, excSomaConn.synIdx));
+    synT = unique(synT, 'rows');
+    
+   [excAxonRows, ~, excAxonSynCount] = unique(synT.axonId);
+   [~, excAxonRows] = ismember(excAxonRows, conn.axonMeta.id);
+    excAxonSynCount = accumarray(excAxonSynCount, 1);
+    
+    conn.axonMeta.synCount(excAxonRows) = ...
+        conn.axonMeta.synCount(excAxonRows) - excAxonSynCount;
+    
+    % HACK(amotta): I'm too lazy to update the `spineSynCount`,
+    % `fullSynCount`, `fullSpineSynCount`, and `fullPriSpineSynCount`
+    % variables. To make sure that nobody accidentally uses wrong numbers,
+    % let's just remove the variables completely.
+    conn.axonMeta(:, { ...
+        'spineSynCount', ...
+        'fullSynCount', ...
+        'fullSpineSynCount', ...
+        'fullPriSpineSynCount', ...
+        'fullPriSpineSynFrac', ...
+        'fullPriSpineSynDens'}) = [];
+    
+    % Same for connectome meta data
+    conn = rmfield(conn, 'connectomeMeta');
 end
