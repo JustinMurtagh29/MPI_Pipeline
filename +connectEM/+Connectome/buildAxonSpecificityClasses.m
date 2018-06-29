5709347d22a2d39f0303a6b735b9f368b031a632
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-06-29 16:11:53 +0200

    Update axon specificity classes to make use of perisoma class

--- +connectEM/+Connectome/buildAxonSpecificityClasses.m
+++ +connectEM/+Connectome/buildAxonSpecificityClasses.m
@@ -10,26 +10,24 @@ function axonClasses = buildAxonSpecificityClasses(conn, axonClasses)
     %% Add specificity thresholds
     % Excitatory axons
     axonClasses(1).specs = struct;
-    axonClasses(1).specs.ProximalDendrite.pThresh = 0.009347;
-    axonClasses(1).specs.ApicalDendrite.pThresh = 0.011567;
-    axonClasses(1).specs.SmoothDendrite.pThresh = 0.002976;
+    axonClasses(1).specs.ApicalDendrite.pThresh     = 0.011567;
+    axonClasses(1).specs.SmoothDendrite.pThresh     = 0.002976;
     
     % Inhibitory axons
     axonClasses(2).specs = struct;
-    axonClasses(2).specs.Somata.pThresh = 0.168331;
-    axonClasses(2).specs.ProximalDendrite.pThresh = 0.073651;
-    axonClasses(2).specs.ApicalDendrite.pThresh = 0.016054;
-    axonClasses(2).specs.SmoothDendrite.pThresh = 0.016215;
+    axonClasses(2).specs.Perisoma.pThresh           = 0.119379;
+    axonClasses(2).specs.ApicalDendrite.pThresh     = 0.016054;
+    axonClasses(2).specs.SmoothDendrite.pThresh     = 0.016215;
     axonClasses(2).specs.AxonInitialSegment.pThresh = 0.000823;
     
     % Thalamocortical axons
     axonClasses(3).specs = struct;
-    axonClasses(3).specs.ProximalDendrite.pThresh = 0.028024;
+    axonClasses(3).specs.Perisoma.pThresh           = 0.017675;
     
     % Corticocortical axons
     axonClasses(4).specs = struct;
-    axonClasses(4).specs.ApicalDendrite.pThresh = 0.014586;
-    axonClasses(4).specs.SmoothDendrite.pThresh = 0.004950;
+    axonClasses(4).specs.ApicalDendrite.pThresh     = 0.014586;
+    axonClasses(4).specs.SmoothDendrite.pThresh     = 0.004950;
     
     %% Find specific axons
    [classConn, targetClasses] = ...
