84530e678c29ce1890dcfd66ddd217b699b2d879
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-07-11 11:11:04 +0200

    Perform analysis for all axon classes

--- +connectEM/+Specificity/+Seeded/pairwiseComparison.m
+++ +connectEM/+Specificity/+Seeded/pairwiseComparison.m
@@ -39,41 +39,82 @@ curPlot = @(ax, data) histogram( ...
     'DisplayStyle', 'stairs', ...
     'LineWidth', 2);
 
-curFig = figure();
-curFig.Color = 'white';
+for curAxonClass = reshape(axonClasses, 1, [])
+    curAxonIds = curAxonClass.axonIds;
+    
+    curFig = figure();
+    curFig.Color = 'white';
+    curFig.Position(3:4) = [1280, 1350];
 
-for curRow = 1:(curClassCount - 1)
-    for curCol = 1:(curClassCount - 1)
-       [curSpecsA, curSpecsB] = ...
-            pairwiseSeededTargetClassSpec(classConn, [curRow, curCol]);
+    for curRow = 1:(curClassCount - 1)
+        for curCol = 1:(curClassCount - 1)
+           [curSpecsA, curSpecsB] = ...
+                pairwiseSeededTargetClassSpec( ...
+                    classConn, [curRow, curCol], curAxonIds);
+
+            curAx = curCol + (curRow - 1) * (curClassCount - 1);
+            curAx = subplot(curClassCount - 1, curClassCount - 1, curAx);
+
+            hold(curAx, 'on');
+            curPlot(curAx, curSpecsA(:, curRow));
+            curPlot(curAx, curSpecsB(:, curRow));
+            
+            xlabel(curAx, { ...
+                'Fraction of synapses'; ...
+                sprintf('onto %s', targetClasses{curRow})});
+        end
         
-        curAxIdx = curCol + (curRow - 1) * (curClassCount - 1);
-        curAx = subplot(curClassCount - 1, curClassCount - 1, curAxIdx);
+        curAxes = flip(curFig.Children(1:(curClassCount - 1)));
+        set(curAxes, 'YScale', 'log');
         
-        hold(curAx, 'on');
-        curPlot(curAx, curSpecsA(:, curRow));
-        curPlot(curAx, curSpecsB(:, curCol));
+        curMaxY = max(arrayfun(@(a) a.YLim(end), curAxes));
+        set(curAxes, 'YLim', [0, curMaxY]);
     end
-end
 
-curAxes = curFig.Children;
-set(curAxes, ...
-    'XLim', [0, 1], ...
-    'TickDir', 'out', ...
-    'PlotBoxAspectRatio', [1, 1, 1], ...
-    'DataAspectRatioMode', 'auto');
+    curAxes = flip(curFig.Children);
+    
+    for curRow = 1:(curClassCount - 1)
+        curAx = curAxes(1 + (curRow - 1) * (curClassCount - 1));
+        ylabel(curAx, sprintf('%s-seeded', targetClasses{curRow}));
+    end
+    
+    for curCol = 1:(curClassCount - 1)
+        curAx = curAxes(curCol);
+        title(curAx, ...
+            sprintf('%s-seeded', targetClasses{curCol}), ...
+            'FontWeight', 'normal', 'FontSize', 11);
+    end
+    
+    set(curAxes, ...
+        'XLim', [0, 1], ...
+        'TickDir', 'out', ...
+        'PlotBoxAspectRatio', [1, 1, 1], ...
+        'DataAspectRatioMode', 'auto');
+    
+    annotation( ...
+        curFig, ...
+        'textbox', [0, 0.9, 1, 0.1], ...
+        'String', { ...
+            info.filename; ...
+            info.git_repos{1}.hash; ...
+            curAxonClass.title}, ...
+        'EdgeColor', 'none', 'HorizontalAlignment', 'center');
+end
 
 %% Utilities
 function [specsA, specsB] = ...
-        pairwiseSeededTargetClassSpec(classConn, targetIds)
-    axonIdsA = find(classConn(:, targetIds(1)));
-    axonIdsB = find(classConn(:, targetIds(2)));
+        pairwiseSeededTargetClassSpec(classConn, targetIds, axonIds)
+    if ~exist('axonIds', 'var') || isempty(axonIds)
+        axonIds = reshape(1:size(classConn, 1), [], 1);
+    end
     
-    specsA = axonTargetClassSpec(classConn, axonIdsA, targetIds(1));
-    specsB = axonTargetClassSpec(classConn, axonIdsB, targetIds(2));
+    specsA = axonTargetClassSpec(classConn, axonIds, targetIds(1));
+    specsB = axonTargetClassSpec(classConn, axonIds, targetIds(2));
 end
 
 function specs = axonTargetClassSpec(classConn, axonIds, targetId)
+    axonIds = axonIds(classConn(axonIds, targetId) > 0);
+    
     specs = classConn(axonIds, :);
     specs(:, targetId) = specs(:, targetId) - 1;
     specs = specs ./ sum(specs, 2);
