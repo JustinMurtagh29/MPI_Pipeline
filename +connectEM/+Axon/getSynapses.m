2d97fa4c5768869da599dde93eccf1e767ff7913
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-07-13 13:29:14 +0200

    Return all synapses with additional in-connectome flag

--- +connectEM/+Axon/getSynapses.m
+++ +connectEM/+Axon/getSynapses.m
@@ -1,17 +1,32 @@
-function synIds = getSynapses(conn)
-    % synIds = getSynapses(conn)
+function [synIds, inConn] = getSynapses(conn, syn)
+    % synIds = getSynapses(param, conn)
     %   Returns a cell array, where `synIds{i}` contains the indices of the
-    %   synapses of axon `i`.
+    %   synapses of axon `i`. `inConn{i}(j)` is a logical indicating
+    %   whether synapse `synIds{i}(j)` is also contained in the connectome.
     %
     % Written by
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
-    connT = conn.connectome;
-   [~, synIds] = ismember( ...
-        connT.edges(:, 1), conn.axonMeta.id);
-    synIds = accumarray( ...
-        synIds, (1:size(synIds, 1))', size(conn.axons), ...
-        @(ids) {sort(cell2mat(connT.synIdx(ids)))}, {zeros(0, 1)});
+    maxSegId = max( ...
+        double(max(cellfun(@max, conn.axons))), ...
+        double(max(cellfun(@max, syn.synapses.presynId))));
+    axonLUT = Agglo.buildLUT(maxSegId, conn.axons);
     
-    % Sanity check
-    assert(isequal(cellfun(@numel, synIds), conn.axonMeta.synCount));
+    inConn = false(height(syn.synapses), 1);
+    inConn(cell2mat(conn.connectome.synIdx)) = true;
+    
+    axonIds = cellfun( ...
+        @(ids) reshape(setdiff(axonLUT(ids), 0), [], 1), ...
+        syn.synapses.presynId, 'UniformOutput', false);
+    
+    synIds = repelem( ...
+        reshape(1:height(syn.synapses), [], 1), ...
+        cellfun(@numel, axonIds));
+    axonIds = cell2mat(axonIds);
+    
+    synIds = accumarray( ...
+        axonIds, synIds, size(conn.axons), ...
+        @(ids) {reshape(ids, [], 1)}, {zeros(0, 1)});
+    inConn = cellfun( ...
+        @(ids) inConn(ids), synIds, ...
+        'UniformOutput', false);
 end
