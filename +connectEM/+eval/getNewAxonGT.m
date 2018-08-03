e1960e391c49f819f43234873aa87576a00c6895
Benedikt Staffler <benedikt.staffler@brain.mpg.de>
2018-08-03 17:40:29 +0200

    optional nhood input

--- +connectEM/+eval/getNewAxonGT.m
+++ +connectEM/+eval/getNewAxonGT.m
@@ -1,15 +1,24 @@
-function [ skels, segIds ] = getNewAxonGT(p)
+function [ skels, segIds ] = getNewAxonGT(p, nhood)
 %GETNEWAXONGT Load the axons in the evaluationData/new_axon_gt/ folder and
 % transform them to the ROI2017 alignment.
+%
 % INPUT p: (Optional) struct
 %           Segmentation parameter struct for ROI2017.
 %           (Default: Gaba.getSegParameters('ex145_ROI2017'))
+%       nhood: (Optional) int
+%           Neighborhood for the extraction of segment ids.
+%           (see also Skeleton.getSegmentIdsOfNodes)
+%           (Default: 0)
 % OUTPUT skels: [10x1] cell
 %           Cell array with skeleton objects for each new_axon_gt nml file.
 %        segIds: [Nx1] cell
 %           Segment ids for the skeletons in skels. If only calculated if
 %           nargout > 1.
 % Author: Benedikt Staffler <benedikt.staffler@brain.mpg.de>
+
+if ~exist('nhood', 'var') || isempty(nhood)
+    nhood = 0;
+end
 
 thisPath = mfilename('fullpath');
 ax_gt_path = fileparts(fileparts(thisPath));
@@ -23,10 +32,9 @@ skels = skeleton.loadSkelCollection(ax_gt_path, [], true);
 skels = convertToROI2017Alignment(skels);
 
 if nargout > 1
-    segIds = cellfun(@(x)Skeleton.getSegmentIdsOfSkel(p, x), skels, ...
-        'uni', 0);
-    % each nml has exactly one tree
-    segIds = cellfun(@(x)x{1}, segIds, 'uni', 0);
+    segIds = cellfun( ...
+        @(x)Skeleton.getSegmentIdsOfNodes(p, x.nodes{1}(:, 1:3), nhood), ...
+        skels, 'uni', 0);
 end
 end
 
