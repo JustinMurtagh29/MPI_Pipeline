0ac5375a7b5df4247fcfe49700a31eb7f34b6817
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-08-06 10:29:20 +0200

    Use latest connectome to plot coinnervation matrix

--- +connectEM/+Figure/coinnervation.m
+++ +connectEM/+Figure/coinnervation.m
@@ -4,7 +4,7 @@ clear;
 
 %% Configuration
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
-connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-19-a_dendrites-wholeCells-03-v2-classified_spine-syn-clust.mat');
+connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-19-a-partiallySplit-v2_dendrites-wholeCells-03-v2-classified_SynapseAgglos-v8-classified.mat');
 
 targetClasses = { ...
     'Somata', 'SO'; ...
