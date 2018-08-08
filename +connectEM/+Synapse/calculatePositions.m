7997e4d6f294610fc9e17e724fb1409aaca4b836
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-08-08 15:53:54 +0200

    Add option to bias synapse location towards pre- or postsynaptic side

--- +connectEM/+Synapse/calculatePositions.m
+++ +connectEM/+Synapse/calculatePositions.m
@@ -1,16 +1,30 @@
-function pos = calculatePositions(param, syn)
-    % pos = calculatePosition(param, syn)
+function pos = calculatePositions(param, syn, where)
+    % pos = calculatePosition(param, syn, where)
     %
     % Written by
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    
+    if ~exist('where', 'var') || isempty(where)
+        where = 'prePost';
+    end
+    
     points = Seg.Global.getSegToCentroidMap(param);
     weights = Seg.Global.getSegToSizeMap(param);
-
-    pos = cellfun( ...
-        @vertcat, ...
-        syn.synapses.presynId, ...
-        syn.synapses.postsynId, ...
-        'UniformOutput', false);
+    
+    switch where
+        case 'pre'
+            pos = syn.synapses.presynId;
+        case 'post'
+            pos = syn.synapses.postsynId;
+        case 'prePost'    
+            pos = cellfun( ...
+                @vertcat, ...
+                syn.synapses.presynId, ...
+                syn.synapses.postsynId, ...
+                'UniformOutput', false);
+        otherwise
+            error('Invalid input argument');
+    end
 
     wmean = @(w, d) sum((w ./ sum(w)) .* d);
     pos = cell2mat(cellfun( ...
