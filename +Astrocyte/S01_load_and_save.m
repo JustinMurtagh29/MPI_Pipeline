e41937aa252d895a7e1b9cae2804f339f4fe9081
Yagmur Yener <yyener@gaba.opt.rzg.mpg.de>
2018-09-12 11:47:41 +0200

    wkcubes location copy

--- +Astrocyte/S01_load_and_save.m
+++ +Astrocyte/S01_load_and_save.m
@@ -25,8 +25,8 @@ syn = load(conn.info.param.synFile); % synapses (segment IDs)
 %location in the L4 dataset
 box_offset = [4179, 4994, 2264];
 box_shape = [178, 178, 72];
-bbox = [box_offset' , (box_offset+box_shape)'];
-
+bbox = [box_offset' , (box_offset+box_shape-1)'];
+%%
 %Region annotated by CNN (voxel-wise)
 %pred: binary thresholded predictions 1 means astro
 %prediction: predicted probabilities continuous between -1.7 and 1.7
@@ -45,5 +45,17 @@ save('/gaba/u/yyener/astrocyte/synapses/syn_points.mat', 'syn_points')
 save('/gaba/u/yyener/astrocyte/synapses/syn.mat', 'syn')
 save('/gaba/u/yyener/astrocyte/synapses/param.mat', 'param')
 save('/gaba/u/yyener/astrocyte/synapses/conn.mat', 'conn')
+
+%% segmentation volumes
+
+% for speed
+param.seg = struct;
+param.seg.root = '/tmpscratch/amotta/l4/2012-09-28_ex145_07x2_ROI2017/segmentation/1';
+param.seg.backend = 'wkwrap';
+%save('/gaba/u/yyener/astrocyte/synapses/param.mat', 'param')
+
+seg = loadSegDataGlobal(param.seg, bbox);
+save('/gaba/u/yyener/astrocyte/synapses/seg.mat', 'seg')
+
 
 
