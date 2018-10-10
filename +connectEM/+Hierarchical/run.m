a602efbdfd0aadd7cd75d3d874fea17ccc6cb33d
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-10-10 19:58:04 +0200

    Prepare for larger-scale experiment with hierarchical agglomeration

--- +connectEM/+Hierarchical/run.m
+++ +connectEM/+Hierarchical/run.m
@@ -1,119 +1,54 @@
-% Written by
-%   Alessandro Motta <alessandro.motta@brain.mpg.de>
-clear
+function [mergedBorders, mergeScores] = run(class, edges, borders)
+    % Written by
+    %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    edgeT = table;
+    edgeT.edge = edges;
+    edgeT.borderIds = num2cell(transpose(1:size(edgeT, 1)));
+    edgeT.score = cellfun(@(ids) median(class( ...
+        unique(cell2mat(borders(ids))))), edgeT.borderIds);
 
-%% Load membrane and segmentation data
-load('/home/amotta/Desktop/mrnet.mat');
+    segCount = numel(unique(edges));
+    mergedBorders = cell(segCount - 1, 1);
+    mergeScores = nan(segCount - 1, 1);
 
-box = [
-    4856, 5367; ...
-    3289, 3800; ...
-    3969, 4224];
+    for curId = 1:(segCount - 1)
+       [curScore, curEdgeId] = max(edgeT.score);
+        curEdge = edgeT.edge(curEdgeId, :);
 
-%% Build edges and borders
-[edges, borders] = SynEM.Svg.findEdgesAndBorders(seg);
-borders = reshape({borders.PixelIdxList}, [], 1);
-segPoints = Seg.Local.calcSegmentPoint(seg);
+        mergedBorders(curId) = edgeT.borderIds(curEdgeId, :);
+        mergeScores(curId) = curScore;
+        
+        curMask = ismember(edgeT.edge, curEdge);
+        edgeT.edge(curMask) = curEdge(1);
+        edgeT.edge = sort(edgeT.edge, 2);
+        edgeT(all(curMask, 2), :) = [];
+        if isempty(edgeT); break; end
+        
+        %% Update interfaces
+       [~, curUni, curOldToUni] = unique(edgeT.edge, 'rows');
+        curUniCount = accumarray(curOldToUni, 1);
 
-%%
-edgeT = table;
-edgeT.edge = edges;
-edgeT.vxIds = borders;
-edgeT.score = cellfun(@(ids) median(class(ids)), edgeT.vxIds);
+        % NOTE(amotta): Which edges consist of multiple borders? The scores
+        % of these edges need to be updated by merging the borders and then
+        % recalculating the score.
+        curMultUni = curUni(curUniCount > 1);
+        if isempty(curMultUni); continue; end
+       [~, curOldToUni] = ismember(curOldToUni, find(curUniCount > 1));
 
-maxSegId = max(edgeT.edge(:));
-mergedEdges = nan(maxSegId - 1, 3);
-
-binEdges = linspace(-1.1, +1.1, 101);
-fig = figure();
-ax = axes(fig);
-hold(ax, 'on');
-
-for curId = 1:(maxSegId - 1)
-   [~, curUni, curOldToUni] = unique(edgeT.edge, 'rows');
-    curUniCount = accumarray(curOldToUni, 1);
-    
-    % NOTE(amotta): Which edges consist of multiple borders? The scores of
-    % these edges need to be updated by merging the borders and then
-    % recalculating the score.
-    curMultUni = curUni(curUniCount > 1);
-   [~, curOldToUni] = ismember(curOldToUni, find(curUniCount > 1));
-    
-    % Merge borders and update scores for edges with multiplicity.
-    edgeT.vxIds(curMultUni) = accumarray( ...
-        nonzeros(curOldToUni), find(curOldToUni), [], ...
-        @(ids) {unique(cell2mat(edgeT.vxIds(ids)))});
-    edgeT.score(curMultUni) = cellfun( ...
-        @(ids) median(class(ids)), ...
-        edgeT.vxIds(curMultUni));
-    edgeT = edgeT(curUni, :);
-    
-    if mod(curId, 500) == 1
-        histogram( ...
-            ax, edgeT.score, ...
-            'BinEdges', binEdges, ...
-            'DisplayStyle', 'stairs', ...
-            'LineWidth', 2);
-        % keyboard
+        % Merge borders and update scores for edges with multiplicity.
+        edgeT.borderIds(curMultUni) = accumarray( ...
+            nonzeros(curOldToUni), find(curOldToUni), [], ...
+            @(ids) {cell2mat(edgeT.borderIds(ids))});
+        edgeT.score(curMultUni) = cellfun(@(ids) median( ...
+            class(unique(cell2mat(borders(ids))))), ...
+            edgeT.borderIds(curMultUni));
+        edgeT = edgeT(curUni, :);
     end
     
-   [curMaxScore, curEdgeId] = max(edgeT.score)
-    curEdge = edgeT.edge(curEdgeId, :);
+    % Sanity check
+    assert(isequal(isnan(mergeScores), ...
+        cellfun(@isempty, mergedBorders)));
     
-    mergedEdges(curId, 1:2) = curEdge;
-    mergedEdges(curId, 3) = curMaxScore;
-    
-    % Assume to merge
-    curMask = ismember(edgeT.edge, curEdge);
-    
-    edgeT.edge(curMask) = curEdge(1);
-    edgeT.edge = sort(edgeT.edge, 2);
-    edgeT(all(curMask, 2), :) = [];
-    
-    maxSegId = maxSegId + 1;
-end
-
-%% Show result
-curThresh = -0.58;
-curThreshId = find(mergedEdges(:, 3) < curThresh, 1);
-
-curLUT = 1:maxSegId;
-
-for curId = 1:(curThreshId - 1)
-    curLUT(curLUT == mergedEdges(curId, 2)) = mergedEdges(curId, 1);
-end
-
-curLUT = uint32([0, curLUT]);
-curSegNew = curLUT(1 + seg);
-
-%%
-colors = uint8(255 * hsv(double(maxSegId)));
-colors = colors(randperm(size(colors, 1)), :);
-colors = [0, 0, 0; colors];
-
-curSeg = colors(1 + seg(:), :);
-curSeg = reshape(curSeg, [size(seg), 3]);
-curSeg = permute(curSeg, [1, 2, 4, 3]);
-
-curSegNew = colors(1 + curSegNew(:), :);
-curSegNew = reshape(curSegNew, [size(seg), 3]);
-curSegNew = permute(curSegNew, [1, 2, 4, 3]);
-
-%% Build skeleton
-skel = skeleton();
-skel = skel.setParams( ...
-    '2018-10-01_ex144_st08x2', [11.24, 11.24, 28], [0, 0, 0]);
-
-curIds = ceil(linspace(1, size(mergedEdges, 1) - 1, 100));
-curDigits = ceil(log10(1 + numel(curIds)));
-
-for curIdx = 1:numel(curIds)
-    curId = curIds(curIdx);
-    curEdge = mergedEdges(curId, 1:2);
-    curScore = mergedEdges(curId, 3);
-    
-    curName = sprintf( ...
-        '%0*d. %d-%d. Score: %.2f', ...
-        curDigits, curIdx, curEdge, curScore);
-    skel = skel.addTree(curName, box(:, 1)' + segPoints(curEdge, :) - 1);
+    mergedBorders(isnan(mergeScores)) = [];
+    mergeScores(isnan(mergeScores)) = [];
 end
---
+++ +connectEM/+Hierarchical/runBox.m
@@ -0,0 +1,108 @@
+function [mergedEdges, mergeScores] = runBox(param, box, varargin)
+    % Written by
+    %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    opt = struct;
+    opt.margin = [0; 0; 0];
+    opt = Util.modifyStruct(opt, varargin{:});
+    
+    boxLarge = box + [-1, +1] .* opt.margin(:);
+    boxLarge = max(boxLarge, param.bbox(:, 1));
+    boxLarge = min(boxLarge, param.bbox(:, 2));
+    
+    seg = loadSegDataGlobal(param.seg, boxLarge);
+    coreSegIds = findCoreSegIds(box, boxLarge, seg);
+    
+    maxSegId = Seg.Global.getMaxSegId(param);
+    corrEdges = loadCorrespondences(param, boxLarge);
+    corrLUT = buildCorrespondenceLUT(maxSegId, corrEdges);
+    seg(seg ~= 0) = corrLUT(seg(seg ~= 0));
+    
+   [edges, borders] = SynEM.Svg.findEdgesAndBorders(seg);
+    borders = reshape({borders.PixelIdxList}, [], 1);
+    class = loadClassData(param.class, boxLarge);
+    
+   [mergedBorders, mergeScores] = ...
+        connectEM.Hierarchical.run(class, edges, borders);
+    
+    mask = any(ismember(edges, coreSegIds), 2);
+    mask = cellfun(@(ids) any(mask(ids)), mergedBorders);
+    
+    mergedEdges = cellfun( ...
+        @(ids) unique(edges(ids, :), 'rows'), ...
+        mergedBorders(mask), 'UniformOutput', false);
+    mergeScores = mergeScores(mask);
+end
+
+function coreSegIds = findCoreSegIds(box, boxLarge, seg)
+    if ~isequal(box, boxLarge)
+        off = box - boxLarge;
+        seg = seg( ...
+            (1 + off(1, 1)):(end + off(1, 2)), ...
+            (1 + off(2, 1)):(end + off(2, 2)), ...
+            (1 + off(3, 1)):(end + off(3, 2)));
+    end
+    
+    coreSegIds = setdiff(seg, 0);
+    coreSegIds = reshape(coreSegIds, [], 1);
+end
+
+function lut = buildCorrespondenceLUT(maxSegId, corr)
+    lut = zeros(maxSegId, 1, 'like', corr);
+    lut(1:end) = 1:numel(lut);
+    
+    % NOTE(amotta): This assumes that `corr` are sorted both column- and
+    % row-wise. This should be true for the result of `loadCorrespondences`.
+    for i = 1:size(corr, 1); lut(corr(i, 2)) = lut(corr(i, 1)); end
+end
+
+function corrEdges = loadCorrespondences(param, box)
+    corrDir = param.correspondence.saveFolder;
+    
+    tileIds = box - param.bbox(:, 1) + 1;
+    tileIds = ceil(tileIds ./ param.tileSize);
+    
+    tileCount = tileIds(:, 2) - tileIds(:, 1) + 1;
+    corrEdges = repmat({zeros(0, 2, 'uint32')}, [tileCount', 3]);
+    
+    for curDim = 1:3
+        for curX = 1:(tileCount(1) - (curDim == 1))
+            for curY = 1:(tileCount(2) - (curDim == 2))
+                for curZ = 1:(tileCount(3) - (curDim == 3))
+                   [curIdsOne, curIdsTwo] = deal([ ...
+                        tileIds(1) + curX - 1, ...
+                        tileIds(2) + curY - 1, ...
+                        tileIds(3) + curZ - 1]);
+                    curIdsTwo(curDim) = curIdsTwo(curDim) + 1;
+                    
+                    curCorr = sprintf( ...
+                        '%s%sglobal.mat', ...
+                        num2str(curIdsOne, '%02d'), ...
+                        num2str(curIdsTwo, '%02d'));
+                    curCorr = fullfile(corrDir, curCorr);
+                    curCorr = load(curCorr);
+                    
+                    % NOTE(amotta): Only use correspondences where the
+                    % contact area is at least 10 vx and the contact makes
+                    % up at least 90 % of the smaller segment-border areas.
+                    %
+                    % See +connectEM/collectGlobalCorrespondences.m in
+                    % commit 20ed1a080f50397f32c19f77d57eb9c8cba92c57.
+                   [~, curMask] = ismember( ...
+                       curCorr.uniqueCorrespondences, ...
+                       curCorr.uniqueSegments);
+                    curMask = min(curCorr.countsS(curMask), [], 2);
+                    
+                    curMask = ...
+                        curCorr.countsC > 10 ...
+                      & curCorr.countsC ./ curMask > 0.9;
+                  
+                    curCorr = curCorr.uniqueCorrespondences(curMask, :);
+                    corrEdges{curX, curY, curZ, curDim} = curCorr;
+                end
+            end
+        end
+    end
+    
+    corrEdges = cell2mat(corrEdges(:));
+    corrEdges = unique(sort(corrEdges, 2), 'rows');
+end
