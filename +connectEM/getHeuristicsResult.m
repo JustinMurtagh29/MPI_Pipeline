cb2be57e2c2d0da70aca2585f4615f9b1c6b1d86
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-10-11 11:39:06 +0200

    Prepare to calculate heuristics on L4 dataset

--- +connectEM/getHeuristicsResult.m
+++ +connectEM/getHeuristicsResult.m
@@ -1,47 +1,43 @@
-function getHeuritisticResult(p)
+function getHeuristicsResult(p)
     % Collect results of vessel, nuclei and mylein detection
 
-        % Get vessel scores
-        binaryMap(1).root = '/gaba/u/mberning/wkCubes/2012-09-28_ex145_07x2_ROI2016_vessel/segmentation/1/';
-        binaryMap(1).prefix = '2012-09-28_ex145_07x2_ROI2016_vessel_mag1'; 
-        binaryMap(1).segId = 1;
-        % Get myelin scores
-        binaryMap(2).root = '/gaba/u/mberning/wkCubes/2012-09-28_ex145_07x2_ROI2016_vessel/segmentation/1/';
-        binaryMap(2).prefix = '2012-09-28_ex145_07x2_ROI2016_vessel_mag1'; 
-        binaryMap(2).segId = 3;
-        % Get nuclei scores
-        binaryMap(3).root = '/gaba/u/mberning/wkCubes/2012-09-28_ex145_07x2_ROI2016_nuclei/segmentation/1/';
-        binaryMap(3).prefix = '2012-09-28_ex145_07x2_ROI2016_nuclei_mag1'; 
-        binaryMap(3).segId = 2;
+    % Get vessel scores
+    binaryMap(1).root = '/gaba/u/mberning/wkCubes/2012-09-28_ex145_07x2_ROI2016_vessel/segmentation/1/';
+    binaryMap(1).prefix = '2012-09-28_ex145_07x2_ROI2016_vessel_mag1';
+    binaryMap(1).segId = 1;
+    % Get myelin scores
+    binaryMap(2).root = '/gaba/u/mberning/wkCubes/2012-09-28_ex145_07x2_ROI2016_vessel/segmentation/1/';
+    binaryMap(2).prefix = '2012-09-28_ex145_07x2_ROI2016_vessel_mag1';
+    binaryMap(2).segId = 3;
+    % Get nuclei scores
+    binaryMap(3).root = '/gaba/u/mberning/wkCubes/2012-09-28_ex145_07x2_ROI2016_nuclei/segmentation/1/';
+    binaryMap(3).prefix = '2012-09-28_ex145_07x2_ROI2016_nuclei_mag1';
+    binaryMap(3).segId = 2;
+    
+    info = Util.runInfo();
+    info.param = rmfield(info.param, 'p');
 
-
-    for cube=1:numel(p.local)
-        bbox = p.local(cube).bboxSmall;
-        inputCell{cube} = {bbox};
-    end
+    inputCell = arrayfun( ...
+        @(l) {l.bboxSmall}, p.local, 'UniformOutput', false);
     functionH = @connectEM.getSegmentHeuristicsScore;
 
     % Execute and retrieve results
-    seg = p.seg;
-    cluster = Cluster.getCluster('-l h_vmem=12G');
     job = Cluster.startJob(functionH, inputCell, ...
-        'name', 'heuristicLookup', 'sharedInputs', {seg binaryMap}, ...
-        'sharedInputsLocation', [1 2], 'cluster', cluster, ...
-        'numOutputs', 2);
+        'name', 'heuristicLookup', 'sharedInputs', {p.seg, binaryMap}, ...
+        'cluster', {'memory', 12}, 'numOutputs', 2);
     Cluster.waitForJob(job);
     scores = fetchOutputs(job);
 
     % Reformatting for save file
-    segIds = cat(1, scores{:,2});
-    sc = cat(1, scores{:,1}); 
-    vesselScore = cat(1, sc{:,1});
-    myelinScore = cat(1, sc{:,2});
-    nucleiScore = cat(1, sc{:,3});
-
-    Util.save([p.saveFolder 'heuristicResult.mat'], segIds, vesselScore, myelinScore, nucleiScore);
-end
-
-function scoresSub = extractScores(scores, idx)
-    temp = cellfun(@(x)x{idx}, scores(:,1), 'uni', 0);
-    scoresSub = cell2mat(temp);
+    sc = cat(1, scores{:, 1}); 
+    segIds = cat(1, scores{:, 2});
+    vesselScore = cat(1, sc{:, 1});
+    myelinScore = cat(1, sc{:, 2});
+    nucleiScore = cat(1, sc{:, 3});
+    
+    saveFile = fullfile(p.saveFolder, 'heuristicResult.mat');
+    
+    Util.save( ...
+        saveFile, info, segIds, ...
+        vesselScore, myelinScore, nucleiScore);
 end
--- +connectEM/getSegmentHeuristicsScore.m
+++ +connectEM/getSegmentHeuristicsScore.m
@@ -1,4 +1,5 @@
-function [segmentOverlap, uniqueSegId,voxelCount] = getSegmentHeuristicsScore(seg, labelMap, bbox)
+function [segmentOverlap, uniSegIds, voxelCount] = ...
+        getSegmentHeuristicsScore(seg, labelMap, bbox)
     % Takes segmentation struct (root, prefix, bbox) and looks up the fraction 
     % of each segment overlapping with labelMap (specified by root, prefix/backend and segId to lookup)
     % if segId has two values, the function uses all values between these
@@ -10,31 +11,37 @@ function [segmentOverlap, uniqueSegId,voxelCount] = getSegmentHeuristicsScore(se
     % each connected component in the labelMap which is associated with the
     % segment that overlaps the most with the connected component.
     
-
     seg = loadSegDataGlobal(seg, bbox);
-    uniqueSegId = unique(seg(seg ~= 0));
-    segmentOverlap = cell(length(labelMap),1);
-    if nargout > 2
-        voxelCount = cell(length(labelMap),1);
-    end
-    for i=1:length(labelMap)
+    uniSegIds = reshape(setdiff(seg, 0), [], 1);
+    
+   [~, segSizes] = ismember(seg(:), uniSegIds);
+    segSizes = accumarray(nonzeros(segSizes), 1, size(uniSegIds));
+    
+    segmentOverlap = cell(numel(labelMap),1);
+    if nargout > 2; voxelCount = cell(numel(labelMap), 1); end
+    
+    for i = 1:numel(labelMap)
         class = loadSegDataGlobal(labelMap(i), bbox);
+        
         if numel(labelMap(i).segId) == 2
-            class = class >= labelMap(i).segId(1) & class < labelMap(i).segId(2); % make logical map with all values between segId(1) and segId(2)
+            % make logical map with all values between segId(1) and segId(2)
+            class = ...
+                class >= labelMap(i).segId(1) ...
+              & class <  labelMap(i).segId(2);
         else
-            class = class == labelMap(i).segId; % make logical map with all values equal to segId
+            % make logical map with all values equal to segId
+            class = class == labelMap(i).segId; 
         end
-        if isfield(labelMap(i),'function')
+        
+        if isfield(labelMap(i), 'function')
             class = labelMap(i).function(class);
         end
-        segOccurences = histc(seg(seg~=0), uniqueSegId); 
-        classVoxels = histc(seg(seg~=0 & class), uniqueSegId);
-        if size(classVoxels,2) > 1
-            % If only one voxel is found, histc switches dimensionality of output for some reason
-            segmentOverlap{i} = classVoxels' ./ segOccurences;
-        else
-            segmentOverlap{i} = classVoxels ./ segOccurences;
-        end
+        
+       [~, segFracs] = ismember(seg(class(:)), uniSegIds);
+        segFracs = accumarray(nonzeros(segFracs), 1, size(uniSegIds));
+        segFracs = segFracs ./ segSizes;
+        segmentOverlap{i} = segFracs;
+        
         if nargout > 2
             % calculate the voxelCount of each labeled connected component (CC) and associate it
             % with the segment it overlaps the most
@@ -58,11 +65,9 @@ function [segmentOverlap, uniqueSegId,voxelCount] = getSegmentHeuristicsScore(se
             mostOverlapSegs = unique(mostOverlapSegs);
             
             
-            voxelCount{i} = zeros(numel(uniqueSegId),1);
-            [~,ind] = ismember(mostOverlapSegs,uniqueSegId);
+            voxelCount{i} = zeros(numel(uniSegIds),1);
+            [~,ind] = ismember(mostOverlapSegs,uniSegIds);
             voxelCount{i}(ind) = voxelCounts(mostOverlapSegs);  % associate these segIds with the voxelCount
         end
     end
-
 end
-
