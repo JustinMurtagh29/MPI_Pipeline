408d4c86f3a3ea48c64a395ca37c1934fdb675a2
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-11-27 15:46:49 +0100

    Export excitatory axons with smooth dendrite specificity

--- +connectEM/+Axon/+Script/debugExcitatorySpecificities.m
+++ +connectEM/+Axon/+Script/debugExcitatorySpecificities.m
@@ -7,10 +7,6 @@ rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
 connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-19-a-partiallySplit-v2_dendrites-wholeCells-03-v2-classified_SynapseAgglos-v8-classified.mat');
 outputDir = '/home/amotta/Desktop/exc-specs';
 
-targetClasses = { ...
-    'Somata', 'ProximalDendrite', 'ApicalDendrite', ...
-    'SmoothDendrite', 'AxonInitialSegment', 'OtherDendrite'};
-
 minSynPre = 10;
 info = Util.runInfo();
 
@@ -33,7 +29,7 @@ synT.ontoTarget = conn.denMeta.targetClass(synT.postAggloId);
 
 specClasses = ...
     connectEM.Connectome.buildAxonSpecificityClasses( ...
-        conn, axonClasses(1:(end - 1)));
+        conn, axonClasses(1:2));
 
 %% Export samples
 clear cur*;
@@ -41,7 +37,7 @@ clear cur*;
 rng(0);
 mkdir(outputDir);
 
-curAxonIds = specClasses(3).specs.ApicalDendrite.axonIds;
+curAxonIds = specClasses(3).specs.SmoothDendrite.axonIds;
 curAxonIds = curAxonIds(randperm(numel(curAxonIds)));
 curAxonIds = curAxonIds(1:10);
 
