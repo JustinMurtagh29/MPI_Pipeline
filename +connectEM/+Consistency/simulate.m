dbe267b41d3e87a1396c34d6bb4355c99e1e57ae
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-12-06 15:40:28 +0100

    Backup

--- +connectEM/+Consistency/simulate.m
+++ +connectEM/+Consistency/simulate.m
@@ -4,26 +4,28 @@ clear;
 
 %% Configuration
 numSyn = 2;
-numSteps = 400;
-numRuns = 1000;
+numSteps = 2;
+numRuns = 10000;
 
 tau = 10; % ms
 
 wMax = 1;
-nuPos = 0.2;
+nuPos = 0.25;
 aPos = @(w) (wMax - w) * nuPos;
 
-nuNeg = 0.2;
+nuNeg = 0.25;
 aNeg = @(w) w * nuNeg;
 
 wMat = nan(numSyn, numSteps, numRuns);
 
-errProb = 0.25;
+errProb = 0;
+stdpProb = 0.25;
 
 %% Simulate
 rng(0);
 for curRun = 1:numRuns
-    wMat(:, 1, curRun) = min(1, lognrnd(-1.65, 1, [2, 1]));
+    curHasStdp = rand(1) < stdpProb;
+    wMat(:, 1, curRun) = min(1, lognrnd(-2, 1, [2, 1]));
 
     for curStep = 2:numSteps
         curOldW = wMat(:, curStep - 1, curRun);
@@ -33,20 +35,26 @@ for curRun = 1:numRuns
         % curDeltaT = curDeltaT + 2 * sum(curOldW);
 
         if curDeltaT > 0
-            curDeltaW = +nuPos * aPos(curOldW);
+            curDeltaW = +aPos(curOldW);
         else
-            curDeltaW = -nuNeg * aNeg(curOldW);
+            curDeltaW = -aNeg(curOldW);
         end
 
         % Make synapses unreliable
         curErr = rand(numSyn, 1) < errProb;
         curDeltaW = (1 - curErr) .* curDeltaW;
+        
+        curDeltaW = curDeltaW * curHasStdp;
 
         wMat(:, curStep, curRun) = curOldW + curDeltaW;
     end
 end
 
+ltpMask = all(wMat(:, end, :) > wMat(:, 1, :), 1);
+ltdMask = all(wMat(:, end, :) < wMat(:, 1, :), 1);
+
 %%
+%{
 figure;
 
 subplot(2, 1, 1);
@@ -58,8 +66,10 @@ ylim([0, 1]);
 subplot(2, 1, 2);
 plot(std(wMat(:, :, 1), 0, 1) ./ mean(wMat(:, :, 1), 1));
 ylim([0, sqrt(2)]);
+%}
 
 %%
+%{
 cvs = std(wMat, 0, 1) ./ mean(wMat, 1);
 cvMeans = reshape(mean(cvs, 3), [], 1);
 cvStd = reshape(std(cvs, 0, 3), [], 1);
@@ -70,6 +80,7 @@ plot(cvMeans, 'black');
 plot(cvMeans + cvStd, 'k--');
 plot(cvMeans - cvStd, 'k--');
 ylim([0, sqrt(2)]);
+%}
 
 %%
 cvs = std(wMat, 0, 1) ./ mean(wMat, 1);
@@ -86,6 +97,14 @@ histogram(cvs(:, end, :), ...
     'BinEdges', curEdges, ...
     'Normalization', 'probability', ...
     'DisplayStyle', 'stairs', 'LineWidth', 2);
+histogram(cvs(:, end, ltpMask), ...
+    'BinEdges', curEdges, ...
+    'Normalization', 'probability', ...
+    'DisplayStyle', 'stairs', 'LineWidth', 2);
+histogram(cvs(:, end, ltdMask), ...
+    'BinEdges', curEdges, ...
+    'Normalization', 'probability', ...
+    'DisplayStyle', 'stairs', 'LineWidth', 2);
 
 %%
 asis = 10 .^ mean(log10(wMat), 1);
@@ -96,12 +115,38 @@ hold on;
 
 histogram(asis(:, 1, :), ...
     'BinEdges', curEdges, ...
-    'Normalization', 'probability', ...
     'DisplayStyle', 'stairs', 'LineWidth', 2);
 histogram(asis(:, end, :), ...
     'BinEdges', curEdges, ...
-    'Normalization', 'probability', ...
     'DisplayStyle', 'stairs', 'LineWidth', 2);
+%{
+histogram(asis(:, 1, ltpMask), ...
+    'BinEdges', curEdges, ...
+    'DisplayStyle', 'stairs', 'LineWidth', 2);
+histogram(asis(:, end, ltdMask), ...
+    'BinEdges', curEdges, ...
+    'DisplayStyle', 'stairs', 'LineWidth', 2);
+
+curKnots = unique(-asis(:, [1, end], :));
+curA = histcounts(-asis(:, 1, :), curKnots, 'Normalization', 'cdf');
+curB = histcounts(-asis(:, end, :), curKnots, 'Normalization', 'cdf');
+
+[~, curThresh] = max(curB - curA);
+curThresh = -curKnots(curThresh);
+
+curLtpFrac = ...
+    mean(asis(:, end, :) > curThresh) ...
+  - mean(asis(:, 1, :) > curThresh);
+curLtpFrac = 0.125;
+
+curBefore = histcounts(asis(:, 1, :), curEdges);
+curAfter = histcounts(asis(:, end, :), curEdges);
+curLtp = histcounts(asis(:, 1, ltpMask), curEdges);
+% curLtd = curAfter - (curBefore - curLtpFrac * curBefore);
+curLtd = curAfter - (curBefore - curLtp);
+
+histogram('BinEdges', curEdges, 'BinCounts', max(curLtd, 0))
+%}
 
 %% Simulate
 %{
