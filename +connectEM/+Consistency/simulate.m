ce831761f9b022cb3e8d290edad3f9db5648b7d9
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-12-06 18:29:48 +0100

    Backup

--- +connectEM/+Consistency/simulate.m
+++ +connectEM/+Consistency/simulate.m
@@ -5,15 +5,15 @@ clear;
 %% Configuration
 numSyn = 2;
 numSteps = 2;
-numRuns = 10000;
+numRuns = 1000;
 
 tau = 10; % ms
 
 wMax = 1;
-nuPos = 0.25;
+nuPos = 0.5;
 aPos = @(w) (wMax - w) * nuPos;
 
-nuNeg = 0.25;
+nuNeg = 0.5;
 aNeg = @(w) w * nuNeg;
 
 wMat = nan(numSyn, numSteps, numRuns);
@@ -21,11 +21,14 @@ wMat = nan(numSyn, numSteps, numRuns);
 errProb = 0;
 stdpProb = 0.25;
 
+mu = -2;
+sigma = 1;
+
 %% Simulate
 rng(0);
 for curRun = 1:numRuns
     curHasStdp = rand(1) < stdpProb;
-    wMat(:, 1, curRun) = min(1, lognrnd(-2, 1, [2, 1]));
+    wMat(:, 1, curRun) = min(1, lognrnd(mu, sigma, [2, 1]));
 
     for curStep = 2:numSteps
         curOldW = wMat(:, curStep - 1, curRun);
@@ -35,9 +38,9 @@ for curRun = 1:numRuns
         % curDeltaT = curDeltaT + 2 * sum(curOldW);
 
         if curDeltaT > 0
-            curDeltaW = +aPos(curOldW);
+            curDeltaW = +aPos(curOldW) * exp(-curDeltaT / tau);
         else
-            curDeltaW = -aNeg(curOldW);
+            curDeltaW = -aNeg(curOldW) * exp(+curDeltaT / tau);
         end
 
         % Make synapses unreliable
@@ -119,7 +122,16 @@ histogram(asis(:, 1, :), ...
 histogram(asis(:, end, :), ...
     'BinEdges', curEdges, ...
     'DisplayStyle', 'stairs', 'LineWidth', 2);
+
 %{
+curBefore = histcounts(asis(:, 1, :), curEdges);
+curLtp = histcounts(asis(:, 1, ltpMask), curEdges);
+curLtpEst = 0.125 * curBefore;
+
+histogram( ...
+    'BinEdges', curEdges, 'BinCounts', max(curBefore - curLtp, 0), ...
+    'DisplayStyle', 'stairs', 'LineWidth', 2)
+
 histogram(asis(:, 1, ltpMask), ...
     'BinEdges', curEdges, ...
     'DisplayStyle', 'stairs', 'LineWidth', 2);
@@ -147,10 +159,185 @@ curLtd = curAfter - (curBefore - curLtp);
 
 histogram('BinEdges', curEdges, 'BinCounts', max(curLtd, 0))
 %}
+    
+%%
+%{
+curEdges = linspace(-15, 0, 31);
+curPBefore = sum(log(logncdf(wMat(:, 1, :), mu, sigma)), 1);
+curPAfter = sum(log(logncdf(wMat(:, end, :), mu, sigma)), 1);
+
+figure; hold on;
+histogram(curPBefore, 'BinEdges', curEdges, 'DisplayStyle', 'stairs', 'LineWidth', 2);
+histogram(curPAfter, 'BinEdges', curEdges, 'DisplayStyle', 'stairs', 'LineWidth', 2);
+
+curAx = gca;
+curAx.YScale = 'log';
+%}
+
+%%
+curSaSdT = table;
+curSaSdT.areas = transpose(sort(squeeze(wMat(:, end, :)), 1));
+curSaSdT.cv = std(curSaSdT.areas, 0, 2) ./ mean(curSaSdT.areas, 2);
+curSaSdT.avgLogAreas = mean(log10(curSaSdT.areas), 2);
+
+curCtrlT = table;
+curCtrlT.areas = transpose(sort(squeeze(wMat(:, 1, :)), 1));
+curCtrlT.cv = std(curCtrlT.areas, 0, 2) ./ mean(curCtrlT.areas, 2);
+curCtrlT.avgLogAreas = mean(log10(curCtrlT.areas), 2);
+
+% Density difference map
+curLimX = [-eps, 1.5];
+curLimY = [-1.5, 0.5];
+
+curTicksX = linspace(curLimX(1), curLimX(2), 4);
+curTicksY = linspace(curLimY(1), curLimY(2), 5);
+
+curImSize = [301, 301];
+
+[curImGridY, curImGridX] = ndgrid( ...
+    linspace(curLimY(1), curLimY(2), curImSize(1)), ...
+    linspace(curLimX(1), curLimX(2), curImSize(2)));
+curImGrid = cat(2, curImGridY(:), curImGridX(:));
+
+curSaSdImg = horzcat( ...
+    curSaSdT.avgLogAreas, curSaSdT.cv);
+curMask = ...
+    curLimY(1) <= curSaSdImg(:, 1) ...
+    & curSaSdImg(:, 1) <= curLimY(2) ...
+    & curLimX(1) <= curSaSdImg(:, 2) ...
+    & curSaSdImg(:, 2) <= curLimX(2);
+
+curSaSdImg = ksdensity( ...
+    curSaSdImg(curMask, :), curImGrid, ...
+    'Support', transpose(cat(1, curLimY, curLimX)), ...
+    'BoundaryCorrection', 'reflection');
+curSaSdImg = mean(curMask) * curSaSdImg / sum(curSaSdImg(:));
+curSaSdImg = reshape(curSaSdImg, curImSize);
+
+curCtrlImg = horzcat( ...
+    curCtrlT.avgLogAreas, curCtrlT.cv);
+curMask = ...
+    curLimY(1) <= curCtrlImg(:, 1) ...
+    & curCtrlImg(:, 1) <= curLimY(2) ...
+    & curLimX(1) <= curCtrlImg(:, 2) ...
+    & curCtrlImg(:, 2) <= curLimX(2);
+
+curCtrlImg = ksdensity( ...
+    curCtrlImg(curMask, :), curImGrid, ...
+    'Support', transpose(cat(1, curLimY, curLimX)), ...
+    'BoundaryCorrection', 'reflection');
+curCtrlImg = mean(curMask) * curCtrlImg / sum(curCtrlImg(:));
+curCtrlImg = reshape(curCtrlImg, curImSize);
+
+curMax = max(max(curSaSdImg(:)), max(curCtrlImg(:)));
+curDiffImg = curSaSdImg - curCtrlImg;
+curMaxDiff = max(abs(curDiffImg(:)));
+
+curFig = figure();
+curFig.Color = 'white';
+curFig.Position(3:4) = [360, 1020];
+
+curAx = subplot(3, 1, 1);
+image(curAx, ...
+    uint8(double(intmax('uint8')) ...
+    * curSaSdImg / curMax));
+colormap(curAx, jet(256));
+
+curBar = colorbar('peer', curAx);
+curBar.Ticks = curBar.Limits;
+curBar.TickLabels = {'0', sprintf('%.3g', curMax)};
+
+curAx = subplot(3, 1, 2);
+image(curAx, ...
+    uint8(double(intmax('uint8')) ...
+    * curCtrlImg / curMax));
+colormap(curAx, jet(256));
+
+curBar = colorbar('peer', curAx);
+curBar.Ticks = curBar.Limits;
+curBar.TickLabels = {'0', sprintf('%.3g', curMax)};
+
+curAx = subplot(3, 1, 3);
+image(curAx, ...
+    uint8(double(intmax('uint8')) ...
+    * (1 + curDiffImg / curMaxDiff) / 2));
+colormap(curAx, jet(256));
+curBar = colorbar('peer', curAx);
+curBar.Label.String = { ...
+    'Fraction of observe pairs'; ...
+    'relative to null model'};
+curBar.Ticks = [ ...
+    curBar.Limits(1), ...
+    mean(curBar.Limits), ...
+    curBar.Limits(end)];
+curBar.TickLabels = { ...
+    sprintf('%.3g', -curMaxDiff), '0', ...
+    sprintf('%.3g', +curMaxDiff)};
+
+set( ...
+    findobj(curFig.Children, 'Type', 'ColorBar'), ...
+    'Location', 'EastOutside', ...
+    'TickDirection', 'out', ...
+    'Box', 'off');
+
+[~, curTickIdsX] = ismember(curTicksX, ...
+    linspace(curLimX(1), curLimX(2), curImSize(2)));
+curTickLabelsX = arrayfun( ...
+    @num2str, curTicksX, 'UniformOutput', false);
+[~, curTickIdsY] = ismember(curTicksY, ...
+    linspace(curLimY(1), curLimY(2), curImSize(1)));
+curTickLabelsY = arrayfun( ...
+    @num2str, curTicksY, 'UniformOutput', false);
+
+curAxes = reshape(flip(findobj(curFig, 'Type', 'Axes')), 1, []);
+arrayfun(@(ax) hold(ax, 'on'), curAxes);
+
+set(curAxes, ...
+    'Box', 'off', ...
+    'TickDir', 'out', ...
+    'YDir', 'normal', ...
+    'YTick', curTickIdsY, ...
+    'YTickLabels', curTickLabelsY, ...
+    'XTick', [], ...
+    'PlotBoxAspectRatio', [1, 1, 1], ...
+    'DataAspectRatioMode', 'auto');
+set(curAxes(end), ...
+    'XTick', curTickIdsX, ...
+    'XTickLabels', curTickLabelsX);
+
+arrayfun(@(ax) ylabel(ax, ...
+    'Average log_{10}(ASI area [µm²])'), curAxes);
+xlabel(curAxes(end), 'Coefficient of variation');
+
+%{
+annotation( ...
+    curFig, ...
+    'textbox', [0, 0.9, 1, 0.1], ...
+    'String', { ...
+    info.filename; info.git_repos{1}.hash; ...
+    curConfig.title; curCtrlConfig.title}, ...
+    'EdgeColor', 'none', 'HorizontalAlignment', 'center');
+%}
+
+
+%%
+
+curEdges = linspace(0, 1, 21);
+eW = wMat(:, end, :);
+wCv = std(eW, 0, 1) ./ mean(eW, 1);
+eMask = wCv < 0.4;
+
+figure;
+hold on;
+histogram(eW, 'BinEdges', curEdges, 'Normalization', 'probability');
+histogram(eW(:, :, eMask), 'BinEdges', curEdges, 'Normalization', 'probability');
 
 %% Simulate
 %{
 curInitWeights = wMat(:, end, :);
+
+curInitCv = std(curInitWeights, 0, 1) ./ mean(curInitWeights, 1);
+curInitWeights = curInitWeights(:, :, curInitCv < 0.4);
 
 rng(0);
 for curRun = 1:numRuns
