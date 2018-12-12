d2965f64ce288bb3b0a9891fc70a0f84fc18ade0
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-12-12 15:27:00 +0100

    Add a more pedestrian method for overabundance estimate
    But the old method remains used by default

--- +connectEM/+Consistency/calculateLearnedFraction.m
+++ +connectEM/+Consistency/calculateLearnedFraction.m
@@ -1,12 +1,45 @@
-function [learnedFrac, unlearnedFrac, cvTresh] = ...
+function [learnedFrac, unlearnedFrac, cvThresh] = ...
         calculateLearnedFraction(synT, pairConfig, ctrlPairConfig, varargin)
     % Written by
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
     opt = struct;
     opt.bandWidth = 0.06;
     opt.queryPoints = linspace(eps, sqrt(2), 101);
+    opt.method = 'KernelDensity'; % for backward compatibility
     opt = Util.modifyStruct(opt, varargin{:});
     
+    switch lower(opt.method)
+        case 'kerneldensity', impl = @kernelDensityMethod;
+        case 'maxdifference', impl = @maxDifferenceMethod;
+        otherwise, error('Invalid method "%s"', opt.method);
+    end
+    
+   [learnedFrac, unlearnedFrac, cvThresh] = ...
+        impl(opt, synT, pairConfig, ctrlPairConfig);
+end
+
+function [learnedFrac, unlearnedFrac, cvThresh] = ...
+        maxDifferenceMethod(~, synT, pairConfig, ctrlPairConfig)
+    cv = @(vals) std(vals, 0, 2) ./ mean(vals, 2);
+    pairCvs = sort(cv(synT.area(pairConfig.synIdPairs)), 'ascend');
+    ctrlCvs = sort(cv(synT.area(ctrlPairConfig.synIdPairs)), 'ascend');
+    
+    t = table;
+    t.cv = [pairCvs; ctrlCvs];
+    t.pdf = [ ...
+        +repelem(1 / numel(pairCvs), numel(pairCvs), 1); ...
+        -repelem(1 / numel(ctrlCvs), numel(ctrlCvs), 1)];
+    t = sortrows(t, 'cv', 'ascend');
+    t.cdf = cumsum(t.pdf);
+    
+   [~, cvThreshIdx] = max(t.cdf);
+    cvThresh = t.cv(cvThreshIdx);
+    unlearnedFrac = mean(pairCvs > cvThresh);
+    learnedFrac = t.cdf(cvThreshIdx);
+end
+
+function [learnedFrac, unlearnedFrac, cvThresh] = ...
+        kernelDensityMethod(opt, synT, pairConfig, ctrlPairConfig)
     % Calculate densities
     pairDens = calculateCvDensity(opt, synT, pairConfig);
     ctrlDens = calculateCvDensity(opt, synT, ctrlPairConfig);
@@ -21,7 +54,7 @@ function [learnedFrac, unlearnedFrac, cvTresh] = ...
     
     learnedFrac = sum(pairDens(1:threshId)) - sum(ctrlDens(1:threshId));
     unlearnedFrac = sum(pairDens((threshId + 1):end));
-    cvTresh = opt.queryPoints(threshId);
+    cvThresh = opt.queryPoints(threshId);
 end
 
 function cvDens = calculateCvDensity(opt, synT, pairConfig)
