7a01622c37c1dcc5aa2b7c21577cce00c84e3486
Alessandro Motta <alessandro.motta@brain.mpg.de>
2018-12-19 14:21:13 +0100

    Add rudimentary plotting of results

--- +connectEM/+Consistency/analyseWeightMatrix.m
+++ +connectEM/+Consistency/analyseWeightMatrix.m
@@ -73,3 +73,78 @@ curAreas = accumarray( ...
 Util.log('Clustering weight matrix');
 [curLink, curDist] = connectEM.Consistency.linkage(curAreas);
 Util.log('Done!');
+
+[~, ~, curPerm] = dendrogram(curLink, 0);
+
+%% Plot synapse sizes
+curTemp = curAreas(curPerm, :);
+
+curFig = figure();
+curFig.Color = 'white';
+curAx = axes(curFig);
+curIm = imagesc(curAx, curTemp);
+curIm.AlphaData = 1 - isnan(curTemp);
+curAx.Color = 'black';
+curAx.Box = 'off';
+
+curCbar = colorbar('peer', curAx);
+curCbar.TickDirection = 'out';
+curCbar.Label.String = 'log_{10}(median axon-spine interface area [µm²])';
+
+axis(curAx, 'equal');
+xlim(curAx, [1, numel(curDendIds)]);
+ylim(curAx, [1, numel(curAxonIds)]);
+
+xlabel(curAx, 'Dendrites');
+ylabel(curAx, 'Axons (clustered)');
+
+curAx.TickDir = 'out';
+xticks(curAx, [1, numel(curDendIds)]);
+xticklabels(curAx, arrayfun( ...
+    @num2str, xticks(curAx), 'UniformOutput', false));
+yticks(curAx, [1, numel(curAxonIds)]);
+yticklabels(curAx, arrayfun( ...
+    @num2str, yticks(curAx), 'UniformOutput', false));
+
+title(curAx, ...
+    {info.filename; info.git_repos{1}.hash; 'Weight matrix'}, ...
+    'FontWeight', 'normal', 'FontSize', 10);
+
+%% Plot distance matrix
+curTemp = curDist(curPerm, curPerm);
+
+curFig = figure();
+curFig.Color = 'white';
+curAx = axes(curFig);
+% NOTE(amotta): For some reason MATLAB throws an error if `curAx` is passed
+% as first input argument to `imagesc` (despite this being a valid input
+% according to the documentation).
+curIm = imagesc(curTemp, [0, 1.14]); % HACK(amotta): Hard-coded limits!
+curIm.AlphaData = 1 - isnan(curTemp);
+axis(curAx, 'square');
+curAx.Color = 'black';
+curAx.Box = 'off';
+
+curCbar = colorbar('peer', curAx);
+curCbar.TickDirection = 'out';
+curCbar.Label.String = 'Squared Euclidean distance';
+
+axis(curAx, 'square');
+curAx.TickDir = 'out';
+curAx.XAxisLocation = 'top';
+
+xlim(curAx, [1, numel(curAxonIds)]);
+xlabel(curAx, 'Axons (clustered)');
+xticks(curAx, [1, numel(curAxonIds)]);
+xticklabels(curAx, arrayfun( ...
+    @num2str, xticks(curAx), 'UniformOutput', false));
+
+ylim(curAx, [1, numel(curAxonIds)]);
+ylabel(curAx, 'Axons (clustered)');
+yticks(curAx, [1, numel(curAxonIds)]);
+yticklabels(curAx, arrayfun( ...
+    @num2str, yticks(curAx), 'UniformOutput', false));
+
+title(curAx, ...
+    {info.filename; info.git_repos{1}.hash; 'Distance matrix'}, ...
+    'FontWeight', 'normal', 'FontSize', 10);
