a3a15951b77ec586e7bb1aea7742df171926dedf
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-01-14 15:42:30 +0100

    Take into account relative TC / CC frequency. Improve plot.

--- +connectEM/+Number/boppEtAl2017JNeurosci.m
+++ +connectEM/+Number/boppEtAl2017JNeurosci.m
@@ -1,11 +1,8 @@
 % Written by
 %   Alessandro Motta <alessandro.motta@brain.mpg.de>
 
-%% TODO
-% [ ] Take into account relative frequencies of VGluT2+/-
-
 %% Configuration
-% NOTE(amotta): This values were extracted from panel 5b of
+% NOTE(amotta): These following values were extracted from panel 5b of
 % Bopp, Holler-Rickauer, Martin, Schuhknecht (2017). J Neurosci
 %
 % Rows corresponds to VGluT2- and VGluT2+
@@ -17,21 +14,56 @@ synPerBoutonProb = ...
     synPerBoutonProb ...
  ./ sum(synPerBoutonProb, 2);
 
-numAxons = 100;
+% See global "percentage of VGluT2 synapses" values for S1 in Table 1. This
+% synapse fraction also roughly corresponds to the axon fraction under the
+% following assumptions:
+%
+% * The following simulation is restricted to excitatory axons
+% * Excitatory axons make exclusively asymmetric synapses
+% * All excitatory axons make exactly the same number of synapses
+tcSynFrac = 0.172;
+
+numAxons = 1000;
 numSynsPerAxon = 10;
+
+info = Util.runInfo();
+Util.showRunInfo(info);
 
 %% Simulate
 rng(0);
-ccVec = arrayfun(@(~) mean(simulateAxon( ...
-    synPerBoutonProb(1, :), numSynsPerAxon) > 1), 1:numAxons);
-tcVec = arrayfun(@(~) mean(simulateAxon( ...
-    synPerBoutonProb(2, :), numSynsPerAxon) > 1), 1:numAxons);
+
+tcMask = linspace(0, 1, numAxons) < tcSynFrac;
+fracMultiSynBoutons = arrayfun(@(isTc) mean(simulateAxon( ...
+    synPerBoutonProb(1 + isTc, :), numSynsPerAxon) > 1), tcMask);
 
 %% Plot
-figure; hold on;
-binEdges = linspace(0, 1, 11);
-histogram(ccVec, binEdges, 'DisplayStyle', 'stairs', 'LineWidth', 2);
-histogram(tcVec, binEdges, 'DisplayStyle', 'stairs', 'LineWidth', 2);
+curFig = figure();
+curAx = axes(curFig);
+hold(curAx, 'on');
+
+curColors = get(groot, 'defaultAxesColorOrder');
+curBinEdges = linspace(0, 1, 11);
+
+curHist = @(data, color) histogram(curAx, ...
+    'BinCounts', histcounts(data, curBinEdges) / numAxons, ...
+    'BinEdges', curBinEdges, 'EdgeColor', 'none', ...
+    'FaceColor', color, 'FaceAlpha', 1);
+curHist(fracMultiSynBoutons, curColors(1, :));
+curHist(fracMultiSynBoutons(~tcMask), 'black');
+
+curLeg = legend('Thalamocortical axons', 'Corticocortical axons');
+set(curLeg, 'Location', 'NorthEast', 'Box', 'off');
+
+curFig.Position(3:4) = [390, 265];
+curFig.Color = 'white';
+curAx.TickDir = 'out';
+
+xlim(curAx, curBinEdges([1, end]));
+xlabel(curAx, 'Fraction of multi-synaptic boutons');
+ylabel(curAx, 'Fraction of axons');
+title(curAx,  ...
+    {info.filename; info.git_repos{1}.hash}, ...
+    'FontWeight', 'normal', 'FontSize', 10);
 
 %% Utilities
 function synsPerBouton = simulateAxon(synPerBoutonProb, numSyn)
