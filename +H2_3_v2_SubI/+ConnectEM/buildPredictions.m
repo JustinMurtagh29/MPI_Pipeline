6e947a58986cbe8afdc7e253138c8fddedb2cb60
Sahil Loomba <sahil.loomba@brain.mpg.de>
2019-02-06 15:37:15 +0100

    wip

---
+++ +H2_3_v2_SubI/+ConnectEM/buildPredictions.m
@@ -0,0 +1,31 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+
+%% HACKHACKHACK
+% NOTE(amotta): This is a huge mess. The training data is located in my
+% repository, SynEM is from Benedikt's repository, and the SynEM classifier
+% is loaded from the pipeline repo.
+%   Let's get rid of the dependency on the pipeline repo by building the
+% feature map de-novo. The `paper` version is identical to the one stored
+% in the pipeline repository up to different `border` values in the
+% `AverageFilter`. But this doesn't affect `fm.invertDirection`.
+addpath('/gaba/u/amotta/code/benedikt', '-end');
+
+%% Configuration
+config = 'ex144_08x2_mrNet';
+classifierFile = '/gaba/u/amotta/l23/2018-10-19-connect-em/classifier.mat';
+
+areaT = 10;
+fmap = SynEM.getFeatureMap('paper');
+
+info = Util.runInfo();
+Util.showRunInfo(info);
+
+%% Loading data
+config = loadConfig(config);
+param = config.param;
+
+%% Build predictions
+ConnectEM.Pipeline.buildPredictions( ...
+    param, classifierFile, 'areaT', areaT, 'fmap', fmap);
