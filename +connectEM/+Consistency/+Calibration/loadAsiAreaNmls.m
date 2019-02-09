93b85e01c0f788191a5a6606f6fcc53de921084d
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-02-09 12:26:17 +0100

    Write high-level function to load all calibration data

---
+++ +connectEM/+Consistency/+Calibration/loadAsiAreaNmls.m
@@ -0,0 +1,37 @@
+function calibT = loadAsiAreaNmls(param, nmlDir)
+    % calibT = loadAsiAreaNmls(param, nmlDir)
+    %
+    % Written by
+    %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    nmlNames = dir(fullfile(nmlDir, '*.nml'));
+    nmlNames([nmlNames.isdir]) = [];
+    nmlNames = {nmlNames.name};
+    
+    calcArea = @connectEM.Consistency.Calibration.calculateAsiArea;
+    
+    calibT = [];
+    for curId = 1:numel(nmlNames)
+        curNmlName = nmlNames{curId};
+        curNmlFile = fullfile(nmlDir, curNmlName);
+        
+        curParts = '^asi-(\d+)_axon-(\d+)_spine-head-(\d+)\.nml$';
+        curParts = regexpi(curNmlName, curParts, 'tokens', 'once');
+        curParts = cellfun(@str2double, curParts);
+        
+        curNml = slurpNml(curNmlFile);
+        curArea = calcArea(param.raw.voxelSize, curNml);
+        
+        curCalib = struct;
+        curCalib.asiId = curParts(1);
+        curCalib.axonId = curParts(2);
+        curCalib.shId = curParts(3);
+        curCalib.area = curArea;
+        curCalib.nmlFile = curNmlFile;
+        
+        % HACK(amotta): Lazy initialization
+        if isempty(calibT); calibT = curCalib; end
+        calibT(curId) = curCalib; %#ok
+    end
+    
+    calibT = struct2table(calibT, 'AsArray', true);
+end
