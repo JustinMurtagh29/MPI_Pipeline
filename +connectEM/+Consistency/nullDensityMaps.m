63b62a9eb795dde430382ed47ceb8fc85dd90d5e
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-02-13 14:19:09 +0100

    Build null density maps for significance test

---
+++ +connectEM/+Consistency/nullDensityMaps.m
@@ -0,0 +1,45 @@
+function nullMaps = nullDensityMaps(asiAreas, varargin)
+    opts = struct;
+    opts.numMaps = 1;
+    opts.scheduler = '';
+    opts = Util.modifyStruct(opts, varargin{:});
+    
+    % NOTE(amotta): Make sure that user has specified bandwidth for kernel
+    % density estimate. Otherwise we're comparing apples and oranges...
+    assert(isfield(opts, 'bandWidth'));
+    assert(not(isempty(opts.bandWidth)));
+    
+    args = arrayfun(@(i) {i}, 1:opts.numMaps, 'UniformOutput', false);
+    sharedArgs = {asiAreas, varargin{:}}; %#ok
+    
+    if isempty(opts.scheduler)
+        nullMaps = cellfun( ...
+            @(args) nullDensityMap( ...
+                asiAreas, args{:}, varargin{:}), ...
+            args, 'UniformOutput', false);
+    else
+        job = Cluster.startJob( ...
+            @nullDensityMap, args, ...
+            'sharedInputs', sharedArgs, ...
+            'sharedInputsLocation', [1, 2 + (1:numel(varargin))], ...
+            'cluster', {'scheduler', opts.scheduler}, ...
+            'numOutputs', 1, 'taskGroupSize', 20);
+        Cluster.waitForJob(job);
+        
+        nullMaps = fetchOutputs(job);
+        nullMaps = cat(1, nullMaps{:});
+    end
+    
+    nullMaps = reshape(nullMaps, 1, 1, []);
+    nullMaps = cell2mat(nullMaps);
+end
+
+function nullMap = nullDensityMap(asiAreas, rngSeed, varargin)
+    rng(rngSeed);
+    
+    asiAreaPairs = 2 * floor(numel(asiAreas) / 2);
+    asiAreaPairs = randperm(numel(asiAreas), asiAreaPairs);
+    asiAreaPairs = reshape(asiAreas(asiAreaPairs), [], 2);
+    
+    nullMap = connectEM.Consistency.densityMap(asiAreaPairs, varargin{:});
+end
