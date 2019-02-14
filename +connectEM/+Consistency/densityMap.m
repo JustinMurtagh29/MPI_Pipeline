3f73f3cffc37f3e77d425f8e58a726dae9dffa77
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-02-14 11:44:56 +0100

    Add support for `kde2d` to density map function

--- +connectEM/+Consistency/densityMap.m
+++ +connectEM/+Consistency/densityMap.m
@@ -6,12 +6,10 @@ function [map, bw] = densityMap(asiAreaPairs, varargin)
     opts.yLim = [-1.5, 0.5];
     opts.mapSize = [301, 301];
     opts.bandWidth = [];
-    opts = Util.modifyStruct(opts, varargin{:});
+    opts.method = 'ksdensity';
     
-   [gridY, gridX] = ndgrid( ...
-        linspace(opts.yLim(1), opts.yLim(2), opts.mapSize(1)), ...
-        linspace(opts.xLim(1), opts.xLim(2), opts.mapSize(2)));
-    grid = horzcat(gridY(:), gridX(:));
+    opts = Util.modifyStruct(opts, varargin{:});
+    opts.method = lower(opts.method);
     
     log10AvgArea = log10(mean(asiAreaPairs, 2));
     cv = std(asiAreaPairs, 0, 2) ./ mean(asiAreaPairs, 2);
@@ -22,15 +20,34 @@ function [map, bw] = densityMap(asiAreaPairs, varargin)
       & opts.xLim(1) <= map(:, 2) & map(:, 2) <= opts.xLim(2);
     support = horzcat(opts.yLim(:), opts.xLim(:));
     
-    bwKvPair = {};
-    if ~isempty(opts.bandWidth)
-        bwKvPair = {'Bandwidth', opts.bandWidth};
-    end
+    switch opts.method
+        case 'ksdensity'
+           [gridY, gridX] = ndgrid( ...
+                linspace(opts.yLim(1), opts.yLim(2), opts.mapSize(1)), ...
+                linspace(opts.xLim(1), opts.xLim(2), opts.mapSize(2)));
+            grid = horzcat(gridY(:), gridX(:));
+
+            bwKvPair = {};
+            if ~isempty(opts.bandWidth)
+                bwKvPair = {'Bandwidth', opts.bandWidth};
+            end
+
+           [map, ~, bw] = ksdensity( ...
+                map(mask, :), grid, ...
+                'BoundaryCorrection', 'reflection', ...
+                'Support', support, bwKvPair{:});
+            map = reshape(map, opts.mapSize);
+            
+        case 'kde2d'
+           [bw, map] = connectEM.Libs.kde2d( ...
+                map(mask, :), opts.mapSize(1), ...
+                support(1, :), support(2, :), ...
+                opts.bandWidth);
+            map = transpose(map);
+            
+        otherwise
+            error('Invalid method "%s"', opts.method);
+    end   
     
-   [map, ~, bw] = ksdensity( ...
-        map(mask, :), grid, ...
-        'BoundaryCorrection', 'reflection', ...
-        'Support', support, bwKvPair{:});
-    map = reshape(map, opts.mapSize);
     map = mean(mask) * map / sum(map(:));
 end
