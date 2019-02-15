0794fbb57543607213d9760df1d6c4ce98345375
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-02-15 16:24:56 +0100

    Use pre-computed axon-spine interface table for weight matrix clustering

--- +connectEM/+Consistency/analyseWeightMatrix.m
+++ +connectEM/+Consistency/analyseWeightMatrix.m
@@ -5,47 +5,30 @@ clear;
 %% Configuration
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
 shFile = fullfile(rootDir, 'aggloState', 'dendrites_wholeCells_02_v3_auto.mat');
+connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-19-a-linearized_dendrites-wholeCells-03-v2-classified_SynapseAgglos-v8-classified.mat');
 
 info = Util.runInfo();
 Util.showRunInfo(info);
 
 %% Loading data
-param = load(fullfile(rootDir, 'allParameter.mat'));
-param = param.p;
+[curDir, curAsiFile] = fileparts(connFile);
+curAsiFile = fullfile(curDir, sprintf('%s_asiT.mat', curAsiFile));
 
-[conn, syn, connFile] = ...
-    connectEM.Consistency.loadConnectome(param);
+asiT = load(curAsiFile);
+asiT = asiT.asiT;
 
-% Loading spine head agglomerates
-shAgglos = load(shFile, 'shAgglos');
-shAgglos = shAgglos.shAgglos;
-
-% Loading augmented graph
-graph = Graph.load(rootDir);
-graph(~graph.borderIdx, :) = [];
-
-borderAreas = fullfile(rootDir, 'globalBorder.mat');
-borderAreas = load(borderAreas, 'borderArea2');
-borderAreas = borderAreas.borderArea2;
-
-graph.borderArea = borderAreas(graph.borderIdx);
-clear borderAreas;
-
-%% Build axon-spine interface areas
-asiT = ...
-    connectEM.Connectome.buildAxonSpineInterfaces( ...
-        param, graph, shAgglos, conn, syn);
-asiT = asiT(asiT.type == 'PrimarySpine', :);
-asiT.relId = reshape(1:height(asiT), [], 1);
+asiT = asiT(asiT.area > 0, :);
+asiT = connectEM.Consistency.Calibration.apply(asiT);
 
 %% Build connectivity matrix
 clear cur*;
+[~, curIds, curUniIds] = unique( ...
+    asiT(:, {'preAggloId', 'postAggloId'}), 'rows');
 
-[pairT, ~, curIds] = unique(asiT(:, { ...
-    'preAggloId', 'postAggloId'}), 'rows');
-pairT.axonClass = conn.axonMeta.axonClass(pairT.preAggloId);
-pairT.targetClass = conn.denMeta.targetClass(pairT.postAggloId);
-pairT.asiIds = accumarray(curIds, asiT.relId, [], @(ids) {ids});
+pairT = asiT(curIds, ...
+    {'preAggloId', 'postAggloId', 'axonClass', 'targetClass'});
+pairT.asiIds = accumarray( ...
+    curUniIds, reshape(1:height(asiT), [], 1), [], @(ids) {ids});
 
 %% Actually analyse weight matrix
 clear cur*;
@@ -70,6 +53,7 @@ curAreas = accumarray( ...
     curAreas, curPairT.medLog10AsiArea, ...
    [numel(curAxonIds), numel(curDendIds)], [], nan);
 
+%% Actually do the clustering
 Util.log('Clustering weight matrix');
 [curAxonLink, curAxonDist] = connectEM.Consistency.linkage(curAreas);
 [curDendLink, curDendDist] = connectEM.Consistency.linkage(curAreas');
@@ -80,6 +64,87 @@ curFig = figure;
 [~, ~, curDendPerm] = dendrogram(curDendLink, 0);
 close(curFig);
 clear curFig;
+
+%% Show dendrogram
+figure(); dendrogram(curAxonLink, 0);
+figure(); dendrogram(curDendLink, 0);
+
+%%
+%{
+curDataFile = '/mnt/mpibr/data/Personal/mottaa/L4/2018-12-19-Clustering-of-Weight-Matrix/corticocortical-onto-other-dendrites.mat';
+curData = load(curDataFile);
+curAxonLink = curData.curAxonLink;
+curAxonLink = curAxonLink(not(isnan(curAxonLink(:, end))), :);
+%}
+
+%% Find threshold with a second reasonably-sized cluster
+assert(issorted(curAxonLink(:, 1:2), 2));
+
+curAxonCount = max(reshape(curAxonLink(:, 1:2), [], 1));
+curAxonCount = curAxonCount + 1 - size(curAxonLink, 1);
+
+curOut = nan(0, 3);
+
+for i = 1:500
+    curGraph = graph( ...
+        cat(1, curAxonLink(:, 1), curAxonLink(:, 2)), ...
+        repmat(transpose((1:size(curAxonLink, 1)) + curAxonCount), 2, 1));
+
+    curCompIds = conncomp(curGraph);
+    curCompSizes = accumarray(curCompIds(:), 1);
+    
+    curSecCompSize = feval( ...
+        @(s) reshape(s(1:2), 1, []), ...
+        sort([0; curCompSizes], 'descend'));
+    curOut(end + 1, :) = [size(curOut, 1), curSecCompSize]; %#ok
+    
+    curAxonLink(end, :) = [];
+end
+
+% Plot
+curFig = figure;
+curFig.Color = 'white';
+curFig.Position(3:4) = [310, 180];
+curAx = axes(curFig);
+hold(curAx, 'on');
+
+plot(curOut(:, 1), curOut(:, 2), 'LineWidth', 2);
+plot(curOut(:, 1), curOut(:, 3), 'LineWidth', 2);
+
+set(curAx, 'TickDir', 'out', 'YScale', 'log');
+
+curLeg = legend(curAx, { ...
+    'Largest cluster', 'Second largest cluster'});
+set(curLeg, 'Location', 'SouthEast', 'Box', 'off');
+
+xlabel(curAx, 'Linkage cut-off');
+ylabel(curAx, 'Cluster size');
+
+title(curAx, ...
+    {info.filename; info.git_repos{1}.hash}, ...
+    'FontWeight', 'normal', 'FontSize', 10);
+
+%% Extract the largest two clusters
+curAxonLink = curData.curAxonLink;
+curAxonLink = curAxonLink(not(isnan(curAxonLink(:, end))), :);
+assert(issorted(curAxonLink(:, 1:2), 2));
+
+curAxonCount = max(reshape(curAxonLink(:, 1:2), [], 1));
+curAxonCount = curAxonCount + 1 - size(curAxonLink, 1);
+
+% Use linkage cut-off of 200
+curAxonLink((end - 199):end, :) = [];
+
+curGraph = graph( ...
+    cat(1, curAxonLink(:, 1), curAxonLink(:, 2)), ...
+    repmat(transpose((1:size(curAxonLink, 1)) + curAxonCount), 2, 1));
+
+curCompIds = conncomp(curGraph);
+curCompSizes = accumarray(curCompIds(:), 1);
+[~, curSortIds] = sort(curCompSizes, 'descend');
+
+curAxonIdsA = find(curCompIds(1:curAxonCount) == curSortIds(1));
+curAxonIdsB = find(curCompIds(1:curAxonCount) == curSortIds(2));
 
 %% Plot synapse sizes
 curTemp = curAreas(curAxonPerm, curDendPerm);
@@ -116,7 +181,8 @@ title(curAx, ...
     'FontWeight', 'normal', 'FontSize', 10);
 
 %% Plot distance matrix
-curTemp = curAxonDist(curAxonPerm, curAxonPerm);
+curIds = [curAxonIdsA, curAxonIdsB];
+curTemp = curAxonDist(curIds, curIds);
 
 curFig = figure();
 curFig.Color = 'white';
@@ -138,15 +204,15 @@ axis(curAx, 'square');
 curAx.TickDir = 'out';
 curAx.XAxisLocation = 'top';
 
-xlim(curAx, [1, numel(curAxonIds)]);
+% xlim(curAx, [1, numel(curAxonIds)]);
 xlabel(curAx, 'Axons (clustered)');
-xticks(curAx, [1, numel(curAxonIds)]);
+% xticks(curAx, [1, numel(curAxonIds)]);
 xticklabels(curAx, arrayfun( ...
     @num2str, xticks(curAx), 'UniformOutput', false));
 
-ylim(curAx, [1, numel(curAxonIds)]);
+% ylim(curAx, [1, numel(curAxonIds)]);
 ylabel(curAx, 'Axons (clustered)');
-yticks(curAx, [1, numel(curAxonIds)]);
+% yticks(curAx, [1, numel(curAxonIds)]);
 yticklabels(curAx, arrayfun( ...
     @num2str, yticks(curAx), 'UniformOutput', false));
 
