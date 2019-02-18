9aeec7ea7640e081074baeaf1cf771774eddc9db
Sahil Loomba <sahil.loomba@brain.mpg.de>
2019-02-18 16:26:52 +0100

    try minScore relaxed thr

--- +H2_3_v2_SubI/+Script/exportAgglos.m
+++ +H2_3_v2_SubI/+Script/exportAgglos.m
@@ -11,7 +11,7 @@ datasetName = 'H2_3_v2_U1_SubI_mr2e_wsmrnet';
 voxelSize = [11.24, 11.24, 28];
 
 % Agglomerate down to this score treshold
-minScore = -0.90;
+minScore = 0.1;
 
 info = Util.runInfo();
 
@@ -41,7 +41,7 @@ agglos = agglos(idxSort);
 % HACK(amotta): Convert graph table into historical graph table;
 graphS = struct;
 graphS.edges = graph.edge;
-
+Util.log('Now writing out nmls...')
 outDir = fullfile(rootDir,'29Nov2018_agglomeration', ['score_', num2str(minScore)]);
 mkdir(outDir)
 for i=2:100
