f2b37ad28cccb38f59404c7be54089f5dbe1f587
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-03-08 17:44:43 +0100

    Give ranges in consistency table

--- +connectEM/+Figure/consistencyTable.m
+++ +connectEM/+Figure/consistencyTable.m
@@ -10,25 +10,48 @@ rowNames = {'Exc', 'CC', 'TC'};
 colNames = {'All', 'PD', 'AD', 'OD'};
 colorN = 256;
 
-% First dimension corresponds to small (1) and large (2) low-CV regions
-data = nan(2, numel(rowNames), numel(colNames));
+% First dimension corresponds to minimum and maximum.
+% Second dimension corresponds to small (1) and large (2) low-CV regions.
+data = nan(2, 2, numel(rowNames), numel(colNames));
 
 % Numbers from connectEM.Connectome plotSynapseSizeConsistency
-% Commit f0fef574d62da20d7892884c70a1e2f1190e9561
-data(:, 1, 1) = [15.83, 16.23];
-data(:, 1, 2) = [13.24, 11.92];
-data(:, 1, 3) = [ 5.69,  8.77];
-data(:, 1, 4) = [12.29, 13.17];
+% Commit 2574326f1e904c8c68a080a60dfc43b567e9b9e1
 
-data(:, 2, 1) = [15.64, 17.42];
-data(:, 2, 2) = [13.22, 11.98];
-data(:, 2, 3) = [  nan,   nan];
-data(:, 2, 4) = [13.47, 11.35];
+% Excitatory synapses onto all targets
+data(:, 1, 1, 1) = [15.2, 18.6];
+data(:, 2, 1, 1) = [15.8, 19.7];
+% Excitatory synapses onto proximal dendrites
+data(:, 1, 1, 2) = [12.8, 17.9];
+data(:, 2, 1, 2) = [11.4, 17.9];
+% Excitatory synapses onto apical dendrites
+data(:, 1, 1, 3) = [ 5.1,  7.4];
+data(:, 2, 1, 3) = [ 8.2,  9.7];
+% Excitatory synapses onto other dendrites
+data(:, 1, 1, 4) = [11.5, 17.9];
+data(:, 2, 1, 4) = [12.6, 19.9];
 
-data(:, 3, 1) = [ 2.05,  6.61];
-data(:, 3, 2) = [  nan,   nan];
-data(:, 3, 3) = [  nan,   nan];
-data(:, 3, 4) = [ 0.66,  3.72];
+% Corticocortical synapses onto all targets
+data(:, 1, 2, 1) = [15.2, 20.7];
+data(:, 2, 2, 1) = [16.9, 22.0];
+% Corticocortical synapses onto proximal dendrites
+data(:, 1, 2, 2) = [12.7, 17.6];
+data(:, 2, 2, 2) = [11.5, 17.5];
+% Corticocortical synapses onto apical dendrites
+data(:, :, 2, 3) = nan;
+% Corticocortical synapses onto other dendrites
+data(:, 1, 2, 4) = [12.8, 18.5];
+data(:, 2, 2, 4) = [10.5, 19.7];
+
+% Thalamocortical synapses onto all targets
+data(:, 1, 3, 1) = [ 1.7,  6.7];
+data(:, 2, 3, 1) = [ 6.2, 15.4];
+% Thalamocortical synapses onto proximal dendrites
+data(:, :, 3, 2) = nan;
+% Thalamocortical synapses onto apical dendrites
+data(:, :, 3, 3) = nan;
+% Thalamocortical synapses onto other dendrites
+data(:, 1, 3, 4) = [ 0.3,  5.8];
+data(:, 2, 3, 4) = [ 3.4, 16.7];
 
 colors = reshape(linspace(0, 1, colorN), [], 1);
 colors = { ...
@@ -37,7 +60,8 @@ colors = { ...
 assert(isequal(numel(colors), size(data, 1)));
 
 %% Plot
-curCaxisMax = max(data(:), [], 'omitnan');
+curCaxisMax = mean(reshape(data, 2, []));
+curCaxisMax = max(curCaxisMax, [], 'omitnan');
 curCaxisMax = ceil(curCaxisMax / 10) * 10;
 
 close all;
@@ -56,8 +80,9 @@ for curRow = 1:numel(rowNames)
         curCoord(:, 2) = (curRow - 1) + [0, 0, 1] + 0.5;
         
         for curId = 1:2
-            curData = data(curId, curRow, curCol);
-            if isnan(curData); continue; end
+            curData = data(:, curId, curRow, curCol);
+            if all(isnan(curData)); continue; end
+            curMean = mean(curData);
             
             switch curId
                 case 1, curCoord(2, :) = [curCoord(1, 1), curCoord(3, 2)];
@@ -68,7 +93,8 @@ for curRow = 1:numel(rowNames)
             curShape = polyshape(curCoord(:, 1), curCoord(:, 2));
             
             curColor = linspace(0, curCaxisMax, colorN);
-            curColor = colors{curId}(discretize(curData, curColor), :);
+            curColor = discretize(mean(curData), curColor);
+            curColor = colors{curId}(curColor, :);
             
             plot(curShape, 'FaceColor', curColor, 'FaceAlpha', 1);
             
@@ -76,7 +102,7 @@ for curRow = 1:numel(rowNames)
                 ax, ...
                 mean(curCoord(:, 1)), ...
                 mean(curCoord(:, 2)), ...
-                sprintf('%.0f', curData), ...
+                sprintf('%.0f-%.0f', curData), ...
                 'HorizontalAlignment', 'center');
         end
     end
@@ -99,5 +125,5 @@ ylim(ax, [0, numel(rowNames)] + 0.5);
 yticks(ax, 1:numel(rowNames));
 yticklabels(ax, rowNames);
 
-fig.Position(3:4) = [310, 250];
+fig.Position(3:4) = [420, 330];
 connectEM.Figure.config(fig, info);
