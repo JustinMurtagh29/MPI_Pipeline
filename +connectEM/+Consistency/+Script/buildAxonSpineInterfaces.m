89eb972fcef2a9265053d9d37384bb1e6ca9b26d
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-03-08 10:04:46 +0100

    Build axon-spine interface table for partially split axon state

--- +connectEM/+Consistency/+Script/buildAxonSpineInterfaces.m
+++ +connectEM/+Consistency/+Script/buildAxonSpineInterfaces.m
@@ -4,6 +4,7 @@ clear;
 
 %% Configuration
 rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-19-a-partiallySplit-v2_dendrites-wholeCells-03-v2-classified_SynapseAgglos-v8-classified.mat');
 shFile = fullfile(rootDir, 'aggloState', 'dendrites_wholeCells_02_v3_auto.mat');
 
 runId = datestr(now, 30);
@@ -15,7 +16,7 @@ Util.showRunInfo(info);
 param = load(fullfile(rootDir, 'allParameter.mat'));
 param = param.p;
 
-[conn, syn, connFile] = connectEM.Consistency.loadConnectome(param);
+[conn, syn] = connectEM.Connectome.load(param, connFile);
 conn = connectEM.Connectome.prepareForSpecificityAnalysis(conn);
 
 % Loading spine head agglomerates
