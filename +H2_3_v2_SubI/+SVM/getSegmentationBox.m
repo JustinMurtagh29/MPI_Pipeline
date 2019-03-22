2d3890a8a56d7ffff9e604df9d849c7d4821e5b2
Sahil Loomba <sahil.loomba@brain.mpg.de>
2019-03-22 10:21:16 +0100

    svm to seg for wk

--- +H2_3_v2_SubI/+SVM/getSegmentationBox.m
+++ +H2_3_v2_SubI/+SVM/getSegmentationBox.m
@@ -1,9 +1,9 @@
-function [seg, synCom, vcCom, miCom] = getSegmentationBox(p, cubeId)
+function [seg, synCom, vcCom, miCom] = getSegmentationBox(p,segSVM, i,j,k)
 % make segmentation to upload in wk
 % Author: Benedikt Staffler <benedikt.staffler@brain.mpg.de>
 %       : Sahil Loomba <sahil.loomba@brain.mpg.de>
 
-m = load(fullfile(p.local(cubeId).saveFolder, 'svmPredictions.mat'),...
+m = load(fullfile(p.local(i,j,k).saveFolder, 'svmPredictions.mat'),...
              pred17, pred17_2, pred17_4, bbox);
 m1 = m.pred17_2; % use for mitos
 m2 = m.pred17_4; % use for vesicles
@@ -27,6 +27,6 @@ for i = 1:3
     seg(idx) = 0;
 end
 
-seg_bbox = seg;
-
+saveSegDataGlobal(segSVM, p.local(i,j,k).bboxSmall(:,1)', seg); 
+Util.save([p.local(i,j,k).saveFolder 'svmSegData.mat'], seg,synCom, vcCom, miCom,);
 end
