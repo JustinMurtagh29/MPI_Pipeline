954129f1c63c668e4b5461423d2718363f5481f5
Sahil Loomba <sahil.loomba@brain.mpg.de>
2019-03-28 11:34:30 +0100

    FFN with increasing trainSize

--- +Mk1_F6_JS_SubI_v1/+ConnectEM/evaluateWithFFN.m
+++ +Mk1_F6_JS_SubI_v1/+ConnectEM/evaluateWithFFN.m
@@ -5,6 +5,23 @@ load(fullfile(param.saveFolder,'connectEM','features_all.mat'))
 info = Util.runInfo();
 Util.showRunInfo(info);
 
+Util.log(' create a neural network')
+net = feedforwardnet(6);
+% set early stopping parameters
+net.divideParam.trainRatio = 0.85; % training set [%]
+net.divideParam.valRatio = 0.15; % validation set [%]
+% net.divideParam.testRatio = 0.15; % test set [%]
+% number of hidden layer neurons
+for i=1:net.numLayers-1
+    % hidden layer transfer function
+    net.layers{i}.transferFcn = 'logsig';
+end
+% network training
+net.trainFcn = 'trainb';
+net.trainParam.epochs = 1000;
+net.performFcn = 'mse';
+
+%% data
 Util.log('divide into training and test set')
 rng(0);
 testFrac = 0.2;
@@ -31,39 +48,42 @@ curTestFeats = [ ...
 curTestLabels = repmat(gtTest.label, 2, 1);
 gtTestModified = repmat(gtTest,2,1); % because of inverted features
 
-% compile train data
+%{% compile train data
 curTrainFeats = [ ...
            [rawFeatsTrain; ...
             fm.invertDirection(rawFeatsTrain)], ...
            [classFeatsTrain; ...
             fm.invertDirection(classFeatsTrain)]];
 curTrainLabels = repmat(gtTrain.label, 2, 1);
+%}
 
-Util.log(' create a neural network')
-net = feedforwardnet(6);
-% set early stopping parameters
-net.divideParam.trainRatio = 0.85; % training set [%]
-net.divideParam.valRatio = 0.15; % validation set [%]
-% % net.divideParam.testRatio = 0.15; % test set [%]
+% train with increasing training data sizes
+trainFrac = [0.2, 0.4, 0.6, 0.8, 1];
+trainSizes = floor(trainFrac.*height(gtTrain));
+rng(1); % for reproducibility
+curRandIds = randperm(height(gtTrain));
 
-%% number of hidden layer neurons
-for i=1:net.numLayers-1
-    % hidden layer transfer function
-    net.layers{i}.transferFcn = 'logsig';
+classifiers = cell(0);
+results = cell(0, 2);
+
+for curTrainSize = trainSizes
+        curTrainIds = curRandIds(1:curTrainSize);
+
+        curTrainFeats = [ ...
+           [rawFeatsTrain(curTrainIds, :); ...
+            fm.invertDirection(rawFeatsTrain(curTrainIds, :))], ...
+           [classFeatsTrain(curTrainIds, :); ...
+            fm.invertDirection(classFeatsTrain(curTrainIds, :))]];
+        curTrainLabels = repmat(gtTrain.label(curTrainIds), 2, 1);
+
+        [net,tr,Y,E]= train(net,curTrainFeats',curTrainLabels');
+
+        curScores = net(curTestFeats');
+        curScores = reshape(curScores,'',1);
+        
+        classifiers{end + 1} = net;
+        results(end + 1, :) = {curTestLabels, curScores}; %#ok
 end
-
-%net = configure(net,inputs',outputs');
-
-%% network training
-net.trainFcn = 'trainb';
-net.trainParam.epochs = 1000;
-
-net.performFcn = 'mse';
-
-[net,tr,Y,E]= train(net,curTrainFeats',curTrainLabels');
-
-%% network response after training
-curTestScores = net(curTestFeats');
 
 %% Plot aggregate results
 curFig = figure();
@@ -72,12 +92,21 @@ curFig.Color = 'white';
 curAx = axes(curFig);
 curAx.TickDir = 'out';
 hold(curAx, 'on');
+for curTrainSizeIdx = 1:numel(trainSizes)
+    curTrainSize = trainSizes(curTrainSizeIdx);
     
-[curPrec, curRec, threshVec] = ...
-    TypeEM.Classifier.buildPrecRec(curTestScores, curTestLabels);
-curLine = ...
-    TypeEM.Classifier.plotPrecisionRecall(curAx, curPrec, curRec);
+    curLabels = results(curTrainSizeIdx, 1);
+    curScores = results(curTrainSizeIdx, 2);
 
+    curScores = cell2mat(curScores);
+    curLabels = cell2mat(curLabels);
+    
+    [curPrec, curRec, threshVec] = ...
+        TypeEM.Classifier.buildPrecRec(curScores, curLabels);
+    curLine = ...
+        TypeEM.Classifier.plotPrecisionRecall(curAx, curPrec, curRec);
+end
+ 
 curLines = flip(curAx.Children);
 axis(curAx, 'square');
 
@@ -86,6 +115,6 @@ title(curAx, ...
     'FontWeight', 'normal', 'FontSize', 10);
 savefig(fullfile(param.saveFolder,'connectEM',['precrec_test_FFN.fig']))
 saveas(gcf,fullfile(param.saveFolder,'connectEM',['precrec_test_FFN.png']))
-Util.save(fullfile(param.saveFolder,'connectEM','evaluateWithFFn.mat'),net)
+Util.save(fullfile(param.saveFolder,'connectEM','evaluateWithFFn.mat'),net,info)
 
 
