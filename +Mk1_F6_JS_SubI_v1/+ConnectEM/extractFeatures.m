1d3d9623e4798b8102deb4fccb12570a6c1089b5
Sahil Loomba <sahil.loomba@brain.mpg.de>
2019-03-28 10:38:56 +0100

    extractFeatures for other network tests

---
+++ +Mk1_F6_JS_SubI_v1/+ConnectEM/extractFeatures.m
@@ -0,0 +1,65 @@
+function [allFeats, allLabels, allGt] = extractFeatures(boxesUsed)
+
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+% Modified by
+%   Sahil Loomba <sahil.loomba@brain.mpg.de>
+%% HACKHACKHACK
+% NOTE(amotta): This is a huge mess. The training data is located in my
+% repository, SynEM is from Benedikt's repository, and the SynEM classifier
+% is loaded from the pipeline repo.
+%   Let's get rid of the dependency on the pipeline repo by building the
+% feature map de-novo. The `paper` version is identical to the one stored
+% in the pipeline repository up to different `border` values in the
+% `AverageFilter`. But this doesn't affect `fm.invertDirection`.
+addpath('/gaba/u/sahilloo/repos/benedikt', '-end');
+addpath(genpath('/gaba/u/sahilloo/repos/amotta/matlab/'))
+
+rootDir = '/tmpscratch/sahilloo/data/Mk1_F6_JS_SubI_v1/pipelineRun_mr2e_wsmrnet/';
+param = load(fullfile(rootDir, 'allParameter.mat'));
+param = param.p;
+
+nmlDir = fullfile( param.saveFolder, 'tracings', 'connectEM','proofread', boxesUsed);
+
+warning('Constructing FeatureMap de-novo');
+fm = SynEM.getFeatureMap('paper');
+
+info = Util.runInfo();
+Util.showRunInfo(info);
+
+%% Load ground truth
+nmlFiles = dir(fullfile(nmlDir, '*.nml'));
+nmlFiles(cat(1, nmlFiles.isdir)) = [];
+nmlFiles = fullfile(nmlDir, {nmlFiles.name});
+
+gt = ConnectEM.loadGroundTruth(nmlFiles);
+
+% Make sure that there are no multiplicities
+assert(numel(gt.borderId) == numel(unique(gt.borderId)));
+
+% Ignore borders without labels
+gt(~gt.label, :) = [];
+
+%% Load feature data for all borders
+[rawFeats, rawBorderIds] = ...
+    ConnectEM.loadFeaturesForBorderIds( ...
+        param, 'InterfaceRawFeatures.mat', gt.borderId);
+[classFeats, classBorderIds] = ...
+    ConnectEM.loadFeaturesForBorderIds( ...
+        param, 'InterfaceClassFeatures.mat', gt.borderId);
+assert(isequal(rawBorderIds, classBorderIds));
+
+[~, curRowIds] = ismember(rawBorderIds, gt.borderId);
+gt = gt(curRowIds, :);
+
+% augment data
+allFeats = [ ...
+   [rawFeats; ...
+    fm.invertDirection(rawFeats)], ...
+   [classFeats; ...
+    fm.invertDirection(classFeats)]];
+allLabels = repmat(gt.label, 2, 1);
+allGt = repmat(gt,2,1); % because of inverted features
+
+Util.save(fullfile(param.saveFolder,'connectEM',['features_' boxesUsed '.mat']), allLabels, allFeats, allGt)
+end
