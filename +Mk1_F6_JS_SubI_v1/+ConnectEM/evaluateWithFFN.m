d1bbcfc397bb422fecb2e0010c66d47a1fb7a8a7
Sahil Loomba <sahil.loomba@brain.mpg.de>
2019-03-28 15:28:14 +0100

    cant find bug: increasing training size PR curves are wrong

--- +Mk1_F6_JS_SubI_v1/+ConnectEM/evaluateWithFFN.m
+++ +Mk1_F6_JS_SubI_v1/+ConnectEM/evaluateWithFFN.m
@@ -5,22 +5,6 @@ load(fullfile(param.saveFolder,'connectEM','features_all.mat'))
 info = Util.runInfo();
 Util.showRunInfo(info);
 
-Util.log(' create a neural network')
-net = feedforwardnet(6);
-% set early stopping parameters
-net.divideParam.trainRatio = 0.85; % training set [%]
-net.divideParam.valRatio = 0.15; % validation set [%]
-% net.divideParam.testRatio = 0.15; % test set [%]
-% number of hidden layer neurons
-for i=1:net.numLayers-1
-    % hidden layer transfer function
-    net.layers{i}.transferFcn = 'logsig';
-end
-% network training
-net.trainFcn = 'trainb';
-net.trainParam.epochs = 1000;
-net.performFcn = 'mse';
-
 %% data
 Util.log('divide into training and test set')
 rng(0);
@@ -46,7 +30,6 @@ curTestFeats = [ ...
    [classFeatsTest; ...
     fm.invertDirection(classFeatsTest)]];
 curTestLabels = repmat(gtTest.label, 2, 1);
-gtTestModified = repmat(gtTest,2,1); % because of inverted features
 
 %{% compile train data
 curTrainFeats = [ ...
@@ -67,7 +50,7 @@ classifiers = cell(0);
 results = cell(0, 2);
 
 for curTrainSize = trainSizes
-        curTrainIds = curRandIds(1:curTrainSize);
+       curTrainIds = curRandIds(1:curTrainSize);
 
         curTrainFeats = [ ...
            [rawFeatsTrain(curTrainIds, :); ...
@@ -76,13 +59,36 @@ for curTrainSize = trainSizes
             fm.invertDirection(classFeatsTrain(curTrainIds, :))]];
         curTrainLabels = repmat(gtTrain.label(curTrainIds), 2, 1);
 
+        display(['train '])
+        curTrainSize
+        size(curTrainFeats)
+        size(curTrainLabels)
+
+        
+        
+        Util.log(' create a neural network')
+        net = feedforwardnet(6);
+        % set early stopping parameters
+        %net.divideParam.trainRatio = 0.85; % training set [%]
+        %net.divideParam.valRatio = 0.15; % validation set [%]
+        % net.divideParam.testRatio = 0.15; % test set [%]
+        % number of hidden layer neurons
+        for i=1:net.numLayers-1
+            % hidden layer transfer function
+            net.layers{i}.transferFcn = 'logsig';
+        end
+        % network training
+        net.trainFcn = 'trainb';
+        net.trainParam.epochs = 1000;
+        net.performFcn = 'mse';
+        
         [net,tr,Y,E]= train(net,curTrainFeats',curTrainLabels');
 
         curScores = net(curTestFeats');
-        curScores = reshape(curScores,'',1);
         
         classifiers{end + 1} = net;
         results(end + 1, :) = {curTestLabels, curScores}; %#ok
+        clear net
 end
 
 %% Plot aggregate results
@@ -110,11 +116,20 @@ end
 curLines = flip(curAx.Children);
 axis(curAx, 'square');
 
+curLegs = arrayfun( ...
+    @(n) sprintf('%d training edges', n), ...
+    trainSizes, 'UniformOutput', false);
+
+curLegs = legend( ...
+    curLines, curLegs, ...
+    'Location', 'EastOutside');
+curLegs.Box = 'off';
+
 title(curAx, ...
     {info.filename; info.git_repos{1}.hash}, ...
     'FontWeight', 'normal', 'FontSize', 10);
-savefig(fullfile(param.saveFolder,'connectEM',['precrec_test_FFN.fig']))
+%savefig(fullfile(param.saveFolder,'connectEM',['precrec_test_FFN.fig']))
 saveas(gcf,fullfile(param.saveFolder,'connectEM',['precrec_test_FFN.png']))
-Util.save(fullfile(param.saveFolder,'connectEM','evaluateWithFFn.mat'),net,info)
+%Util.save(fullfile(param.saveFolder,'connectEM','evaluateWithFFn.mat'),classifiers,info)
 
 
