9ff89490816ba7ce3c1f7a2a891185cdd2a21b01
Sahil Loomba <sahil.loomba@brain.mpg.de>
2019-04-03 16:09:57 +0200

    do grid search for agglomeration

---
+++ +connectEM/aggloGridSearchSimple.m
@@ -0,0 +1,65 @@
+function aggloGridSeachSimple(p)
+
+info = Util.runInfo();
+
+outputFolder = [p.saveFolder datestr(clock,30) 'gridSearchAgglomeration/'];
+if ~exist(outputFolder, 'dir')
+    mkdir(outputFolder);
+end
+
+display('loading dependent variables...');
+tic;
+m = load([p.saveFolder 'allParameterWithSynapses.mat']);
+p = m.p;
+graph = load([p.saveFolder 'graph.mat']);
+segmentMeta = load([p.saveFolder 'segmentMeta.mat'], 'voxelCount', 'point', 'maxSegId');
+segmentMeta.point = segmentMeta.point';
+borderMeta = load([p.saveFolder 'globalBorder.mat'], 'borderSize', 'borderCoM');
+toc;
+
+% do grid search on these parameter values
+borderSize = [25 50 100 200 300 500]; % border below this size are excluded from dendrite subgraph during inital agglomeration
+segmentSize = [25 50 100 300 500 700 1000]; % segment below ...
+probThreshold = [0.94 0.95 0.96 0.97 0.98 0.99]; % threshold on neurite continuity probability for CC
+sizeThreshold = 100; % threshold on final agglomerate size in voxels (single segments not collected!)
+
+% create job input parameters
+inputArguments = inputArgumentsFromParameterSets(borderSize, segmentSize, probThreshold, sizeThreshold);
+inputArgumentFilename = arrayfun(@(x) fullfile(outputFolder, num2str(x,'%.5i'),'.mat'), 1:size(inputArguments,1),'uni',0)';
+inputArguments = mat2cell(inputArguments, ones(size(inputArguments,1),1), size(inputArguments,2));
+inputArguments = cellfun(@(x,y)[num2cell(x) y], inputArguments, inputArgumentFilename, 'uni', 0);
+
+
+job = Cluster.startJob( ...
+    @doAgglomeration, inputArguments, ...
+    'sharedInputs', {graph, segmentMeta, borderMeta, info}, ...
+    'sharedInputsLocation', [6,7,8, 9], ...
+    'name', 'gridSearch', ...
+    'cluster', {'memory', 12, ...
+        'time', '24:00:00', 'priority', 100});
+end
+
+end
+
+function [agglos, agglosSize, agglosEdges] = doAgglomeration(...
+            borderSizeThr,segmentSizeThr, probThreshold, sizeThreshold, outputFile,...
+            graph, segmentMeta, borderMeta, info)
+    display(['Cut graph at border size:' num2str(borderSizeThr), 'segment size:' num2str(segmentSizeThr)]);
+    graphCut = connectEM.cutGraphSimple(p, graph, segmentMeta, borderMeta, borderSizeThr, segmentSizeThr);
+    display(['Performing agglomeration on graph with thr prob:' num2str(probThreshold), 'agglo size:' num2str(sizeThreshold)]);
+    [agglos, agglosSize, agglosEdges] = connectEM.partitionSortAndKeepOnlyLarge(graphCut, segmentMeta, probThreshold, sizeThreshold);
+    display('Saving:');
+    tic;
+    % Lets save some more so that we can always understand whats happening
+    clearvars borderMeta segmentMeta graph graphCut
+    save(outputFile);
+    toc;
+end
+
+function inputArguments = inputArgumentsFromParameterSets(a, b, c, d, e)
+
+    [aG, bG, cG, dG, eG] = ndgrid(a, b, c, d, e);
+    inputArguments = cat(2, aG(:), bG(:), cG(:), dG(:), eG(:)); 
+
+end
+
