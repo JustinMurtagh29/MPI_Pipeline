941f63ad726a1bed936b2a5e5e8eb1d1b576f881
Sahil Loomba <sahil.loomba@brain.mpg.de>
2019-07-02 11:15:45 +0200

    leftover

--- +connectEM/debugSpines.m
+++ +connectEM/debugSpines.m
@@ -1,9 +1,10 @@
-% find out why the agglomeration stopped while growing
+% find out if spines are being attached in the graph partition
 
-%segId = 17059232;
-segId = 55128332
-edges= graph.edges;
-prob = graph.prob;
+%segId = 7222660; % segment ID of spinehead
+segId = 7011483
+% find which agglo contains spine head id
+maxSegId = Seg.Global.getMaxSegId(p);
+idxFound = Agglo.findParentAggloForThisSeg(segId, agglos, maxSegId);
 
 allEdges= ismember(edges,segId);
 outEdges = edges(find(allEdges(:,1) | allEdges(:,2)),:);
@@ -30,9 +31,9 @@ theseEdgesNodes = changem(double(theseEdgesSegId), 1:size(theseCoM,1), outEdges(
 for i=1:size(theseEdgesNodes,1)
 curEdges = theseEdgesNodes(i,:);
 curNodes = theseCoM(curEdges,:);
-skel = skel.addTree(['tree_' num2str(curEdges(1)) '-' num2str(curEdges(2)) '_' num2str(theseProb(i),'%03f') ], curNodes);
+skel = skel.addTree(['tree_' num2str(curEdges(1)) '-' num2str(curEdges(2)) '_' num2str(theseProb(i),'%03f') ], curNodes)
 
 end
 
-skel.write(fullfile(outputFolder, ['debugSegId' num2str(segId) '.nml']))
+skel.write(fullfile(outputFolder, ['debugSpine' num2str(segId) '.nml']))
 
