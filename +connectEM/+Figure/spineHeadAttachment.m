18c0a29bad47aeae2d1aab3053b66fe0c16fff8b
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-08-21 15:39:52 +0200

    Visualize attachment for all automatically attached spine heads

--- +connectEM/+Figure/spineHeadAttachment.m
+++ +connectEM/+Figure/spineHeadAttachment.m
@@ -12,11 +12,12 @@ connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-19-a_dendrites
 autoFile = fullfile(rootDir, 'aggloState', 'dendrites_wholeCells_02_v3_auto.mat');
 
 shId = 14;
-cam = struct;
-cam.CameraPosition  = [26.6944, 30.3816, -1.4454];
-cam.CameraTarget    = [2.9505, 2.0485, 8.2460];
-cam.CameraUpVector  = [-0.6758, 0.6708, 0.3053];
-cam.CameraViewAngle = 10.9061;
+
+cams = struct;
+cams(1).CameraPosition  = [-79.0698, 2.9561, -19.2050];
+cams(1).CameraTarget    = [3.1753, 2.9561, 7.5180];
+cams(1).CameraUpVector  = [0, 0, 1];
+cams(1).CameraViewAngle = 10.9061;
 
 info = Util.runInfo();
 Util.showRunInfo(info);
@@ -68,22 +69,37 @@ shT = shT( ...
 shT.dendId = cell2mat(shT.dendIds);
 shT.dendIds = [];
 
-%% Prototyping
+%% Rendering
 clear cur*;
 
 curSh = shT(shT.id == shId, :);
-curShSegIds = double(curSh.agglo{1});
+curShs = shT(shT.dendId == curSh.dendId & shT.autoAttached, :);
 curDendSegIds = double(conn.dendrites{curSh.dendId});
 
-curNeckSegIds = double(curSh.edges{1}(:, 2));
-curNeckSegIds = setdiff(curNeckSegIds, [0; curShSegIds], 'stable');
-curNeckSegIds = curNeckSegIds(1:(end - 1));
+curShs.neckSegIds = cellfun( ...
+    @(head, neck) setdiff(neck, [0; head], 'stable'), ...
+    curShs.agglo, curShs.edges, 'UniformOutput', false);
 
-curDendSegIds = setdiff(curDendSegIds, [curShSegIds; curNeckSegIds]);
-curAgglos = [{curShSegIds}; num2cell(curNeckSegIds); {curDendSegIds}];
+curSpineSegIds = cell2mat([curShs.agglo; curShs.neckSegIds]);
+curDendSegIds = setdiff(curDendSegIds, curSpineSegIds);
+
+curAgglos = cellfun( ...
+    @(head, neck) [{head}; num2cell(neck(:))], ...
+    curShs.agglo, curShs.neckSegIds, 'UniformOutput', false);
+
+curCmap = jet(120);
+curCmap = curCmap((size(curCmap, 1) - 100) / 2 + (1:100), :);
+
+curColors = cellfun( ...
+    @(a) feval(@(cols) cols(1:(end - 1), :), curCmap( ...
+        ceil(linspace(1, size(curCmap, 1), 1 + numel(a))), :)), ...
+    curAgglos, 'UniformOutput', false);
+
+curAgglos = cat(1, curAgglos{:}, {curDendSegIds});
+curColors = cat(1, curColors{:}, curCmap(end, :));
 curLUT = Agglo.buildLUT(maxSegId, curAgglos);
 
-curBox = segPos(curNeckSegIds, :);
+curBox = segPos(curShs.neckSegIds{curShs.id == shId}, :);
 curBox = transpose(cat(1, min(curBox, [], 1), max(curBox, [], 1)));
 curBox = max(1, curBox - [+256, -256]);
 
@@ -101,37 +117,39 @@ curFig = figure();
 curAx = axes(curFig);
 hold(curAx, 'on');
 
-curColors = prism(numel(curIsos));
-curColors = flip(curColors, 1);
-
 for curIdx = 1:numel(curIsos)
     curIso = curIsos{curIdx};
     curIso.vertices = reshape(curIso.vertices, [], 3);
     curIso.vertices = curIso.vertices .* voxelSize / 1E3;
     
     curPatch = patch(curIso);
+    curPatch.FaceColor = curColors(curIdx, :);
     curPatch.EdgeAlpha = 0;
-    curPatch.FaceColor = curColors(curIdx, :);    
 end
 
 axis(curAx, 'equal');
 
-curKeys = fieldnames(cam);
+curKeys = fieldnames(cams);
 for curIdx = 1:numel(curKeys)
     curKey = curKeys{curIdx};
-    curAx.(curKey) = cam.(curKey);
+    curAx.(curKey) = cams.(curKey);
 end
 
 camlight(curAx);
 
-for curDim = 'XYZ'
+for curDimIdx = 1:3
+    curDim = char(double('X') - 1 + curDimIdx);
+    curLims = [1, diff(curBox(curDimIdx, :))];
+    curLims = curLims * voxelSize(curDimIdx) / 1E3;
+    
     curAxis = sprintf('%sAxis', curDim);
     curAxis = curAx.(curAxis);
     curAxis.Visible = 'off';
+    curAxis.Limits = curLims;
 end
 
 annotation( ...
     curFig, 'textbox', [0, 0.9, 1, 0.1], ...
     'String', {info.filename; info.git_repos{1}.hash}, ...
     'EdgeColor', 'none', 'HorizontalAlignment', 'center');
-connectEM.Figure.config(curFig, info);
+connectEM.Figure.config(curFig);
