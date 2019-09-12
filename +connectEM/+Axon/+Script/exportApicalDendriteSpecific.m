53db82c610cf707aa9577133c41ad18d02ccd2c4
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-09-13 00:08:31 +0200

    Export skeleton of "AD-specific" axon with its synapses

--- +connectEM/+Axon/+Script/exportApicalDendriteSpecific.m
+++ +connectEM/+Axon/+Script/exportApicalDendriteSpecific.m
@@ -16,10 +16,14 @@ param = load(fullfile(rootDir, 'allParameter.mat'), 'p');
 param = param.p;
 
 connOld = load(connOldFile);
-[connNew, synNew, axonClasses] = ...
-    connectEM.Connectome.load(param, connNewFile);
+[connNew, synNew] = connectEM.Connectome.load(param, connNewFile);
+synNewT = connectEM.Connectome.buildSynapseTable(connNew, synNew);
+
+axonsNew = load(connNew.info.param.axonFile);
+axonsNew = axonsNew.axons(axonsNew.indBigAxons);
 
 maxSegId = Seg.Global.getMaxSegId(param);
+segPos = Seg.Global.getSegToPointMap(param);
 
 %% Translate indices to old connectome
 axonNewLUT = Agglo.buildLUT(maxSegId, connNew.axons);
@@ -34,3 +38,38 @@ adAxonIdNew = cellfun( ...
 
 disp(targetClasses);
 disp(classConn(adAxonIdNew, :));
+
+%% Build NML of axon with synapses
+clear cur*;
+
+skel = skeleton();
+skel = Skeleton.setParams4Pipeline(skel, param);
+skel = Skeleton.setDescriptionFromRunInfo(skel, info);
+
+curAxon = connNew.axonMeta.parentId(adAxonIdNew);
+curAxon = axonsNew(curAxon);
+
+curSynT = synNewT;
+curSynT = synNewT(curSynT.preAggloId == adAxonIdNew, :);
+curSynT.targetClass = connNew.denMeta.targetClass(curSynT.postAggloId);
+
+curSynPos = synNew.synapses;
+curSynPos = curSynPos(curSynT.id, :);
+
+curSynPos = cellfun( ...
+    @vertcat, ...
+    curSynPos.presynId, ...
+    curSynPos.postsynId, ...
+    'UniformOutput', false);
+curSynPos = cellfun( ...
+    @(ids) segPos(ids, :), curSynPos, ...
+    'UniformOutput', false);
+
+skel = skel.addTree( ...
+    sprintf('Axon %d', adAxonIdNew), ...
+    curAxon.nodes(:, 1:3), curAxon.edges);
+skel = Skeleton.fromMST( ...
+    curSynPos, param.raw.voxelSize, skel);
+skel.names(2:end) = arrayfun( ...
+    @char, curSynT.targetClass, ...
+    'UniformOutput', false);
