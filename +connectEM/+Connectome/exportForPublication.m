280031e89e03e646241c0e79e35e7127d83b97f0
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-10-11 10:07:17 +0200

    Cosmetics

--- +connectEM/+Connectome/exportForPublication.m
+++ +connectEM/+Connectome/exportForPublication.m
@@ -239,8 +239,9 @@ function numericToHdf5(file, dset, data)
     % NOTE(amotta): Remove trailing singleton dimension
     if numel(sz) == 2 && sz(2) == 1; sz = sz(1); end
     
-    % NOTE(amotta): Compression is counter-productive in this particular case.
-    % Storage usage reported by h5ls is around 30 % to 50 % with deflate (9).
+    % NOTE(amotta): Compression is counter-productive in this particular
+    % case. Storage usage reported by h5ls is around 30 % to 50 % with
+    % deflate (9).
     h5create(file, dset, sz, 'Datatype', class(data));
     h5write(file, dset, data);
 end
