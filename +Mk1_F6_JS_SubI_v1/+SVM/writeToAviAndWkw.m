98602fec67cef1084d6600a6911b55b1a51e50c9
Sahil Loomba <sahil.loomba@brain.mpg.de>
2019-11-11 10:38:01 +0100

    svm mat files to avi and wkw

--- +Mk1_F6_JS_SubI_v1/+SVM/writeToAviAndWkw.m
+++ +Mk1_F6_JS_SubI_v1/+SVM/writeToAviAndWkw.m
@@ -1,14 +1,23 @@
 % write a test bbox prediction to webknossos
 % These predictions were generated from mr_1g_cont5 cnn with python
+% This script generates avi movie and webknossos wkw segmentation
 
 addpath(genpath('/u/sahilloo/repos/Benedikt/'));
+datasetName = 'Mk1_F6_JS_SubI_v1';
+svmPredNetwork = 'mr_2e_cont4';
 
-rootDir = '/tmpscratch/sahilloo/data/H2_3_v2_U1_SubI/pipelineRun_mr2e_wsmrnet/';
+rootDir = fullfile('/tmpscratch/sahilloo/data/', datasetName, 'pipelineRun_mr2e_wsmrnet/');
 m = load(fullfile(rootDir, 'allParameter.mat'));
 p = m.p;
 
+outputDirWkw = fullfile('/tmpscratch/sahilloo/data/', datasetName, ['svm_' svmPredNetwork '_wkw']);
+mkdir(outputDirWkw)
+
+outputDir = fullfile('/tmpscratch/sahilloo/data/', datasetName, ['svm_' svmPredNetwork '_mat']);
+mkdir(outputDir)
+
 Util.log('load python svm predictions')
-m = load('/tmpscratch/sahilloo/data/H2_3_v2_U1_SubI/svm_mr_1g_cont5_slurm/old/1.mat');
+m = load(fullfile('/tmpscratch/sahilloo/data/', datasetName, ['svm_' svmPredNetwork '_slurm'], '1.mat'));
 pred = m.pred;
 bbox_wk = [m.offset, m.shape];
 bbox = Util.convertWebknossosToMatlabBbox(bbox_wk);
@@ -36,15 +45,14 @@ for i = 1:3
 end
 
 Util.log('make avi file')
-Visualization.movieMakerSeg(raw,seg,fullfile(rootDir,'seg_svm.avi'))
+Visualization.movieMakerSeg(raw,seg,fullfile(outputDir,'seg_svm.avi'))
 
 % write to WKW
-segSVM.root = fullfile(rootDir,'SVMPredictionsWkwTest/1/');
+segSVM.root = fullfile(outputDirWkw,'1');
 segSVM.backend = 'wkwrap';
 if ~exist(segSVM.root,'dir')
     mkdir(segSVM.root);
 end
-
 wkwInit('new',segSVM.root,32, 32, 'uint32', 1);
 saveSegDataGlobal(segSVM, double(bbox(:,1)'), seg); 
 
