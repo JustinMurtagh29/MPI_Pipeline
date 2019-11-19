18b0ffe588686041ed37f12ddea1c4f3cd9b26fd
Alessandro Motta <alessandro.motta@brain.mpg.de>
2019-11-19 11:51:22 +0100

    Run SynEM on L2/3 dataset

---
+++ +L23/+SynEM/run.m
@@ -0,0 +1,47 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+
+%% Configuration
+rootDir = '/tmpscratch/amotta/l23/2018-10-09-mrnet-pipeline-run';
+
+% NOTE(amotta): This is a version of SynEM that only uses hand-designed
+% feature and that was trained on the extended training set. According to
+% Benedikt's PhD thesis, this is the configuration that performs best on
+% the L2/3 dataset.
+classifierFile = 'SynapseClassifier_synem_tr2.mat';
+scoreFile = strrep(classifierFile, 'Classifier', 'Scores');
+paramFile = strrep(classifierFile, 'Classifier', 'Parameters');
+
+info = Util.runInfo();
+
+%% Load data
+param = fullfile(rootDir, 'allParameter.mat');
+param = load(param);
+param = param.p;
+
+classifierFile = fullfile(rootDir, classifierFile);
+paramFile = fullfile(rootDir, paramFile);
+
+curData = load(classifierFile, 'classifier', 'fm');
+classifier = curData.classifier;
+fm = curData.fm;
+keyboard
+
+%% Run synapse detection
+clear cur*;
+
+cluster = Cluster.config( ...
+    'memory', 24, 'time', '100:00:00');
+[synParam, job] = SynEM.Seg.predictDataset( ...
+    param, fm, classifier, scoreFile, cluster, ...
+    [], true, true, true, false, false);
+wait(job);
+
+%% Save synapse parameters
+curOut = struct;
+curOut.info = info;
+curOut.p = synParam;
+
+Util.saveStruct(paramFile, curOut);
+Util.protect(paramFile);
