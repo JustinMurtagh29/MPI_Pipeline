821479bbeaae7ec748c83bdcaa386cc3f506ab15
Sahil Loomba <sahil.loomba@brain.mpg.de>
2020-01-08 11:50:19 +0100

    save per box SVM coms

--- +Mk1_F6_JS_SubI_v1/+SVM/writeToAviAndWkw.m
+++ +Mk1_F6_JS_SubI_v1/+SVM/writeToAviAndWkw.m
@@ -27,25 +27,23 @@ end
 wkwInit('new',segSVM.root,32, 32, 'uint32', 1);
 
 boxIds = 1:prod(p.tiles);
-
-for i=1:numel(boxIds)
-    boxId = i;
-    
-    Util.log('load python svm predictions')
+tic;
+for boxId = 1:numel(boxIds)
     m = load(fullfile('/tmpscratch/sahilloo/data/', datasetName, ['svm_' svmPredNetwork '_slurm'], [num2str(boxId) '.mat']));
     pred = m.pred;
     bbox_wk = [m.offset, m.shape];
     bbox = Util.convertWebknossosToMatlabBbox(bbox_wk);
     
-    predOut = zeros([bbox_wk(4:end), 3]);
-    predOut(:,:,:,2) = pred(:,:,:,3);
-    predOut(:,:,:,3) = pred(:,:,:,4);
+    predOut = zeros([bbox_wk(4:end), 3]); % segId 1 -> PSD, here it is empty
+    predOut(:,:,:,2) = pred(:,:,:,3); % segId 2 -> VC
+    predOut(:,:,:,3) = pred(:,:,:,4); % segId 3 -> Mito
     
     Util.log('obtain seg')
     t = 0.5;
     minArea = 3000;
     nhood = [5,5,3];
-    seg = SVM.getSVMSegmentation(predOut, t, minArea, nhood);
+    [seg, synCom, vcCom, miCom] = ...
+        Paper.SynEM.MethComp.preprocessSVMForAnnotation(predOut, t, minArea, nhood);
     
     % further delete small components
     for i = 1:3
@@ -61,7 +59,9 @@ for i=1:numel(boxIds)
         raw = Seg.IO.loadRaw(p, bbox);
         Visualization.movieMakerSeg(raw,seg,fullfile(outputDir,['seg_svm_' num2str(boxId,'%05d') '.avi']))
     end
-    saveSegDataGlobal(segSVM, double(bbox(:,1)'), seg); 
+    saveSegDataGlobal(segSVM, double(bbox(:,1)'), seg);
+    Util.save([p.local(boxId).saveFolder 'svmSegData.mat'], seg, synCom, vcCom, miCom);
+    Util.progressBar(boxId,numel(boxIds));
 end
 % create all resolutions for segmentation
 %thisBBox = [1, 1, 1; (ceil(p.bbox(:, 2) ./ 1024) .* 1024)']';
