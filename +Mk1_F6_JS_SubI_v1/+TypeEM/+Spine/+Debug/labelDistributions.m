fab084ac06f560ed9fc51e3ac5b8097e38cca0d0
Sahil Loomba <sahil.loomba@brain.mpg.de>
2020-01-09 18:58:01 +0100

    stats

--- +Mk1_F6_JS_SubI_v1/+TypeEM/+Spine/+Debug/labelDistributions.m
+++ +Mk1_F6_JS_SubI_v1/+TypeEM/+Spine/+Debug/labelDistributions.m
@@ -1,15 +1,12 @@
 % get label distributions in training and test data
-
-numGlia = sum(gtTrain.label(:,1)>0);
-numAxon = sum(gtTrain.label(:,2)>0);
-numDend = sum(gtTrain.label(:,3)>0);
-trainStatNum = [numGlia, numAxon, numDend];
+numSH = sum(gtTrain.label(:,1)>0);
+numNSH = sum(gtTrain.label(:,1)<0);
+trainStatNum = [numSH, numNSH];
 trainStatFrac = trainStatNum./size(gtTrain.label,1);
 
-numGlia = sum(gtTest.label(:,1)>0);
-numAxon = sum(gtTest.label(:,2)>0);
-numDend = sum(gtTest.label(:,3)>0);
-testStatNum = [numGlia, numAxon, numDend];
+numSH = sum(gtTest.label(:,1)>0);
+numNSH = sum(gtTest.label(:,1)<0);
+testStatNum = [numSH, numNSH];
 testStatFrac = testStatNum./size(gtTest.label,1);
 
 figure;
@@ -23,8 +20,8 @@ x = [1,2];
 y = [trainStatFrac;testStatFrac];
 barh(x,y,'grouped');
 set(gca,'yticklabels',{'train','test'})
-legend({'glia','axon','dendrite'})
-saveas(gcf,fullfile(param.saveFolder,'typeEM','spine','train_test_statistics.png'))
+legend({'SH','nonSH'})
+saveas(gcf,fullfile(param.saveFolder,'typeEM','spine','train_test_statistics_dense.png'))
 close all
 
 
