f3d1c29c4375bfc29bed6843ef576e57db13c713
Sahil Loomba <sahil.loomba@brain.mpg.de>
2020-01-09 17:48:16 +0100

    leftover

--- +Mk1_F6_JS_SubI_v1/+TypeEM/buildSegmentFeatures.m
+++ +Mk1_F6_JS_SubI_v1/+TypeEM/buildSegmentFeatures.m
@@ -3,7 +3,10 @@
 clear;
 
 %% Configuration
-config = 'ex144_08x2_mrNet';
+rootDir = ['/tmpscratch/sahilloo/data/Mk1_F6_JS_SubI_v1/pipelineRun_mr2e_wsmrnet/'];
+load(fullfile(rootDir,'allParameter.mat'))
+param = p;
+param.experimentName = 'Mk1_F6_JS_SubI_v1_mrnet_wsmrnet';
 
 classEmParam = struct;
 classEmParam.agglo = struct;
@@ -14,9 +17,6 @@ fileName = 'segmentFeatures.mat';
 
 info = Util.runInfo();
 Util.showRunInfo(info);
-
-config = loadConfig(config);
-param = config.param;
 
 %% Calculate features
 job = TypeEM.Pipeline.buildFeatures(param, classEmParam, fileName);
