3be3ef7291cca24a52f317d9322192d48a8796ed
Alessandro Motta <alessandro.motta@brain.mpg.de>
2020-06-03 10:40:19 +0200

    Determine synapse types for automated pre-RoboEM state

---
+++ +connectEM/+Synapse/+Script/classifyTypeAutoPreRobo.m
@@ -0,0 +1,55 @@
+% Written by
+%   Alessandro Motta <alessandro.motta@brain.mpg.de>
+clear;
+
+%% Configuration
+rootDir = '/gaba/u/mberning/results/pipeline/20170217_ROI';
+shFile = fullfile(rootDir, 'aggloState', 'dendrites_wholeCells_02_v3_auto.mat');
+connFile = fullfile(rootDir, 'connectomeState', 'connectome_axons-04_dendrites-wholeCells-autoSpines-v1-classified-v2_SynapseAgglos-autoPreRobo-v1.mat');
+
+% As in Benedikt's +L4/updateParamsToNewestFiles.m
+% Commit hash `590d8538d65463151d43491e2446e25ca11dd5f6`
+graphFile = fullfile(rootDir, 'graphNew.mat');
+synScoreFile = fullfile(rootDir, 'globalSynScores.mat');
+
+info = Util.runInfo();
+
+%% Loading data
+param = load(fullfile(rootDir, 'allParameter.mat'));
+param = param.p;
+
+% Prepare for load synapse scores
+param.svg.graphFile = graphFile;
+param.svg.synScoreFile = synScoreFile;
+
+% Synapse scores
+maxSegId = Seg.Global.getMaxSegId(param);
+segPoints = Seg.Global.getSegToPointMap(param);
+graph = Seg.IO.loadGraph(param, false);
+
+conn = load(connFile);
+
+synFile = conn.info.param.synFile;
+syn = load(synFile);
+
+% Spine heads
+shAgglos = load(shFile, 'shAgglos');
+shAgglos = shAgglos.shAgglos;
+
+%% Classify synapses
+somaAgglos = conn.denMeta.targetClass == 'Somata';
+somaAgglos = conn.dendrites(somaAgglos);
+
+syn.synapses.type = connectEM.Synapse.classifyType( ...
+    param, syn.synapses, graph.synScores, shAgglos, somaAgglos);
+
+%% Generate output
+out = syn;
+out.info = info;
+
+[outDir, outFile] = fileparts(synFile);
+outFile = sprintf('%s_classified.mat', outFile);
+outFile = fullfile(outDir, outFile);
+
+Util.saveStruct(outFile, out);
+Util.protect(outFile);
