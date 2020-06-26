294a23ae246a7ff7b3de59dba6ee1c19a55eeaf0
Sahil Loomba <sahil.loomba@brain.mpg.de>
2020-06-26 15:01:09 +0200

    random boxes increase

--- +connectEM/buildSeedBoxes.m
+++ +connectEM/buildSeedBoxes.m
@@ -12,10 +12,10 @@
 param = p;
 outDir = fullfile(param.saveFolder,'tracings', 'box-seeded');
 mkdir(outDir)
-numBoxes = 11;
+numBoxes = 40;
 boxDim = 5000;% 2500 for agglo eval
 centerBoxDim = 100000; % 15000 for agglo eval
-outFiles = arrayfun(@(x) fullfile(outDir,sprintf('dense-typeEM-spines-%02d.nml',x)),1:numBoxes,'uni',0);
+outFiles = arrayfun(@(x) fullfile(outDir,sprintf('calibration-box-spines-%02d.nml',x)),1:numBoxes,'uni',0);
 %% Build boxes
 centerPos = round(mean(param.bbox, 2));
 
